b68a102dd5e1eeedf32deed39ecef8f5b81d3414
DYGames <dygames@naver.com>
2023-03-15 00:40:27 +0900

    fix: 비정상적인 줄바꿈을 하던 오류 수정

--- src/main/kotlin/view/RenderBoard.kt
+++ src/main/kotlin/view/RenderBoard.kt
@@ -1,11 +1,13 @@
 package view
 
 class RenderBoard {
-    fun renderBoard(stones: List<Pair<Int, Pair<Int, Int>>>) {
+    fun renderBoard(stones: List<Pair<Int, Pair<Int, Int>>>, size: Pair<Int, Int>) {
 
-        for (i in 0 until (15 * 15)) {
-            val x = i % 15
-            val y = i / 15
+        for (i in 0 until (size.first * size.second)) {
+            if (i > 0 && i % size.first == 0)
+                println()
+            val x = i % size.first
+            val y = i / size.first
 
             val stone = stones.find {
                 it.second.first == x && it.second.second == y
@@ -13,20 +15,17 @@ class RenderBoard {
             if (stone != null) {
                 if (stone.second.first == 0 && stone.first == 0)
                     print(BLACK_LEFT_STONE)
-                else if (stone.second.first == 14 && stone.first == 0)
+                else if (stone.second.first == size.first - 1 && stone.first == 0)
                     print(BLACK_RIGHT_STONE)
                 else if (stone.first == 0)
                     print(BLACK_INNER_STONE)
 
                 if (stone.second.first == 0 && stone.first == 1)
                     print(WHITE_LEFT_STONE)
-                else if (stone.second.first == 14 && stone.first == 1)
+                else if (stone.second.first == size.first - 1 && stone.first == 1)
                     print(WHITE_RIGHT_STONE)
                 else if (stone.first == 1)
                     print(WHITE_INNER_STONE)
-
-                if (i > 0 && i % 14 == 0)
-                    println()
                 continue
             }
 
@@ -39,22 +38,20 @@ class RenderBoard {
             else if (x == 0) print(EMPTY_LEFT_SLOT)
             else if (x == 14) print(EMPTY_RIGHT_SLOT)
             else print(EMPTY_INNER_SLOT)
-
-            if (i > 0 && i % 14 == 0)
-                println()
         }
     }
 
     companion object {
-        private const val EMPTY_INNER_SLOT = "─┼─"
-        private const val EMPTY_LEFT_SLOT = "├─"
         private const val EMPTY_TOP_SLOT = "─┬─"
-        private const val EMPTY_RIGHT_SLOT = "─┤"
         private const val EMPTY_BOTTOM_SLOT = "─┴─"
         private const val EMPTY_LEFT_TOP_SLOT = "┌─"
         private const val EMPTY_RIGHT_TOP_SLOT = "─┐"
         private const val EMPTY_LEFT_BOTTOM_SLOT = "└─"
         private const val EMPTY_RIGHT_BOTTOM_SLOT = "─┘"
+
+        private const val EMPTY_INNER_SLOT = "─┼─"
+        private const val EMPTY_LEFT_SLOT = "├─"
+        private const val EMPTY_RIGHT_SLOT = "─┤"
 
         private const val BLACK_INNER_STONE = "─●─"
         private const val BLACK_LEFT_STONE = "●─"
@@ -63,5 +60,6 @@ class RenderBoard {
         private const val WHITE_INNER_STONE = "─○─"
         private const val WHITE_LEFT_STONE = "○─"
         private const val WHITE_RIGHT_STONE = "─○"
+
     }
 }
--- src/test/kotlin/view/RenderBoardTest.kt
+++ src/test/kotlin/view/RenderBoardTest.kt
@@ -5,6 +5,6 @@ import org.junit.jupiter.api.Test
 class RenderBoardTest {
     @Test
     fun `프린트 테스트`() {
-        RenderBoard().renderBoard(listOf(0 to (1 to 2)))
+        RenderBoard().renderBoard(listOf(0 to (1 to 2)), 15 to 15)
     }
 }
