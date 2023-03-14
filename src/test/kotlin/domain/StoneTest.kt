4d4fe00a36435b465083ebb38fe1625e0b1179e9
EmilyCh0 <emily9803@ewhain.net>
2023-03-14 17:49:35 +0900

    feat(Stone): 보드 범위 내인지 확인하는 기능 구현

--- src/test/kotlin/domain/StoneTest.kt
+++ src/test/kotlin/domain/StoneTest.kt
@@ -2,10 +2,21 @@ package domain
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 
 class StoneTest {
     @Test
     fun `좌표를 저장한다`() {
         assertDoesNotThrow { Stone(1, 3) }
+    }
+
+    @Test
+    fun `15, 0이면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> { Stone(15, 0) }
+    }
+
+    @Test
+    fun `0, 15이면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException>("잘못된 위치입니다.") { Stone(0, 15) }
     }
 }
