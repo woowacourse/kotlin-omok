b8c945d3a425a120fae70d78f2025b7b70fb50d1
s9hn <tnghqn@naver.com>
2023-03-14 17:49:19 +0900

    [REFACTOR(Coordination)] 범위값 상수화

--- src/main/kotlin/model/domain/Coordination.kt
+++ src/main/kotlin/model/domain/Coordination.kt
@@ -3,9 +3,15 @@ package model.domain
 @JvmInline
 value class Coordination private constructor(val value: Int) {
     companion object {
+        private const val MINIMUM_VALUE = 1
+        private const val MAXIMUM_VALUE = 15
+        private val range = MINIMUM_VALUE..MAXIMUM_VALUE
+
         fun from(value: Int): Coordination? {
-            if (value !in 1..15) return null
+            if (value !in range) return null
             return Coordination(value)
         }
+
+        fun fromNotNull(value: Int): Coordination = Coordination(value)
     }
 }
