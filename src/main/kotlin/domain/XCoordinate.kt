8a7a0a57e21c1bdae3dd3e672ddbbe66c39acfa6
ki960213 <ki960213@github.com>
2023-03-14 15:38:40 +0900

    refactor: 좌표들의 범위값을 상수화

--- src/main/kotlin/domain/XCoordinate.kt
+++ src/main/kotlin/domain/XCoordinate.kt
@@ -3,12 +3,14 @@ package domain
 class XCoordinate private constructor(x: Char) {
 
     init {
-        require(x in 'A'..'O') { X_COORDINATE_RANGE_ERROR }
+        require(x in X_MIN_RANGE..X_MAX_RANGE) { X_COORDINATE_RANGE_ERROR }
     }
 
     companion object {
         private const val X_COORDINATE_RANGE_ERROR = "x 좌표는 A에서 O 사이의 문자로 생성해야 합니다."
-        private val COORDINATES: Map<Char, XCoordinate> = ('A'..'O').associateWith { XCoordinate(it) }
+        const val X_MIN_RANGE = 'A'
+        const val X_MAX_RANGE = 'O'
+        private val COORDINATES: Map<Char, XCoordinate> = (X_MIN_RANGE..X_MAX_RANGE).associateWith { XCoordinate(it) }
 
         fun of(x: Char): XCoordinate {
             val upperX = x.uppercase()[0]
--- src/main/kotlin/domain/YCoordinate.kt
+++ src/main/kotlin/domain/YCoordinate.kt
@@ -3,12 +3,14 @@ package domain
 class YCoordinate private constructor(y: Int) {
 
     init {
-        require(y in 1..15) { Y_COORDINATE_RANGE_ERROR }
+        require(y in Y_MIN_RANGE..Y_MAX_RANGE) { Y_COORDINATE_RANGE_ERROR }
     }
 
     companion object {
         private const val Y_COORDINATE_RANGE_ERROR = "y 좌표는 1에서 15 사이의 숫자로 생성해야 합니다."
-        private val COORDINATES: Map<Int, YCoordinate> = (1..15).associateWith { YCoordinate(it) }
+        const val Y_MIN_RANGE = 1
+        const val Y_MAX_RANGE = 15
+        private val COORDINATES: Map<Int, YCoordinate> = (Y_MIN_RANGE..Y_MAX_RANGE).associateWith { YCoordinate(it) }
 
         fun of(y: Int): YCoordinate = COORDINATES[y] ?: YCoordinate(y)
     }
