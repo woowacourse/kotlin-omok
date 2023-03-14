457600bac0a640f95c25789d50d775ffd64d53e6
s9hn <tnghqn@naver.com>
2023-03-14 21:19:42 +0900

    [REFACTOR(Board)] Board 생성 로직 수정

--- src/main/kotlin/model/domain/Board.kt
+++ src/main/kotlin/model/domain/Board.kt
@@ -1,13 +1,13 @@
 package model.domain
 
-class Board(system: Map<Coordination, MutableList<Coordination>>) {
+class Board(val system: Map<Int, MutableList<Stone>>) {
     companion object {
         private const val SIZE = 15
         private const val CORRECTION_VALUE = 1
 
-        private val OMOK_BOARD: Map<Coordination, MutableList<Coordination>> =
+        private val OMOK_BOARD: Map<Int, MutableList<Stone>> =
             List(SIZE) { row ->
-                Coordination.fromNotNull(row + CORRECTION_VALUE) to mutableListOf<Coordination>()
+                row to MutableList(SIZE) { Stone.EMPTY }
             }.toMap()
 
         fun create(): Board = Board(OMOK_BOARD)
