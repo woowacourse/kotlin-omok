80186230be273ae7f5a3f113db8a042a7924c779
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-14 21:11:52 +0900

    refactor (OmokController): 오목게임을 실행 할 수 있도록 변경

---
+++ src/main/kotlin/omok/controller/OmokController.kt
@@ -0,0 +1,14 @@
+package omok.controller
+
+import omok.domain.OmokGame
+import omok.view.InputView
+import omok.view.OutputView
+
+class OmokController {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+
+    fun run() {
+        OmokGame().play(inputView::inputPoint, outputView::outputBoard)
+    }
+}
--- src/main/kotlin/omok/domain/OmokGame.kt
+++ src/main/kotlin/omok/domain/OmokGame.kt
@@ -1,6 +1,19 @@
 package omok.domain
 
 class OmokGame {
+    fun play(getPoint: (String, OmokPoint?) -> OmokPoint, output: (OmokBoard) -> Unit) {
+        var playingOmokBoard = OmokBoard(YLine())
+        var point: OmokPoint? = null
+        while (true) {
+            point = getPoint("흑", point)
+            playingOmokBoard = playBlack(playingOmokBoard, point)
+            output(playingOmokBoard)
+            point = getPoint("백", point)
+            playingOmokBoard = playWhite(playingOmokBoard, point)
+            output(playingOmokBoard)
+        }
+    }
+
     fun playBlack(omokBoard: OmokBoard, point: OmokPoint): OmokBoard {
         return runCatching { omokBoard.placeStone(point, StoneState.BLACK) }
             .getOrElse { omokBoard }
