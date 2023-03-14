f61d3eafdefcef807eae9e559fcadba3a4490a6d
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-14 14:39:15 +0900

    refactor (Coordinate): 매직 넘버 수정

--- src/main/kotlin/omok/domain/XCoordinate.kt
+++ src/main/kotlin/omok/domain/XCoordinate.kt
@@ -3,10 +3,12 @@ package omok.domain
 @JvmInline
 value class XCoordinate(val value: Char) {
     init {
-        require(value in 'A'..'O') { ERROR_X_COORDINATE_OUT_OF_RANGE }
+        require(value in X_MIN_RANGE..X_MAX_RANGE) { ERROR_X_COORDINATE_OUT_OF_RANGE }
     }
 
     companion object {
+        private const val X_MIN_RANGE = 'A'
+        private const val X_MAX_RANGE = 'O'
         private const val ERROR_X_COORDINATE_OUT_OF_RANGE = "X 좌표의 범위는 A부터 O까지 입니다."
     }
 }
--- src/main/kotlin/omok/domain/YCoordinate.kt
+++ src/main/kotlin/omok/domain/YCoordinate.kt
@@ -3,10 +3,12 @@ package omok.domain
 @JvmInline
 value class YCoordinate(val value: Int) {
     init {
-        require(value in 1..15) { ERROR_Y_COORDINATE_OUT_OF_RANGE }
+        require(value in Y_MIN_RANGE..Y_MAX_RANGE) { ERROR_Y_COORDINATE_OUT_OF_RANGE }
     }
 
     companion object {
+        private const val Y_MIN_RANGE = 1
+        private const val Y_MAX_RANGE = 15
         private const val ERROR_Y_COORDINATE_OUT_OF_RANGE = "Y 좌표의 범위는 1부터 15까지 입니다."
     }
 }
