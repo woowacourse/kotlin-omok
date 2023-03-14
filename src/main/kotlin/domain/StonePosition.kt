0c09508caa399c4df462713e931f5e4321e1ae8e
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-14 17:01:29 +0900

    feat[StonePosition]: x좌표와 y좌표 범위에 대해 값 검사하는 기능 추가

--- src/main/kotlin/domain/StonePosition.kt
+++ src/main/kotlin/domain/StonePosition.kt
@@ -2,9 +2,17 @@ package domain
 
 data class StonePosition private constructor(
     val x: Int,
-    val y: Int
+    val y: Int,
 ) {
     companion object {
-        fun from(x: Int, y: Int) = StonePosition(x, y)
+
+        private const val MINIMUM_POSITION_NUMBER = 1
+        private const val MAXIMUM_POSITION_NUMBER = 15
+
+        fun from(x: Int, y: Int): StonePosition? {
+            if (x !in MINIMUM_POSITION_NUMBER..MAXIMUM_POSITION_NUMBER) return null
+            if (y !in MINIMUM_POSITION_NUMBER..MAXIMUM_POSITION_NUMBER) return null
+            return StonePosition(x, y)
+        }
     }
 }
