e8c1d408c7c3bc639e390c42879ca82f3df8db3a
boogieWoogie <kimjinuk1999@gmail.com>
2023-03-14 16:16:29 +0900

    feat(Stones): 돌이 5개 이상 연속할 경우 승리를 판단하는 기능 추가

--- src/main/kotlin/Stones.kt
+++ src/main/kotlin/Stones.kt
@@ -1,13 +1,41 @@
-
-
 class Stones(list: List<Stone>) {
     private val _list = list.toList()
     val list: List<Stone>
         get() = _list.toList()
+
+    val isWin: Boolean
+        get() = checkWin()
 
     constructor(vararg stone: Stone) : this(stone.toList())
 
     fun putStone(stone: Stone): Stones = Stones(list + stone)
 
     fun getLatestStone(): Point = _list.last().point
+
+    private fun checkWin(): Boolean {
+        val N = 15
+        val dx = intArrayOf(1, 1, 0, -1)
+        val dy = intArrayOf(0, 1, 1, 1)
+
+        for (i in 1..N) {
+            for (j in 1..N) {
+                if (list.contains(Stone(Point(i, j))).not()) continue
+
+                for (k in 0 until 4) {
+                    var cnt = 1
+                    var nx = i + dx[k]
+                    var ny = j + dy[k]
+
+                    while (nx in 1..N && ny in 1..N && list.contains(Stone(Point(nx, ny)))) {
+                        cnt++
+                        nx += dx[k]
+                        ny += dy[k]
+
+                        if (cnt >= 5) return true
+                    }
+                }
+            }
+        }
+        return false
+    }
 }
--- src/test/kotlin/StonesTest.kt
+++ src/test/kotlin/StonesTest.kt
@@ -1,5 +1,6 @@
 import org.assertj.core.api.Java6Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class StonesTest {
     @Test
@@ -16,5 +17,44 @@ class StonesTest {
         val nextStones = stones.putStone(Stone(1, 11))
         val expected = nextStones.getLatestStone()
         assertThat(expected).isEqualTo(Point(1, 11))
+    }
+
+    @Test
+    fun `5개 이상의 돌이 연속되면 승리한다`() {
+        assertAll(
+            "5개 이상의 돌이 연속되면 승리한다",
+            {
+                val stones = Stones(Stone(1, 1), Stone(1, 2), Stone(1, 3), Stone(1, 4), Stone(1, 5))
+                val actual = stones.isWin
+                assertThat(actual).isTrue
+            },
+            {
+                val stones = Stones(Stone(1, 1), Stone(1, 2), Stone(1, 3), Stone(1, 4), Stone(1, 5), Stone(1, 6))
+                val actual = stones.isWin
+                assertThat(actual).isTrue
+            },
+            {
+                val stones = Stones(Stone(1, 1), Stone(2, 2), Stone(3, 3), Stone(4, 4), Stone(5, 5))
+                val actual = stones.isWin
+                assertThat(actual).isTrue
+            }
+        )
+    }
+
+    @Test
+    fun `5개 이상의 돌이 연속되지 않으면 승리가 아니다`() {
+        assertAll(
+            "5개 이상의 돌이 연속되지 않으면 승리가 아니다",
+            {
+                val stones = Stones(Stone(1, 1), Stone(1, 2), Stone(1, 3), Stone(1, 4))
+                val actual = stones.isWin
+                assertThat(actual).isFalse
+            },
+            {
+                val stones = Stones(Stone(1, 1), Stone(2, 2), Stone(4, 4), Stone(5, 5))
+                val actual = stones.isWin
+                assertThat(actual).isFalse
+            }
+        )
     }
 }
