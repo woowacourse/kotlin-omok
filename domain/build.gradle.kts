f440f9ddbe85edf884d639845626df599c914611
woowahan-pjs <jason@woowahan.com>
2023-03-14 22:31:08 +0900

    feat: set up the android project

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,14 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    kotlin("jvm")
 }
 
 group = "camp.nextstep.edu"
 version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
 
 dependencies {
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
