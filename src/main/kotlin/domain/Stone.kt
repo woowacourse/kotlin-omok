76b2c9e5582a6c09e32dd6993a58a72ab0f7f3f2
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-14 17:13:27 +0900

    feat[Stone]: 위치와 타입을 가지는 Stone 클래스 생성

---
+++ src/main/kotlin/domain/Stone.kt
@@ -0,0 +1,11 @@
+package domain
+
+class Stone(
+    val position: StonePosition,
+    val type: StoneType
+) {
+    companion object {
+        fun from(position: StonePosition, type: StoneType): Stone =
+            Stone(position, type)
+    }
+}
---
+++ src/main/kotlin/domain/StoneType.kt
@@ -0,0 +1,5 @@
+package domain
+
+enum class StoneType {
+    BLACK, WHITE
+}
