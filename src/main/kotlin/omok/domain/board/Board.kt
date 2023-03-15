c599c47530f66edca51e9b3c3c1c3c36a6c8fc79
Gloria <ippnsj@gmail.com>
2023-03-15 21:57:50 +0900

    feat: 흑만 3-3과 4-4, 장목을 금수로 제한하는 기능 구현

--- src/main/kotlin/omok/domain/board/Board.kt
+++ src/main/kotlin/omok/domain/board/Board.kt
@@ -1,13 +1,17 @@
 package omok.domain.board
 
+import omok.domain.judgment.BlackReferee
+import omok.domain.player.Black
 import omok.domain.player.Stone
 
 class Board(private val _positions: MutableMap<Position, Stone?> = POSITIONS.associateWith { null }.toMutableMap()) {
+    private val referee = BlackReferee()
     val positions: Map<Position, Stone?>
         get() = _positions.toMap()
 
     fun place(position: Position, stone: Stone) {
         require(isEmpty(position)) { "[ERROR] 해당 위치는 비어있지 않습니다." }
+        if (stone == Black) require(!referee.isForbiddenPlacement(positions, position)) { "[ERROR] 해당 위치는 금수입니다." }
         _positions[position] = stone
     }
 
