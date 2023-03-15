49b98ddd23e5d71f9d0ead4ec90cc5ce1e8af2d9
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-03-15 16:51:54 +0900

    refactor(ConsoleRenderBoard): 로직 분리 및 상수화

--- src/main/kotlin/view/ConsoleRenderBoard.kt
+++ src/main/kotlin/view/ConsoleRenderBoard.kt
@@ -6,53 +6,75 @@ import dto.StoneDTO
 
 class ConsoleRenderBoard : RenderBoard {
     override fun render(stones: List<StoneDTO>, size: PointDTO) {
+        for (index in 0 until (size.x * size.y)) {
+            makeNewLine(index, size)
 
-        for (i in 0 until (size.x * size.y)) {
-            if (i > 0 && i % size.x == 0)
-                println()
+            val x = index % size.x
+            val y = index / size.x
 
-            val x = i % size.x
-            val y = i / size.x
-            if (i % (size.x) == 0)
-                print("%d\t".format(size.x - y))
+            renderRowNumber(index, size, y)
 
-            val stone = stones.find {
-                it.coordinate.x == x && it.coordinate.y == size.y - y - 1
-            }
+            val stone = findStone(stones, x, y, size)
             if (stone != null) {
-                if (stone.coordinate.x == 0 && stone.color == ColorDTO.BLACK)
-                    print(BLACK_LEFT_STONE)
-                else if (stone.coordinate.x == size.x - 1 && stone.color == ColorDTO.BLACK)
-                    print(BLACK_RIGHT_STONE)
-                else if (stone.color == ColorDTO.BLACK)
-                    print(BLACK_INNER_STONE)
-
-                if (stone.coordinate.x == 0 && stone.color == ColorDTO.WHITE)
-                    print(WHITE_LEFT_STONE)
-                else if (stone.coordinate.x == size.x - 1 && stone.color == ColorDTO.WHITE)
-                    print(WHITE_RIGHT_STONE)
-                else if (stone.color == ColorDTO.WHITE)
-                    print(WHITE_INNER_STONE)
+                renderStone(stone, size)
                 continue
             }
 
-            if (x == 0 && y == 0) print(EMPTY_LEFT_TOP_SLOT)
-            else if (x == 14 && y == 0) print(EMPTY_RIGHT_TOP_SLOT)
-            else if (y == 0) print(EMPTY_TOP_SLOT)
-            else if (x == 0 && y == 14) print(EMPTY_LEFT_BOTTOM_SLOT)
-            else if (x == 14 && y == 14) print(EMPTY_RIGHT_BOTTOM_SLOT)
-            else if (y == 14) print(EMPTY_BOTTOM_SLOT)
-            else if (x == 0) print(EMPTY_LEFT_SLOT)
-            else if (x == 14) print(EMPTY_RIGHT_SLOT)
-            else print(EMPTY_INNER_SLOT)
+            renderEmptySlot(x, y, size)
         }
+        renderColumnChar(size)
+    }
+
+    private fun findStone(
+        stones: List<StoneDTO>, x: Int, y: Int, size: PointDTO
+    ): StoneDTO? {
+        val stone = stones.find {
+            it.coordinate.x == x && it.coordinate.y == size.y - y - 1
+        }
+        return stone
+    }
+
+    private fun renderColumnChar(size: PointDTO) {
         println()
         print("    ")
         for (i in 0 until size.y) {
             print("%c  ".format(('A'.code + i).toChar()))
         }
-
         println()
+    }
+
+    private fun renderRowNumber(index: Int, size: PointDTO, y: Int) {
+        if (index % (size.x) == LEFT_START_INDEX) {
+            print("%d\t".format(size.x - y))
+        }
+    }
+
+    private fun makeNewLine(index: Int, size: PointDTO) {
+        if (index > 0 && index % size.x == LEFT_START_INDEX) println()
+    }
+
+    private fun renderStone(stone: StoneDTO, size: PointDTO) {
+        if (stone.coordinate.x == LEFT_START_INDEX && stone.color == ColorDTO.BLACK) print(BLACK_LEFT_STONE)
+        else if (stone.coordinate.x == size.x - COORDINATE_FIXER && stone.color == ColorDTO.BLACK) print(
+            BLACK_RIGHT_STONE
+        )
+        else if (stone.color == ColorDTO.BLACK) print(BLACK_INNER_STONE)
+
+        if (stone.coordinate.x == LEFT_START_INDEX && stone.color == ColorDTO.WHITE) print(WHITE_LEFT_STONE)
+        else if (stone.coordinate.x == size.x - 1 && stone.color == ColorDTO.WHITE) print(WHITE_RIGHT_STONE)
+        else if (stone.color == ColorDTO.WHITE) print(WHITE_INNER_STONE)
+    }
+
+    private fun renderEmptySlot(x: Int, y: Int, size: PointDTO) {
+        if (x == LEFT_START_INDEX && y == TOP_START_INDEX) print(EMPTY_LEFT_TOP_SLOT)
+        else if (x == size.x - COORDINATE_FIXER && y == LEFT_START_INDEX) print(EMPTY_RIGHT_TOP_SLOT)
+        else if (y == TOP_START_INDEX) print(EMPTY_TOP_SLOT)
+        else if (x == LEFT_START_INDEX && y == size.y - COORDINATE_FIXER) print(EMPTY_LEFT_BOTTOM_SLOT)
+        else if (x == size.x - COORDINATE_FIXER && y == size.y - COORDINATE_FIXER) print(EMPTY_RIGHT_BOTTOM_SLOT)
+        else if (y == size.y - COORDINATE_FIXER) print(EMPTY_BOTTOM_SLOT)
+        else if (x == LEFT_START_INDEX) print(EMPTY_LEFT_SLOT)
+        else if (x == size.x - COORDINATE_FIXER) print(EMPTY_RIGHT_SLOT)
+        else print(EMPTY_INNER_SLOT)
     }
 
     companion object {
@@ -74,5 +96,10 @@ class ConsoleRenderBoard : RenderBoard {
         private const val WHITE_INNER_STONE = "─○─"
         private const val WHITE_LEFT_STONE = "○─"
         private const val WHITE_RIGHT_STONE = "─○"
+
+        private const val COORDINATE_FIXER = 1
+        private const val LEFT_START_INDEX = 0
+        private const val TOP_START_INDEX = 0
+        private const val FIRST_LINE_INDEX = 0
     }
 }
