8b2919e13eebd3f0a337f1393be63c5361f9ca1a
EmilyCh0 <emily9803@ewhain.net>
2023-03-15 15:47:49 +0900

    feat(State): 두 state를 합한다(연산자 오버로딩)

--- src/main/kotlin/domain/turn/State.kt
+++ src/main/kotlin/domain/turn/State.kt
@@ -15,6 +15,31 @@ class State(
 
     fun isEmpty(stone: Stone): Boolean = !_state[stone.row][stone.column]
 
+    operator fun plus(other: State): State {
+        return State(
+            this.value.zip(other.value) { row1, row2 ->
+                row1.zip(row2) { column1, column2 ->
+                    column1 || column2
+                }
+            }
+        )
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as State
+
+        if (_state != other._state) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return _state.hashCode()
+    }
+
     companion object {
         const val BOARD_SIZE = 15
     }
