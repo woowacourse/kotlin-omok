79b1d01e300373bba0d6d0f2f2e45fc952151161
s9hn <tnghqn@naver.com>
2023-03-15 21:41:09 +0900

    [FEAT(OmokRule)] OmokRule 싱글톤 구현

--- src/main/kotlin/model/domain/OmokRule.kt
+++ src/main/kotlin/model/domain/OmokRule.kt
@@ -1,37 +1,43 @@
 package model.domain
 
 object OmokRule {
+    private const val START = 0
+    private const val END = 14
+    private const val NOTHING = 0
+    private const val OMOK = 5
+    private val COORDINATION_SYSTEM_RANGE = START..END
     fun isOmok(board: Board, location: Location, stone: Stone): Boolean {
         val row = location.coordinationX.value
         val col = location.coordinationY.value
-
-        val directions = listOf(
+        val directions: List<Pair<Int, Int>> = listOf(
             Pair(-1, 1),
             Pair(0, 1),
             Pair(1, 1),
             Pair(1, 0),
         )
 
-        return directions.any { (dx, dy) ->
-            var count = 0
+        for ((dx, dy) in directions) {
+            var count = NOTHING
             var (x, y) = Pair(row, col)
 
-            while (board.get(location) == stone) {
-                count++
-                x += dx
-                y += dy
+            x -= dx * 4
+            y -= dy * 4
+
+            for (cell in 0 until 9) {
+                if ((x in COORDINATION_SYSTEM_RANGE) and (y in COORDINATION_SYSTEM_RANGE)) {
+                    if (board.get(Location(Coordination.from(x)!!, Coordination.from(y)!!)) == stone) {
+                        count++
+                        if (count >= OMOK) return true
+                    } else {
+                        count = NOTHING
+                    }
+
+                    x += dx
+                    y += dy
+                }
             }
-
-            x = row - dx
-            y = col - dy
-
-            while (board.get(location) == stone) {
-                count++
-                x -= dx
-                y -= dy
-            }
-
-            count >= 5
         }
+
+        return false
     }
 }
