c50cf874be0757e79b194fd22e24f211b026b0af
s9hn <tnghqn@naver.com>
2023-03-15 21:39:59 +0900

    [REFACTOR(Coordination)] 좌표 범위값 수정

--- src/main/kotlin/model/domain/Coordination.kt
+++ src/main/kotlin/model/domain/Coordination.kt
@@ -3,8 +3,8 @@ package model.domain
 @JvmInline
 value class Coordination private constructor(val value: Int) {
     companion object {
-        private const val MINIMUM_VALUE = 1
-        private const val MAXIMUM_VALUE = 15
+        private const val MINIMUM_VALUE = 0
+        private const val MAXIMUM_VALUE = 14
         private val range = MINIMUM_VALUE..MAXIMUM_VALUE
 
         fun from(value: Int): Coordination? {
--- src/test/kotlin/model/domain/CoordinationTest.kt
+++ src/test/kotlin/model/domain/CoordinationTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class CoordinationTest {
 
-    @ValueSource(ints = [1, 7, 15])
+    @ValueSource(ints = [0, 7, 14])
     @ParameterizedTest
     fun `좌표는 1이상 15이하이다`(number: Int) {
         // given
@@ -18,7 +18,7 @@ class CoordinationTest {
         assertThat(actual is Coordination).isTrue
     }
 
-    @ValueSource(ints = [-10, 0, 16])
+    @ValueSource(ints = [-10, -1, 16])
     @ParameterizedTest
     fun `좌표가 1이상 15이하가 아니면 null 을 반환한다`(number: Int) {
         // given
