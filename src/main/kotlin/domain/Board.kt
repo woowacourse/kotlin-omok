b1186b0eb8cffca4e224e9392b778ad045ccff2a
EmilyCh0 <emily9803@ewhain.net>
2023-03-15 15:14:38 +0900

    feat(Board): 해당 턴의 수를 착수한다

--- src/main/kotlin/domain/Board.kt
+++ src/main/kotlin/domain/Board.kt
@@ -4,10 +4,18 @@ import domain.turn.BlackTurn
 import domain.turn.WhiteTurn
 
 class Board(
-    private val blackTurn: BlackTurn,
-    private val whiteTurn: WhiteTurn
+    val blackTurn: BlackTurn,
+    val whiteTurn: WhiteTurn
 ) {
     fun canMove(stone: Stone): Boolean {
         return blackTurn.isEmpty(stone) && whiteTurn.isEmpty(stone)
+    }
+
+    fun moveWhite(stone: Stone) {
+        whiteTurn.move(stone)
+    }
+
+    fun moveBlack(stone: Stone) {
+        blackTurn.move(stone)
     }
 }
