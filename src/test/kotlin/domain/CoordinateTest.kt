0e69470721f2d093479c4176812a3cb74fa158b6
Doyeop Kim <dygames34@gmail.com>
2023-03-15 17:30:27 +0900

    fix(CoordinateTest): 0을 기준으로 하는 좌표계를 사용하게 수정

--- src/test/kotlin/domain/CoordinateTest.kt
+++ src/test/kotlin/domain/CoordinateTest.kt
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test
 
 class CoordinateTest {
     @Test
-    fun `좌표 생성시 오목판 밖에서 생성하면 null을 반환한다`() {
+    fun `좌표 생성시 15x15 오목판 밖에서 생성하면 null을 반환한다`() {
         // given
-        val coordinate = Coordinate.from(16, 16)
+        val coordinate = Coordinate.from(15, 15)
 
         // when
 
@@ -16,9 +16,9 @@ class CoordinateTest {
     }
 
     @Test
-    fun `좌표 생성시 오목판 안에서 생성하면 좌표 객체를 생성한다`() {
+    fun `좌표 생성시 15x15 오목판 안에서 생성하면 좌표 객체를 생성한다`() {
         // given
-        val coordinate = Coordinate.from(15, 15)
+        val coordinate = Coordinate.from(14, 14)
 
         // when
 
