7d0d267584824161e3faf15f0cd4371147d884eb
s9hn <tnghqn@naver.com>
2023-03-15 21:40:14 +0900

    [REFACTOR(Board)] 오목판 캐싱해제

--- src/main/kotlin/model/domain/Board.kt
+++ src/main/kotlin/model/domain/Board.kt
@@ -3,24 +3,22 @@ package model.domain
 class Board(val system: Map<Int, MutableList<Stone>>) {
     companion object {
         private const val SIZE = 15
-        private const val CORRECTION_VALUE = 1
-
-        private val OMOK_BOARD: Map<Int, MutableList<Stone>> =
-            List(SIZE) { row ->
+        fun create(): Board {
+            val system = List(SIZE) { row ->
                 row to MutableList(SIZE) { Stone.EMPTY }
             }.toMap()
-
-        fun create(): Board = Board(OMOK_BOARD)
+            return Board(system)
+        }
     }
 
     fun get(location: Location): Stone? = system[location.coordinationX.value]?.get(location.coordinationY.value)
 
     fun placeStone(location: Location, stone: Stone): Boolean {
+        if (get(location) != Stone.EMPTY) return false
+
         val row = location.coordinationX.value
         val col = location.coordinationY.value
         if (row !in 0 until SIZE || col !in 0 until SIZE) return false
         system[row]?.set(col, stone)?.let { return true } ?: return false
     }
-
-
 }
