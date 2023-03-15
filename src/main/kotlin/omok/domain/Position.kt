03e07eec9fb680b8ac3e2e123a21823536bbf23e
Yerin <myheaven_b@naver.com>
2023-03-15 21:12:46 +0900

    feat: Position 예외처리 추가

--- src/main/kotlin/omok/domain/Position.kt
+++ src/main/kotlin/omok/domain/Position.kt
@@ -1,6 +1,11 @@
 package omok.domain
 
 data class Position(val horizontalAxis: HorizontalAxis, val verticalAxis: Int) {
+    init {
+        require(horizontalAxis in HorizontalAxis.values()) { HORIZONTAL_AXIS_ERROR_MSG }
+        require(verticalAxis in 1..15) { VERTICAL_AXIS_ERROR_MSG }
+    }
+
     private var empty = true
 
     fun isEmpty(): Boolean {
@@ -12,6 +17,8 @@ data class Position(val horizontalAxis: HorizontalAxis, val verticalAxis: Int) {
     }
 
     companion object {
+        private const val HORIZONTAL_AXIS_ERROR_MSG = "가로축은 A와 O 사이 입니다."
+        private const val VERTICAL_AXIS_ERROR_MSG = "세로축은 A와 O 사이 입니다."
         val POSITIONS = HorizontalAxis.values().flatMap { horizontalAxis ->
             (1..15).map { verticalAxis ->
                 Position(horizontalAxis, verticalAxis)
