030383b76ae7ca6f20934a619a03e2d1b49bd2a5
EmilyCh0 <emily9803@ewhain.net>
2023-03-15 16:34:14 +0900

    feat(OmokGame): 흑,백 반복하며 게임을 진행하고 승자가 있을 때 게임이 끝나는 기능 구현

---
+++ src/main/kotlin/domain/OmokGame.kt
@@ -0,0 +1,44 @@
+package domain
+
+import domain.turn.BlackTurn
+import domain.turn.WhiteTurn
+
+class OmokGame(
+    private val board: Board = Board(BlackTurn(), WhiteTurn()),
+    private val referee: Referee = Referee()
+) {
+    fun runGame(
+        getStone: () -> Stone,
+        onFinish: (Color) -> Unit
+    ) {
+        while (true) {
+            val blackStone = getStone()
+            blackTurn(blackStone)
+            if (isVictory(Color.BLACK, onFinish)) break
+
+            val whiteStone = getStone()
+            whiteTurn(whiteStone)
+            if (isVictory(Color.WHITE, onFinish)) break
+        }
+    }
+
+    private fun isVictory(color: Color, onFinish: (Color) -> Unit): Boolean {
+        if (referee.isWin(board.getTotalState())) {
+            onFinish(color)
+            return true
+        }
+        return false
+    }
+
+    private fun blackTurn(stone: Stone) {
+        if (board.canMove(stone)) {
+            board.moveBlack(stone)
+        }
+    }
+
+    private fun whiteTurn(stone: Stone) {
+        if (board.canMove(stone)) {
+            board.moveWhite(stone)
+        }
+    }
+}
