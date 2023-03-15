5cea3854d91b0ebdd41a4ef9f718e214afdb075c
EmilyCh0 <emily9803@ewhain.net>
2023-03-15 15:14:38 +0900

    feat(Board): 해당 턴의 수를 착수한다

--- src/test/kotlin/domain/BoardTest.kt
+++ src/test/kotlin/domain/BoardTest.kt
@@ -21,4 +21,36 @@ class BoardTest {
         // then
         assertThat(actual).isFalse
     }
+
+    @Test
+    fun `화이트 턴의 1,1위치에 착수한다`() {
+        // given
+        val stone = Stone(1, 1)
+        val blackTurn = BlackTurn()
+        val whiteTurn = WhiteTurn()
+        val board = Board(blackTurn, whiteTurn)
+
+        // when
+        board.moveWhite(stone)
+
+        // then
+        assertThat(board.whiteTurn.isEmpty(stone)).isFalse
+        assertThat(board.blackTurn.isEmpty(stone)).isTrue
+    }
+
+    @Test
+    fun `블랙 턴의 1,1위치에 착수한다`() {
+        // given
+        val stone = Stone(1, 1)
+        val blackTurn = BlackTurn()
+        val whiteTurn = WhiteTurn()
+        val board = Board(blackTurn, whiteTurn)
+
+        // when
+        board.moveBlack(stone)
+
+        // then
+        assertThat(board.whiteTurn.isEmpty(stone)).isTrue
+        assertThat(board.blackTurn.isEmpty(stone)).isFalse
+    }
 }
