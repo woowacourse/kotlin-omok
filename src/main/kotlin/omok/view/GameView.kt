1b459e918055808befefe419b3fcd35dbcb807b7
Yerin <myheaven_b@naver.com>
2023-03-15 20:22:56 +0900

    refactor: 상수화 처리

--- src/main/kotlin/omok/view/GameView.kt
+++ src/main/kotlin/omok/view/GameView.kt
@@ -1,7 +1,8 @@
 package omok.view
 
-import omok.domain.ColorState
 import omok.domain.Position
+import omok.domain.state.Turn
+import omok.domain.state.Win
 
 class GameView {
     private val omokBoard = StringBuilder(OMOK_BOARD)
@@ -11,30 +12,34 @@ class GameView {
         println(omokBoard)
     }
 
-    fun readPosition(colorState: ColorState, lastPosition: String?): String {
-        when (colorState) {
-            ColorState.Black -> println("%s의 차례입니다.".format("흑"))
-            ColorState.White -> println("%s의 차례입니다.".format("백"))
+    fun readPosition(state: Turn, lastPosition: String?): String {
+        when (state) {
+            Turn.Black -> print(TURN_MSG.format(BLACK_MSG))
+            Turn.White -> print(TURN_MSG.format(WHITE_MSG))
         }
-        lastPosition?.let { println(" (마지막 돌의 위치: %s)".format(lastPosition)) }
-        print("위치를 입력하세요: ")
+        lastPosition?.let { println(LAST_STONE_MSG.format(lastPosition)) }
+        print(INPUT_LAST_POSITION_MSG)
         return readln()
     }
 
-    fun printBoard(colorState: ColorState, position: Position) {
-        val index = ((position.horizontalAxis.value) * 3) + ((15 - position.verticalAxis) * 47)
-        when (colorState) {
-            ColorState.Black -> omokBoard.setCharAt(index, '●')
-            ColorState.White -> omokBoard.setCharAt(index, '○')
+    fun printBoard(state: Turn, position: Position) {
+        val index = ((position.horizontalAxis.value) * CALCULATE_THREE) + ((CALCULATE_FIFTEEN - position.verticalAxis) * CALCULATE_FORTY_SEVEN)
+        when (state) {
+            Turn.Black -> omokBoard.setCharAt(index, BLACK_STONE)
+            Turn.White -> omokBoard.setCharAt(index, WHITE_STONE)
         }
         println(omokBoard)
     }
 
-    fun printWinMessage(colorState: ColorState) {
-        when (colorState) {
-            ColorState.Black -> println("%s의 승리입니다.".format("흑"))
-            ColorState.White -> println("%s의 승리입니다.".format("백"))
+    fun printWinMessage(state: Win) {
+        when (state) {
+            Win.Black -> println(WIN_MSG.format(BLACK_MSG))
+            Win.White -> println(WIN_MSG.format(WHITE_MSG))
         }
+    }
+
+    fun printRetry() {
+        println(RETRY_MES)
     }
 
     companion object {
@@ -56,5 +61,19 @@ class GameView {
                 " 2 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤\n" +
                 " 1 └──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┘\n" +
                 "   A  B  C  D  E  F  G  H  I  J  K  L  M  N  O"
+
+        private const val TURN_MSG = "%s의 차례입니다."
+        private const val BLACK_MSG = "흑"
+        private const val WHITE_MSG = "백"
+        private const val LAST_STONE_MSG = "(마지막 돌의 위치: %s)"
+        private const val INPUT_LAST_POSITION_MSG = "위치를 입력하세요: "
+        private const val RETRY_MES = "해당 위치에 돌을 놓을 수 없습니다."
+        private const val BLACK_STONE = '●'
+        private const val WHITE_STONE = '◌'
+        private const val WIN_MSG = "%s의 승리입니다."
+
+        private const val CALCULATE_THREE = 3
+        private const val CALCULATE_FIFTEEN = 15
+        private const val CALCULATE_FORTY_SEVEN = 47
     }
 }
