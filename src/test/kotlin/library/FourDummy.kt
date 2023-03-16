7ece7cdefce135362a36da581f51bd0794c186cd
briandr97 <briandr@naver.com>
2023-03-16 13:56:03 +0900

    feat(ForbiddenFour): 44 금수 라이브러리 테스트

---
+++ src/test/kotlin/library/FourDummy.kt
@@ -0,0 +1,41 @@
+package library
+
+import domain.Coordinate
+import domain.CoordinateState
+import domain.Position
+
+object FourDummy {
+    // 3   ? X X X
+    // 4   X
+    // 5   X
+    // 6   X
+    //     3 4 5 6
+    val forbiddenFourCoordinate1 = Position(Coordinate(3), Coordinate(3))
+    fun getForbiddenFourBoard1(): List<List<CoordinateState>> {
+        val board = List(15) { MutableList(15) { CoordinateState.EMPTY } }
+        board[3][4] = CoordinateState.BLACK
+        board[3][5] = CoordinateState.BLACK
+        board[3][6] = CoordinateState.BLACK
+        board[4][3] = CoordinateState.BLACK
+        board[5][3] = CoordinateState.BLACK
+        board[6][3] = CoordinateState.BLACK
+        return board
+    }
+
+    // 3   ? X X X
+    // 4     X
+    // 5       X
+    // 6         X
+    //     3 4 5 6
+    val forbiddenFourCoordinate2 = Position(Coordinate(3), Coordinate(3))
+    fun getForbiddenFourBoard2(): List<List<CoordinateState>> {
+        val board = List(15) { MutableList(15) { CoordinateState.EMPTY } }
+        board[3][4] = CoordinateState.BLACK
+        board[3][5] = CoordinateState.BLACK
+        board[3][6] = CoordinateState.BLACK
+        board[4][4] = CoordinateState.BLACK
+        board[5][5] = CoordinateState.BLACK
+        board[6][6] = CoordinateState.BLACK
+        return board
+    }
+}
