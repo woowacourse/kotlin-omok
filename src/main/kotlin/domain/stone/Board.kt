de2a188fcd2b6aa592dc87a4164eb12a9596762c
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-16 13:10:44 +0900

    feat[board]: board에 스톤들을 관리하도록 구현

--- src/main/kotlin/domain/stone/Board.kt
+++ src/main/kotlin/domain/stone/Board.kt
@@ -1,5 +1,16 @@
 package domain.stone
 
-import domain.state.State
+class Board {
 
-class Board(val state: State)
+    val board: MutableList<MutableList<StoneType>> = MutableList(16) { MutableList(16) { StoneType.EMPTY } }
+    val stones: Stones = Stones()
+
+    fun putStone(stone: Stone) {
+        board[(stone.position.y)][stone.position.x] = stone.type
+        stones.add(stone)
+    }
+
+    companion object {
+        const val BOARD_SIZE = 15
+    }
+}
---
+++ src/test/kotlin/domain/stone/BoardTest.kt
@@ -0,0 +1,21 @@
+package domain.stone
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+class BoardTest {
+
+    @Test
+    fun `board에 스톤을 추가 할 수 있다`() {
+        val board = Board()
+        board.putStone(Stone(StonePosition.from(1, 1)!!, StoneType.BLACK))
+        board.putStone(Stone(StonePosition.from(3, 1)!!, StoneType.WHITE))
+
+        assertAll(
+            { assertThat(board.board[1][1]).isEqualTo(StoneType.BLACK) },
+            { assertThat(board.board[1][3]).isEqualTo(StoneType.WHITE) },
+            { assertThat(board.stones.values.size).isEqualTo(2) },
+        )
+    }
+}
