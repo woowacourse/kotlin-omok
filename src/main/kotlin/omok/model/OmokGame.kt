da70efd8713eb25a20b31b5de908e6b7f63c2c9a
SangHyun Moon <no1msh1217@gmail.com>
2023-03-16 15:29:40 +0900

    feat: 오목이 완성되면 게임이 끝나도록 구현

--- src/main/kotlin/omok/model/OmokGame.kt
+++ src/main/kotlin/omok/model/OmokGame.kt
@@ -1,17 +1,31 @@
 package omok.model
 
+import omok.model.judge.OmokRule
+import omok.model.state.State
+import omok.model.state.Stay
 import omok.model.stone.Coordinate
+import omok.model.stone.GoStone
 
 class OmokGame(val board: Board) {
     fun start(coordinate: () -> Coordinate, showBoard: (Board) -> Unit) {
         while (true) {
-            turn(coordinate, showBoard)
+            val newStone = turn(coordinate, showBoard)
+            val state = checkBoard(newStone)
+
+            if (state !is Stay) break
         }
     }
 
-    fun turn(coordinate: () -> Coordinate, showBoard: (Board) -> Unit) {
+    fun turn(coordinate: () -> Coordinate, showBoard: (Board) -> Unit): GoStone {
         board.addStone(board.getNextColor(), getValidCoordinate(coordinate))
         showBoard(board)
+        return board.lastPlacedStone ?: throw IllegalArgumentException("바둑판 위에 놓인 돌이 없습니다")
+    }
+
+    fun checkBoard(stone: GoStone): State {
+        val rule = OmokRule(board, stone)
+
+        return rule.checkWin()
     }
 
     private fun getValidCoordinate(getCoordinate: () -> Coordinate): Coordinate {
