4ae2570326eed9ccd437a495e437afe302cf7aae
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-16 13:53:37 +0900

    feat: Controller 구현

---
+++ src/main/kotlin/controller/OmokController.kt
@@ -0,0 +1,36 @@
+package controller
+
+import domain.state.BlackTurn
+import domain.state.End
+import domain.state.State
+import domain.state.WhiteTurn
+import domain.stone.Board
+import domain.stone.Stone
+import domain.stone.StoneType
+import view.InputView
+import view.OutputView
+
+class OmokController(
+    val inputView: InputView = InputView(),
+    val outputView: OutputView = OutputView(),
+) {
+
+    fun run() {
+        val board: Board = Board()
+        var state: State = BlackTurn(board)
+
+        outputView.printOmokStart()
+        while (state !is End) {
+            outputView.printTurn(state, board.stones)
+
+            when (state) {
+                is BlackTurn -> state = state.put(Stone(inputView.inputStonePosition(), StoneType.BLACK))
+                is WhiteTurn -> state = state.put(Stone(inputView.inputStonePosition(), StoneType.WHITE))
+            }
+
+            outputView.printBoard(board.stones)
+        }
+
+        outputView.printWinner(state)
+    }
+}
