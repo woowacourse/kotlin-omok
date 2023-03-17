99e5db9405d0acb901bf579e789038172be7ba55
Doyeop Kim <dygames34@gmail.com>
2023-03-17 16:44:51 +0900

    refactor: Controller가 View 구현에 의존하지 않도록 Interface를 가지게 변경, 함수 순서 변경

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,9 +1,10 @@
 import controller.GameController
+import view.ConsoleGameView
 
 fun main() {
     Thread.setDefaultUncaughtExceptionHandler { _, exception ->
         exception.printStackTrace()
     }
-    val game = GameController()
+    val game = GameController(ConsoleGameView())
     game.process()
 }
--- src/main/kotlin/controller/GameController.kt
+++ src/main/kotlin/controller/GameController.kt
@@ -8,24 +8,19 @@ import domain.Players
 import domain.Stones
 import domain.WhitePlayer
 import dto.PointDTO
-import view.ConsoleGameView
+import view.GameView
 
-class GameController(
-    private val gameView: ConsoleGameView = ConsoleGameView()
-) {
-
-    private fun renderBoard(stones: Stones) {
-        gameView.renderBoard(
-            stones.value.map {
-                StoneMapper.domainToDTO(it)
-            },
-            PointMapper.domainToDTO(Board.BOARD_SIZE)
-        )
-    }
-
-    private fun getStoneCoordinateOrNull(stones: Stones): PointDTO? {
-        return if (stones.value.isEmpty()) null
-        else PointMapper.domainToDTO(stones.value.last().coordinate.point)
+class GameController(private val gameView: GameView) {
+    fun process() {
+        val players = Players(listOf(BlackPlayer(), WhitePlayer()))
+        val stones = Stones()
+        val board = Board(players, stones)
+        gameView.startGame()
+        val winner = board.repeatTurn {
+            readStone(it, stones)
+        }
+        renderBoard(stones)
+        gameView.renderWinner(ColorMapper.domainToDTO(winner.color))
     }
 
     private fun readStone(color: Color, stones: Stones): Coordinate {
@@ -39,15 +34,17 @@ class GameController(
         return Coordinate.from(point.x, point.y) ?: return readStone(color, stones)
     }
 
-    fun process() {
-        val players = Players(listOf(BlackPlayer(), WhitePlayer()))
-        val stones = Stones()
-        val board = Board(players, stones)
-        gameView.startGame()
-        val winner = board.repeatTurn {
-            readStone(it, stones)
-        }
-        renderBoard(stones)
-        gameView.renderWinner(ColorMapper.domainToDTO(winner.color))
+    private fun getStoneCoordinateOrNull(stones: Stones): PointDTO? {
+        return if (stones.value.isEmpty()) null
+        else PointMapper.domainToDTO(stones.value.last().coordinate.point)
+    }
+
+    private fun renderBoard(stones: Stones) {
+        gameView.renderBoard(
+            stones.value.map {
+                StoneMapper.domainToDTO(it)
+            },
+            PointMapper.domainToDTO(Board.BOARD_SIZE)
+        )
     }
 }
