7c4fccb99ee2fabe3d32fe224b413c77995aab16
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-03-17 13:52:26 +0900

    refactor(GmaeController): 함수 호출 순으로 함수 순서 변경

--- src/main/kotlin/controller/GameController.kt
+++ src/main/kotlin/controller/GameController.kt
@@ -13,19 +13,16 @@ import view.ConsoleGameView
 class GameController(
     private val gameView: ConsoleGameView = ConsoleGameView()
 ) {
-
-    private fun renderBoard(stones: Stones) {
-        gameView.renderBoard(
-            stones.value.map {
-                StoneMapper.domainToDTO(it)
-            },
-            PointMapper.domainToDTO(Board.BOARD_SIZE)
-        )
-    }
-
-    private fun getStoneCoordinateOrNull(stones: Stones): PointDTO? {
-        return if (stones.value.isEmpty()) null
-        else PointMapper.domainToDTO(stones.value.last().coordinate.point)
+    fun process() {
+        val players = Players(listOf(BlackPlayer(), WhitePlayer()))
+        val stones = Stones()
+        val board = Board(players, stones)
+        gameView.startGame()
+        val winner = board.repeatTurn {
+            readStone(it, stones)
+        }
+        renderBoard(stones)
+        gameView.renderWinner(ColorMapper.domainToDTO(winner.color))
     }
 
     private fun readStone(color: Color, stones: Stones): Coordinate {
@@ -39,15 +36,17 @@ class GameController(
         return Coordinate.from(point.x, point.y) ?: return readStone(color, stones)
     }
 
-    fun process() {
-        val players = Players(listOf(BlackPlayer(), WhitePlayer()))
-        val stones = Stones()
-        val board = Board(players, stones)
-        gameView.startGame()
-        val winner = board.repeatTurn {
-            readStone(it, stones)
-        }
-        renderBoard(stones)
-        gameView.renderWinner(ColorMapper.domainToDTO(winner.color))
+    private fun renderBoard(stones: Stones) {
+        gameView.renderBoard(
+            stones.value.map {
+                StoneMapper.domainToDTO(it)
+            },
+            PointMapper.domainToDTO(Board.BOARD_SIZE)
+        )
+    }
+
+    private fun getStoneCoordinateOrNull(stones: Stones): PointDTO? {
+        return if (stones.value.isEmpty()) null
+        else PointMapper.domainToDTO(stones.value.last().coordinate.point)
     }
 }
