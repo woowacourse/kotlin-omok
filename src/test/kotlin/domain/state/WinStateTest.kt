f470773cdd605c6d1bcbcf828523207b71aaf845
tmdgh1592 <tmdgh1592@naver.com>
2023-03-18 17:00:09 +0900

    test: WinState 클래스 테스트 코드 작성

---
+++ src/test/kotlin/domain/state/WinStateTest.kt
@@ -0,0 +1,38 @@
+package domain.state
+
+import ONE_FOUR_STONE
+import domain.rule.OmokRule
+import domain.rule.RenjuRule
+import domain.stone.Stones
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class WinStateTest {
+    private lateinit var playingState: PlayerState
+    private lateinit var rule: OmokRule
+
+    @BeforeEach
+    fun setUp() {
+        playingState = WinState(Stones())
+        rule = RenjuRule()
+    }
+
+    @Test
+    fun `승리 상태는 게임이 종료된 상태이다`() {
+        assertThat(playingState.isPlaying).isFalse
+    }
+
+    @Test
+    fun `승리 상태는 반칙을 하지 않은 상태이다`() {
+        assertThat(playingState.isFoul).isFalse
+    }
+
+    @Test
+    fun `게임이 종료된 상태에서 오목알을 새롭게 추가하지 못한다`() {
+        val expected = playingState.add(ONE_FOUR_STONE, rule)
+        val actual = playingState
+
+        assertThat(expected).isEqualTo(actual)
+    }
+}
