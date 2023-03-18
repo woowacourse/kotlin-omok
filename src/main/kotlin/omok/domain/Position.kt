00d31bea85fafebec63841a7b93eae492dd2dc55
hyunji1203 <hj27720@gmail.com>
2023-03-19 00:01:01 +0900

    refactor(Position): format 함수 사용

--- src/main/kotlin/omok/domain/Position.kt
+++ src/main/kotlin/omok/domain/Position.kt
@@ -4,8 +4,8 @@ data class Position(val horizontalAxis: HorizontalAxis, val verticalAxis: Int) {
     private var empty = true
 
     init {
-        require(horizontalAxis in HorizontalAxis.values()) { HORIZONTAL_AXIS_ERROR_MSG }
-        require(verticalAxis in MIN_VERTICAL_AXIS..MAX_VERTICAL_AXIS) { VERTICAL_AXIS_ERROR_MSG }
+        require(horizontalAxis in HorizontalAxis.values()) { HORIZONTAL_AXIS_ERROR_MSG.format(MIN_HORIZONTAL_AXIS, MAX_HORIZONTAL_AXIS) }
+        require(verticalAxis in MIN_VERTICAL_AXIS..MAX_VERTICAL_AXIS) { VERTICAL_AXIS_ERROR_MSG.format(MIN_VERTICAL_AXIS, MAX_VERTICAL_AXIS) }
     }
 
     fun isEmpty(): Boolean = empty
@@ -17,8 +17,10 @@ data class Position(val horizontalAxis: HorizontalAxis, val verticalAxis: Int) {
     companion object {
         private const val MIN_VERTICAL_AXIS = 1
         private const val MAX_VERTICAL_AXIS = 15
-        private const val HORIZONTAL_AXIS_ERROR_MSG = "가로축은 A와 O 사이 입니다."
-        private const val VERTICAL_AXIS_ERROR_MSG = "세로축은 1과 15 사이 입니다."
+        private const val MIN_HORIZONTAL_AXIS = "A"
+        private const val MAX_HORIZONTAL_AXIS = "O"
+        private const val HORIZONTAL_AXIS_ERROR_MSG = "가로축은 %s와 %s 사이입니다."
+        private const val VERTICAL_AXIS_ERROR_MSG = "세로축은 %d과 %d 사이 입니다."
         val POSITIONS = HorizontalAxis.values().flatMap { horizontalAxis ->
             (MIN_VERTICAL_AXIS..MAX_VERTICAL_AXIS).map { verticalAxis ->
                 Position(horizontalAxis, verticalAxis)
