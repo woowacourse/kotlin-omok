31ad7d6abd29720f3f1d47b766a44bcb358e1496
EmilyCh0 <emily9803@ewhain.net>
2023-03-18 14:22:08 +0900

    refactor(Referee): 오목 판단 로직 메서드 분리

--- src/main/kotlin/domain/Referee.kt
+++ src/main/kotlin/domain/Referee.kt
@@ -10,51 +10,56 @@ class Referee(
             for (j in 0 until size) {
                 if (omokBoard.value[i][j] == State.EMPTY) continue
 
-                // 가로 방향 체크
-                if (j + 4 < size &&
-                    omokBoard.value[i][j] == state &&
-                    omokBoard.value[i][j + 1] == state &&
-                    omokBoard.value[i][j + 2] == state &&
-                    omokBoard.value[i][j + 3] == state &&
-                    omokBoard.value[i][j + 4] == state
-                ) {
-                    return true
-                }
-
-                // 세로 방향 체크
-                if (i + 4 < size &&
-                    omokBoard.value[i][j] == state &&
-                    omokBoard.value[i + 1][j] == state &&
-                    omokBoard.value[i + 2][j] == state &&
-                    omokBoard.value[i + 3][j] == state &&
-                    omokBoard.value[i + 4][j] == state
-                ) {
-                    return true
-                }
-
-                // 대각선 방향 체크 (좌상단에서 우하단)
-                if (i + 4 < size && j + 4 < size &&
-                    omokBoard.value[i][j] == state &&
-                    omokBoard.value[i + 1][j + 1] == state &&
-                    omokBoard.value[i + 2][j + 2] == state &&
-                    omokBoard.value[i + 3][j + 3] == state &&
-                    omokBoard.value[i + 4][j + 4] == state
-                ) {
-                    return true
-                }
-
-                // 대각선 방향 체크 (우상단에서 좌하단)
-                if (i + 4 < size && j - 4 >= 0 &&
-                    omokBoard.value[i][j] == state &&
-                    omokBoard.value[i + 1][j - 1] == state &&
-                    omokBoard.value[i + 2][j - 2] == state &&
-                    omokBoard.value[i + 3][j - 3] == state &&
-                    omokBoard.value[i + 4][j - 4] == state
-                ) {
-                    return true
-                }
+                if (j + 4 < size && isHorizontalOmok(i, j, omokBoard, state)) return true
+                if (i + 4 < size && isVerticalOmok(i, j, omokBoard, state)) return true
+                if (i + 4 < size && j + 4 < size && isRightDiagonalOmok(i, j, omokBoard, state)) return true
+                if (i + 4 < size && j - 4 >= 0 && isLeftDiagonalOmok(i, j, omokBoard, state)) return true
             }
         }
+        return false
+    }
+
+    private fun isHorizontalOmok(i: Int, j: Int, omokBoard: OmokBoard, state: State): Boolean {
+        if (
+            omokBoard.value[i][j] == state &&
+            omokBoard.value[i][j + 1] == state &&
+            omokBoard.value[i][j + 2] == state &&
+            omokBoard.value[i][j + 3] == state &&
+            omokBoard.value[i][j + 4] == state
+        ) return true
+        return false
+    }
+
+    private fun isVerticalOmok(i: Int, j: Int, omokBoard: OmokBoard, state: State): Boolean {
+        if (
+            omokBoard.value[i][j] == state &&
+            omokBoard.value[i + 1][j] == state &&
+            omokBoard.value[i + 2][j] == state &&
+            omokBoard.value[i + 3][j] == state &&
+            omokBoard.value[i + 4][j] == state
+        ) return true
+        return false
+    }
+
+    private fun isRightDiagonalOmok(i: Int, j: Int, omokBoard: OmokBoard, state: State): Boolean {
+        if (
+            omokBoard.value[i][j] == state &&
+            omokBoard.value[i + 1][j + 1] == state &&
+            omokBoard.value[i + 2][j + 2] == state &&
+            omokBoard.value[i + 3][j + 3] == state &&
+            omokBoard.value[i + 4][j + 4] == state
+        ) return true
+        return false
+    }
+
+    private fun isLeftDiagonalOmok(i: Int, j: Int, omokBoard: OmokBoard, state: State): Boolean {
+        if (
+            omokBoard.value[i][j] == state &&
+            omokBoard.value[i + 1][j - 1] == state &&
+            omokBoard.value[i + 2][j - 2] == state &&
+            omokBoard.value[i + 3][j - 3] == state &&
+            omokBoard.value[i + 4][j - 4] == state
+        ) return true
         return false
     }
 
