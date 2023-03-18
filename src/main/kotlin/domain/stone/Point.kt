6b6df07e84fbf55f001601ec91ff1cd869fe6181
boogi-woogi <88770426+boogi-woogi@users.noreply.github.com>
2023-03-19 01:26:17 +0900

    refactor(Point): 매직넘버 상수화

--- src/main/kotlin/domain/stone/Point.kt
+++ src/main/kotlin/domain/stone/Point.kt
@@ -3,8 +3,13 @@ package domain.stone
 data class Point(val x: Int, val y: Int) {
 
     init {
-        require(x in 0..14 && y in 0..14) {
-            // TODO: MESSAGE
+        require(x in POINT_RANGE && y in POINT_RANGE) {
+            ERROR_RANGE
         }
+    }
+
+    companion object {
+        private val POINT_RANGE = (0..14)
+        private const val ERROR_RANGE = "[ERROR] X와 Y의 범위는 1이상 15이하입니다."
     }
 }
