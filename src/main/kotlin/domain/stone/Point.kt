3a49eecb662f39b70b3945139d8a05fc33745145
boogieWoogie <kimjinuk1999@gmail.com>
2023-03-18 18:24:31 +0900

    test(Point): 좌표가 범위를 벗어난 경우 예외가 발생한다

--- src/main/kotlin/domain/stone/Point.kt
+++ src/main/kotlin/domain/stone/Point.kt
@@ -1,3 +1,10 @@
 package domain.stone
 
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Int, val y: Int) {
+
+    init {
+        require(x in 0..14 && y in 0..14) {
+            // TODO: MESSAGE
+        }
+    }
+}
