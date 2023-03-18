2b122915a7a22ccb0ef5e46eabb4db0e3968b724
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-18 19:12:40 +0900

    refactor (InputView): 함수명 변경

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -18,15 +18,15 @@ class InputView {
         val lastLocation = omokPoint?.let { "(마지막 돌의 위치: ${it.x.toChar()}${it.y.value})" } ?: ""
         println("${stoneState.name()}의 차례입니다. $lastLocation")
         print("위치를 입력하세요: ")
-        return getPoint()
+        return readPoint()
     }
 
-    private fun getPoint(): OmokPoint = runCatching {
+    private fun readPoint(): OmokPoint = runCatching {
         readln().let {
             val x: Char = it[0].uppercaseChar()
             val y: Int = it.substring(1).toInt()
             return OmokPoint(XCoordinate(x), YCoordinate(y))
         }
     }.onFailure { println(it.message) }
-        .getOrElse { getPoint() }
+        .getOrElse { readPoint() }
 }
