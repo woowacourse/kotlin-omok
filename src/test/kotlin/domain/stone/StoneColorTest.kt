78c8d8496f90117a2cb928608d1762eaecebf58e
tmdgh1592 <tmdgh1592@naver.com>
2023-03-18 16:46:14 +0900

    test: Stone 클래스 테스트 코드 작성

--- src/test/kotlin/domain/stone/StoneColorTest.kt
+++ src/test/kotlin/domain/stone/StoneColorTest.kt
@@ -1,4 +1,5 @@
-import domain.stone.StoneColor
+package domain.stone
+
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
--- src/test/kotlin/domain/stone/StonesTest.kt
+++ src/test/kotlin/domain/stone/StonesTest.kt
@@ -1,4 +1,11 @@
-import domain.stone.Stones
+package domain.stone
+
+import ONE_FIVE_STONE
+import ONE_FOUR_STONE
+import ONE_ONE_STONE
+import ONE_THREE_STONE
+import ONE_TWO_STONE
+import domain.position.Position
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -49,5 +56,22 @@ class StonesTest {
         val expected = stones.checkWin(ONE_ONE_STONE)
 
         assertThat(expected).isFalse
+    }
+
+    @Test
+    fun `마지막 놓은 오목알을 반환한다`() {
+        val stones = Stones(ONE_ONE_STONE, ONE_TWO_STONE, ONE_THREE_STONE)
+        val expected = stones.lastStone
+
+        assertThat(expected).isEqualTo(ONE_THREE_STONE)
+    }
+
+    @Test
+    fun `오목알이 놓인 위치 목록을 반환한다`() {
+        val stones = Stones(ONE_ONE_STONE, ONE_TWO_STONE, ONE_THREE_STONE)
+        val expected = stones.getPositions()
+        val actual = listOf(Position(1, 1), Position(1, 2), Position(1, 3))
+
+        assertThat(expected).isEqualTo(actual)
     }
 }
