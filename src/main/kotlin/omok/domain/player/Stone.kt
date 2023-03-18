c5e3243fcb3b2a38593a81e6b609b03b1ae6cce7
gloria <ippnsj@gmail.com>
2023-03-18 21:32:03 +0900

    refactor: 오목돌을 흑과 백으로 제한하도록 수정

--- src/main/kotlin/omok/domain/player/Stone.kt
+++ src/main/kotlin/omok/domain/player/Stone.kt
@@ -3,6 +3,6 @@ package omok.domain.player
 import omok.domain.board.Position
 import omok.domain.judgment.PlacementReferee
 
-interface Stone {
-    fun canPlace(referee: PlacementReferee, board: Map<Position, Stone?>, position: Position): Boolean = true
+sealed class Stone {
+    abstract fun canPlace(referee: PlacementReferee, board: Map<Position, Stone?>, position: Position): Boolean
 }
