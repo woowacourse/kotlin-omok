512b1827061662c584c818ae83ff9f078f698bf4
vrexpert <vrexpert2302@gmail.com>
2023-03-19 18:11:27 +0900

    refactor(End): OmokTest 에서 EndTest 이름 변경

--- src/test/kotlin/model/domain/state/EndTest.kt
+++ src/test/kotlin/model/domain/state/EndTest.kt
@@ -1,26 +1,25 @@
-package model.domain
+package model.domain.state
 
-import model.domain.state.State
-import model.domain.state.black.BlackOmok
 import model.domain.tools.Board
 import model.domain.tools.Coordination
 import model.domain.tools.Location
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class OmokTest {
+class EndTest {
+
     @Test
-    fun `Omok 에서 place 를 호출하면 예외가 발생한다`() {
+    fun `End 에서 place 를 호출하면 예외가 발생한다`() {
         // given
-        val state: State = BlackOmok()
-        val location = Location(Coordination.from(1), Coordination.from(1))
         val board = Board.create()
+        val state: State = BlackOmok(board)
+        val location = Location(Coordination.from(1), Coordination.from(1))
 
         // when
 
         // then
         assertThrows<IllegalStateException>("") {
-            state.place(location, board)
+            state.place(location)
         }
     }
 }
