f19906d9d92b49e270fd2d3ae65fdd62ae3197d6
vrexpert <vrexpert2302@gmail.com>
2023-03-19 17:54:19 +0900

    refactor(Turn): 추상클래스 Turn 재정의

--- src/main/kotlin/model/domain/state/Turn.kt
+++ src/main/kotlin/model/domain/state/Turn.kt
@@ -3,29 +3,9 @@ package model.domain.state
 import model.domain.rule.OmokRule
 import model.domain.tools.Board
 import model.domain.tools.Location
-import model.domain.tools.Stone
 
-abstract class Turn : State {
-    abstract val stoneColor: Stone
-    abstract val retryTurn: RetryTurn
-    abstract val omok: Omok
-    abstract val turn: Turn
-
-    override fun place(location: Location, board: Board): State {
-        val y = location.coordinationX.value
-        val x = location.coordinationY.value
-
-        return when {
-            isForbidden(board, x, y) -> retryTurn
-            !board.placeStone(location, stoneColor) -> retryTurn
-            OmokRule.isOmok(board, location, stoneColor) -> omok
-            else -> turn
-        }
-    }
-
-    abstract fun isForbidden(board: Board, x: Int, y: Int): Boolean
-
-    override fun retry(): State {
-        throw IllegalStateException("")
-    }
+abstract class Turn(override val board: Board) : State {
+    abstract override fun place(location: Location): State
+    protected fun isOmok(location: Location): Boolean = OmokRule.isOmok(board, location, stone)
+    abstract fun isForbidden(location: Location): Boolean
 }
