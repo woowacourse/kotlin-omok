5d3820fb60454828fb7caf1533aad49f2b6700f5
vrexpert <vrexpert2302@gmail.com>
2023-03-20 11:16:22 +0900

    refactor(BoardView): 사이즈에 따라 보드를 동적으로 생성

--- src/main/kotlin/view/BoardView.kt
+++ src/main/kotlin/view/BoardView.kt
@@ -6,7 +6,55 @@ import model.domain.tools.Stone.BLACK
 import model.domain.tools.Stone.EMPTY
 import model.domain.tools.Stone.WHITE
 
-class BoardView {
+object BoardView {
+
+    private const val BLACK_STONE = '●'
+    private const val WHITE_STONE = '◌'
+    private const val LEFT_INTERVAL = 4
+    private const val RIGHT_INTERVAL = 3
+    private const val BOARD_SIZE = 15
+    private const val WITHOUT_TOP_BOTTOM = BOARD_SIZE - 2
+    private const val WITHOUT_TOP = BOARD_SIZE - 1
+    private const val BEFORE_BOTTOM = 2
+    private const val LINE_UP_CRITERIA = 10
+
+    private val alphabet = List(BOARD_SIZE) { " ${(it + 65).toChar().uppercaseChar()}" }
+
+    private val start = buildString {
+        append("┌")
+        append("──┬".repeat(WITHOUT_TOP_BOTTOM))
+        append("──┐")
+    }
+    private val middle = buildString {
+        append("├")
+        append("──┼".repeat(WITHOUT_TOP_BOTTOM))
+        append("──┤")
+    }
+    private val end = buildString {
+        append("└")
+        append("──┴".repeat(WITHOUT_TOP_BOTTOM))
+        append("──┘")
+    }
+    private val boardLast = buildString {
+        append(" ")
+        append(" ").repeat(LEFT_INTERVAL)
+        alphabet.forEach { append(" $it") }
+    }
+
+    private val board = buildList {
+        add(" $BOARD_SIZE $start")
+        for (row in WITHOUT_TOP downTo BEFORE_BOTTOM) {
+            add(getLineCriteria(row))
+        }
+        add("  1 $end")
+        add(boardLast)
+    }
+
+    private fun getLineCriteria(rowNumber: Int): String {
+        if (rowNumber >= LINE_UP_CRITERIA) return " $rowNumber $middle"
+        return "  $rowNumber $middle"
+    }
+
     fun printBoard(omokBoard: Board) {
         omokBoard.system.values.forEachIndexed { row, rowLine ->
             println(printBoardLine(row, rowLine))
@@ -29,31 +77,5 @@ class BoardView {
         BLACK -> BLACK_STONE
         WHITE -> WHITE_STONE
         EMPTY -> null
-    }
-
-    companion object {
-        private const val BLACK_STONE = '●'
-        private const val WHITE_STONE = '◌'
-        private const val LEFT_INTERVAL = 4
-        private const val RIGHT_INTERVAL = 3
-
-        private val board = listOf(
-            " 15 ┌──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┐",
-            " 14 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            " 13 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            " 12 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            " 11 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            " 10 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            "  9 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            "  8 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            "  7 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            "  6 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            "  5 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            "  4 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            "  3 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            "  2 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤",
-            "  1 └──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┘",
-            "    A  B  C  D  E  F  G  H  I  J  K  L  M  N  O",
-        )
     }
 }
