05cb668e063df987c8d22b1fc84ff3b1dbdbb2f5
vrexpert <vrexpert2302@gmail.com>
2023-03-20 11:16:58 +0900

    refactor(OmokGame): 오목 게임 재작성 및 테스트 구현

--- src/main/kotlin/model/domain/OmokGame.kt
+++ src/main/kotlin/model/domain/OmokGame.kt
@@ -1,55 +1,22 @@
 package model.domain
 
-import model.domain.state.Omok
-import model.domain.state.RetryTurn
 import model.domain.state.State
-import model.domain.state.black.BlackOmok
-import model.domain.state.black.BlackTurn
-import model.domain.state.white.WhiteOmok
-import model.domain.state.white.WhiteTurn
-import model.domain.tools.Board
 import model.domain.tools.Coordination
+import model.domain.tools.Dot
 import model.domain.tools.Location
 import model.domain.tools.Stone
-import model.domain.tools.Stone.BLACK
-import model.domain.tools.Stone.WHITE
 
-class OmokGame(private val board: Board) {
-    private var player: State = BlackTurn()
+class OmokGame {
+    fun playNextTurn(state: State, getDot: (Stone) -> Dot): State {
+        val location: Location = getLocation(state, getDot)
 
-    fun gameStart(getCoordination: (Stone) -> Pair<Int, Int>, printBoard: (Board) -> Unit) {
-        while (player !is Omok) {
-            printBoard(board)
-            play(getCoordination)
-            changeState()
-        }
+        if (state.board.canPlace(location)) return state.place(location)
+        return state
     }
 
-    private fun play(getCoordination: (Stone) -> Pair<Int, Int>) {
-        val value = getCoordination(getStoneColor())
+    private fun getLocation(state: State, getDot: (Stone) -> Dot): Location = runCatching {
+        val dot = getDot(state.stone)
 
-        val location = Location(Coordination.from(value.first), Coordination.from(value.second))
-        player = player.place(location, board)
-    }
-
-    private fun changeState() {
-        when (player) {
-            is RetryTurn -> player = player.retry()
-            is BlackTurn -> player = WhiteTurn()
-            is WhiteTurn -> player = BlackTurn()
-        }
-    }
-
-    fun getWinner(printBoard: (Board) -> Unit, printWinner: (Stone) -> Unit) {
-        printWinner(getStoneColor())
-        printBoard(board)
-    }
-
-    private fun getStoneColor() = when (player) {
-        is BlackTurn -> BLACK
-        is BlackOmok -> BLACK
-        is WhiteTurn -> WHITE
-        is WhiteOmok -> WHITE
-        else -> throw IllegalStateException()
-    }
+        Location(Coordination.from(dot.row), Coordination.from(dot.col))
+    }.getOrNull() ?: getLocation(state, getDot)
 }
--- src/test/kotlin/model/domain/OmokGameTest.kt
+++ src/test/kotlin/model/domain/OmokGameTest.kt
@@ -1,3 +1,41 @@
 package model.domain
 
-class OmokGameTest
+import model.domain.state.BlackTurn
+import model.domain.tools.Board
+import model.domain.tools.Dot
+import model.domain.tools.Stone
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class OmokGameTest {
+    @Test
+    fun `돌을 놓을 수 없으면 상태가 바뀌지 않는다 `() {
+        // given
+        val dots = mutableListOf(Dot(1, 1), Dot(1, 1))
+        fun getTestDot(stone: Stone): Dot = dots.removeFirst()
+        val state = BlackTurn(Board.create())
+        val omokGame = OmokGame()
+
+        // when
+        omokGame.playNextTurn(state, ::getTestDot)
+        val actual = omokGame.playNextTurn(state, ::getTestDot)
+        // then
+        assertThat(actual).isEqualTo(state)
+    }
+
+    @Test
+    fun `돌을 놓을 수 있으면 상태가 바뀐다`() {
+// given
+        val dots = mutableListOf(Dot(1, 1), Dot(2, 2))
+        fun getTestDot(stone: Stone): Dot = dots.removeFirst()
+        val state = BlackTurn(Board.create())
+        val omokGame = OmokGame()
+
+        // when
+        omokGame.playNextTurn(state, ::getTestDot)
+        val actual = omokGame.playNextTurn(state, ::getTestDot)
+
+        // then
+        assertThat(actual).isNotEqualTo(state)
+    }
+}
