239bb8cd0aca4b3e8fc462c71a54da2a174d3fdc
mendel <rhthrhrl0@naver.com>
2023-03-20 11:00:25 +0900

    docs: 기능 목록 문서 수정

--- src/main/kotlin/domain/board/PlacedBoard.kt
+++ src/main/kotlin/domain/board/PlacedBoard.kt
@@ -9,9 +9,13 @@ class PlacedBoard(
     private val board: Map<Position, Color?> = POSITIONS.associateWith { null }.toMutableMap()
 ) {
     private val _placedBoard = board.filter { it.value != null }
-    val size: Int = _placedBoard.size
-    val whiteSize: Int = _placedBoard.filter { it.value == Color.WHITE }.size
-    val blackSize: Int = _placedBoard.filter { it.value == Color.BLACK }.size
+    val size: Int
+        get() = _placedBoard.size
+    val whiteSize: Int
+        get() = _placedBoard.filter { it.value == Color.WHITE }.size
+    val blackSize: Int
+        get() = _placedBoard.filter { it.value == Color.BLACK }.size
+
     private fun isPossiblePut(position: Position): Boolean {
         return board[position] == null
     }
