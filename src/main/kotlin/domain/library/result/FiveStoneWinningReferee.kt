3e42d5d276cbfbcf1f02d19c3bd227858135d4cc
boogieWoogie <kimjinuk1999@gmail.com>
2023-03-20 14:42:34 +0900

    refactor(FiveStoneWinngReferee): 함수 안에 존재하는 변수의 이름 변경

--- src/main/kotlin/domain/library/result/FiveStoneWinningReferee.kt
+++ src/main/kotlin/domain/library/result/FiveStoneWinningReferee.kt
@@ -10,19 +10,19 @@ class FiveStoneWinningReferee : WinningReferee {
         val dx = intArrayOf(1, 1, 0, -1)
         val dy = intArrayOf(0, 1, 1, 1)
 
-        for (i in 1..N) {
-            for (j in 1..N) {
-                if (placedStones.contains(Stone(Point(i, j), color)).not()) continue
+        for (x in 1..N) {
+            for (y in 1..N) {
+                if (placedStones.contains(Stone(Point(x, y), color)).not()) continue
 
-                for (k in 0 until 4) {
+                for (movingDirection in 0 until 4) {
                     var cnt = 1
-                    var nx = i + dx[k]
-                    var ny = j + dy[k]
+                    var nx = x + dx[movingDirection]
+                    var ny = y + dy[movingDirection]
 
                     while (nx in 1..N && ny in 1..15 && placedStones.contains(Stone(Point(nx, ny), color))) {
                         cnt++
-                        nx += dx[k]
-                        ny += dy[k]
+                        nx += dx[movingDirection]
+                        ny += dy[movingDirection]
 
                         if (cnt >= 5) return true
                     }
