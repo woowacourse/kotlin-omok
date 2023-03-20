30677335b839453a825c4a0b2f15a339afc99940
boogieWoogie <kimjinuk1999@gmail.com>
2023-03-20 14:43:10 +0900

    test(OmokGameState): Running 상태를 반환하는 케이스 테스트 코드 추가

--- src/test/kotlin/OmokGameStateTest.kt
+++ src/test/kotlin/OmokGameStateTest.kt
@@ -24,4 +24,23 @@ class OmokGameStateTest {
 
         assertThat(actual).isEqualTo(expected)
     }
+
+    @Test
+    fun `같은 색깔의 돌이 5개 이상 연속되지 않으면 게임은 계속 진행된다`() {
+
+        val board = Board(
+            _placedStones = listOf(
+                Stone(1, 1, Color.BLACK),
+                Stone(1, 2, Color.BLACK),
+                Stone(1, 3, Color.BLACK),
+                Stone(1, 4, Color.BLACK),
+                Stone(1, 5, Color.BLACK)
+            )
+        )
+
+        val actual = OmokGameState.valueOf(board, Color.BLACK)
+        val expected = OmokGameState.Running
+
+        assertThat(actual).isEqualTo(expected)
+    }
 }
