5d305c3c7f5b42f86dfe3aab4638eb815ad48e66
SangHyun Moon <no1msh1217@gmail.com>
2023-03-21 22:59:18 +0900

    feat: 오목판의 크기를 가변적으로 변경 및 불필요한 매개변수 감소

--- src/main/kotlin/omok/model/game/Board.kt
+++ src/main/kotlin/omok/model/game/Board.kt
@@ -4,9 +4,8 @@ import omok.model.stone.Coordinate
 import omok.model.stone.GoStone
 import omok.model.stone.GoStoneColor
 
-class Board {
-    private val _board: List<MutableList<GoStone?>> =
-        List(15) { MutableList(15) { null } }
+class Board(val sizeX: Int = DEFAULT_BOARD_X_SIZE, val sizeY: Int = DEFAULT_BOARD_Y_SIZE) {
+    private val _board: List<MutableList<GoStone?>> = List(sizeY) { MutableList(sizeX) { null } }
     val board: List<List<GoStone?>>
         get() = _board.map { it.toList() }
 
@@ -15,17 +14,18 @@ class Board {
 
     fun getNextColor(): GoStoneColor = GoStoneColor.getNextColor(lastPlacedStone?.color)
 
-    fun addStone(color: GoStoneColor, coordinate: Coordinate) {
-        lastPlacedStone = GoStone(color, coordinate)
-        _board[coordinate.x - 1][coordinate.y - 1] = lastPlacedStone
+    fun addStone(goStone: GoStone) {
+        lastPlacedStone = goStone
+        _board[goStone.coordinate.y - 1][goStone.coordinate.x - 1] = lastPlacedStone
     }
 
     fun canAdd(coordinate: Coordinate): Boolean {
-        if (_board[coordinate.x - 1][coordinate.y - 1] == null) return true
+        if (_board[coordinate.y - 1][coordinate.x - 1] == null) return true
         else throw IllegalArgumentException("해당 위치에 이미 바둑돌이 있습니다.")
     }
 
     companion object {
-        const val BOARD_LENGTH = 15
+        private const val DEFAULT_BOARD_X_SIZE = 15
+        private const val DEFAULT_BOARD_Y_SIZE = 15
     }
 }
