dfc1e27634c6f78ffef7a3f7494413054ba08ccc
s9hn <tnghqn@naver.com>
2023-03-21 17:13:02 +0900

    [REFACTOR(OmokGame)] state로 코드 이동!

--- src/main/kotlin/model/domain/OmokGame.kt
+++ src/main/kotlin/model/domain/OmokGame.kt
@@ -1,55 +1,35 @@
 package model.domain
 
 import model.domain.state.Omok
-import model.domain.state.RetryTurn
 import model.domain.state.State
-import model.domain.state.black.BlackOmok
 import model.domain.state.black.BlackTurn
-import model.domain.state.white.WhiteOmok
-import model.domain.state.white.WhiteTurn
 import model.domain.tools.Board
 import model.domain.tools.Coordination
 import model.domain.tools.Location
 import model.domain.tools.Stone
-import model.domain.tools.Stone.BLACK
-import model.domain.tools.Stone.WHITE
 
 class OmokGame(private val board: Board) {
     private var state: State = BlackTurn()
 
-    fun gameStart(getCoordination: (Stone) -> Pair<Int, Int>, updateBoard: (Board) -> Unit) {
+    fun start(
+        getCoordination: (Stone) -> Pair<Int, Int>,
+        updateBoard: (Board) -> Unit
+    ) {
         while (state !is Omok) {
             updateBoard(board)
-            play(getCoordination)
-            changeState()
+            state = play(getCoordination, state)
         }
     }
 
-    private fun play(getCoordination: (Stone) -> Pair<Int, Int>) {
-        val value = getCoordination(getStoneColor())
-
+    private fun play(getCoordination: (Stone) -> Pair<Int, Int>, state: State): State {
+        val value = getCoordination(state.stone)
         val location = Location(Coordination.from(value.first), Coordination.from(value.second))
-        state = state.place(location, board)
+
+        return state.place(location, board)
     }
 
-    private fun changeState() {
-        when (state) {
-            is RetryTurn -> state = state.retry()
-            is BlackTurn -> state = WhiteTurn()
-            is WhiteTurn -> state = BlackTurn()
-        }
-    }
-
-    fun getWinner(printBoard: (Board) -> Unit, printWinner: (Stone) -> Unit) {
-        printWinner(getStoneColor())
-        printBoard(board)
-    }
-
-    private fun getStoneColor() = when (state) {
-        is BlackTurn -> BLACK
-        is BlackOmok -> BLACK
-        is WhiteTurn -> WHITE
-        is WhiteOmok -> WHITE
-        else -> throw IllegalStateException()
+    fun getWinner(findWinner: (Stone) -> Unit, updateBoard: (Board) -> Unit) {
+        findWinner(state.stone)
+        updateBoard(board)
     }
 }
