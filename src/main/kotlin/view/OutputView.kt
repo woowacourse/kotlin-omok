ef6265ebc9fde3a83300e2fa872b59cd6ee7b615
briandr97 <briandr@naver.com>
2023-03-22 01:25:44 +0900

    refactor(OutputView): 함수 분리

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,31 +1,26 @@
 package view
 
-import domain.Board
 import domain.CoordinateState
+import domain.CoordinateState.BLACK
+import domain.CoordinateState.EMPTY
+import domain.CoordinateState.WHITE
 import domain.Position
-import domain.constant.Constant.BOARD_SIZE
+import domain.domain.Row
+import domain.domain.Stones
+import domain.view.constant.ViewConstant.BOARD_SIZE
 
 object OutputView {
-    private const val BLACK = "흑"
-    private const val WHITE = "백"
+    private const val BLACK_TEXT = "흑"
+    private const val WHITE_TEXT = "백"
 
     fun printStart() {
         println("오목 게임을 시작합니다.")
     }
 
-    fun printBoard(board: Board) {
-        board.board.forEachIndexed { y, colors ->
+    fun printBoard(stones: Stones) {
+        stones.stones.forEachIndexed { y, row ->
             print("${BOARD_SIZE - y} ".padStart(4, ' '))
-            colors.forEachIndexed { x, color ->
-                if (color == CoordinateState.WHITE) print(BoardParts.WHITE_STONE.value)
-                if (color == CoordinateState.BLACK) print(BoardParts.BLACK_STONE.value)
-                if (color == CoordinateState.EMPTY) print(BoardParts.getPart(color, x, y).value)
-                if (x != BOARD_SIZE - 1) {
-                    repeat(2) {
-                        print(BoardParts.GENERAL.value)
-                    }
-                }
-            }
+            loopRowElements(y, row)
             println()
         }
         print("    ")
@@ -33,6 +28,31 @@ object OutputView {
             print(it.name + "  ")
         }
         println()
+    }
+
+    private fun loopRowElements(y: Int, row: Row) {
+        row.row.forEachIndexed { x, color ->
+            printWhiteStone(color)
+            printBlackStone(color)
+            printEmpty(x, y, color)
+        }
+    }
+
+    private fun printWhiteStone(color: CoordinateState) {
+        if (color == WHITE) print(BoardParts.WHITE_STONE.value)
+    }
+
+    private fun printBlackStone(color: CoordinateState) {
+        if (color == BLACK) print(BoardParts.BLACK_STONE.value)
+    }
+
+    private fun printEmpty(x: Int, y: Int, color: CoordinateState) {
+        if (color == EMPTY) print(BoardParts.getPart(x, y).value)
+        if (x != BOARD_SIZE - 1) {
+            repeat(2) {
+                print(BoardParts.GENERAL.value)
+            }
+        }
     }
 
     fun printTurn(turn: CoordinateState) {
@@ -44,7 +64,7 @@ object OutputView {
             println()
             return
         }
-        println(" (마지막 돌의 위치: ${AlphabetCoordinate.convertAlphabet(position.getX())}${BOARD_SIZE - position.getY()})")
+        println(" (마지막 돌의 위치: ${AlphabetCoordinate.convertAlphabet(position.x)}${BOARD_SIZE - position.y})")
     }
 
     fun printRequestPosition() {
@@ -56,7 +76,7 @@ object OutputView {
     }
 
     private fun CoordinateState.toName(): String {
-        return if (this == CoordinateState.BLACK) BLACK else WHITE
+        return if (this == BLACK) BLACK_TEXT else WHITE_TEXT
     }
 
     fun printError() {
