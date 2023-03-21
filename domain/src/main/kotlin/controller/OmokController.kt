51779c3f94c8aec710c6bb8bce8aadd1dce3b38e
boogieWoogie <kimjinuk1999@gmail.com>
2023-03-21 23:27:44 +0900

    refactor(OmokGameState): 캡슐화를 위해 우승자를 추출하는 것을 OmokGameState로 이동

--- domain/src/main/kotlin/controller/OmokController.kt
+++ domain/src/main/kotlin/controller/OmokController.kt
@@ -14,17 +14,9 @@ class OmokController(private val omokGame: OmokGame = OmokGame()) {
             do{
                 omokGame.placeStone(OutputView::printOmokBoard, InputView::requestPoint)
             }while(omokGame.state == OmokGameState.Running)
-            getResult(omokGame.state)
+            OutputView.printWinner(omokGame.state.getResult())
         }.onFailure { ex ->
             OutputView.printExceptionMessage(ex)
         }
-    }
-
-    private fun getResult(omokGameState: OmokGameState){
-        if(omokGameState is OmokGameState.End){
-            OutputView.printWinner(omokGameState.winningColor)
-            return
-        }
-        throw IllegalStateException("[ERROR] 오목 게임의 결과를 알 수 없습니다.")
     }
 }
--- domain/src/main/kotlin/domain/OmokGameState.kt
+++ domain/src/main/kotlin/domain/OmokGameState.kt
@@ -4,6 +4,7 @@ import domain.board.Board
 import domain.library.result.FiveStoneWinningReferee
 import domain.library.result.WinningReferee
 import domain.stone.Color
+import domain.stone.Stone
 
 sealed class OmokGameState {
 
@@ -11,7 +12,16 @@ sealed class OmokGameState {
 
     data class End(val winningColor: Color) : OmokGameState()
 
+    fun getResult(): Color {
+        if (this is End) {
+            return winningColor
+        }
+        throw IllegalStateException(ERROR_RESULT)
+    }
+
     companion object {
+
+        private const val ERROR_RESULT = "[ERROR] 게임이 종료되지 않았습니다."
 
         fun valueOf(
             currentBoard: Board,
