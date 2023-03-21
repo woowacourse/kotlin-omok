70056d0e403cdee1e2a4897b5c6a005d712fc2ce
s9hn <tnghqn@naver.com>
2023-03-21 17:09:57 +0900

    [REFACTOR(Coordination, Location)] 린트 적용

--- src/main/kotlin/model/domain/tools/Coordination.kt
+++ src/main/kotlin/model/domain/tools/Coordination.kt
@@ -1,7 +1,9 @@
 package model.domain.tools
 
 @JvmInline
-value class Coordination private constructor(val value: Int) {
+value class Coordination private constructor(
+    val value: Int
+) {
     companion object {
         fun from(value: Int): Coordination = Coordination(value)
     }
--- src/main/kotlin/model/domain/tools/Location.kt
+++ src/main/kotlin/model/domain/tools/Location.kt
@@ -1,5 +1,6 @@
 package model.domain.tools
 
-import model.domain.tools.Coordination
-
-data class Location(val coordinationX: Coordination, val coordinationY: Coordination)
+data class Location(
+    val coordinationX: Coordination,
+    val coordinationY: Coordination
+)
