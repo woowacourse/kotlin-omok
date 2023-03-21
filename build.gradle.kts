7728604daff27199228abbe51435a27d7d952fc6
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-21 16:56:33 +0900

    feat: 렌주룰 라이브러리 사용

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
+    val currentVersion = "v1.0.3-alpha"
+    implementation("com.github.tmdgh1592:budool-omok-rule:$currentVersion")
+
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
     testImplementation("org.assertj", "assertj-core", "3.22.0")
     testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
