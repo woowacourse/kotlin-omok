685a252fd08231ca1a9370b8ced748506350b972
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-03-21 22:28:34 +0900

    refactor: 일부 함수화

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -1,22 +1,19 @@
 package woowacourse.omok
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
-import omok.controller.Controller
 import omok.domain.Turn
 import omok.domain.board.Board
 import omok.domain.board.Position
 import omok.domain.judgment.WinningReferee
 import omok.domain.player.Black
 import omok.domain.player.White
-import omok.view.InputView
-import omok.view.OutputView
+import omok.view.model.toPresentation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,31 +21,58 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val boardUI = findViewById<TableLayout>(R.id.board)
-
         val board = Board()
-        var turn = Turn(setOf(Black, White))
+        val turn = Turn(setOf(Black, White))
         val winningReferee = WinningReferee()
-        boardUI.children.filterIsInstance<TableRow>().forEachIndexed { index, t ->
-            t.children.filterIsInstance<ImageView>().forEachIndexed { index2, t2 ->
+
+        boardUI.children.filterIsInstance<TableRow>().forEachIndexed { row, t ->
+            t.children.filterIsInstance<ImageView>().forEachIndexed { column, t2 ->
                 t2.setOnClickListener {
-                    val selectedPosition = Position(Pair(index2, 14 - index))
-                    board.place(selectedPosition, turn.now)
-                    Log.i("positionTest", selectedPosition.toString())
-                    if (turn.now == Black)
-                        t2.setImageResource(R.drawable.black_stone)
-                    else
-                        t2.setImageResource(R.drawable.white_stone)
-
-                    if (winningReferee.hasFiveOrMoreStoneInRow(board.positions, selectedPosition))
-                        if (turn.now == Black) {
-                            Log.i("positionWin", "흑")
-                        } else
-                            Log.i("positionWin", "백")
-
-                    turn.changeTurn()
+                    val selectedPosition = Position(Pair(column, 14 - row))
+                    val check = place(board, selectedPosition, turn)
+                    if (check) {
+                        if (turn.now == Black)
+                            t2.setImageResource(R.drawable.black_stone)
+                        else
+                            t2.setImageResource(R.drawable.white_stone)
+                        if (checkWinner(selectedPosition, board, winningReferee)) {
+                            Toast.makeText(
+                                this,
+                                "승자는 ${turn.now.toPresentation()}입니다!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                        turn.changeTurn()
+                    }
                 }
             }
         }
     }
 
+    private fun place(
+        board: Board,
+        selectedPosition: Position,
+        turn: Turn
+    ): Boolean {
+        if (board.positions[selectedPosition] != null) {
+            Toast.makeText(this, "빈 칸이 아닙니다", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        runCatching {
+            board.place(selectedPosition, turn.now)
+        }.onFailure {
+            Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
+    private fun checkWinner(
+        selectedPosition: Position,
+        board: Board,
+        winningReferee: WinningReferee
+    ): Boolean {
+        return (winningReferee.hasFiveOrMoreStoneInRow(board.positions, selectedPosition))
+    }
 }
