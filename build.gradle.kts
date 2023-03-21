98d6557c6f1c3b019d0820b48352c8d17eea20ca
woowahan-pjs <jason@woowahan.com>
2023-03-21 10:20:07 +0900

    feat: add main activity and resources

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,14 @@
 plugins {
-    kotlin("jvm") version "1.8.10"
+    val agpVersion = "7.4.0"
+    id("com.android.application") version agpVersion apply false
+    id("com.android.library") version agpVersion apply false
+
+    val kotlinVersion = "1.8.10"
+    kotlin("android") version kotlinVersion apply false
+    kotlin("jvm") version kotlinVersion apply false
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 }
 
-group = "camp.nextstep.edu"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
-    testImplementation("org.assertj", "assertj-core", "3.22.0")
-    testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
-}
-
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
-    test {
-        useJUnitPlatform()
-    }
-    ktlint {
-        verbose.set(true)
-    }
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
-    kotlin("jvm")
+    kotlin("jvm") version "1.8.10"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 }
 
 group = "camp.nextstep.edu"
 version = "1.0-SNAPSHOT"
 
 dependencies {
+    val currentVersion = "v1.0.3-alpha"
+    implementation("com.github.tmdgh1592:budool-omok-rule:$currentVersion")
+
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
     testImplementation("org.assertj", "assertj-core", "3.22.0")
     testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
