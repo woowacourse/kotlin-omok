a590dca5057fc58ceb7f013694c8f868fd63de9f
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-21 22:26:39 +0900

    refactor: 렌주율 라이브러리 적용하여 작성 및 수정

--- src/main/kotlin/domain/state/BlackTurn.kt
+++ src/main/kotlin/domain/state/BlackTurn.kt
@@ -1,20 +1,24 @@
 package domain.state
 
-import domain.rule.OmokRule
 import domain.stone.Board
 import domain.stone.Stone
+import rule.type.Violation
 
-class BlackTurn(board: Board) : Running(board) {
+class BlackTurn(val board: Board) : Running() {
+
     override fun put(stone: Stone): State {
-        if (!isValidPut(stone)) return BlackTurn(board)
-        if (checkForbidden(board, stone)) return BlackTurn(board)
-        board.putStone(stone)
-        if (OmokRule.isWinCondition(board.board, stone)) return Win(stone)
+        if (blackRenjuRule.checkAnyFoulCondition(
+                board.blackStonesPosition,
+                board.whiteStonesPosition,
+                stone.point,
+            ) != Violation.NONE
+        ) {
+            return BlackTurn(board)
+        }
+        if (blackRenjuRule.checkWin(board.blackStonesPosition, board.whiteStonesPosition, stone.point)) {
+            return Win(stone)
+        }
+        runCatching { board.putStone(stone) }.onFailure { return BlackTurn(board) }
         return WhiteTurn(board)
-    }
-
-    private fun checkForbidden(board: Board, stone: Stone): Boolean {
-        return OmokRule.countOpenThrees(board.board, stone) >= OmokRule.MIN_OPEN_THREES ||
-            OmokRule.countOpenFours(board.board, stone) >= OmokRule.MIN_OPEN_FOURS
     }
 }
--- src/test/kotlin/domain/stone/BlackTurnTest.kt
+++ src/test/kotlin/domain/stone/BlackTurnTest.kt
@@ -5,13 +5,14 @@ import domain.state.End
 import domain.state.WhiteTurn
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import rule.wrapper.point.Point
 
 class BlackTurnTest {
 
     @Test
     fun `stone을 추가할 수 없는 상태라면 추가하지 않고 BlackTurn을 반환`() {
         val board: Board = Board()
-        val stone: Stone = Stone(StonePosition.from(2, 1)!!, StoneType.BLACK)
+        val stone: Stone = Stone(Point(2, 1), StoneType.BLACK)
         board.putStone(stone)
 
         val blackTurn: BlackTurn = BlackTurn(board)
@@ -24,7 +25,7 @@ class BlackTurnTest {
     @Test
     fun `stone를 추가한 후 WhiteTurn를 반환`() {
         val board: Board = Board()
-        val stone: Stone = Stone(StonePosition.from(2, 1)!!, StoneType.BLACK)
+        val stone: Stone = Stone(Point(2, 1), StoneType.BLACK)
 
         val blackTurn: BlackTurn = BlackTurn(board)
 
@@ -36,17 +37,91 @@ class BlackTurnTest {
     @Test
     fun `오목 조건 충족하면 End 상태로 Black이 Win`() {
         val board: Board = Board()
-        board.putStone(Stone(StonePosition.from(1, 1)!!, StoneType.BLACK))
-        board.putStone(Stone(StonePosition.from(2, 2)!!, StoneType.BLACK))
-        board.putStone(Stone(StonePosition.from(3, 3)!!, StoneType.BLACK))
-        board.putStone(Stone(StonePosition.from(4, 4)!!, StoneType.BLACK))
+        board.putStone(Stone(Point(1, 1), StoneType.BLACK))
+        board.putStone(Stone(Point(2, 2), StoneType.BLACK))
+        board.putStone(Stone(Point(3, 3), StoneType.BLACK))
+        board.putStone(Stone(Point(4, 4), StoneType.BLACK))
 
-        val stone: Stone = Stone(StonePosition.from(5, 5)!!, StoneType.BLACK)
+        val stone: Stone = Stone(Point(5, 5), StoneType.BLACK)
 
         val blackTurn: BlackTurn = BlackTurn(board)
 
         assertThat(
             blackTurn.put(stone) is End,
+        ).isEqualTo(true)
+    }
+
+    @Test
+    fun `블랙돌을 놓으려는 위치가 장목이면 BlackTurn을 반환`() {
+        val board: Board = Board()
+
+        board.putStone(Stone(Point(1, 1), StoneType.BLACK))
+        board.putStone(Stone(Point(2, 2), StoneType.BLACK))
+        board.putStone(Stone(Point(3, 3), StoneType.BLACK))
+        board.putStone(Stone(Point(4, 4), StoneType.BLACK))
+        board.putStone(Stone(Point(6, 6), StoneType.BLACK))
+
+        val stone: Stone = Stone(Point(5, 5), StoneType.BLACK)
+
+        val blackTurn: BlackTurn = BlackTurn(board)
+
+        assertThat(
+            blackTurn.put(stone) is BlackTurn,
+        ).isEqualTo(true)
+    }
+
+    @Test
+    fun `블랙돌을 놓으려는 위치가 3-3이면 BlackTurn을 반환`() {
+        val board: Board = Board()
+
+        board.putStone(Stone(Point(1, 1), StoneType.BLACK))
+        board.putStone(Stone(Point(2, 2), StoneType.BLACK))
+        board.putStone(Stone(Point(2, 3), StoneType.BLACK))
+        board.putStone(Stone(Point(3, 1), StoneType.BLACK))
+
+        val stone: Stone = Stone(Point(2, 1), StoneType.BLACK)
+
+        val blackTurn: BlackTurn = BlackTurn(board)
+
+        assertThat(
+            blackTurn.put(stone) is BlackTurn,
+        ).isEqualTo(true)
+    }
+
+    @Test
+    fun `블랙돌을 놓으려는 위치가 4-4이면 BlackTurn을 반환`() {
+        val board: Board = Board()
+
+        board.putStone(Stone(Point(15, 3), StoneType.BLACK))
+        board.putStone(Stone(Point(14, 3), StoneType.BLACK))
+        board.putStone(Stone(Point(12, 3), StoneType.BLACK))
+        board.putStone(Stone(Point(11, 3), StoneType.BLACK))
+        board.putStone(Stone(Point(10, 3), StoneType.BLACK))
+        board.putStone(Stone(Point(12, 4), StoneType.BLACK))
+        board.putStone(Stone(Point(12, 7), StoneType.BLACK))
+        board.putStone(Stone(Point(12, 9), StoneType.BLACK))
+        board.putStone(Stone(Point(12, 10), StoneType.BLACK))
+        board.putStone(Stone(Point(9, 10), StoneType.BLACK))
+        board.putStone(Stone(Point(8, 10), StoneType.BLACK))
+        board.putStone(Stone(Point(6, 10), StoneType.BLACK))
+        board.putStone(Stone(Point(8, 11), StoneType.BLACK))
+        board.putStone(Stone(Point(8, 8), StoneType.BLACK))
+        board.putStone(Stone(Point(7, 8), StoneType.BLACK))
+        board.putStone(Stone(Point(6, 8), StoneType.BLACK))
+        board.putStone(Stone(Point(6, 5), StoneType.BLACK))
+        board.putStone(Stone(Point(5, 5), StoneType.BLACK))
+        board.putStone(Stone(Point(5, 6), StoneType.BLACK))
+        board.putStone(Stone(Point(5, 7), StoneType.BLACK))
+        board.putStone(Stone(Point(4, 7), StoneType.BLACK))
+        board.putStone(Stone(Point(5, 4), StoneType.WHITE))
+        board.putStone(Stone(Point(9, 8), StoneType.WHITE))
+
+        val stone: Stone = Stone(Point(8, 3), StoneType.BLACK)
+
+        val blackTurn: BlackTurn = BlackTurn(board)
+
+        assertThat(
+            blackTurn.put(stone) is BlackTurn,
         ).isEqualTo(true)
     }
 }
