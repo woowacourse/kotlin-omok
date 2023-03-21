c9021d55c5ce12c3147960d4d49c578cc923006d
SangHyun Moon <no1msh1217@gmail.com>
2023-03-21 21:43:02 +0900

    feat: 불필요한 상태패턴 제거 및 대체할 Enum 클래스 추가

---
+++ src/main/kotlin/omok/model/PlacementState.kt
@@ -0,0 +1,9 @@
+package omok.model
+
+enum class PlacementState {
+    WIN,
+    STAY,
+    OPEN_DOUBLE_FOUR,
+    OPEN_DOUBLE_THREE,
+    LONG_LINE,
+}
--- src/main/kotlin/omok/model/state/State.kt
+++
@@ -1,5 +0,0 @@
-package omok.model.state
-
-import omok.model.game.Board
-
-abstract class State(val board: Board)
