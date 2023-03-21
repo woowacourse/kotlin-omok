d1697be960c5a606e8cf3fbc624b117f202b6d14
briandr97 <briandr@naver.com>
2023-03-22 01:26:28 +0900

    refactor(OmokView): OmokView에서 Observer 구현

--- src/main/kotlin/view/OmokView.kt
+++ src/main/kotlin/view/OmokView.kt
@@ -1,28 +1,59 @@
-package view
+package domain.view
 
-import domain.Board
 import domain.CoordinateState
+import domain.CoordinateState.BLACK
 import domain.Position
+import domain.domain.state.BlackTurn
+import domain.domain.state.State
+import domain.domain.state.WhiteTurn
+import domain.domain.state.WhiteWin
+import view.InputView
+import view.OutputView
 
-object OmokView {
-    fun printStart() {
+class OmokView : Observer {
+    private var preState: CoordinateState = BLACK
+
+    fun printStart(state: State) {
         OutputView.printStart()
+        drawBoard(state)
     }
 
-    fun putPhase(board: Board, turn: CoordinateState): Position {
-        OutputView.printBoard(board)
-        OutputView.printTurn(turn)
-        OutputView.printLastPosition(board.lastPosition)
-        OutputView.printRequestPosition()
+    fun getInputPosition(): Position {
         return InputView.inputPosition()
     }
 
-    fun printResult(board: Board, turn: CoordinateState) {
-        OutputView.printBoard(board)
-        OutputView.printWinner(turn)
+    private fun retryInput() {
+        OutputView.printError()
+        OutputView.printRequestPosition()
     }
 
-    fun printError() {
-        OutputView.printError()
+    private fun drawBoard(state: State) {
+        preState = state.getTurn()
+        with(OutputView) {
+            printBoard(state.stones)
+            printTurn(state.getTurn())
+            printLastPosition(state.stones.lastPosition)
+            printRequestPosition()
+        }
+    }
+
+    private fun printResult(state: State) {
+        OutputView.printBoard(state.stones)
+        OutputView.printWinner(state.getTurn())
+    }
+
+    override fun update(state: State) {
+        when (state) {
+            is BlackTurn -> updateView(state)
+            is WhiteTurn -> updateView(state)
+            is WhiteWin -> printResult(state)
+        }
+    }
+
+    private fun updateView(state: State) {
+        when (preState == state.getTurn()) {
+            true -> retryInput()
+            false -> drawBoard(state)
+        }
     }
 }
