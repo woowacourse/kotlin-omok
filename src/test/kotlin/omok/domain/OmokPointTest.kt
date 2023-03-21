55443ddf60229606250ff4dd94f5d94e0d4821cb
inseonyun <yuninseon@naver.com>
2023-03-22 01:02:20 +0900

    refactor(OmokPointTest): 다중 테스트 코드 assertAll로 그룹화

--- src/test/kotlin/omok/domain/OmokPointTest.kt
+++ src/test/kotlin/omok/domain/OmokPointTest.kt
@@ -2,14 +2,17 @@ package omok.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.assertThrows
 
 class OmokPointTest {
     @Test
     fun `좌표를 만들 수 있다`() {
         val omokPoint = OmokPoint(1, 1)
-        assertThat(omokPoint.x.value).isEqualTo(1)
-        assertThat(omokPoint.y.value).isEqualTo(1)
+        assertAll(
+            { assertThat(omokPoint.x.value).isEqualTo(1) },
+            { assertThat(omokPoint.y.value).isEqualTo(1) }
+        )
     }
 
     @Test
@@ -17,6 +20,7 @@ class OmokPointTest {
         val exception = assertThrows<IllegalArgumentException> { OmokPoint(16, 1) }
         assertThat(exception.message).isEqualTo("X 범위를 벗어납니다.")
     }
+
     @Test
     fun `Y좌표가 1-15의 범위를 벗어나면 예외가 발생한다`() {
         val exception = assertThrows<IllegalArgumentException> { OmokPoint(1, 16) }
