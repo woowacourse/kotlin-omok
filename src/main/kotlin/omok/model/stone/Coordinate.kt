a62e7a4920ad947edf360fa3e77bc896668397dd
SangHyun Moon <no1msh1217@gmail.com>
2023-03-21 23:00:26 +0900

    refactor: 좌표 클래스를 data 클래스로 변경 및 팩토리 메서드 적용

--- src/main/kotlin/omok/model/stone/Coordinate.kt
+++ src/main/kotlin/omok/model/stone/Coordinate.kt
@@ -1,46 +1,21 @@
 package omok.model.stone
 
-import omok.model.game.Board
-
-class Coordinate(val mark: String) {
-    val x: Int
-    val y: Int
-
-    init {
-        val first = mark.first().uppercaseChar() - 'A' + 1
-        val second = mark.substring(1)
-        require(first.isWithInRange() && second.isWithinRange()) { "위치를 알파벳숫자 형태로 입력해주세요. (잘못된 값: $mark)" }
-
-        x = Board.BOARD_LENGTH - second.toInt() + 1
-        y = first
-    }
-
-    private fun Int.isWithInRange(): Boolean = this in BOARD_SIZE_RANGE
-
-    private fun String.isWithinRange(): Boolean {
-        val number = this.toIntOrNull()
-        return number?.isWithInRange() ?: false
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Coordinate
-
-        if (x != other.x) return false
-        if (y != other.y) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = x
-        result = 31 * result + y
-        return result
-    }
+data class Coordinate(val x: Int, val y: Int) {
 
     companion object {
-        val BOARD_SIZE_RANGE: IntRange = 1..Board.BOARD_LENGTH
+        private const val MINIMUM = 1
+
+        fun createWithMark(mark: String): Coordinate {
+            val first = mark.first().uppercaseChar() - 'A' + 1
+            val second = mark.substring(1)
+            require(first >= MINIMUM && second.isWithinRange()) { "위치를 알파벳숫자 형태로 입력해주세요. (잘못된 값: $mark)" }
+
+            return Coordinate(first, second.toInt())
+        }
+
+        private fun String.isWithinRange(): Boolean {
+            val number = this.toIntOrNull() ?: return false
+            return number >= MINIMUM
+        }
     }
 }
--- src/test/kotlin/omok/model/stone/CoordinateTest.kt
+++ src/test/kotlin/omok/model/stone/CoordinateTest.kt
@@ -9,20 +9,20 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class CoordinateTest {
     @Test
-    fun `알파벳+숫자 형태의 입력을 뒤집어서 위치를 생성한다`() {
-        val coordinate = Coordinate("H11")
+    fun `알파벳+숫자 형태의 입력으로 알파벳은 순서에 맞게끔 숫자로 변환하여 위치를 생성한다`() {
+        val coordinate = Coordinate.createWithMark("H11")
 
         assertAll(
-            { assertThat(coordinate.x).isEqualTo(5) },
-            { assertThat(coordinate.y).isEqualTo(8) }
+            { assertThat(coordinate.x).isEqualTo(8) },
+            { assertThat(coordinate.y).isEqualTo(11) }
         )
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["33", "aa", "4a", "H-1", "H17", "z8", "d7d"])
+    @ValueSource(strings = ["33", "aa", "4a", "H-1", "d7d"])
     fun `알파벳+숫자 형태가 아닌 입력은 오류가 발생한다`(input: String) {
         assertThatIllegalArgumentException()
-            .isThrownBy { Coordinate(input) }
+            .isThrownBy { Coordinate.createWithMark(input) }
             .withMessageContaining("위치를 알파벳숫자 형태로 입력해주세요. (잘못된 값: $input")
     }
 }
