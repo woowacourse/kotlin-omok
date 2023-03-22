3bf95f6392171434776a629b9612f05750bc870b
hyemdooly <hyemdooly@gmail.com>
2023-03-23 00:36:00 +0900

    feat(MainActivity): MainActivity 구현

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -6,18 +6,54 @@ import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import domain.game.Omok
+import domain.game.Omok.Companion.OMOK_BOARD_SIZE
+import domain.player.BlackPlayer
+import domain.player.Players
+import domain.player.WhitePlayer
+import domain.point.Point
+import domain.rule.BlackRenjuRule
+import domain.rule.WhiteRenjuRule
+import domain.stone.StoneColor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val turnEventListener = TurnEventListener(applicationContext)
+        val gameEventListener = GameEventListener(applicationContext, findViewById(R.id.description))
+        val omok = Omok(turnEventListener, gameEventListener)
+        var players = Players(BlackPlayer(rule = BlackRenjuRule()), WhitePlayer(rule = WhiteRenjuRule()))
+
+        gameEventListener.onStartGame()
+        gameEventListener.onStartTurn(players.curPlayerColor, players.getLastPoint())
         val board = findViewById<TableLayout>(R.id.board)
         board
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { index, view ->
+                view.setOnClickListener {
+                    if (players.isPlaying) {
+                        turnEventListener.point = calculateIndexToPoint(index)
+                        val endTurnPlayers = omok.takeTurn(players)
+                        if (endTurnPlayers.isPut(players)) setStone(view, players)
+                        players = endTurnPlayers
+                        gameEventListener.onEndTurn(players)
+                        omok.endGame(players)
+                    }
+                }
+            }
     }
+
+    private fun setStone(view: ImageView, players: Players) {
+        when (players.curPlayerColor) {
+            StoneColor.BLACK -> view.setImageResource(R.drawable.black_stone)
+            StoneColor.WHITE -> view.setImageResource(R.drawable.white_stone)
+        }
+    }
+
+    private fun calculateIndexToPoint(index: Int): Point = Point(index / OMOK_BOARD_SIZE + 1, index % OMOK_BOARD_SIZE + 1)
 }
