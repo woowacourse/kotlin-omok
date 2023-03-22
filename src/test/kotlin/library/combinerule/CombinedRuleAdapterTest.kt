2a5e8f174be2c7698e961ac7d9ad4c4997e2def2
changhwan <chang5hustle@gmail.com>
2023-03-22 20:42:26 +0900

    test(CombineRuleAdapter): CombineRuleAdapter 테스트 작성

---
+++ src/test/kotlin/library/combinerule/CombinedRuleAdapterTest.kt
@@ -0,0 +1,52 @@
+package library.combinerule
+
+import domain.CoordinateState
+import domain.Position
+import domain.domain.BoardState
+import domain.library.ark.CombinedRuleAdapter
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CombinedRuleAdapterTest {
+
+    @Test
+    fun `흑의 승리조건일시 True를 반환한다`() {
+        val board = List(15) { MutableList(15) { CoordinateState.EMPTY } }
+        board[4][3] = CoordinateState.BLACK
+        board[4][4] = CoordinateState.BLACK
+        board[4][5] = CoordinateState.BLACK
+        board[4][7] = CoordinateState.BLACK
+        val targetCoordinate = Position(4, 6)
+
+        assertThat(CombinedRuleAdapter().isBlackWin(targetCoordinate, BoardState(board))).isTrue
+    }
+
+    @Test
+    fun `백의 승리조건일시 True를 반환한다`() {
+        val board = List(15) { MutableList(15) { CoordinateState.EMPTY } }
+        board[4][3] = CoordinateState.WHITE
+        board[4][4] = CoordinateState.WHITE
+        board[4][5] = CoordinateState.WHITE
+        board[4][7] = CoordinateState.WHITE
+        val targetCoordinate = Position(4, 6)
+
+        assertThat(CombinedRuleAdapter().isWhiteWin(targetCoordinate, BoardState(board))).isTrue
+    }
+
+    @Test
+    fun `금수가 아니고 놓을곳이 비어있다면 True를 반환한다`() {
+        val board = List(15) { MutableList(15) { CoordinateState.EMPTY } }
+        board[4][3] = CoordinateState.WHITE
+        board[4][4] = CoordinateState.WHITE
+
+        val targetCoordinate = Position(4, 6)
+
+        assertThat(
+            CombinedRuleAdapter().checkAddablePosition(
+                BoardState(board),
+                CoordinateState.WHITE,
+                targetCoordinate,
+            ),
+        ).isTrue
+    }
+}
