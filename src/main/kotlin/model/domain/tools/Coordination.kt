8c476200beb8d3cf51b88be8904c3e8907021957
s9hn <tnghqn@naver.com>
2023-03-22 12:03:12 +0900

    [REFACTOR(Coordination)] 캐싱해주기

--- src/main/kotlin/model/domain/tools/Coordination.kt
+++ src/main/kotlin/model/domain/tools/Coordination.kt
@@ -2,9 +2,14 @@ package model.domain.tools
 
 @JvmInline
 value class Coordination private constructor(
-    val value: Int
+    val value: Int,
 ) {
     companion object {
-        fun from(value: Int): Coordination = Coordination(value)
+        private const val MINIMUM_NUMBER = 0
+        private const val MAXIMUM_NUMBER = 14
+        private val NUMBERS: Map<Int, Coordination> =
+            (MINIMUM_NUMBER..MAXIMUM_NUMBER).associateWith(::Coordination)
+
+        fun from(value: Int): Coordination = requireNotNull(NUMBERS[value])
     }
 }
