72e4965b5be4cd40764ad130797f65ad39279330
changhwan <chang5hustle@gmail.com>
2023-03-22 16:10:32 +0900

    refactor(Controller): 새로운 알고리즘으로 수정

--- src/main/kotlin/Controller.kt
+++ src/main/kotlin/Controller.kt
@@ -1,11 +1,11 @@
 package domain
 
-import domain.library.cldhfleks2.SubdivideRuleAdapter
+import domain.library.ark.CombinedRuleAdapter
 import view.OmokView
 
 class Controller {
     fun run() {
-        val omokGame = OmokGame(gameRule = SubdivideRuleAdapter())
+        val omokGame = OmokGame(gameRule = CombinedRuleAdapter())
         OmokView.printStart()
         omokGame.progressTurn(OmokView::putPhase, OmokView::printError)
         OmokView.printResult(omokGame.board, omokGame.turn)
