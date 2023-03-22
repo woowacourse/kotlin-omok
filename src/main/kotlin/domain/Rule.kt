d5400eed424c2d23b690156f56f62e65a9553506
ki960213 <ki960213@github.com>
2023-03-22 18:16:13 +0900

    refactor(Direction): Direction 클래스 위치 변경

--- src/main/kotlin/domain/Rule.kt
+++ src/main/kotlin/domain/Rule.kt
@@ -158,17 +158,6 @@ object Rule {
     private fun Stone.isPlacedOnBlank(stones: Set<Stone>): Boolean = this !in stones
 }
 
-private enum class Direction(val dx: Int, val dy: Int) {
-    LEFT(-1, 0),
-    RIGHT(1, 0),
-    UP(0, 1),
-    DOWN(0, -1),
-    UP_LEFT(-1, 1),
-    UP_RIGHT(1, 1),
-    DOWN_LEFT(-1, -1),
-    DOWN_RIGHT(1, -1),
-}
-
 private enum class Inclination(val dx: Int, val dy: Int, val directions: List<Direction>) {
     HORIZONTAL(1, 0, listOf(Direction.LEFT, Direction.RIGHT)),
     VERTICAL(0, 1, listOf(Direction.UP, Direction.DOWN)),
