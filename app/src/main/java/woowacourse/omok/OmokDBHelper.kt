f9ac00479c4b22bc34d774a8704b3cac353f5d3b
boogieWoogie <kimjinuk1999@gmail.com>
2023-03-22 15:46:48 +0900

    feat(OmokDBHelper): 오목 데이터를 저장할 DB 생성

---
+++ app/src/main/java/woowacourse/omok/OmokDBHelper.kt
@@ -0,0 +1,28 @@
+package woowacourse.omok
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class OmokDBHelper(
+    context: Context?
+) : SQLiteOpenHelper(
+    context,
+    "woogi.db",
+    null,
+    1
+) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(
+            "CREATE TABLE ${OmokSpecification.TABLE_NAME}(" +
+                "placedIndex int," +
+                "color varchar(10)" +
+                ");"
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS ${OmokSpecification.TABLE_NAME}")
+        onCreate(db)
+    }
+}
---
+++ app/src/main/java/woowacourse/omok/OmokSpecification.kt
@@ -0,0 +1,8 @@
+package woowacourse.omok
+
+object OmokSpecification {
+
+    const val TABLE_NAME = "omok"
+    const val COLUMN_PLACED_INDEX = "placedIndex"
+    const val COLOR_STONE_COLOR = "color"
+}
