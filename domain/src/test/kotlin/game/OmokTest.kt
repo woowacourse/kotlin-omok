bbc9fdd7035839190a02e9d67f06c3c27fd70fad
hyemdooly <hyemdooly@gmail.com>
2023-03-23 00:55:04 +0900

    refactor(OmokTest): OmokTest 테스트 재구현

--- domain/src/test/kotlin/game/OmokTest.kt
+++ domain/src/test/kotlin/game/OmokTest.kt
@@ -1,11 +1,14 @@
 package game
 
 import domain.game.Omok
+import domain.player.BlackPlayer
 import domain.player.Players
+import domain.player.WhitePlayer
 import domain.point.Point
 import domain.rule.BlackRenjuRule
 import domain.rule.OmokRule
 import domain.rule.WhiteRenjuRule
+import domain.state.FoulState
 import domain.stone.StoneColor
 import listener.OmokGameEventListener
 import listener.OmokTurnEventListener
@@ -16,6 +19,8 @@ import org.junit.jupiter.api.Test
 class OmokTest {
     private lateinit var blackRule: OmokRule
     private lateinit var whiteRule: OmokRule
+    private lateinit var gameListener: OmokGameEventListener
+    private lateinit var turnListener: OmokTurnEventListener
 
     private val blackPositions: MutableList<Point> by lazy {
         mutableListOf(
@@ -39,13 +44,9 @@ class OmokTest {
     fun setUp() {
         blackRule = BlackRenjuRule()
         whiteRule = WhiteRenjuRule()
-    }
 
-    @Test
-    fun `한 플레이어라도 승리할 때까지 차례를 번갈아가면서 돌을 놓는다`() {
-        var round = 0
-        var curColor = StoneColor.WHITE
-        val gameListener = object : OmokGameEventListener {
+        gameListener = object : OmokGameEventListener {
+            var curColor = StoneColor.WHITE
             override fun onStartTurn(stoneColor: StoneColor, point: Point?) {
                 curColor = curColor.next()
                 assertThat(stoneColor).isEqualTo(curColor)
@@ -59,7 +60,8 @@ class OmokTest {
                 assertThat(winnerStoneColor).isEqualTo(StoneColor.BLACK)
             }
         }
-        val turnListener = object : OmokTurnEventListener {
+        turnListener = object : OmokTurnEventListener {
+            var round = 0
             override fun onTakeTurn(stoneColor: StoneColor): Point {
                 return if (round % 2 == 0) {
                     round++
@@ -72,7 +74,13 @@ class OmokTest {
 
             override fun onNotPlaceable() {}
         }
-        val omok = Omok(turnListener, gameListener, blackRule, whiteRule)
-        omok.run()
+    }
+
+    @Test
+    fun `차례마다 플레이어는 돌을 놓을 수 있고 바뀐 플레이어들 객체를 반환한다`() {
+        val omok = Omok(turnListener, gameListener)
+        val players = Players(BlackPlayer(rule = blackRule), WhitePlayer(rule = whiteRule))
+        val actual = omok.takeTurn(players)
+        assertThat(actual).usingRecursiveComparison().isNotEqualTo(players)
     }
 }
