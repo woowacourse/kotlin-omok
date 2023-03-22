ca46bbff219af4dcbed0b0be30f3791e65c19e60
briandr97 <briandr@naver.com>
2023-03-23 03:37:41 +0900

    refactor(Board): 옵저버 여러개 등록할 수 있도록 변경

--- domain/src/main/kotlin/domain/Board.kt
+++ domain/src/main/kotlin/domain/Board.kt
@@ -6,20 +6,20 @@ import domain.domain.state.State
 import domain.view.Observer
 
 class Board : Observable {
-    private var observer: Observer? = null
+    private var observers = mutableListOf<Observer>()
     var state: State = BlackTurn(Stones())
         private set
 
     override fun registerObserver(observer: Observer) {
-        this.observer = observer
+        observers.add(observer)
     }
 
     override fun removeObserver(observer: Observer) {
-        this.observer = null
+        observers.remove(observer)
     }
 
     override fun notifyObserver() {
-        observer?.update(state)
+        observers.forEach { it.update(state) }
     }
 
     fun next(position: Position) {
