c4c2577a632b5c2b32b579852f98ed71e5a7e027
briandr97 <briandr@naver.com>
2023-03-23 00:39:59 +0900

    refactor(Stones): 하나의 파일에는 하나의 클래스가 오도록 리뷰 반영

--- domain/src/main/kotlin/domain/Stones.kt
+++ domain/src/main/kotlin/domain/Stones.kt
@@ -23,13 +23,3 @@ class Stones {
         const val NOT_EMPTY = "color가 아닙니다. EMPTY가 들어왔습니다."
     }
 }
-
-class Row {
-    private val _row = MutableList(BOARD_SIZE) { EMPTY }
-    val row: List<CoordinateState> get() = _row.toList()
-
-    operator fun get(x: Int) = row[x]
-    operator fun set(x: Int, value: CoordinateState) {
-        _row[x] = value
-    }
-}
