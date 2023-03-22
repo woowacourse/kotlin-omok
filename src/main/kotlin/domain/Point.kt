858708cf84cd1e9f5d92b79b08711a9cdca752bd
ki960213 <ki960213@github.com>
2023-03-22 18:16:49 +0900

    feat(Point): 해당 방향으로 이동한 좌표를 얻을 수 있는 메서드 구현

--- src/main/kotlin/domain/Point.kt
+++ src/main/kotlin/domain/Point.kt
@@ -5,4 +5,8 @@ data class Point(private val xCoordinate: XCoordinate, private val yCoordinate: 
     val y: Int = yCoordinate.y
 
     constructor(x: Char, y: Int) : this(XCoordinate.of(x), YCoordinate.of(y))
+
+    constructor(x: Int, y: Int) : this(XCoordinate.of(x), YCoordinate.of(y))
+
+    infix fun goTo(direction: Direction): Point = Point(x + direction.dx, y + direction.dy)
 }
