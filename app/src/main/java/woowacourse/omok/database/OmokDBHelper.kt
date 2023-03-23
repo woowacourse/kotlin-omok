bcdac68c77220809aa56f72d6c0e8d79c362f326
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-23 15:57:28 +0900

    feat: 오목 데이터베이스 구현

---
+++ app/src/main/java/woowacourse/omok/database/OmokDBHelper.kt
@@ -0,0 +1,38 @@
+package woowacourse.omok.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import woowacourse.omok.database.OmokConstact.TABLE_COLUMN_ID
+import woowacourse.omok.database.OmokConstact.TABLE_COLUMN_POSITION_X
+import woowacourse.omok.database.OmokConstact.TABLE_COLUMN_POSITION_Y
+import woowacourse.omok.database.OmokConstact.TABLE_COLUMN_STONE_COLOR
+import woowacourse.omok.database.OmokConstact.TABLE_NAME
+
+class OmokDBHelper(
+    context: Context?,
+) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "lope_db"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        var sql: String = "CREATE TABLE $TABLE_NAME (" +
+            "$TABLE_COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "$TABLE_COLUMN_STONE_COLOR INTEGER NOT NULL," +
+            "$TABLE_COLUMN_POSITION_X INTEGER NOT NULL," +
+            "$TABLE_COLUMN_POSITION_Y INTEGER NOT NULL" +
+            ")"
+
+        db!!.execSQL(sql)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        val sql: String = "DROP TABLE IF EXISTS $TABLE_NAME"
+
+        db!!.execSQL(sql)
+        onCreate(db)
+    }
+}
