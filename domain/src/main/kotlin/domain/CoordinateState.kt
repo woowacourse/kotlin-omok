5d0f068ac0ded12ebf0feabb2f6864b00085af34
briandr97 <briandr@naver.com>
2023-03-24 01:38:04 +0900

    feat(CoordinateState): ordinal로 CoordinateState를 찾는 함수 구현

--- domain/src/main/kotlin/domain/CoordinateState.kt
+++ domain/src/main/kotlin/domain/CoordinateState.kt
@@ -4,4 +4,12 @@ enum class CoordinateState {
     BLACK,
     WHITE,
     EMPTY,
+    ;
+
+    companion object {
+        fun findByOrdinal(num: Int): CoordinateState {
+            return values().find { it.ordinal == num }
+                ?: throw IllegalArgumentException("잘못된 인자입니다.")
+        }
+    }
 }
