c6ffe7154a1ca5d6216d51f24a2e321415e979b0
EmilyCh0 <emily9803@ewhain.net>
2023-03-23 11:34:46 +0900

    test(OmokGameTest): 승리, 금수 테스트

--- domain/src/test/kotlin/domain/OmokGameTest.kt
+++ domain/src/test/kotlin/domain/OmokGameTest.kt
@@ -1,30 +1,51 @@
 package domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class OmokGameTest {
+
     @Test
-    fun `흑돌이 오목을 완성하면 게임이 종료된다`() {
-        val omokGame = OmokGame(omokGameListener = listener())
-        omokGame.runGame()
+    fun `흑돌이 오목을 완성하면 흑이 승리한다`() {
+
+        val board =
+            MutableList(OmokBoard.BOARD_SIZE) { MutableList(OmokBoard.BOARD_SIZE) { State.EMPTY } }
+
+        board[0][0] = State.BLACK
+        board[0][1] = State.BLACK
+        board[0][2] = State.BLACK
+        board[0][3] = State.BLACK
+        board[0][4] = State.BLACK
+
+        val omokGame = OmokGame(
+            omokBoard = OmokBoard(board),
+            omokGameListener = listener()
+        )
+
+        val actual = omokGame.isVictory(State.BLACK)
+        assertThat(actual).isEqualTo(true)
+    }
+
+    @Test
+    internal fun `금수인 경우 착수할 수 없다`() {
+        val board =
+            MutableList(OmokBoard.BOARD_SIZE) { MutableList(OmokBoard.BOARD_SIZE) { State.EMPTY } }
+
+        board[5][5] = State.BLACK
+        board[4][5] = State.BLACK
+        board[3][4] = State.BLACK
+        board[3][6] = State.BLACK
+
+        val omokGame = OmokGame(
+            omokBoard = OmokBoard(board),
+            omokGameListener = listener()
+        )
+
+        val actual = omokGame.successTurn(Stone(3, 5), State.BLACK)
+        assertThat(actual).isEqualTo(false)
     }
 
     private fun listener() = object : OmokGameListener {
-        val black = (1..5).map { Stone(it, 0) }.toMutableList()
-        val white = (1..5).map { Stone(it, 3) }.toMutableList()
-        val turn = mutableListOf<Stone>()
-        var index = 0
-
-        init {
-            repeat(5) {
-                turn.add(black.removeFirst())
-                turn.add(white.removeFirst())
-            }
-        }
-
-        override fun onStoneRequest(): Stone {
-            return turn[index++]
-        }
 
         override fun onMove(omokBoard: OmokBoard, state: State, stone: Stone) {}
 
