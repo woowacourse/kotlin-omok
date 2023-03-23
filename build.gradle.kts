f8c549206c27971f25533c2706b25c54a4ea9b62
tmdgh1592 <tmdgh1592@naver.com>
2023-03-23 14:42:19 +0900

    chore: 불필요한 의존성 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.android.library") version agpVersion apply false
 
     val kotlinVersion = "1.8.10"
-    kotlin("android") version kotlinVersion apply false
+    kotlin("android") version "1.7.10" apply false
     kotlin("jvm") version kotlinVersion apply false
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 }
