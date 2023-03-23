3a4955708369f5580cf679bdf6979757bbebdb22
boogieWoogie <kimjinuk1999@gmail.com>
2023-03-23 14:48:26 +0900

    style(OmokContract): ktlint 포맷팅

--- app/src/main/java/woowacourse/omok/OmokContract.kt
+++ app/src/main/java/woowacourse/omok/OmokContract.kt
@@ -2,7 +2,7 @@ package woowacourse.omok
 
 import android.provider.BaseColumns
 
-object OmokContract: BaseColumns {
+object OmokContract : BaseColumns {
     const val TABLE_NAME = "omok"
     const val COLUMN_PLACED_INDEX = "placedIndex"
     const val COLUMN_STONE_COLOR = "color"
