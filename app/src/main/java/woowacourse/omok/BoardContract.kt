1557266ed55e1f36f67b3c29d7488dc3abe36d47
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-03-23 13:54:25 +0900

    feat: SQLite 테이블 생성

--- app/src/main/java/woowacourse/omok/BoardContract.kt
+++ app/src/main/java/woowacourse/omok/BoardContract.kt
@@ -3,10 +3,8 @@ package woowacourse.omok
 import android.provider.BaseColumns
 
 object BoardContract : BaseColumns {
-    const val DATABASE_NAME = "Omok.db"
+    const val DATABASE_NAME = "Omok_db"
     const val TABLE_NAME = "board"
-    const val TABLE_COLUMN_ID = "id"
-    const val TABLE_COLUMN_COLUMN = "column"
-    const val TABLE_COLUMN_ROW = "row"
+    const val TABLE_COLUMN_POSITION_INDEX = "positionIndex"
     const val TABLE_COLUMN_STONE = "stone"
 }
--- app/src/main/java/woowacourse/omok/BoardDBHelper.kt
+++ app/src/main/java/woowacourse/omok/BoardDBHelper.kt
@@ -4,10 +4,20 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class BoardDBHelper(context: Context?) : SQLiteOpenHelper(context, BoardContract.DATABASE_NAME, null, 1) {
+class BoardDBHelper(context: Context?) :
+    SQLiteOpenHelper(context, BoardContract.DATABASE_NAME, null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(
+            "CREATE TABLE ${BoardContract.TABLE_NAME}(" +
+                " _id int primary key," +
+                " ${BoardContract.TABLE_COLUMN_POSITION_INDEX} int, " +
+                " ${BoardContract.TABLE_COLUMN_STONE} int" +
+                ");"
+        )
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS ${BoardContract.TABLE_NAME}")
+        onCreate(db)
     }
 }
