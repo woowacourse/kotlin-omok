0110b6b89d2c09cf1095e3ea28bb95b73d4f64b4
otter <a33397397@gmail.com>
2023-03-24 10:39:46 +0900

    feat: 보드 클릭시 돌을 놓는 기능 구현
    - 도메인 연결
    - 보드 좌표 기록
    - 오목 돌 변경

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -4,20 +4,59 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import domain.state.State
+import domain.state.end.End
+import domain.state.running.BlackTurn
+import domain.state.running.Running
+import domain.state.running.WhiteTurn
+import domain.stone.Board
+import domain.stone.StonePosition
+import view.OutputView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val board = findViewById<TableLayout>(R.id.board)
+        val boardMap: Board = Board()
+        var state: State = BlackTurn()
+
+        val outputView: OutputView = OutputView()
+
+        outputView.printOmokStart()
         board
             .children
             .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { row, it ->
+                it.children.filterIsInstance<ImageView>()
+                    .forEachIndexed { col, view ->
+                        view.setOnClickListener {
+                            val stonePosition: StonePosition = StonePosition(col + 1, row + 1)
+                            if (!state.isEnd() && !(state as Running).isPlaced(boardMap, stonePosition)) {
+                                view.setImageResource(getStoneImage(state))
+                                state = state.next(boardMap, stonePosition)
+
+                                outputView.printBoard(boardMap.stones)
+                                outputView.printTurn(state, boardMap.stones)
+                            }
+                            if (state.isEnd()) {
+                                Toast.makeText(this, "${OutputView().getWinnerText(state as End)}돌이 승리하였습니다.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+            }
+    }
+
+    private fun getStoneImage(state: State): Int {
+        return when (state) {
+            is BlackTurn -> R.drawable.stone_black_pokemon_ball_luxury
+            is WhiteTurn -> R.drawable.stone_white_pokemon_ball_premier
+            else -> 0
+        }
     }
 }
