9fdd79f70784b80a4470ee11ef0cc3b378514edb
vrexpert <vrexpert2302@gmail.com>
2023-03-24 17:11:10 +0900

    feat: 메인 액티비티 구현

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -4,20 +4,116 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import model.domain.OmokGame
+import model.domain.OmokGame.Companion.BOARD_SIZE
+import model.domain.state.BlackTurn
+import model.domain.state.End
+import model.domain.state.State
+import model.domain.state.WhiteTurn
+import model.domain.tools.Board
+import model.domain.tools.Location
+import model.domain.tools.Stone
+import woowacourse.omok.OmokViewUtil.getDot
+import woowacourse.omok.OmokViewUtil.getStoneMessage
+import woowacourse.omok.model.data.OmokDbHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var state: State
+    private lateinit var omokDbAdapter: OmokDbAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        runOmok()
+    }
 
-        val board = findViewById<TableLayout>(R.id.board)
-        board
-            .children
-            .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+    private fun runOmok() {
+        val omokGame = OmokGame()
+        val board = Board.from(BOARD_SIZE)
+
+        omokDbAdapter = OmokDbAdapter(OmokDbHelper(this))
+        setOmokTurn(board)
+        omokDbAdapter.load(state.board)
+        loadBoardView(omokGame)
+    }
+
+    private fun setOmokTurn(board: Board) {
+        if (omokDbAdapter.nextStone() == Stone.BLACK) {
+            state = BlackTurn(board)
+            return
+        }
+        state = WhiteTurn(board)
+    }
+
+    private fun loadBoardView(omokGame: OmokGame): TableLayout? {
+
+        return findViewById<TableLayout>(R.id.board).apply {
+            children
+                .filterIsInstance<TableRow>()
+                .flatMap { it.children }
+                .filterIsInstance<ImageView>()
+                .forEachIndexed { index, view ->
+                    setClickListener(omokGame, index, view)
+                    setStoneWithBoard(index, view)
+                }
+        }
+    }
+
+    private fun setClickListener(omokGame: OmokGame, index: Int, view: ImageView) {
+        view.setOnClickListener {
+            if (state is End) {
+                viewWinAndDeleteStone()
+                return@setOnClickListener
+            }
+            clickEvent(omokGame, index, view)
+        }
+    }
+
+    private fun setStoneWithBoard(index: Int, view: ImageView) {
+        val dot = getDot(index)
+        val stoneColor = state.board.getStone(Location(dot.row, dot.col))
+        setStoneImage(stoneColor, view)
+    }
+
+    private fun clickEvent(omokGame: OmokGame, index: Int, view: ImageView) {
+        val nextState = omokGame.playNextTurn(state, getDot(index))
+
+        putStone(nextState, index, view)
+        state = nextState
+    }
+
+    private fun putStone(nextState: State, index: Int, view: ImageView) {
+        if (state == nextState) {
+            Toast.makeText(this, FORBIDDEN_MESSAGE, Toast.LENGTH_LONG)
+                .show()
+            return
+        }
+        setStoneImage(state.stone, view)
+        omokDbAdapter.saveStone(index, state.stone)
+    }
+
+    private fun viewWinAndDeleteStone() {
+        Toast.makeText(
+            this,
+            WINNER_MESSAGE.format(getStoneMessage(state.stone)),
+            Toast.LENGTH_SHORT
+        ).show()
+        omokDbAdapter.deleteStones()
+    }
+
+    private fun setStoneImage(stone: Stone, view: ImageView) {
+        when (stone) {
+            Stone.BLACK -> view.setImageResource(R.drawable.black_stone)
+            Stone.WHITE -> view.setImageResource(R.drawable.white_stone)
+            Stone.EMPTY -> {}
+        }
+    }
+
+    companion object {
+        private const val WINNER_MESSAGE = "%s 이 승리했습니다!"
+        private const val FORBIDDEN_MESSAGE = "돌을 놓을 수 없습니다."
     }
 }
