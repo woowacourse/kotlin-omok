31d02f32fc0a9b4c49c214d7190c7c9621bf0bfe
hyunji1203 <hj27720@gmail.com>
2023-03-24 16:50:08 +0900

    feat(Board): 흑돌과 백돌 수 허용 로직 분리

--- domain/src/main/kotlin/omok/domain/Board.kt
+++ domain/src/main/kotlin/omok/domain/Board.kt
@@ -1,17 +1,24 @@
 package omok.domain
 
 import omok.domain.judgement.LineJudgement
-import omok.domain.state.Turn
 
 class Board(val blackPlayer: Player, val whitePlayer: Player) {
     private val positions = Position.POSITIONS.toList()
 
-    fun isPlaceable(turn: Turn, position: Position): Boolean {
-        val blackStone = BlackStone(position)
+    fun isBlackPlaceable(position: Position): Boolean {
+        val stone = BlackStone(position)
         return when {
-            LineJudgement(blackPlayer, blackStone).check() -> true
-            blackStone.judgePossibility(blackPlayer, whitePlayer) -> false
-            else -> turn == Turn.White || positions.find { it == position }?.isEmpty() == true
+            LineJudgement(blackPlayer, stone).check() -> true
+            stone.judgePossibility(blackPlayer, whitePlayer) -> false
+            else -> positions.find { it == position }?.isEmpty() == true
+        }
+    }
+
+    fun isWhitePlaceable(position: Position): Boolean {
+        val stone = WhiteStone(position)
+        return when {
+            LineJudgement(whitePlayer, stone).check() -> true
+            else -> positions.find { it == position }?.isEmpty() == true
         }
     }
 
