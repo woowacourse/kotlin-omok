72a85179bef5e1cdfdd93d99676ea15919ffd9ae
otter <a33397397@gmail.com>
2023-03-24 14:46:26 +0900

    refactor: 보드 클릭 이벤트 메서드 분리

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -18,15 +18,14 @@ import view.OutputView
 
 class MainActivity : AppCompatActivity() {
 
+    val outputView: OutputView = OutputView()
+    val board = findViewById<TableLayout>(R.id.board)
+    val boardMap: Board = Board()
+    var state: State = BlackTurn()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val board = findViewById<TableLayout>(R.id.board)
-        val boardMap: Board = Board()
-        var state: State = BlackTurn()
-
-        val outputView: OutputView = OutputView()
 
         outputView.printOmokStart()
         board
@@ -35,21 +34,23 @@ class MainActivity : AppCompatActivity() {
             .forEachIndexed { row, it ->
                 it.children.filterIsInstance<ImageView>()
                     .forEachIndexed { col, view ->
-                        view.setOnClickListener {
-                            val stonePosition: StonePosition = StonePosition(col + 1, row + 1)
-                            if (!state.isEnd() && !(state as Running).isPlaced(boardMap, stonePosition)) {
-                                view.setImageResource(getStoneImage(state))
-                                state = state.next(boardMap, stonePosition)
-
-                                outputView.printBoard(boardMap.stones)
-                                outputView.printTurn(state, boardMap.stones)
-                            }
-                            if (state.isEnd()) {
-                                Toast.makeText(this, "${OutputView().getWinnerText(state as End)}돌이 승리하였습니다.", Toast.LENGTH_SHORT).show()
-                            }
-                        }
+                        view.setOnClickListener { coordinateViewClickEvent(row, col, view) }
                     }
             }
+    }
+
+    private fun coordinateViewClickEvent(row: Int, col: Int, view: ImageView) {
+        val stonePosition: StonePosition = StonePosition(col + 1, row + 1)
+        if (!state.isEnd() && !(state as Running).isPlaced(boardMap, stonePosition)) {
+            view.setImageResource(getStoneImage(state))
+            state = state.next(boardMap, stonePosition)
+
+            outputView.printBoard(boardMap.stones)
+            outputView.printTurn(state, boardMap.stones)
+        }
+        if (state.isEnd()) {
+            Toast.makeText(this, "${OutputView().getWinnerText(state as End)}돌이 승리하였습니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun getStoneImage(state: State): Int {
