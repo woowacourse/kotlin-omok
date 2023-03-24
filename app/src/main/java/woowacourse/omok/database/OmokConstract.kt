c50ab612ca1909b7ab2164354ab9c977114eab76
hyunji1203 <hj27720@gmail.com>
2023-03-24 16:46:07 +0900

    feat: 오목 DB를 관리해주는 클래스 생성

---
+++ app/src/main/java/woowacourse/omok/database/OmokConstract.kt
@@ -0,0 +1,7 @@
+package woowacourse.omok.database
+
+object OmokConstract {
+    const val TABLE_NAME = "board"
+    const val TABLE_COLUMN_COLOR = "color"
+    const val TABLE_COLUMN_POSITION = "position"
+}
