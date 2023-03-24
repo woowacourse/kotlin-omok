e4547e713791d3eddb352f3c9aa614f25ee5778c
vrexpert <vrexpert2302@gmail.com>
2023-03-24 17:09:09 +0900

    feat: Omok table 상수화

---
+++ app/src/main/java/woowacourse/omok/model/data/OmokContract.kt
@@ -0,0 +1,9 @@
+package woowacourse.omok.model.data
+
+import android.provider.BaseColumns
+
+object OmokContract : BaseColumns {
+    const val TABLE_NAME = "omok"
+    const val TABLE_COLUMN_INDEX = "stoneIndex"
+    const val TABLE_COLUMN_STONE_COLOR = "stoneColor"
+}
