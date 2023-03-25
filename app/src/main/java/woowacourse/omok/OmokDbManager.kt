dd828a71610d8181c41b5f67d815357849621c15
chws0508 <chws0508@naver.com>
2023-03-25 16:03:42 +0900

    feat(): 데이터베이스 관리 클래스 구현

---
+++ app/src/main/java/woowacourse/omok/OmokDbManager.kt
@@ -0,0 +1,49 @@
+package woowacourse.omok
+
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+import domain.*
+
+class OmokDbManager(omokDbHelper: OmokDbHelper) {
+
+    private val omokWritableDb: SQLiteDatabase = omokDbHelper.writableDatabase
+
+    fun getOmokGame(): OmokGame {
+        val stones = getAllStonesInDatabase()
+        return OmokGame(Board(stones, RenjuRuleAdapter())).apply {
+            currentColor = nextColor(stones.getLastStone()?.color ?: Color.WHITE)
+        }
+    }
+
+    fun updateOmokDatabase(stone: Stone?) {
+        if (stone != null) {
+            val contentValues = getStoneContentValues(stone)
+            omokWritableDb.insert(OmokContract.TABLE_NAME, null, contentValues)
+        }
+    }
+
+    fun deleteOmokDatabase() {
+        omokWritableDb.delete(OmokContract.TABLE_NAME, null, null)
+    }
+
+    private fun getAllStonesInDatabase(): Stones {
+        var stones = Stones(listOf())
+        val cursor = omokWritableDb.rawQuery("SELECT * FROM ${OmokContract.TABLE_NAME}", null)
+        while (cursor.moveToNext()) {
+            val color =
+                cursor.getString(cursor.getColumnIndexOrThrow(OmokContract.TABLE_COLUMN_COLOR))
+            val x = cursor.getInt(cursor.getColumnIndexOrThrow(OmokContract.TABLE_COLUMN_X))
+            val y = cursor.getInt(cursor.getColumnIndexOrThrow(OmokContract.TABLE_COLUMN_Y))
+            stones = stones.addStone(Stone(Converter.stringToColor(color), Position(x, y)))
+        }
+        return stones
+    }
+
+    private fun getStoneContentValues(stone: Stone): ContentValues {
+        val values = ContentValues()
+        values.put("color", Converter.colorToString(stone.color))
+        values.put("x", stone.position.x)
+        values.put("y", stone.position.y)
+        return values
+    }
+}
