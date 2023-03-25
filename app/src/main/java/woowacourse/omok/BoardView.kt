952587e9ecce6322f088eaa07c0dace173f260e2
changhwan <chang5hustle@gmail.com>
2023-03-25 16:14:27 +0900

    refactor(OmokGame): BoardClass 생성

---
+++ app/src/main/java/woowacourse/omok/BoardView.kt
@@ -0,0 +1,31 @@
+package woowacourse.omok
+
+import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.TableRow
+import androidx.core.view.children
+
+abstract class BoardView(private val value: TableLayout) {
+    private fun setBoardTask() {
+        value
+            .children
+            .filterIsInstance<TableRow>()
+            .forEachIndexed { rowIndex, tableRow ->
+                setCoordinateTask(
+                    tableRow,
+                    rowIndex
+                )
+            }
+    }
+
+    private fun setCoordinateTask(
+        tableRow: TableRow,
+        rowIndex: Int
+    ) = tableRow.children
+        .filterIsInstance<ImageView>()
+        .forEachIndexed() { columIndex, imageView ->
+            boardTask(imageView = imageView, columIndex = columIndex, rowIndex = rowIndex)
+        }
+
+    abstract fun boardTask(imageView: ImageView, rowIndex: Int, columIndex: Int)
+}
--- app/src/main/java/woowacourse/omok/gameactivity/GameActivity.kt
+++ app/src/main/java/woowacourse/omok/gameactivity/GameActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
-import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.children
 import domain.domain.CoordinateState
 import domain.domain.OmokGame
 import domain.domain.Position
@@ -14,7 +12,6 @@ import domain.domain.ProgressState
 import domain.library.combinerule.CombinedRuleAdapter
 import woowacourse.omok.FinishActivity
 import woowacourse.omok.R
-import woowacourse.omok.util.setOnSingleClickListener
 
 class GameActivity : AppCompatActivity() {
 
@@ -29,7 +26,6 @@ class GameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_game)
 
         val board = findViewById<TableLayout>(R.id.board)
-        setBoardOnclickListener(board)
         initViewId()
     }
 
@@ -39,32 +35,32 @@ class GameActivity : AppCompatActivity() {
         messageView = MessageView(findViewById(R.id.tv_message), omokGame)
     }
 
-    private fun setBoardOnclickListener(board: TableLayout) {
-        board
-            .children
-            .filterIsInstance<TableRow>()
-            .forEachIndexed { rowIndex, tableRow ->
-                setCoordinateOnclickListener(
-                    tableRow,
-                    rowIndex
-                )
-            }
-    }
-
-    private fun setCoordinateOnclickListener(
-        tableRow: TableRow,
-        rowIndex: Int
-    ) = tableRow.children
-        .filterIsInstance<ImageView>()
-        .forEachIndexed() { columIndex, imageView ->
-            imageView.setOnSingleClickListener {
-                coordinateClickListener(
-                    imageView = imageView,
-                    rowIndex = rowIndex,
-                    columIndex = columIndex
-                )
-            }
-        }
+//    private fun setBoardOnclickListener(board: TableLayout) {
+//        board
+//            .children
+//            .filterIsInstance<TableRow>()
+//            .forEachIndexed { rowIndex, tableRow ->
+//                setCoordinateOnclickListener(
+//                    tableRow,
+//                    rowIndex
+//                )
+//            }
+//    }
+//
+//    private fun setCoordinateOnclickListener(
+//        tableRow: TableRow,
+//        rowIndex: Int
+//    ) = tableRow.children
+//        .filterIsInstance<ImageView>()
+//        .forEachIndexed() { columIndex, imageView ->
+//            imageView.setOnSingleClickListener {
+//                coordinateClickListener(
+//                    imageView = imageView,
+//                    rowIndex = rowIndex,
+//                    columIndex = columIndex
+//                )
+//            }
+//        }
 
     private fun coordinateClickListener(imageView: ImageView, rowIndex: Int, columIndex: Int) {
         val turnStoneColor = omokGame.turn
