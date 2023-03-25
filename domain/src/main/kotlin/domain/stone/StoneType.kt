b6316d434e458d08e1f6cbf3442bc55dba931bc7
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-25 18:51:16 +0900

    feat: StoneType 변수값 선언

--- domain/src/main/kotlin/domain/stone/StoneType.kt
+++ domain/src/main/kotlin/domain/stone/StoneType.kt
@@ -1,5 +1,11 @@
 package domain.stone
 
-enum class StoneType {
-    BLACK, WHITE, EMPTY
+enum class StoneType(val number: Int) {
+    BLACK(0), WHITE(1), EMPTY(2);
+
+    companion object {
+        fun getStoneType(number: Int): StoneType? {
+            return StoneType.values().find { it.number == number }
+        }
+    }
 }
