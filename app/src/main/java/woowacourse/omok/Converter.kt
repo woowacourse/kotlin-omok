0c91ca9eb9ef49df556b166c7ea638e0f83107f0
chws0508 <chws0508@naver.com>
2023-03-25 16:03:01 +0900

    refactor(): 컨버터 기능 클래스 구현

---
+++ app/src/main/java/woowacourse/omok/Converter.kt
@@ -0,0 +1,34 @@
+package woowacourse.omok
+
+import domain.Board
+import domain.Color
+import domain.Position
+
+object Converter {
+    fun colorToString(color: Color): String {
+        return when (color) {
+            Color.BLACK -> "흑"
+            Color.WHITE -> "백"
+        }
+    }
+
+    fun stringToColor(message: String): Color {
+        return when (message) {
+            "흑" -> Color.BLACK
+            "백" -> Color.WHITE
+            else -> throw java.lang.IllegalArgumentException("잘못된 색")
+        }
+    }
+
+    fun indexToPosition(index: Int): Position {
+        val boardSize = Board.getSize()
+        val x = index / boardSize
+        val y = index % boardSize
+        return Position(x, y)
+    }
+
+    fun positionToIndex(position: Position): Int {
+        val boardSize = Board.getSize()
+        return position.x * boardSize + position.y
+    }
+}
