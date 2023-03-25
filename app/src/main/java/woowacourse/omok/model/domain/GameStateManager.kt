cba535a37cd63dfdf4cd70b0e6c20f8ef39072a7
s9hn <tnghqn@naver.com>
2023-03-26 05:05:26 +0900

    [FEAT(GameStateManager)] 데이터 입력 및 읽기 메서드 구현

---
+++ app/src/main/java/woowacourse/omok/model/domain/GameStateManager.kt
@@ -0,0 +1,32 @@
+package woowacourse.omok.model.domain
+
+import android.content.ContentValues
+import android.database.Cursor
+import woowacourse.omok.model.data.BoardContract
+import woowacourse.omok.model.data.BoardDbHelper
+
+class GameStateManager(boardDbHelper: BoardDbHelper) {
+    private val boardDbOnlyWriteable = boardDbHelper.writableDatabase
+    private val _cursor: Cursor = boardDbOnlyWriteable.query(
+        BoardContract.FeedEntry.TABLE_NAME,
+        arrayOf(BoardContract.FeedEntry.TABLE_COLUMN_LOCATION),
+        null,
+        null,
+        null,
+        null,
+        null,
+    )
+    val cursor: Cursor get() = _cursor
+
+    fun saveGame(location: Int) {
+        val values = ContentValues().apply {
+            put(BoardContract.FeedEntry.TABLE_COLUMN_LOCATION, location)
+        }
+
+        boardDbOnlyWriteable?.insert(BoardContract.FeedEntry.TABLE_NAME, null, values)
+    }
+
+    fun deleteGame() {
+        boardDbOnlyWriteable.delete(BoardContract.FeedEntry.TABLE_NAME, null, null)
+    }
+}
