7e351d9f6ef9318bcdd21745724b2a2adbe2af26
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-03-25 19:10:28 +0900

    feat: 우승자 알려주는 액티비티 구현 및 재시작 버튼과 종료 버튼 구현

---
+++ app/src/main/java/woowacourse/omok/activity/WinnerActivity.kt
@@ -0,0 +1,48 @@
+package woowacourse.omok.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import domain.stone.StoneType
+import woowacourse.omok.R
+
+class WinnerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_winner)
+
+        val restartButton = findViewById<Button>(R.id.restartButton)
+        val endButton = findViewById<Button>(R.id.endButton)
+
+        showWinnerStone()
+        restartButton.setOnClickListener { restartGame() }
+        endButton.setOnClickListener { endGame() }
+    }
+
+    fun showWinnerStone() {
+        val winner = findViewById<TextView>(R.id.winner)
+        val winnerStone = intent.getIntExtra("winner", 0)
+        when (StoneType.getStoneType(winnerStone)) {
+            StoneType.BLACK -> winner.text = BLACK_STONE
+            StoneType.WHITE -> winner.text = WHITE_STONE
+            else -> null
+        }
+    }
+
+    fun restartGame() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun endGame() {
+        finish()
+    }
+
+    companion object {
+        const val BLACK_STONE = "흑"
+        const val WHITE_STONE = "백"
+    }
+}
