fe96c33c8940e57075aab28e37195e58e2c7ac81
chws0508 <chws0508@naver.com>
2023-03-25 16:02:14 +0900

    feat(): 데이터베이스 틀 작성

---
+++ app/src/main/java/woowacourse/omok/OmokDbHelper.kt
@@ -0,0 +1,23 @@
+package woowacourse.omok
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class OmokDbHelper(context: Context?) : SQLiteOpenHelper(context, "${OmokContract.TABLE_NAME}", null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(
+            "CREATE TABLE ${OmokContract.TABLE_NAME} (" +
+                    "${OmokContract.TABLE_COLUMN_COLOR} varchar(2) not null," +
+                    "${OmokContract.TABLE_COLUMN_X} int not null," +
+                    "${OmokContract.TABLE_COLUMN_Y} int not null," +
+                    "UNIQUE (x,y)" +
+                    ");"
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS ${OmokContract.TABLE_NAME}")
+        onCreate(db)
+    }
+}
