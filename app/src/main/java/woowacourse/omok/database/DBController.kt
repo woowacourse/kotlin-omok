807275a32fb5889a5a5124703845cc533d65fb85
hyunji1203 <hj27720@gmail.com>
2023-03-26 15:57:15 +0900

    refactor(DBController): 변수 명 변경

--- app/src/main/java/woowacourse/omok/database/DBController.kt
+++ app/src/main/java/woowacourse/omok/database/DBController.kt
@@ -3,17 +3,17 @@ package woowacourse.omok.database
 import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 
-class DBController(private val wDb: SQLiteDatabase) {
+class DBController(private val writableDb: SQLiteDatabase) {
     fun insertDB(color: String, index: Int) {
         val values = ContentValues()
         values.put(OmokConstract.TABLE_COLUMN_COLOR, color)
         values.put(OmokConstract.TABLE_COLUMN_POSITION, index)
-        wDb.insert(OmokConstract.TABLE_NAME, null, values)
+        writableDb.insert(OmokConstract.TABLE_NAME, null, values)
     }
 
     fun getIndex(color: String): List<Int> {
         val result = mutableListOf<Int>()
-        val cursor = wDb.rawQuery("SELECT * FROM ${OmokConstract.TABLE_NAME} WHERE ${OmokConstract.TABLE_COLUMN_COLOR} = '" + color + "';", null)
+        val cursor = writableDb.rawQuery("SELECT * FROM ${OmokConstract.TABLE_NAME} WHERE ${OmokConstract.TABLE_COLUMN_COLOR} = '" + color + "';", null)
         with(cursor) {
             while (moveToNext()) { result.add(getInt(getColumnIndexOrThrow(OmokConstract.TABLE_COLUMN_POSITION))) }
         }
@@ -22,6 +22,6 @@ class DBController(private val wDb: SQLiteDatabase) {
     }
 
     fun deleteDB() {
-        wDb.execSQL("DELETE FROM ${OmokConstract.TABLE_NAME}")
+        writableDb.execSQL("DELETE FROM ${OmokConstract.TABLE_NAME}")
     }
 }
