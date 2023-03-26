402d15684417d11059991c70a70a31abd328468f
changhwan <chang5hustle@gmail.com>
2023-03-26 23:56:39 +0900

    feat(OmokDbHelper): OmokDbHelper 기초세팅

--- app/src/main/java/woowacourse/omok/data/db/OmokDbHelper.kt
+++ app/src/main/java/woowacourse/omok/data/db/OmokDbHelper.kt
@@ -1,3 +1,27 @@
 package woowacourse.omok.data.db
 
-class OmokDbHelper
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class OmokDbHelper(context: Context, private val tables: List<SQLiteTable>) :
+    SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        tables.forEach {
+            val columns = it.scheme.joinToString(",") { scheme -> "${scheme.name} ${scheme.type}" }
+            db?.execSQL("CREATE TABLE ${it.name} ($columns)")
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        tables.forEach {
+            db?.execSQL("DROP TABLE IF EXISTS ${it.name}")
+        }
+        onCreate(db)
+    }
+
+    companion object {
+        const val DB_VERSION = 1
+        const val DB_NAME = "OMOK"
+    }
+}
