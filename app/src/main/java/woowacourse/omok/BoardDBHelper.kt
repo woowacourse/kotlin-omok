2ea2d10231dc824d506520f0e986dcf1f3ee6614
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-03-26 14:11:17 +0900

    refactor: DBHelper가 모든 데이터 관리 역할하도록 수정

--- app/src/main/java/woowacourse/omok/BoardDBHelper.kt
+++ app/src/main/java/woowacourse/omok/BoardDBHelper.kt
@@ -1,11 +1,18 @@
 package woowacourse.omok
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import omok.domain.Turn
+import omok.domain.board.Board
+import omok.domain.board.Position
+import omok.domain.player.Stone
 
 class BoardDBHelper(context: Context?) :
     SQLiteOpenHelper(context, BoardContract.DATABASE_NAME, null, 1) {
+    private val db = this.writableDatabase
     override fun onCreate(db: SQLiteDatabase?) {
         db?.execSQL(
             "CREATE TABLE ${BoardContract.TABLE_NAME}(" +
@@ -15,6 +22,58 @@ class BoardDBHelper(context: Context?) :
         )
     }
 
+    fun getBoard(): Board {
+        val cells: MutableMap<Position, Stone?> = Board.POSITIONS.associateWith { null }.toMutableMap()
+
+        val cursor = db.rawQuery("SELECT * FROM ${BoardContract.TABLE_NAME}", null)
+        if (cursor.moveToFirst()) makeCells(cursor, cells)
+        cursor.close()
+        return Board(cells)
+    }
+
+    private fun makeCells(
+        cursor: Cursor,
+        cells: MutableMap<Position, Stone?>
+    ) {
+        do {
+            val index = cursor.getInt(cursor.getColumnIndexOrThrow(BoardContract.TABLE_COLUMN_POSITION_INDEX))
+            val stone = cursor.getInt(cursor.getColumnIndexOrThrow(BoardContract.TABLE_COLUMN_STONE))
+            cells[changeIndexToPosition(index)] = changeDataToStone(stone)
+        } while (cursor.moveToNext())
+    }
+
+    fun getTurn(): Turn {
+        val cursor = db.rawQuery("SELECT * FROM ${BoardContract.TABLE_NAME}", null)
+        val count = cursor.count
+
+        cursor.close()
+        if (count != 0 && count % 2 == 1)
+            return Turn(setOf(Stone.WHITE, Stone.BLACK))
+        return Turn(setOf(Stone.BLACK, Stone.WHITE))
+    }
+
+    fun insertData(indexPosition: Int, currentStone: Stone) {
+        val values = ContentValues().apply {
+            put(BoardContract.TABLE_COLUMN_POSITION_INDEX, indexPosition)
+            put(BoardContract.TABLE_COLUMN_STONE, "${changeStoneToData(currentStone)}")
+        }
+        db.insert(BoardContract.TABLE_NAME, null, values)
+    }
+
+    fun deleteData() {
+        db.execSQL("DELETE FROM ${BoardContract.TABLE_NAME}")
+        db.close()
+    }
+
+    private fun changeStoneToData(stone: Stone) = if (stone == Stone.BLACK) 0 else 1
+
+    private fun changeDataToStone(stone: Int) = if (stone == 0) Stone.BLACK else Stone.WHITE
+
+    private fun changeIndexToPosition(index: Int): Position {
+        val row = 14 - (index / 15)
+        val column = index % 15
+        return Position(Pair(column, row))
+    }
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
         db?.execSQL("DROP TABLE IF EXISTS ${BoardContract.TABLE_NAME}")
         onCreate(db)
