2e09fd0b17404cb740046db683b78c56056d5bbf
re4rk <37167652+re4rk@users.noreply.github.com>
2023-03-27 03:07:53 +0900

    feat (RoomActivity): 계정 생성기능 추가

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.omok.SampleData.generateSampleData
 import woowacourse.omok.data.Player
@@ -14,7 +15,6 @@ class MainActivity : AppCompatActivity() {
     val playerDb = OmokPlayerDbHelper(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        generateSampleData(this)
         setContentView(R.layout.activity_main)
         addListenerOnButton()
     }
@@ -23,14 +23,20 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.main_button)
         button.setOnClickListener {
             val nameText = findViewById<EditText>(R.id.main_edit_text)
-            val player = playerDb.getPlayer(nameText.text.toString())
+            val player = playerDb.getPlayer(nameText.text.toString()) ?: createPlayer(nameText)
 
-            when (player) {
-                null -> nameText.error = "존재하지 않는 플레이어입니다."
-                else -> startRoomListActivity(player)
+            if (player == null) {
+                Toast.makeText(this, "이름을 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+            startRoomListActivity(player)
         }
     }
+
+    private fun createPlayer(nameText: EditText): Player? =
+        Player(name = nameText.text.toString(), profile = R.drawable.player_ark)
+            .run { playerDb.insert(this) }
+            .let { playerDb.getPlayer(it) }
 
     private fun startRoomListActivity(player: Player) =
         startActivity(
@@ -38,6 +44,11 @@ class MainActivity : AppCompatActivity() {
                 putExtra("player", player)
             },
         )
+
+    override fun onPause() {
+        super.onPause()
+        generateSampleData(this)
+    }
 
     override fun onDestroy() {
         super.onDestroy()
