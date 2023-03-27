fe8b25d772118e6128e5aae29275754cce13e6c1
otter <a33397397@gmail.com>
2023-03-27 09:47:50 +0900

    refactor(StonePositionDbHelper): 불필요한 nullable 사용 제거

--- app/src/main/java/woowacourse/omok/database/stoneposition/StonePositionDbHelper.kt
+++ app/src/main/java/woowacourse/omok/database/stoneposition/StonePositionDbHelper.kt
@@ -5,10 +5,10 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class StonePositionDbHelper(
-    context: Context?
+    context: Context
 ) : SQLiteOpenHelper(context, "otter.db", null, 1) {
-    override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL(
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
             "CREATE TABLE ${StonePositionConstract.TABLE_NAME} (" +
                 "position_row int," +
                 "position_cal int" +
@@ -16,7 +16,7 @@ class StonePositionDbHelper(
         )
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+    override fun onUpgrade(p0: SQLiteDatabase, p1: Int, p2: Int) {
         TODO("Not yet implemented")
     }
 }
