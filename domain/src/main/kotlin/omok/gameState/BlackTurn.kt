d73d55de1c33418682d06a0e7d8303a6c9f4d624
inseonyun <yuninseon@naver.com>
2023-03-27 23:31:23 +0900

    refactor(BlackTurn): 금수에 착수할 때 좌표 정보 출력하도록 수정

--- domain/src/main/kotlin/omok/gameState/BlackTurn.kt
+++ domain/src/main/kotlin/omok/gameState/BlackTurn.kt
@@ -14,12 +14,12 @@ class BlackTurn(override val omokBoard: OmokBoard) : GameState {
 
         return when {
             referee.isWin(omokBoard, point, stoneState) -> BlackWin(omokBoard.placeStone(point, stoneState))
-            referee.isForbidden(omokBoard, point) -> throw IllegalArgumentException(ERROR_FORBIDDEN)
+            referee.isForbidden(omokBoard, point) -> throw IllegalArgumentException(ERROR_FORBIDDEN.format(point.x, point.y))
             else -> WhiteTurn(omokBoard.placeStone(point, stoneState))
         }
     }
 
     companion object {
-        private const val ERROR_FORBIDDEN = "해당 좌표는 금지된 수 입니다"
+        private const val ERROR_FORBIDDEN = "(%s, %s)는 금지된 수 입니다"
     }
 }
