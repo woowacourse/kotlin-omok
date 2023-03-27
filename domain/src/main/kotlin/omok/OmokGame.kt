172068cb274cc669d53586b74735b697164db34d
Yerin <myheaven_b@naver.com>
2023-03-28 01:11:52 +0900

    style: 함수 접근제어자 변경

--- domain/src/main/kotlin/omok/OmokGame.kt
+++ domain/src/main/kotlin/omok/OmokGame.kt
@@ -20,12 +20,12 @@ class OmokGame(val board: Board = Board(RenjuJudgement(), Player(), Player())) {
         }
     }
 
-    fun blackTurn(position: Position): State {
+    private fun blackTurn(position: Position): State {
         board.putStone(Turn.Black, position)
         return if (board.lineJudge(Turn.Black, position)) Win.Black else Turn.White
     }
 
-    fun whiteTurn(position: Position): State {
+    private fun whiteTurn(position: Position): State {
         board.putStone(Turn.White, position)
         return if (board.lineJudge(Turn.White, position)) Win.White else Turn.Black
     }
