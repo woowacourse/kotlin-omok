14ad02ca80f5fa82285343ef5aa3a6c59e9a5b8a
hyemdooly <hyemdooly@gmail.com>
2023-03-28 14:11:52 +0900

    refactor(PlayersTest): 팩토리 함수 구현으로 인한 변경

--- domain/src/test/kotlin/player/PlayersTest.kt
+++ domain/src/test/kotlin/player/PlayersTest.kt
@@ -26,10 +26,9 @@ class PlayersTest {
 
     @Test
     fun `첫 턴은 흑이며, 현재 턴의 플레이어가 오목알을 성공적으로 놓으면 턴이 백으로 바뀐다`() {
-        val players =
-            Players(BlackPlayer(rule = blackRenjuRule), WhitePlayer(rule = whiteRenjuRule))
+        val players = Players.from(BlackPlayer(rule = blackRenjuRule), WhitePlayer(rule = whiteRenjuRule))
         val actual = players.putPoint(Point(1, 1))
-        val expected = Players(
+        val expected = Players.from(
             BlackPlayer(rule = blackRenjuRule, Point(1, 1)),
             WhitePlayer(rule = whiteRenjuRule)
         )
@@ -39,12 +38,12 @@ class PlayersTest {
 
     @Test
     fun `현재 턴이 백일 때, 현재 턴의 플레이어가 오목알을 성공적으로 놓으면 턴이 흑으로 바뀐다`() {
-        val players = Players(
+        val players = Players.from(
             BlackPlayer(rule = blackRenjuRule, Point(2, 1)),
             WhitePlayer(rule = whiteRenjuRule)
         )
         val actual = players.putPoint(Point(1, 1))
-        val expected = Players(
+        val expected = Players.from(
             BlackPlayer(rule = blackRenjuRule, Point(2, 1)),
             WhitePlayer(rule = whiteRenjuRule, Point(1, 1))
         )
@@ -54,15 +53,14 @@ class PlayersTest {
 
     @Test
     fun `모든 플레이어들의 상태가 플레이 중이면 참을 반환한다`() {
-        val players =
-            Players(BlackPlayer(rule = blackRenjuRule), WhitePlayer(rule = whiteRenjuRule))
+        val players = Players.from(BlackPlayer(rule = blackRenjuRule), WhitePlayer(rule = whiteRenjuRule))
         val actual = players.isPlaying
         assertThat(actual).isTrue
     }
 
     @Test
     fun `한 명이라도 플레이어의 상태가 플레이 중이 아니면 거짓을 반환한다`() {
-        val players = Players(
+        val players = Players.from(
             BlackPlayer(state = FoulState(), rule = blackRenjuRule),
             WhitePlayer(rule = whiteRenjuRule)
         )
@@ -73,14 +71,14 @@ class PlayersTest {
     @Test
     fun `현재 턴의 플레이어의 돌 색깔을 반환한다`() {
         val players =
-            Players(BlackPlayer(rule = blackRenjuRule), WhitePlayer(rule = whiteRenjuRule))
+            Players.from(BlackPlayer(rule = blackRenjuRule), WhitePlayer(rule = whiteRenjuRule))
         val actual = players.curPlayerColor
         assertThat(actual).isEqualTo(StoneColor.BLACK)
     }
 
     @Test
     fun `마지막 놓은 돌을 반환한다`() {
-        val players = Players(
+        val players = Players.from(
             BlackPlayer(rule = blackRenjuRule),
             WhitePlayer(rule = whiteRenjuRule)
         ).putPoint(Point(1, 1)).putPoint(Point(2, 2))
@@ -92,7 +90,7 @@ class PlayersTest {
     @Test
     fun `플레이어 목록을 반환한다`() {
         val players =
-            Players(BlackPlayer(rule = blackRenjuRule), WhitePlayer(rule = whiteRenjuRule))
+            Players.from(BlackPlayer(rule = blackRenjuRule), WhitePlayer(rule = whiteRenjuRule))
         val actual = players.toList()
 
         assertThat(actual).usingRecursiveComparison().isEqualTo(
@@ -106,12 +104,11 @@ class PlayersTest {
     @Test
     fun `자기 자신을 깊은 복사할 수 있다`() {
         val players =
-            Players(BlackPlayer(rule = blackRenjuRule), WhitePlayer(rule = whiteRenjuRule))
+            Players.from(BlackPlayer(rule = blackRenjuRule), WhitePlayer(rule = whiteRenjuRule))
         val actual = players.copy()
         assertThat(actual).isNotSameAs(players)
     }
 
-    private fun Players(blackPlayer: BlackPlayer, whitePlayer: WhitePlayer) = Players(blackPlayer, listOf(blackPlayer, whitePlayer))
     private fun BlackPlayer(rule: BlackRenjuRule, vararg points: Point) =
         BlackPlayer(PlayingState(Points(points.toList())), rule)
     private fun WhitePlayer(rule: WhiteRenjuRule, vararg points: Point) = WhitePlayer(PlayingState(Points(points.toList())), rule)
