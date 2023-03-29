b059ef3b9709a905ef7131c4df954985efea943b
briandr97 <briandr@naver.com>
2023-03-29 22:03:09 +0900

    refactor(OmokGameActivity): 인스턴스 변수 두 개로 줄임
    - OmokGameService를 만들어서 Activity는 어떤 Dao로부터 데이터를 받아오는지 알 필요가 없게함
    - 그저 OmokGameService로부터 필요한 데이터를 받아오면 됨
    - OmokGameService에선 각 Dao정도는 인스턴스 변수로 들고 있어도 되지 않을까..?

--- app/src/main/java/woowacourse/omok/omokgame/OmokGameActivity.kt
+++ app/src/main/java/woowacourse/omok/omokgame/OmokGameActivity.kt
@@ -19,11 +19,7 @@ import domain.domain.state.WhiteWin
 import domain.view.Observer
 import domain.view.OmokView
 import woowacourse.omok.R
-import woowacourse.omok.data.dao.BoardDao
-import woowacourse.omok.data.dao.RoomDao
-import woowacourse.omok.data.dao.UserDao
 import woowacourse.omok.home.HomeActivity.Companion.ROOM_ID
-import woowacourse.omok.home.Room
 import woowacourse.omok.omokgame.OmokGameUtil.loopBoardTable
 import woowacourse.omok.result.ResultActivity
 import woowacourse.omok.util.ContextUtil.longToast
@@ -31,33 +27,17 @@ import woowacourse.omok.util.SnackBarUtil.defaultSnackBar
 
 class OmokGameActivity : AppCompatActivity(), Observer {
     private var preColor: CoordinateState = BLACK
-    private val boardTable by lazy { findViewById<TableLayout>(R.id.board_table) }
-    private val room by lazy { getRoomInfo() }
-
-    private val boardDao = BoardDao(this)
+    private val omokGameService by lazy {
+        OmokGameService(this, intent.getIntExtra(ROOM_ID, -1))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.omok_game_activity)
 
-        val board = Board(boardDao.readBoard(room.roomId))
+        val board = Board(omokGameService.readBoard())
         runOmokGame(board)
         synchronizeConsoleView(board)
-    }
-
-    private fun getRoomInfo(): Room {
-        val roomDao = RoomDao(this)
-        val userDao = UserDao(this)
-
-        val roomId = intent.getIntExtra(ROOM_ID, -1)
-        val roomEntity = roomDao.getRoom(roomId)
-        val room = with(roomEntity) {
-            Room(roomId, roomTitle, userDao.getUser(firstUserId), userDao.getUser(secondUserId))
-        }
-
-        roomDao.closeDb()
-        userDao.closeDb()
-        return room
     }
 
     private fun synchronizeConsoleView(board: Board) {
@@ -65,25 +45,20 @@ class OmokGameActivity : AppCompatActivity(), Observer {
     }
 
     private fun runOmokGame(board: Board) {
-        initView(board.state)
         board.registerObserver(this)
+        initView(board.state)
         setOmokClickListener(board)
     }
 
     private fun initView(state: State) {
-        findViewById<TextView>(R.id.gameTitleText).text = room.roomTitle
-        findViewById<TextView>(R.id.gameFirstUserNameText).text = room.firstUserEntity.userName
-        findViewById<TextView>(R.id.gameSecondUserNameText).text = room.secondUserEntity.userName
-        initDescription(state.getTurn())
-        drawStone(state)
-    }
-
-    private fun initDescription(turn: CoordinateState) {
-        val gameDescriptionText = findViewById<TextView>(R.id.gameDescriptionText)
-        gameDescriptionText.text = DESCRIPTION.format(turn.toColor(), turn.toName(), turn.toColor())
+        findViewById<TextView>(R.id.gameTitleText).text = omokGameService.roomTitle
+        findViewById<TextView>(R.id.gameFirstUserNameText).text = omokGameService.firstUserName
+        findViewById<TextView>(R.id.gameSecondUserNameText).text = omokGameService.secondUserName
+        updateView(state)
     }
 
     private fun setOmokClickListener(board: Board) {
+        val boardTable = findViewById<TableLayout>(R.id.board_table)
         boardTable.loopBoardTable { position: Position, imageView ->
             clickPart(board, position, imageView)
         }
@@ -98,41 +73,43 @@ class OmokGameActivity : AppCompatActivity(), Observer {
 
     override fun update(state: State) {
         when (state) {
-            is BlackTurn -> updateView(state)
-            is WhiteTurn -> updateView(state)
+            is BlackTurn -> updateInfo(state)
+            is WhiteTurn -> updateInfo(state)
             is BlackWin -> win(state)
             is WhiteWin -> win(state)
         }
     }
 
-    private fun updateView(state: State) {
+    private fun updateInfo(state: State) {
         if (preColor == state.getTurn()) {
             defaultSnackBar(findViewById(R.id.root), "놓을 수 없는 수 입니다")
             return
         }
+        updateView(state)
+        omokGameService.updateDb(state, preColor)
+    }
+
+    private fun updateView(state: State) {
         initDescription(state.getTurn())
         drawStone(state)
-        updateDb(state)
+    }
+
+    private fun initDescription(turn: CoordinateState) {
+        val gameDescriptionText = findViewById<TextView>(R.id.gameDescriptionText)
+        gameDescriptionText.text = DESCRIPTION.format(turn.toColor(), turn.toName(), turn.toColor())
     }
 
     private fun drawStone(state: State) {
+        val boardTable = findViewById<TableLayout>(R.id.board_table)
         boardTable.loopBoardTable { position: Position, imageView ->
             val resId = OmokGameUtil.matchColor(state.stones[position.y, position.x])
             if (resId != null) imageView.setImageResource(resId)
         }
     }
 
-    private fun updateDb(state: State) {
-        val y = (state.getLastPosition() ?: return).y
-        val x = (state.getLastPosition() ?: return).x
-        boardDao.insertStone(room.roomId, y, x, preColor)
-    }
-
     private fun win(state: State) {
-        boardTable.loopBoardTable { _, imageView -> imageView.isEnabled = false }
         longToast("${state.getTurn().toName()}의 승리입니다!!!")
-        drawStone(state)
-        updateDb(state)
+        omokGameService.updateDb(state, preColor)
         goToResultActivity(state.getTurn())
     }
 
@@ -152,11 +129,11 @@ class OmokGameActivity : AppCompatActivity(), Observer {
     }
 
     private fun CoordinateState.toName(): String {
-        return if (this == BLACK) room.firstUserEntity.userName else room.secondUserEntity.userName
+        return if (this == BLACK) omokGameService.firstUserName else omokGameService.firstUserName
     }
 
     override fun onDestroy() {
-        boardDao.closeDb()
+        omokGameService.closeDb()
         super.onDestroy()
     }
 
