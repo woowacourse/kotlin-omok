9f8c2327eb2ce559675cf8f468dcb9ff30222518
krrong <khk9664@naver.com>
2023-03-29 20:11:48 +0900

    feat(OmokGame): 이전 상태로 돌아가는 기능 구현

--- domain/src/main/kotlin/domain/State.kt
+++ domain/src/main/kotlin/domain/State.kt
@@ -10,4 +10,12 @@ enum class State {
             EMPTY -> EMPTY
         }
     }
+
+    fun prevState(): State {
+        return when (this) {
+            WHITE -> BLACK
+            BLACK -> WHITE
+            EMPTY -> EMPTY
+        }
+    }
 }
