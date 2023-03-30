86554abfc97fd6b9111028b5228ec0b36e38d195
ki960213 <ki960213@github.com>
2023-03-30 17:32:06 +0900

    test(GameState): GameState에 대한 테스트 코드 생성

--- domain/src/main/kotlin/domain/state/Finished.kt
+++ domain/src/main/kotlin/domain/state/Finished.kt
@@ -6,6 +6,11 @@ import domain.stone.Stone
 import domain.Team
 
 class Finished(override val turn: Team, override val board: Board) : GameState() {
+
+    init {
+        require(board.hasTeamThatCompletedOmok()) { NOT_COMPLETED_OMOK_ERROR }
+    }
+
     override fun placeStoneOnBoard(stone: Stone): GameState {
         throw IllegalStateException("게임이 끝났다면 돌을 둘 수 없습니다.")
     }
@@ -13,4 +18,8 @@ class Finished(override val turn: Team, override val board: Board) : GameState()
     override fun canPlace(stone: Stone): Boolean = false
 
     override fun getLastPoint(): Point? = board.getLastPoint(turn)
+
+    companion object {
+        private const val NOT_COMPLETED_OMOK_ERROR = "오목이 완성되지 않았다면 게임 종료 상태가 될 수 없습니다."
+    }
 }
