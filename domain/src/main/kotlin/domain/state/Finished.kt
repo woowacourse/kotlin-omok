782e570a3759bbe2270a54196b9a717da767100c
ki960213 <ki960213@github.com>
2023-03-30 17:34:54 +0900

    refactor(Finished): 에러 메세지 상수화

--- domain/src/main/kotlin/domain/state/Finished.kt
+++ domain/src/main/kotlin/domain/state/Finished.kt
@@ -12,7 +12,7 @@ class Finished(override val turn: Team, override val board: Board) : GameState()
     }
 
     override fun placeStoneOnBoard(stone: Stone): GameState {
-        throw IllegalStateException("게임이 끝났다면 돌을 둘 수 없습니다.")
+        throw IllegalStateException(STONE_CANT_BE_PLACED_ERROR)
     }
 
     override fun canPlace(stone: Stone): Boolean = false
@@ -20,6 +20,7 @@ class Finished(override val turn: Team, override val board: Board) : GameState()
     override fun getLastPoint(): Point? = board.getLastPoint(turn)
 
     companion object {
+        private const val STONE_CANT_BE_PLACED_ERROR = "게임이 끝났다면 돌을 둘 수 없습니다."
         private const val NOT_COMPLETED_OMOK_ERROR = "오목이 완성되지 않았다면 게임 종료 상태가 될 수 없습니다."
     }
 }
