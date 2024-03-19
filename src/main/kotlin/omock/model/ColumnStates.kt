68129f5bb6a505cbb4ea15c8c9ff2604ff565b0e
김진우 <jinwoo3661@naver.com>
2024-03-19 21:54:09 +0900

    feat: ColumnStates 생성
    Co-authored-by: junjange <wnswkd486@gmail.com>

---
+++ src/main/kotlin/omock/model/ColumnStates.kt
@@ -0,0 +1,14 @@
+package omock.model
+
+class ColumnStates(
+    private val columnStates: MutableList<StoneState>
+){
+
+    fun change(column: Int, player: Player){
+        columnStates[column] = getStoneState(column).put(player)
+    }
+
+    fun getStoneState(column: Int):StoneState{
+        return columnStates[column]
+    }
+}
