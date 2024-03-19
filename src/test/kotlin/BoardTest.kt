1a0106edfb61b742c6c40dd0616a8e974ce6065b
aprilgom <aprilgom@gmail.com>
2024-03-19 14:45:43 +0900

    feat(StoneColor): 보드의 놓는 돌 색 구분

--- src/test/kotlin/BoardTest.kt
+++ src/test/kotlin/BoardTest.kt
@@ -1,3 +1,7 @@
+import omok.Board
+import omok.Point
+import omok.StoneColor
+import omok.turn.BlackTurn
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -5,18 +9,25 @@ import org.junit.jupiter.api.assertThrows
 class BoardTest {
     @Test
     fun `보드에 돌을 놓는다`() {
-        val stone = Stone(1, 2)
+        val point = Point(1, 2)
         val board = Board()
-        board.place(stone)
-        val actual = board.contains(stone)
+        board.place(point, StoneColor.BLACK)
+        val actual = board.contains(point)
         Assertions.assertThat(actual).isTrue()
     }
 
     @Test
     fun `Stone이 좌표 범위 밖을 벗어났을 경우 예외를 표기한다`() {
-        val stone = Stone(-1, -1)
+        val point = Point(-1, -1)
         val board = Board()
-        val actual = assertThrows<IllegalArgumentException> { board.place(stone) }.message
+        val actual = assertThrows<IllegalArgumentException> { board.place(point, StoneColor.WHITE) }.message
         Assertions.assertThat(actual).isEqualTo("보드 밖에 돌을 두었습니다.")
+    }
+
+    @Test
+    fun `특정 좌표에 흑돌을 놓는 기능`() {
+        val board = Board()
+        val blackTurn = BlackTurn(board)
+        blackTurn.placeStone(Point(1, 1))
     }
 }
