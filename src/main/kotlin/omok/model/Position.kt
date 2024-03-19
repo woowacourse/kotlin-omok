18626638cd2c4352da480f68954e8ab03ca9d971
sh1mj1 <7wlgns@gmail.com>
2024-03-19 15:41:38 +0900

    feat(Position): 위치는 행과 열을 가진다
    - 0~14 사이여야 한다

---
+++ src/main/kotlin/omok/model/Position.kt
@@ -0,0 +1,13 @@
+package omok.model
+
+data class Position(val row: Int, val col: Int) {
+    init {
+        require(row in INDEX_RANGE && col in INDEX_RANGE) { "올바르지 않은 위치입니다." }
+    }
+
+    companion object {
+        private const val MIN_INDEX = 0
+        private const val MAX_INDEX = 14
+        private val INDEX_RANGE = MIN_INDEX..MAX_INDEX
+    }
+}
