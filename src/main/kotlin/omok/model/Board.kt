31284486fd5227e870873b2e1229d4f3e76a440c
kkosang <eiwoqp77@gmail.com>
2024-03-19 17:17:11 +0900

    feat: 오목판 위에 돌을 놓는 기능 구현

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -8,4 +8,18 @@ class Board(stones: List<Stone>) {
     fun checkOccupiedCoordinate(coordinate: Coordinate): Boolean {
         return stones.none { it.coordinate == coordinate }
     }
+
+    fun putStoneOnBoard(
+        possibility: Boolean,
+        stone: Stone,
+    ) {
+        when (possibility) {
+            true -> _stones.add(stone)
+            false -> throw IllegalStateException(ERROR_CANT_PUT_STONE)
+        }
+    }
+
+    companion object {
+        const val ERROR_CANT_PUT_STONE = "이미 돌이 착수된 위치입니다."
+    }
 }
