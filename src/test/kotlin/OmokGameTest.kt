90138e463a6463d81f0a7db5cbde6cef857bc8bc
aprilgom <aprilgom@gmail.com>
2024-03-19 15:48:53 +0900

    feat(Board): 돌이 5개 연속으로 놓여져 있을 때 오목체크 기능 구현

--- src/test/kotlin/OmokGameTest.kt
+++ src/test/kotlin/OmokGameTest.kt
@@ -14,4 +14,50 @@ class OmokGameTest {
         blackTurn.placeStone(point)
         assertThat(board.map[point]).isEqualTo(StoneColor.BLACK)
     }
+
+    @Test
+    fun `가로로 같은 색 돌이 5개 연속으로 있을 경우 오목이다`() {
+        val board = Board()
+        board.map[Point(1, 1)] = StoneColor.BLACK
+        board.map[Point(2, 1)] = StoneColor.BLACK
+        board.map[Point(3, 1)] = StoneColor.BLACK
+        board.map[Point(4, 1)] = StoneColor.BLACK
+        board.map[Point(5, 1)] = StoneColor.BLACK
+        val actual = board.startCheckOmok(StoneColor.BLACK)
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `왼쪽 위에서 오른쪽 아래 대각선으로 같은 색 돌이 5개 연속으로 있을 경우 오목이다`() {
+        val board = Board()
+        board.map[Point(1, 1)] = StoneColor.BLACK
+        board.map[Point(2, 2)] = StoneColor.BLACK
+        board.map[Point(3, 3)] = StoneColor.BLACK
+        board.map[Point(4, 4)] = StoneColor.BLACK
+        board.map[Point(5, 5)] = StoneColor.BLACK
+        val actual = board.startCheckOmok(StoneColor.BLACK)
+        assertThat(actual).isTrue()
+    }
+    @Test
+    fun `세로로 같은 색 돌이 5개 연속으로 있을 경우 오목이다`() {
+        val board = Board()
+        board.map[Point(1, 1)] = StoneColor.BLACK
+        board.map[Point(1, 2)] = StoneColor.BLACK
+        board.map[Point(1, 3)] = StoneColor.BLACK
+        board.map[Point(1, 4)] = StoneColor.BLACK
+        board.map[Point(1, 5)] = StoneColor.BLACK
+        val actual = board.startCheckOmok(StoneColor.BLACK)
+        assertThat(actual).isTrue()
+    }
+    @Test
+    fun `오른쪽 위에서 왼쪽 아래 대각선으로 같은 색 돌이 5개 연속으로 있을 경우 오목이다`() {
+        val board = Board()
+        board.map[Point(5, 1)] = StoneColor.BLACK
+        board.map[Point(4, 2)] = StoneColor.BLACK
+        board.map[Point(3, 3)] = StoneColor.BLACK
+        board.map[Point(2, 4)] = StoneColor.BLACK
+        board.map[Point(1, 5)] = StoneColor.BLACK
+        val actual = board.startCheckOmok(StoneColor.BLACK)
+        assertThat(actual).isTrue()
+    }
 }
