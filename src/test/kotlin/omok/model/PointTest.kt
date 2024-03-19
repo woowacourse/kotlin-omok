e17201f531444cabbd03d8235b2b085aafa8ae9c
Hogu59 <dbstjdguswkd@naver.com>
2024-03-19 21:03:24 +0900

    rename(Point): rename coordinate to point

--- src/test/kotlin/omok/model/PointTest.kt
+++ src/test/kotlin/omok/model/PointTest.kt
@@ -4,20 +4,16 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class CoordinateTest {
+class PointTest {
     @ParameterizedTest
     @ValueSource(ints = [0, 16, -5, 280])
     fun `열의 위치가 A ~ O 사이가 아니라면 예외가 발생한다`(col: Int) {
         assertThrows<IllegalArgumentException> {
-            Coordinate("A", col)
+            Point(1, pointValue)
         }
-    }
 
-    @ParameterizedTest
-    @ValueSource(strings = ["adsklfalkdf", "Z", "😀", "", "a", "AB"])
-    fun `행의 위치가 1 ~ 15 사이가 아니라면 예외가 발생한다`(row: String) {
         assertThrows<IllegalArgumentException> {
-            Coordinate(row, 1)
+            Point(pointValue, 1)
         }
     }
 }
