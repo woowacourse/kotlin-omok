b70ee9d93b023a82cf7eef407beb7020cfb34d26
aprilgom <aprilgom@gmail.com>
2024-03-19 15:18:27 +0900

    feat(Board): 이전에 둔 좌표를 불러오는 기능 구현

--- src/main/kotlin/omok/Board.kt
+++ src/main/kotlin/omok/Board.kt
@@ -7,7 +7,12 @@ class Board {
             point.x in 1..15 &&
                 point.y in 1..15,
         ) { "보드 밖에 돌을 두었습니다." }
+        require(map.contains(point).not()) { "그 포인트에 이미 돌이 존재합니다." }
         map[point] = color
+    }
+
+    fun previousPoint(): Point {
+        return map.keys.lastOrNull() ?: throw NoSuchElementException("첫 돌이기에 이전 이력이 없습니다.")
     }
 
     fun contains(point: Point): Boolean {
