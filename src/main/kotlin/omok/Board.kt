b8f7bb90d082a6f93043de32e62905fe4d203653
aprilgom <aprilgom@gmail.com>
2024-03-19 13:56:31 +0900

    chore: omok 패키지 만듦

--- src/main/kotlin/omok/Board.kt
+++ src/main/kotlin/omok/Board.kt
@@ -1,9 +1,11 @@
+package omok
+
 class Board {
     private val map: MutableMap<Stone, Boolean> = mutableMapOf()
     fun place(stone: Stone) {
         require(
             stone.x in 1..15 &&
-                    stone.y in 1..15,
+                stone.y in 1..15,
         ) { "보드 밖에 돌을 두었습니다." }
         map[stone] = true
     }
