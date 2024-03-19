fe39e175639c696ed275e23dcde8405b8badad85
kkosang <eiwoqp77@gmail.com>
2024-03-19 22:06:40 +0900

    refactor: 돌들의 정보 Stones를 가지는 Board의 리팩터링

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -1,25 +1,7 @@
 package omok.model
 
-class Board(stones: List<Stone>) {
-    private var _stones: MutableList<Stone> = stones.toMutableList()
-    val stones: List<Stone>
-        get() = _stones
-
-    fun checkOccupiedCoordinate(coordinate: Coordinate): Boolean {
-        return stones.none { it.coordinate == coordinate }
-    }
-
-    fun putStoneOnBoard(
-        possibility: Boolean,
-        stone: Stone,
-    ) {
-        when (possibility) {
-            true -> _stones.add(stone)
-            false -> throw IllegalStateException(ERROR_CANT_PUT_STONE)
-        }
-    }
-
-    companion object {
-        const val ERROR_CANT_PUT_STONE = "이미 돌이 착수된 위치입니다."
+class Board(val stones: Stones) {
+    fun putStone(stone: Stone) {
+        stones.putStone(stone)
     }
 }
--- src/test/kotlin/omok/model/BoardTest.kt
+++ src/test/kotlin/omok/model/BoardTest.kt
@@ -3,63 +3,24 @@ package omok.model
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class BoardTest {
     private lateinit var board: Board
-    private lateinit var ableCoordinate: Coordinate
-    private lateinit var unableCoordinate: Coordinate
+    private lateinit var blackStone: Stone
+    private lateinit var whiteStone: Stone
+    private lateinit var coordinate: Coordinate
 
     @BeforeEach
     fun setUp() {
-        board =
-            Board(
-                listOf(
-                    Stone("black", Coordinate(5, "A")),
-                    Stone("white", Coordinate(8, "H")),
-                ),
-            )
-
-        ableCoordinate = Coordinate(6, "C")
-        unableCoordinate = Coordinate(5, "A")
+        board = Board(Stones())
+        coordinate = Coordinate(6, "C")
+        blackStone = Stone("black", coordinate)
+        whiteStone = Stone("white", coordinate)
     }
 
     @Test
-    fun `오목판은 판 위에 놓인 돌을 가지고 있다`() {
-        assertThat(board.stones.size).isEqualTo(2)
-    }
-
-    @Test
-    fun `입력받은 좌표가 이미 오목판 위에 돌이 놓인 좌표라면 false를 반환한다`() {
-        val actual = board.checkOccupiedCoordinate(unableCoordinate)
-
-        assertThat(actual).isFalse()
-    }
-
-    @Test
-    fun `입력받은 좌표가 오목판 위에 돌이 없는 좌표라면 true를 반환한다`() {
-        val actual = board.checkOccupiedCoordinate(ableCoordinate)
-
-        assertThat(actual).isTrue()
-    }
-
-    @Test
-    fun `오목판에 돌을 착수할 수 있다`() {
-        val stone = Stone("white", ableCoordinate)
-        val possibility = board.checkOccupiedCoordinate(ableCoordinate)
-        val beforeSize = board.stones.size
-
-        board.putStoneOnBoard(possibility, stone)
-        assertThat(board.stones.size).isGreaterThan(beforeSize)
-    }
-
-    @Test
-    fun `오목판에 돌을 착수할 수 없으면 에러를 던져야한다`() {
-        val stone = Stone("white", unableCoordinate)
-        val possibility = board.checkOccupiedCoordinate(unableCoordinate)
-
-        assertThrows<IllegalStateException> {
-            board.putStoneOnBoard(possibility, stone)
-        }
+    fun `오목판은 착수된 돌을 가지고 있다`() {
+        board.putStone(blackStone)
+        assertThat(board.stones.blackStones.stones).contains(blackStone)
     }
 }
