46a08d5ba8080f05e5e4576b0a164c754cd871d9
kkosang <eiwoqp77@gmail.com>
2024-03-19 16:04:16 +0900

    test: Board 테스트 코드 작성

---
+++ src/test/kotlin/omok/model/BoardTest.kt
@@ -0,0 +1,20 @@
+package omok.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class BoardTest {
+    class Board(val stones: List<Stone>)
+
+    @Test
+    fun `오목판은 판 위에 놓인 돌을 가지고 있다`() {
+        val board = Board(
+            listOf(
+                Stone("black", Coordinate(5, "A")),
+                Stone("white", Coordinate(8, "H")),
+            )
+        )
+
+        assertThat(board.stones.size).isEqualTo(2)
+    }
+}
