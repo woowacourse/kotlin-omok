64b830c5b446663c359c9dc545eeccd6b1c6d163
aprilgom <aprilgom@gmail.com>
2024-03-19 16:41:45 +0900

    feat(OutputView): 게임 시작 메시지 출력 기능 구현

---
+++ src/main/kotlin/omok/view/output/ConsoleOutputView.kt
@@ -0,0 +1,7 @@
+package omok.view.output
+
+class ConsoleOutputView : OutputView {
+    override fun printStartGuide() {
+        println("오목 게임을 시작합니다")
+    }
+}
---
+++ src/main/kotlin/omok/view/output/OutputView.kt
@@ -0,0 +1,5 @@
+package omok.view.output
+
+interface OutputView {
+    fun printStartGuide()
+}
