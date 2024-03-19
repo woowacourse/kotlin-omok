7c54a480f2f80a50f4035cdb26d368f6f47a74ba
JoYehyun <jo990714@naver.com>
2024-03-19 22:34:32 +0900

    feat: 흑,백 순서대로 게임 진행 로직 구현
    Co-authored-by: dpcks0509 <dpcks0509@gmail.com>

--- src/main/kotlin/omok/controller/Controller.kt
+++ src/main/kotlin/omok/controller/Controller.kt
@@ -1,6 +1,9 @@
 package omok.controller
 
+import omok.model.BlackTurn
 import omok.model.Board
+import omok.model.FinishedTurn
+import omok.model.Turn
 import omok.view.InputView
 import omok.view.OutputView
 
@@ -8,6 +11,12 @@ class Controller {
     fun play() {
         val board = Board()
         OutputView.printBoard(board)
-        val point = InputView.readPoint()
+        var turn: Turn = BlackTurn()
+
+        while (turn !is FinishedTurn) {
+            val point = InputView.readPoint()
+            turn = turn.putStone(point, board)
+            OutputView.printBoard(board)
+        }
     }
 }
