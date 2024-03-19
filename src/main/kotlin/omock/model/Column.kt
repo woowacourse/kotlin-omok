af47f9184224adf30c214f8c73ff297a59df3b94
김진우 <jinwoo3661@naver.com>
2024-03-19 21:54:42 +0900

    feat: 인덱스 값 가지는 기능 구현
    Co-authored-by: junjange <wnswkd486@gmail.com>

--- src/main/kotlin/omock/model/Column.kt
+++ src/main/kotlin/omock/model/Column.kt
@@ -1,16 +1,21 @@
 package omock.model
 
 @JvmInline
-    value class Column(val comma: String) {
-        init {
-            require(comma in COLUM_RANGE) {
-                ERROR_ROW_RANGE
-            }
-        }
-        companion object {
-            private const val MIN_COLUMN = 'A'
-            private const val MAX_COLUMN = 'O'
-            val COLUM_RANGE = (MIN_COLUMN..MAX_COLUMN).map { it.toString() }
-            private const val ERROR_ROW_RANGE = "Row는 ${MIN_COLUMN}~${MAX_COLUMN} 사이여야 합니다."
+value class Column(val comma: String) {
+    init {
+        require(comma in COLUM_RANGE) {
+            ERROR_ROW_RANGE
         }
     }
+
+    fun getIndex(): Int {
+        return comma[0].code - MIN_COLUMN.code
+    }
+
+    companion object {
+        private const val MIN_COLUMN = 'A'
+        private const val MAX_COLUMN = 'O'
+        val COLUM_RANGE = (MIN_COLUMN..MAX_COLUMN).map { it.toString() }
+        private const val ERROR_ROW_RANGE = "Row는 ${MIN_COLUMN}~${MAX_COLUMN} 사이여야 합니다."
+    }
+}
--- src/main/kotlin/omock/model/Row.kt
+++ src/main/kotlin/omock/model/Row.kt
@@ -6,15 +6,19 @@ value class Row(val comma: String) {
         require(comma.toIntOrNull() != null) {
             ERROR_COLUMN_TYPE
         }
-        require(comma in COLUM_RANGE) {
+        require(comma in ROW_RANGE) {
             ERROR_COLUMN_RANGE
         }
+    }
+
+    fun getIndex(): Int {
+        return comma.toInt() - 1
     }
 
     companion object {
         private const val MIN_COLUMN = 1
         private const val MAX_COLUMN = 15
-        val COLUM_RANGE = (MIN_COLUMN..MAX_COLUMN).map { it.toString() }
+        val ROW_RANGE = (MAX_COLUMN downTo MIN_COLUMN).map { it.toString() }
         private const val ERROR_COLUMN_TYPE = "Column은 정수형이어야 합니다."
         private const val ERROR_COLUMN_RANGE = "Column은 ${MIN_COLUMN}~${MAX_COLUMN} 사이여야 합니다."
     }
