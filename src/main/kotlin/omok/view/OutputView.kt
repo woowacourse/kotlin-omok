52f114672dbe5fb3b7ccd91473573c7a81df5a5c
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-03-19 15:35:47 +0900

    feat(OutputView): 게임 시작 문구 출력

---
+++ src/main/kotlin/omok/view/OutputView.kt
@@ -0,0 +1,9 @@
+package omok.view
+
+object OutputView {
+    private const val HEADER_START_MESSAGE = "오목 게임을 시작합니다."
+
+    fun printStartHeader() {
+        println(HEADER_START_MESSAGE)
+    }
+}
