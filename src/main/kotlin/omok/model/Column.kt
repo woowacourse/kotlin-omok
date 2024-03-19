e018af1aea2246f256bd761943a56e9fa035681f
Gimun Kim <kmkim7575@gmail.com>
2024-03-19 16:32:35 +0900

    feat: 입력받은 포지션을 enum형태로 표현하고 변환하는 객체 구현
    Co-authored-by: songpink <jejeje04@naver.com>

---
+++ src/main/kotlin/omok/model/Column.kt
@@ -0,0 +1,27 @@
+package omok.model
+
+enum class Column(val title: Char, val value: Int) {
+    A('A', 1),
+    B('B', 2),
+    C('C', 3),
+    D('D', 4),
+    E('E', 5),
+    F('F', 6),
+    G('G', 7),
+    H('H', 8),
+    I('I', 9),
+    J('J', 10),
+    K('K', 11),
+    L('L', 12),
+    M('M', 13),
+    N('N', 14),
+    O('O', 15), ;
+
+    companion object {
+        fun valueOf(col: Char): Column? {
+            return entries.find {
+                it.title == col
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/omok/model/Position.kt
@@ -0,0 +1,23 @@
+package omok.model
+
+data class Position(val row: Row, val col: Column) {
+    companion object {
+        private const val MIN_ROW = 1
+        private const val MAX_ROW = 15
+        private const val MIN_COL = 'A'
+        private const val MAX_COL = 'O'
+        private const val EXCEPTION_ROW_RANGE = "$MIN_ROW 부터 ${MAX_ROW}사이의 정수를 입력해야 합니다"
+        private const val EXCEPTION_COL_RANGE = "$MIN_COL 부터 ${MAX_COL}사이의 알파벳을 입력해야 합니다"
+
+        fun of(
+            row: Int,
+            col: Char,
+        ): Position {
+            require(row in MIN_ROW..MAX_ROW) { EXCEPTION_ROW_RANGE }
+            require(col in MIN_COL..MAX_COL) { EXCEPTION_COL_RANGE }
+            val rowValue = Row.valueOf(row) ?: throw IllegalArgumentException()
+            val colValue = Column.valueOf(col) ?: throw IllegalArgumentException()
+            return Position(rowValue, colValue)
+        }
+    }
+}
