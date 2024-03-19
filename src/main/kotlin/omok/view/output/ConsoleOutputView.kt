7c0b53892674d2a368a533ee683e7a5ac2ab1a46
aprilgom <aprilgom@gmail.com>
2024-03-19 17:29:27 +0900

    feat(ConsoleOutputView): 오목판을 출력한다.

--- src/main/kotlin/omok/view/output/ConsoleOutputView.kt
+++ src/main/kotlin/omok/view/output/ConsoleOutputView.kt
@@ -1,7 +1,45 @@
 package omok.view.output
 
+import omok.model.Board
+import omok.model.entity.StoneColor
+
 class ConsoleOutputView : OutputView {
     override fun printStartGuide() {
         println("오목 게임을 시작합니다")
+    }
+
+    override fun printOneTurn(board: Board) {
+        val strMap = buildOmokBoard(board)
+        println(strMap)
+    }
+
+    private fun buildOmokBoard(board: Board): String {
+        val strMap = """
+            15 ┌──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┐
+            14 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            13 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            12 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            11 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            10 ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            9  ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            8  ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            7  ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            6  ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            5  ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            4  ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            3  ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            2  ├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤
+            1  └──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┘
+               A  B  C  D  E  F  G  H  I  J  K  L  M  N  O
+        """.trimIndent()
+        val sb = StringBuilder(strMap)
+        board.map.forEach {
+            val stoneChar = if (it.value == StoneColor.WHITE) '○' else '●'
+            val x = (it.key.x) * 3
+            val y = 15 - it.key.y
+            val idx = 47 * y + x
+            sb.setCharAt(idx, stoneChar)
+        }
+        return sb.toString()
     }
 }
