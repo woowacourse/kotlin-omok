7ca026ca9d847c17e12e5ac6eb69a00dfa631787
Namyunsuk <yunsuk0328@naver.com>
2024-03-19 20:54:25 +0900

    refactor: 오목 판단 책임 GoStone으로 위임
    - class에서 object로 변경

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -1,60 +1,46 @@
 package omok.model
 
-class Board {
-    private val board = Array(BOARD_SIZE) { Array(BOARD_SIZE) { Stone.NONE } }
-    private val _positionInfo: MutableList<Pair<Int, Int>> = mutableListOf()
-    val positionInfo: List<Pair<Int, Int>>
-        get() = _positionInfo.toList()
+object Board {
+    private const val BOARD_SIZE = 15
+    private const val DEFAULT_STONE_COUNT = 1
+    private const val OMOK_PRECONDITION = 5
+    private val horizontalDirection = DeltaPosition(0, 1)
+    private val verticalDirection = DeltaPosition(1, 0)
+    private val upwardDirection = DeltaPosition(1, 1)
+    private val downwardDirection = DeltaPosition(1, -1)
 
-    fun putStone(
-        position: Position,
+    val board = Array(BOARD_SIZE) { Array(BOARD_SIZE) { Stone.NONE } }
+
+    fun findOmok(
+        lastPosition: Position,
         stone: Stone,
-    ) {
-        board[position.row][position.col] = stone
-    }
-
-    fun judgeWinning(
-        startPosition: Position,
-        stone: Stone,
-    ): Boolean {
-        // 이 함수에서, 주어진 위치에 실제로 player의 돌이 있는지 확인
-        if (board[startPosition.row][startPosition.col] != stone) return false
-
-        return findOmok(startPosition, stone)
-    }
-
-    private fun findOmok(
-        startPosition: Position,
-        stone: Stone,
-    ) = checkWinningPosition(startPosition, horizontalDirection, stone) || // 수평 방향
-        checkWinningPosition(startPosition, verticalDirection, stone) || // 수직 방향
-        checkWinningPosition(startPosition, upwardDirection, stone) || // 대각선 (\) 방향
-        checkWinningPosition(startPosition, downwardDirection, stone) // 대각선 (/) 방향
+    ) = checkWinningPosition(lastPosition, horizontalDirection, stone) ||
+        checkWinningPosition(lastPosition, verticalDirection, stone) ||
+        checkWinningPosition(lastPosition, upwardDirection, stone) ||
+        checkWinningPosition(lastPosition, downwardDirection, stone)
 
     private fun checkWinningPosition(
-        startPosition: Position,
+        lastPosition: Position,
         deltaPosition: DeltaPosition,
         stone: Stone,
     ): Boolean {
-        var stoneCount = DEFAULT_STONE_COUNT // 현재 돌을 포함해야 하므로 1부터 시작
+        var stoneCount = DEFAULT_STONE_COUNT
 
-        // 한 방향으로 이동하며 같은 돌을 세기
-        stoneCount += countSameStones(startPosition, deltaPosition, stone)
+        stoneCount += countSameStones(lastPosition, deltaPosition, stone)
 
-        // 반대 방향으로 이동하며 같은 돌 세기
-        stoneCount += countSameStones(startPosition, deltaPosition, stone)
+        stoneCount += countSameStones(lastPosition, -deltaPosition, stone)
 
-        return stoneCount >= OMOK_PRECONDITION // 5개 이상 연속되면 승리
+        return stoneCount >= OMOK_PRECONDITION
     }
 
     private fun countSameStones(
-        startPosition: Position,
+        lastPosition: Position,
         deltaPosition: DeltaPosition,
         stone: Stone,
     ): Int {
         var sameStoneCount = 0
-        var row = startPosition.row + deltaPosition.deltaRow
-        var col = startPosition.col + deltaPosition.deltaCol
+        var row = lastPosition.row + deltaPosition.deltaRow
+        var col = lastPosition.col + deltaPosition.deltaCol
 
         while (row in 0 until BOARD_SIZE && col in 0 until BOARD_SIZE && board[row][col] == stone) {
             sameStoneCount++
@@ -63,15 +49,5 @@ class Board {
         }
 
         return sameStoneCount
-    }
-
-    companion object {
-        const val BOARD_SIZE = 15
-        private const val DEFAULT_STONE_COUNT = 1
-        private const val OMOK_PRECONDITION = 5
-        private val horizontalDirection = DeltaPosition(0, 1)
-        private val verticalDirection = DeltaPosition(1, 0)
-        private val upwardDirection = DeltaPosition(1, 1)
-        private val downwardDirection = DeltaPosition(1, -1)
     }
 }
