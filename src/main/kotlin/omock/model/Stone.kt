061a5d1b7e0d56dd1a3aeab0c73e9b95bc7a430c
김진우 <jinwoo3661@naver.com>
2024-03-19 16:56:32 +0900

    refactor: 불필요한 원시값 포장 제거
    Co-authored-by: junjange <wnswkd486@gmail.com>

--- src/main/kotlin/omock/model/Stone.kt
+++ src/main/kotlin/omock/model/Stone.kt
@@ -1,3 +1,20 @@
 package omock.model
 
-data class Stone(val coordinate: Coordinate)
+data class Stone(
+    val row: Row, val column: Column
+) {
+    companion object {
+        private val stones = Column.COLUM_RANGE.zip(Row.COLUM_RANGE).map { (columnComma, rowComma) ->
+            Stone(
+                row = Row(rowComma),
+                column = Column(columnComma),
+            )
+        }
+
+        fun from(row: Row, column: Column): Stone {
+            return stones.find {
+                it.row == row && it.column == column
+            } ?: throw IllegalArgumentException()
+        }
+    }
+}
--- src/test/kotlin/omock/StoneTest.kt
+++ src/test/kotlin/omock/StoneTest.kt
@@ -1,18 +1,27 @@
 package omock
 
 import omock.model.Column
-import omock.model.Coordinate
-import omock.model.Row
 import omock.model.Stone
-import org.assertj.core.api.Assertions
+import omock.model.Row
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 
 class StoneTest {
     @Test
-    fun `돌은 좌표를 가지고 있다`() {
-        val coordinate = Coordinate(row = Row("1"), column = Column("A"))
-        val stone = Stone(coordinate)
-        Assertions.assertThat(stone.coordinate).isEqualTo(coordinate)
+    fun `돌은 row, column를 가지고 있다`() {
+        val stone = Stone(row = Row("1"), column = Column("A"))
+
+        assertThat(stone.row.comma).isEqualTo("1")
+        assertThat(stone.column.comma).isEqualTo("A")
+    }
+
+    @Test
+    fun `row, column을 통해 돌을 재활용 할 수 있다`() {
+        val row = Row("1")
+        val column = Column("A")
+        val actual = Stone.from(row = row, column = column)
+        assertThat(actual.row).isEqualTo(row)
+        assertThat(actual.column).isEqualTo(column)
     }
 }
