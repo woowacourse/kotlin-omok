11b2f69d5627ab25514a00579b1b9fb61561521c
aprilgom <aprilgom@gmail.com>
2024-03-19 17:58:51 +0900

    feat(ConsoleInputView): 돌 위치 입력 받는 기능 구현

---
+++ src/main/kotlin/omok/view/input/ConsoleInputView.kt
@@ -0,0 +1,15 @@
+package omok.view.input
+
+import omok.model.entity.Point
+
+class ConsoleInputView : InputView {
+    override fun getStonePoint(): Point {
+        print("위치를 입력하세요: ")
+        val input = readln()
+        val x = alphabetToInt(input[0])
+        val y = input[1].digitToInt()
+        return Point(x, y)
+    }
+
+    private fun alphabetToInt(alphabet: Char): Int = alphabet - 'A' + 1
+}
