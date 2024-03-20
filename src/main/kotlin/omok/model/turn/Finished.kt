d8b8d62dc61357adc75e5a6b225bc6826ba824e6
aprilgom <aprilgom@gmail.com>
2024-03-20 23:30:40 +0900

    refactor(Finished): color에 접근 제한자 추가

--- src/main/kotlin/omok/model/turn/Finished.kt
+++ src/main/kotlin/omok/model/turn/Finished.kt
@@ -3,7 +3,7 @@ package omok.model.turn
 import omok.model.entity.Point
 import omok.model.entity.StoneColor
 
-class Finished(val color: StoneColor) : Turn {
+class Finished(private val color: StoneColor) : Turn {
     override fun placeStone(point: Point): Turn {
         throw IllegalStateException()
     }
