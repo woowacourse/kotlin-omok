2c5c1005cc7222b4747f8ead545e0aba71e07c7c
murjune <sis9221302@gmail.com>
2024-03-20 21:33:30 +0900

    feat(Board) : 오목알 놓을 수 있는지 검증 로직

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -5,12 +5,23 @@ class Board(private val stones: Map<Position, OmokStone>) {
     operator fun plus(
         stone: OmokStone
     ): Board {
+        validate(stone.position)
         return Board(stones + (stone.position to stone))
+    }
+
+    private fun validate(position: Position) {
+        require(position.x in RANGE) { "x는 $MIN ~ $MAX 사이여야 한다" }
+        require(position.y in RANGE) { "y는 $MIN ~ $MAX 사이여야 한다" }
+    }
+
+    fun isInRange(stone: OmokStone): Boolean {
+        val position = stone.position
+        return (position.x in RANGE) || (position.y in RANGE)
     }
 
     operator fun get(position: Position): OmokStone? = stones[position]
 
-    fun last(): OmokStone = stones.entries.last().value
+    fun lastOrNull(): OmokStone? = stones.entries.lastOrNull()?.value
 
     fun isInOmok(position: Position): Boolean {
         val stone = stones[position] ?: return false
@@ -18,6 +29,8 @@ class Board(private val stones: Map<Position, OmokStone>) {
             isInOmok(stone, vector)
         }
     }
+
+    fun isEmptyPosition(stone: OmokStone): Boolean = stones.contains(stone.position).not()
 
     private fun isInOmok(stone: OmokStone, vector: Vector): Boolean {
         return sumLeft(stone, vector) + sumRight(stone, vector) >= OMOK_THRESHOLD
@@ -48,6 +61,9 @@ class Board(private val stones: Map<Position, OmokStone>) {
     }
 
     companion object {
+        private const val MIN = 1
+        private const val MAX = 15
+        private val RANGE = 1..15
         private const val INITIAL_COUNT = 0
         private const val OMOK_THRESHOLD = 4
         private val OMOK_CANDIDATE_RANGE = 0..3
