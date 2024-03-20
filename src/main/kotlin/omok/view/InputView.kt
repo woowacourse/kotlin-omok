977c2079df7ae7a099adb74642cbd62aacc56b05
kkosang <eiwoqp77@gmail.com>
2024-03-20 16:04:17 +0900

    feat: 사용자에게 착수 위치 입력 받아 위치를 반환하는 기능 구현

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -1,11 +1,29 @@
 package omok.view
 
+import omok.model.Column
+import omok.model.Coordinate
+import omok.model.Row
+
 object InputView {
     private const val MESSAGE_INPUT_COORDINATE = "위치를 입력하세요: "
 
-    fun inputStoneCoordinate(): String {
+    fun inputStoneCoordinate(): Coordinate {
         println(MESSAGE_INPUT_COORDINATE)
         val input = readlnOrNull().orEmpty()
-        return input
+        val result = input.validateCoordinate()
+
+        return result.getOrElse { throwable ->
+            println("${throwable.message}")
+            inputStoneCoordinate()
+        }
+    }
+
+    fun String.validateCoordinate(): Result<Coordinate> {
+        return runCatching {
+            Coordinate(
+                Row.from(this.substring(1)),
+                Column.from(this.substring(0, 1)),
+            )
+        }
     }
 }
