c8a41e4da227b9f775b79b08ecffb46c13df8d3f
Gimun Kim <kmkim7575@gmail.com>
2024-03-20 18:46:49 +0900

    refactor: 오목 판에 돌을 추가하는 로직 수정
    Co-authored-by: songpink <jejeje04@naver.com>

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -3,19 +3,53 @@ package omok.model
 class Board(
     stones: List<Stone> = emptyList(),
 ) {
-    private val positions = stones.map { it.position }
-    var stones = stones
+    private val status =
+        Array(BOARD_SIZE) {
+            Array<Color?>(BOARD_SIZE) { null }
+        }
+
+    var stones: List<Stone> = stones.toList()
         private set
 
-    fun place(position: Position) {
-        require(position !in positions) { EXCEPTION_DUPLICATED_POSITION }
+    fun place(position: Position): Array<Array<Color?>> {
+        require(status[position.row.value - 1][position.col.value - 1] == null) {
+            EXCEPTION_DUPLICATED_POSITION
+        }
         val stonesCount = stones.size
-        if (isEven(stonesCount)) {
-            stones = stones.plus(Stone.Black(position))
+        val row = BOARD_SIZE - position.row.value
+        val col = position.col.value - 1
+        when (isEven(stonesCount)) {
+            true -> {
+                addStone(row, position.col.title - 1, Color.BLACK)
+                dfs(row, col, Color.BLACK)
+            }
+            false -> {
+                addStone(row, position.col.title - 1, Color.WHITE)
+                dfs(row, col, Color.WHITE)
+            }
         }
-        if (!isEven(stonesCount)) {
-            stones = stones.plus(Stone.White(position))
-        }
+
+        return status
+    }
+
+    private fun addStone(
+        row: Int,
+        col: Char,
+        color: Color,
+    ) {
+        status[row][Column.valueOf(col)?.value ?: return] = color
+        stones = stones.plus(Stone.Black(Position.of(row, col)))
+    }
+
+    private fun dfs(
+        row: Int,
+        col: Int,
+        color: Color,
+    ) {
+        VerticalDfs(status).apply { search(color, row, col) }
+        HorizontalDfs(status).apply { search(color, row, col) }
+        AscendingDfs(status).apply { search(color, row, col) }
+        DescendingDfs(status).apply { search(color, row, col) }
     }
 
     private fun isEven(num: Int): Boolean {
@@ -25,5 +59,27 @@ class Board(
     companion object {
         private const val EXCEPTION_DUPLICATED_POSITION = "중복된 곳에 착수할 수 없습니다."
         private const val ODD_EVEN_INDICATOR = 2
+        private const val BOARD_SIZE = 15
     }
 }
+
+// fun main() {
+//    val board = Board()
+//    board.place(Position(Row.ONE, Column.E))
+//    board.place(Position(Row.ONE, Column.N))
+//    board.place(Position(Row.TWO, Column.E))
+//    board.place(Position(Row.TWO, Column.N))
+//    board.place(Position(Row.THREE, Column.E))
+//    board.place(Position(Row.FIVE, Column.N))
+//    board.place(Position(Row.FOUR, Column.E))
+//    board.place(Position(Row.TEN, Column.N))
+//    val currentStatus = board.place(Position(Row.FIVE, Column.E))
+//
+//    currentStatus.forEach {
+//        it.forEach { stone ->
+//            print(stone)
+//            print(" ")
+//        }
+//        println()
+//    }
+// }
