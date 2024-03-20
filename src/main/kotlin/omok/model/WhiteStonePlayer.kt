c66903994ac7c69577ebce5fa29f56c124dd8316
Hogu59 <dbstjdguswkd@naver.com>
2024-03-20 14:49:01 +0900

    feat(Player): Implement Player abstract class - Inherit common Player property and methods. - The BlackStonePlayer and WhiteStonePlayers are made by inheriting Player abstract class.

--- src/main/kotlin/omok/model/WhiteStonePlayer.kt
+++ src/main/kotlin/omok/model/WhiteStonePlayer.kt
@@ -1,6 +1,6 @@
 package omok.model
 
-class BlackStonePlayer() : Player() {
+class WhiteStonePlayer : Player() {
     override fun checkContinuity(stone: Stone): Boolean {
         val directions =
             arrayOf(
@@ -12,12 +12,13 @@ class BlackStonePlayer() : Player() {
 
         directions.forEach { direction ->
             var count = 1
+            // 정방향 탐색
             count += countStones(stone.point, direction)
+            // 역방향 탐색
             count += countStones(stone.point, Point(-direction.row, -direction.col))
 
-            if (count == 5) return true
+            if (count >= 5) return true // 연속된 돌이 5개 이상이면 true 반환
         }
-
-        return false
+        return false // 연속된 돌이 5개 미만이면 false 반환
     }
 }
