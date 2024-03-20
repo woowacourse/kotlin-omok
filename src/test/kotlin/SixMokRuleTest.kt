1bf84c3dc18271aab8026f8acf75816b6c20851c
aprilgom <aprilgom@gmail.com>
2024-03-20 16:38:19 +0900

    feat(SixMokRule): 흑이 육목을 두지 못하는 규칙 구현

---
+++ src/test/kotlin/SixMokRuleTest.kt
@@ -0,0 +1,60 @@
+import omok.model.Board
+import omok.model.entity.StoneColor
+import omok.model.rule.SixMokRule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class SixMokRuleTest {
+
+    @Test
+    fun `가로로 같은 색 돌이 6개 연속으로 있을 경우 육목이다`() {
+        val board = Board()
+        board.place(1, 1)
+        board.place(2, 1)
+        board.place(3, 1)
+        board.place(4, 1)
+        board.place(5, 1)
+        board.place(6, 1)
+        val actual = SixMokRule.check(board, StoneColor.BLACK)
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `왼쪽 위에서 오른쪽 아래 대각선으로 같은 색 돌이 6개 연속으로 있을 경우 육목이다`() {
+        val board = Board()
+        board.place(1, 1)
+        board.place(2, 2)
+        board.place(3, 3)
+        board.place(4, 4)
+        board.place(5, 5)
+        board.place(6, 6)
+        val actual = SixMokRule.check(board, StoneColor.BLACK)
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `세로로 같은 색 돌이 6개 연속으로 있을 경우 육목이다`() {
+        val board = Board()
+        board.place(1, 1)
+        board.place(1, 2)
+        board.place(1, 3)
+        board.place(1, 4)
+        board.place(1, 5)
+        board.place(1, 6)
+        val actual = SixMokRule.check(board, StoneColor.BLACK)
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `오른쪽 위에서 왼쪽 아래 대각선으로 같은 색 돌이 6개 연속으로 있을 경우 육목이다`() {
+        val board = Board()
+        board.place(6, 1)
+        board.place(5, 2)
+        board.place(4, 3)
+        board.place(3, 4)
+        board.place(2, 5)
+        board.place(1, 6)
+        val actual = SixMokRule.check(board, StoneColor.BLACK)
+        assertThat(actual).isTrue()
+    }
+}
