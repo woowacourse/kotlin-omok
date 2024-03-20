56ee4ba7fceb32a679dd6b6c1685e136e7162e05
aprilgom <aprilgom@gmail.com>
2024-03-20 15:51:24 +0900

    feat(SamSamRule): 흑이 33일때 돌을 놓지 못하는 규칙 구현

--- src/main/kotlin/omok/model/turn/BlackTurn.kt
+++ src/main/kotlin/omok/model/turn/BlackTurn.kt
@@ -4,12 +4,18 @@ import omok.model.Board
 import omok.model.entity.Point
 import omok.model.entity.StoneColor
 import omok.model.rule.OmokRule
+import omok.model.rule.SamSamRule
 
 class BlackTurn(val board: Board) : Turn {
     override fun placeStone(point: Point): Turn {
         board.place(point, StoneColor.BLACK)
 
-        if (board.isFull() || OmokRule().check(board, StoneColor.BLACK, point)) return Finished(StoneColor.BLACK)
+        if (SamSamRule().check(board, StoneColor.BLACK)) {
+            board.removePoint(point)
+            return BlackTurn(board)
+        }
+
+        if (board.isFull() || OmokRule().check(board, StoneColor.BLACK)) return Finished(StoneColor.BLACK)
 
         return WhiteTurn(board)
     }
