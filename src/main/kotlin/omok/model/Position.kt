f201898495cf321e56fa819e81f5fe62849e5339
sh1mj1 <7wlgns@gmail.com>
2024-03-20 19:25:04 +0900

    refactor(Position): 함수 분리한다
    - move() 함수는 nullable 타입을 리턴한다
    - null 은 보드판을 벗어나는 경우를 의미한다

--- src/main/kotlin/omok/model/Position.kt
+++ src/main/kotlin/omok/model/Position.kt
@@ -2,10 +2,15 @@ package omok.model
 
 data class Position(val row: Int, val col: Int) {
     init {
-        require(row in INDEX_RANGE && col in INDEX_RANGE) { "올바르지 않은 위치입니다." }
+        require(isInBoard(row, col)) { "올바르지 않은 위치입니다." }
     }
 
-    fun move(direction: Direction) = Position(row + direction.row, col + direction.col)
+    fun move(direction: Direction): Position? {
+        if (!isInBoard(row + direction.row, col + direction.col)) return null
+        return Position(row + direction.row, col + direction.col)
+    }
+
+    private fun isInBoard(row: Int, col: Int): Boolean = row in INDEX_RANGE && col in INDEX_RANGE
 
     companion object {
         const val MIN_INDEX = 0
