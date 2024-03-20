f2337e6dc866315251e46473e7c90377bfd8eb7c
aprilgom <aprilgom@gmail.com>
2024-03-20 09:56:22 +0900

    fix(OmomRule): 버그 수정

--- src/main/kotlin/omok/model/rule/OmokRule.kt
+++ src/main/kotlin/omok/model/rule/OmokRule.kt
@@ -5,42 +5,52 @@ import omok.model.entity.Point
 import omok.model.entity.StoneColor
 
 class OmokRule : Rule {
-    private val visited: MutableMap<Point, Boolean> = mutableMapOf()
 
     override fun check(
         board: Board,
         color: StoneColor,
     ): Boolean {
         val lastPoint = board.previousPoint() ?: throw IllegalStateException()
-        return checkOmok(lastPoint, color, 1, board)
+        return checkOmok(lastPoint, color, board)
     }
 
     private fun checkOmok(
         point: Point,
         color: StoneColor,
-        omokCount: Int,
         board: Board,
     ): Boolean {
-        if (visited[point] == true) return false
+        val directions =
+            listOf(
+                1 to 0, // 가로
+                0 to 1, // 세로
+                1 to 1, // 오른쪽 위에서 왼쪽 아래
+                -1 to 1, // 왼쪽 위에서 오른쪽 아래
+            )
+        return directions.any { direction ->
+            val (vecX, vecY) = direction
+            (0..4).any {
+                val left = checkStoneCountOnDirection(-vecX, -vecY, point, board, color, 0, it)
+                val right = checkStoneCountOnDirection(vecX, vecY, point, board, color, 0, 4 - it)
+                left && right
+            }
+        }
+    }
+
+    private tailrec fun checkStoneCountOnDirection(
+        vecX: Int,
+        vecY: Int,
+        point: Point,
+        board: Board,
+        color: StoneColor,
+        omokCount: Int,
+        targetOmokCount: Int,
+    ): Boolean {
+        if (targetOmokCount == 0) return true
         if (board.board.contains(point).not()) return false
         if (board.board[point] != color) return false
-        if (omokCount == 5) return true
-        visited[point] = true
-        val x = point.x
-        val y = point.y
-        val targetList =
-            listOf(
-                Point(x - 1, y - 1),
-                Point(x, y - 1),
-                Point(x + 1, y - 1),
-                Point(x - 1, y),
-                Point(x + 1, y),
-                Point(x + 1, y + 1),
-                Point(x, y + 1),
-                Point(x - 1, y + 1),
-            )
-        return targetList.any {
-            checkOmok(it, color, omokCount + 1, board)
-        }
+        if (omokCount == targetOmokCount) return true
+
+        val newPoint = Point(point.x + vecX, point.y + vecY)
+        return checkStoneCountOnDirection(vecX, vecY, newPoint, board, color, omokCount + 1, targetOmokCount)
     }
 }
