64ef6cf8a2352b1ed14c8506046c41232651a00a
Gimun Kim <kmkim7575@gmail.com>
2024-03-20 22:01:17 +0900

    refactor: 현재 턴을 설정하기 위한 매개변수명 변경
    Co-authored-by: songpink <jejeje04@naver.com>

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -5,19 +5,21 @@ import omok.model.Position
 import omok.model.Stone
 
 class InputView {
-    fun inputPosition(stone: Stone?): Pair<Int?, Char> {
-        stone?.let {
-            println("%s의 차례입니다.".format(it.color.label))
-            showLastPosition(stone.position)
+    fun inputPosition(lastTurn: Stone?): Pair<Int, Char> {
+        lastTurn?.let {
+            val currentColor = Color.getReversedColor(it.color)
+            println("%s의 차례입니다.".format(currentColor.label))
+            showLastPosition(lastTurn.position)
         } ?: println("%s의 차례입니다.".format(Color.BLACK.label))
         print("위치를 입력하세요: ")
         val input = readln()
+        println()
         val col = input[0]
-        val row = input.slice(1 until input.length).toIntOrNull()
+        val row = input.slice(1 until input.length).toInt()
         return Pair(row, col)
     }
 
-    private fun showLastPosition(position: Position)  {
+    private fun showLastPosition(position: Position) {
         println("(마지막 돌의 위치: %s%s)".format(position.col.title, position.row.title))
     }
 }
