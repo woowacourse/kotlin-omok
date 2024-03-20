2febbbfef8cc119f01f24d17bef4138736059bc1
Namyunsuk <yunsuk0328@naver.com>
2024-03-20 21:42:17 +0900

    feat(GoStone): 금수일 시 예외처리 기능 구현

--- src/main/kotlin/omok/model/GoStone.kt
+++ src/main/kotlin/omok/model/GoStone.kt
@@ -4,6 +4,8 @@ abstract class GoStone {
     abstract val stoneType: Stone
 
     fun putStone(position: Position): Stone {
+        require(!OmokRule(Board.board).checkRule(position.row, position.col)) { "금수입니다." }
+
         require(Board.board[position.col][position.row] == Stone.NONE) { "이미 놓여진 자리입니다." }
         Board.board[position.col][position.row] = stoneType
         Board.lastPosition = position
