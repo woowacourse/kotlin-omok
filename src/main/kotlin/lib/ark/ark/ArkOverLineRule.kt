315852a90cfb90ce8008a71a5ac344de6851b108
murjune <sis9221302@gmail.com>
2024-03-20 20:52:54 +0900

    add : ArkLibs

---
+++ src/main/kotlin/lib/ark/ark/ArkOverLineRule.kt
@@ -0,0 +1,17 @@
+package lib.ark.ark
+
+object ArkOverLineRule : ArkRule() {
+    override fun validate(board: List<List<Int>>, position: Pair<Int, Int>): Boolean =
+        directions.map { direction -> checkWhiteWin(board, position, direction) }.contains(true)
+
+    private fun checkWhiteWin(board: List<List<Int>>, position: Pair<Int, Int>, direction: Pair<Int, Int>): Boolean {
+        val oppositeDirection = direction.let { (dx, dy) -> Pair(-dx, -dy) }
+        val (stone1, blink1) = search(board, position, oppositeDirection)
+        val (stone2, blink2) = search(board, position, direction)
+
+        return when {
+            blink1 + blink2 == 0 && stone1 + stone2 > 4 -> true
+            else -> false
+        }
+    }
+}
