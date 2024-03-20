7f28aa3bb42f6659b2568ba6a6bf93d0406e3f9e
murjune <sis9221302@gmail.com>
2024-03-20 20:10:14 +0900

    feat : Board 구현

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -1,17 +1,61 @@
 package omok.model
 
-class Board(initialGrid: Map<Point, OmokStone>) {
-    private val _grid = initialGrid.toMutableMap()
-    val grid get() = _grid.toMap()
+class Board(private val stones: Map<Position, OmokStone>) {
 
-    fun put(
-        point: Point,
-        color: StoneColor,
-    ) {
-        _grid[point] = OmokStone(point, color)
+    operator fun plus(
+        stone: OmokStone
+    ): Board {
+        return Board(stones + (stone.position to stone))
     }
 
-    operator fun get(point: Point): OmokStone? = grid[point]
+    operator fun get(position: Position): OmokStone? = stones[position]
 
-    fun last(): OmokStone = grid.entries.last().value
+    fun last(): OmokStone = stones.entries.last().value
+
+    fun isInOmok(position: Position): Boolean {
+        val stone = stones[position] ?: return false
+        return vectors.any { vector ->
+            isInOmok(stone, vector)
+        }
+    }
+
+    private fun isInOmok(stone: OmokStone, vector: Vector): Boolean {
+        return sumLeft(stone, vector) + sumRight(stone, vector) >= OMOK_THRESHOLD
+    }
+
+    private fun sumRight(stone: OmokStone, vector: Vector): Int {
+        val (position, color) = stone
+        var now = position
+        var count = INITIAL_COUNT
+        for (i in OMOK_CANDIDATE_RANGE) {
+            now += vector
+            if (stones[now]?.color == color) count++
+            else break
+        }
+        return count
+    }
+
+    private fun sumLeft(stone: OmokStone, vector: Vector): Int {
+        val (position, color) = stone
+        var now = position
+        var count = INITIAL_COUNT
+        for (i in OMOK_CANDIDATE_RANGE) {
+            now -= vector
+            if (stones[now]?.color == color) count++
+            else break
+        }
+        return count
+    }
+
+    companion object {
+        private const val INITIAL_COUNT = 0
+        private const val OMOK_THRESHOLD = 4
+        private val OMOK_CANDIDATE_RANGE = 0..3
+        private val vectors = listOf(
+            Vector(1, 1),
+            Vector(1, -1),
+            Vector(0, 1),
+            Vector(1, 0)
+        )
+    }
 }
--- src/test/kotlin/omok/BoardTest.kt
+++ src/test/kotlin/omok/BoardTest.kt
@@ -1,11 +1,17 @@
 package omok
 
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
+import omok.fixtures.createBlackBoard
 import omok.fixtures.createBoard
 import omok.fixtures.createOmokStone
 import omok.fixtures.createPoint
+import omok.model.OmokStone
 import omok.model.StoneColor
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class BoardTest {
     @Test
@@ -31,9 +37,63 @@ class BoardTest {
         val color = StoneColor.BLACK
         val expect = createOmokStone(1, 1, color)
         // when
-        board.put(point, color)
-        val actual = board[point]
+        val newBoard = board + OmokStone(point, color)
+        val actual = newBoard[point]
         // then
         actual shouldBe expect
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1:1", "2:2", "3:3", "4:4", "5:5"], delimiter = ':')
+    fun `오른쪽 연속 5개의 돌이 있으면 오목이다`(x: Int, y: Int) {
+        // given
+        val board =
+            createBlackBoard(
+                createPoint(1, 1),
+                createPoint(2, 2),
+                createPoint(3, 3),
+                createPoint(4, 4),
+                createPoint(5, 5),
+            )
+        // when
+        val actual = board.isInOmok(createPoint(x, y))
+        // then
+        actual.shouldBeTrue()
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1:1", "2:2", "3:3", "5:5"], delimiter = ':')
+    fun `5개의 돌이 연속적이지 않으면 오목이 아니다`(x: Int, y: Int) {
+        // given
+        val board =
+            createBlackBoard(
+                createPoint(1, 1),
+                createPoint(2, 2),
+                createPoint(3, 3),
+                createPoint(5, 5),
+                createPoint(6, 6),
+            )
+        // when
+        val actual = board.isInOmok(createPoint(x, y))
+        // then
+        actual.shouldBeFalse()
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1:1", "2:2", "3:3", "4:4 ", "5:5"], delimiter = ':')
+    fun `5개의 돌의 색이 같지 않으면 오목이 아니다`(x: Int, y: Int) {
+        // given
+        val board =
+            createBoard(
+                createPoint(1, 1),
+                createPoint(2, 1),
+                createPoint(3, 1),
+                createPoint(4, 1),
+                createPoint(5, 1),
+            )
+        // when
+        val actual = board.isInOmok(createPoint(x, y))
+        // then
+        actual.shouldBeFalse()
     }
 }
