d500b88769d089cfb49fe9ebdac9c92ebba60e0c
Gimun Kim <kmkim7575@gmail.com>
2024-03-20 20:01:26 +0900

    feat: 각 착수 현황을 보여주는 기능 추가
    Co-authored-by: songpink <jejeje04@naver.com>

---
+++ src/main/kotlin/omok/view/OutputView.kt
@@ -0,0 +1,43 @@
+package omok.view
+
+import omok.model.Color
+import omok.model.Color.BLACK
+import omok.model.Color.WHITE
+
+class OutputView {
+    fun showCurrentBoard(board: Array<Array<Color?>>) {
+        for (i in 1..<board.size) {
+            print(ROW_INDICATOR.format(BOARD_SIZE - i))
+            showSingleRow(board, i)
+            println(DIVIDER_ROW)
+        }
+        println(COLUMN_INDICATOR)
+    }
+
+    private fun showSingleRow(
+        board: Array<Array<Color?>>,
+        i: Int,
+    ) {
+        for (j in 1..<board[i].size) {
+            print(getSingleCell(board[i][j]))
+            if (j < board[i].size - 1) print(DIVIDER_ROW)
+        }
+    }
+
+    private fun getSingleCell(stone: Color?) =
+        when (stone) {
+            null -> SIGNATURE_BLANK
+            BLACK -> SIGNATURE_BLACK
+            WHITE -> SIGNATURE_WHITE
+        }
+
+    companion object {
+        private const val COLUMN_INDICATOR = "   A B C D E F G H I J K L M N O"
+        private const val ROW_INDICATOR = "%2d─"
+        private const val SIGNATURE_BLANK = "┼"
+        private const val SIGNATURE_BLACK = "●"
+        private const val SIGNATURE_WHITE = "○"
+        private const val DIVIDER_ROW = "─"
+        private const val BOARD_SIZE = 16
+    }
+}
