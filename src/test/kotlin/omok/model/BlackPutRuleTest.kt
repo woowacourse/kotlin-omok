5b5d53f8e3357bdf31518f3a4423980a86cd1d19
murjune <sis9221302@gmail.com>
2024-03-20 22:52:53 +0900

    test: BlackPutTest

---
+++ src/test/kotlin/omok/model/BlackPutRuleTest.kt
@@ -0,0 +1,59 @@
+package omok.model
+
+import io.kotest.matchers.booleans.shouldBeFalse
+import omok.fixtures.createBlackBoard
+import omok.fixtures.createBlackStone
+import omok.fixtures.createPoint
+import org.junit.jupiter.api.Test
+
+class BlackPutRuleTest {
+    @Test
+    fun `장목이면 금수다`() {
+        // given
+        val blackBoard = createBlackBoard(
+            createPoint(1, 1),
+            createPoint(1, 2),
+            createPoint(1, 3),
+            createPoint(1, 4),
+            createPoint(1, 5),
+        )
+        val blackStone = createBlackStone(1, 6)
+        // when
+        val canPut = BlackPutRule.canPut(blackStone, blackBoard)
+        // then
+        canPut.shouldBeFalse()
+    }
+
+    @Test
+    fun `3-3이면 금수다`() {
+        // given
+        val blackBoard = createBlackBoard(
+            createPoint(1, 2),
+            createPoint(1, 3),
+            createPoint(2, 1),
+            createPoint(3, 1),
+        )
+        val blackStone = createBlackStone(1, 1)
+        // when
+        val canPut = BlackPutRule.canPut(blackStone, blackBoard)
+        // then
+        canPut.shouldBeFalse()
+    }
+
+    @Test
+    fun `4-4이면 금수다`() {
+        val blackBoard = createBlackBoard(
+            createPoint(1, 2),
+            createPoint(1, 3),
+            createPoint(1, 4),
+            createPoint(2, 1),
+            createPoint(3, 1),
+            createPoint(4, 1)
+        )
+        val blackStone = createBlackStone(1, 1)
+        // when
+        val canPut = BlackPutRule.canPut(blackStone, blackBoard)
+        // then
+        canPut.shouldBeFalse()
+    }
+}
