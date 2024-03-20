566b82e04acc2790581ec184fa00028d885b2321
Gimun Kim <kmkim7575@gmail.com>
2024-03-20 22:02:08 +0900

    feat: 색상을 반전시키는 기능 구현
    Co-authored-by: songpink <jejeje04@naver.com>

--- src/main/kotlin/omok/model/Color.kt
+++ src/main/kotlin/omok/model/Color.kt
@@ -3,4 +3,14 @@ package omok.model
 enum class Color(val label: String) {
     BLACK("흑"),
     WHITE("백"),
+    ;
+
+    companion object {
+        fun getReversedColor(color: Color): Color {
+            return when (color) {
+                BLACK -> WHITE
+                WHITE -> BLACK
+            }
+        }
+    }
 }
