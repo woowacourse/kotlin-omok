a78130ae0b90750f80571283b44e91711b91dab0
sh1mj1 <7wlgns@gmail.com>
2024-03-21 16:39:58 +0900

    rename(Position): 메서드 이름을 변경한다

--- src/main/kotlin/omok/model/Position.kt
+++ src/main/kotlin/omok/model/Position.kt
@@ -2,15 +2,15 @@ package omok.model
 
 data class Position(val row: Int, val col: Int) {
     init {
-        require(isInBoard(row, col)) { "올바르지 않은 위치입니다." }
+        require(isOnBoard(row, col)) { "올바르지 않은 위치입니다." }
     }
 
     fun move(direction: Direction): Position? {
-        if (!isInBoard(row + direction.row, col + direction.col)) return null
+        if (!isOnBoard(row + direction.row, col + direction.col)) return null
         return Position(row + direction.row, col + direction.col)
     }
 
-    private fun isInBoard(row: Int, col: Int): Boolean = row in INDEX_RANGE && col in INDEX_RANGE
+    private fun isOnBoard(row: Int, col: Int): Boolean = row in INDEX_RANGE && col in INDEX_RANGE
 
     companion object {
         const val MIN_INDEX = 0
