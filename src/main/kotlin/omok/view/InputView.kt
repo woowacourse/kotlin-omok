509eab11fb9aa5ddab1d29d080c218c7c2705046
Gimun Kim <kmkim7575@gmail.com>
2024-03-21 11:47:08 +0900

    refactor: InputView에서 사용되는 메시지 상수화 및 함수분리
    Co-authored-by: songpink <jejeje04@naver.com>

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -6,20 +6,33 @@ import omok.model.Stone
 
 class InputView {
     fun inputPosition(lastTurn: Stone?): Pair<Int, Char> {
-        lastTurn?.let {
-            val currentColor = Color.getReversedColor(it.color)
-            println("%s의 차례입니다.".format(currentColor.label))
-            showLastPosition(lastTurn.position)
-        } ?: println("%s의 차례입니다.".format(Color.BLACK.label))
-        print("위치를 입력하세요: ")
-        val input = readln()
-        println()
-        val col = input[0]
-        val row = input.slice(1 until input.length).toInt()
+        showCurrentTurn(lastTurn)
+        print(MESSAGE_INPUT_POSITION)
+        return getPositionData()
+    }
+
+    private fun getPositionData(): Pair<Int, Char> {
+        val position = readln()
+        val col = position.first()
+        val row = position.drop(1).toInt()
         return Pair(row, col)
     }
 
+    private fun showCurrentTurn(lastTurn: Stone?) {
+        lastTurn?.let {
+            val currentColor = Color.getReversedColor(it.color)
+            println(MESSAGE_PLAYERS_TURN.format(currentColor.label))
+            showLastPosition(lastTurn.position)
+        } ?: println(MESSAGE_PLAYERS_TURN.format(Color.BLACK.label))
+    }
+
     private fun showLastPosition(position: Position) {
-        println("(마지막 돌의 위치: %s%s)".format(position.col.title, position.row.title))
+        println(MESSAGE_LAST_PLACE.format(position.col.title, position.row.title))
+    }
+
+    companion object {
+        private const val MESSAGE_PLAYERS_TURN = "%s의 차례입니다."
+        private const val MESSAGE_INPUT_POSITION = "위치를 입력하세요: "
+        private const val MESSAGE_LAST_PLACE = "(마지막 돌의 위치: %s%s)"
     }
 }
