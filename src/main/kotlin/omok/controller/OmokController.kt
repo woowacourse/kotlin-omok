a779ab8a8d6ff93a12614c7506197404093bf4fc
Gimun Kim <kmkim7575@gmail.com>
2024-03-21 13:07:09 +0900

    refactor: Controller의 하나의 함수에 있던 로직들을 여러 함수로 분리
    Co-authored-by: songpink <jejeje04@naver.com>

--- src/main/kotlin/omok/controller/OmokController.kt
+++ src/main/kotlin/omok/controller/OmokController.kt
@@ -1,24 +1,34 @@
 package omok.controller
 
 import omok.model.Board
+import omok.model.GameResult
 import omok.model.Position
 import omok.view.InputView
 import omok.view.OutputView
 
 class OmokController(val inputView: InputView, val outputView: OutputView) {
-    private val board = Board()
+    private val board: Board by lazy { Board() }
 
     fun start() {
         outputView.showGameStartMessage()
-        while (true) {
+        playUntilFinish()
+    }
+
+    private fun playUntilFinish() {
+        runCatching {
             outputView.showCurrentBoard(board.status)
-            val position = getInputPosition()
-            val result = board.place(Position.of(position.first, position.second))
-            result?.let {
-                outputView.showGameResult(it)
-                return
-            }
+            playEachTurn()?.let { result ->
+                outputView.showGameResult(result)
+            } ?: playUntilFinish()
+        }.onFailure {
+            println(it.message)
+            playUntilFinish()
         }
+    }
+
+    private fun playEachTurn(): GameResult? {
+        val position = getInputPosition()
+        return board.place(Position.of(position.first, position.second))
     }
 
     private fun getInputPosition(): Pair<Int, Char> = inputView.inputPosition(board.stones.lastOrNull())
