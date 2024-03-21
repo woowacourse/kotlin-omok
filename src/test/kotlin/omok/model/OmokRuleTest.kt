a87a24c33fb1265fef3cd29b5b29f50354e4fc72
hxeyexn <kck85075@kumoh.ac.kr>
2024-03-21 10:51:06 +0900

    test(OmokRuleTest): 분리된 렌주룰 기능에 맞게 테스트 수정

--- src/test/kotlin/omok/model/OmokRuleTest.kt
+++ src/test/kotlin/omok/model/OmokRuleTest.kt
@@ -1,5 +1,8 @@
 package omok.model
 
+import omok.model.rule.ExceedFiveChecker
+import omok.model.rule.FourFourChecker
+import omok.model.rule.ThreeThreeChecker
 import omok.view.OutputView
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -9,12 +12,12 @@ class OmokRuleTest {
     fun `3-3 정의 1`() {
         val blackStone = BlackStone()
 
-        blackStone.putStone(Position(2, 11))
-        blackStone.putStone(Position(4, 11))
-        blackStone.putStone(Position(3, 12))
-        blackStone.putStone(Position(3, 13))
+        blackStone.putStone(Position(2, 11)) // C12 (2, 11)
+        blackStone.putStone(Position(4, 11)) // E12 (4, 11)
+        blackStone.putStone(Position(3, 12)) // D13 (3, 12)
+        blackStone.putStone(Position(3, 13)) // D14 (3, 13)
 
-        val actual1 = OmokRule(Board.board).checkRule(3, 11)
+        val actual1 = ThreeThreeChecker.checkThreeThree(3, 11) // D12 (3, 11)
         assertThat(actual1).isEqualTo(true)
     }
 
@@ -22,12 +25,12 @@ class OmokRuleTest {
     fun `3-3 정의 2`() {
         val blackStone = BlackStone()
 
-        blackStone.putStone(Position(1, 5))
-        blackStone.putStone(Position(2, 4))
-        blackStone.putStone(Position(5, 4))
-        blackStone.putStone(Position(4, 4))
+        blackStone.putStone(Position(1, 5)) // B6 (1, 5)
+        blackStone.putStone(Position(2, 4)) // C5 (2, 4)
+        blackStone.putStone(Position(4, 4)) // E5 (4, 4)
+        blackStone.putStone(Position(4, 5)) // E6 (4, 5)
 
-        val actual2 = OmokRule(Board.board).checkRule(4, 2)
+        val actual2 = ThreeThreeChecker.checkThreeThree(4, 2) // E3 (4, 2)
         assertThat(actual2).isEqualTo(true)
     }
 
@@ -35,12 +38,12 @@ class OmokRuleTest {
     fun `3-3 정의 3`() {
         val blackStone = BlackStone()
 
-        blackStone.putStone(Position(9, 3))
-        blackStone.putStone(Position(13, 8))
-        blackStone.putStone(Position(12, 9))
-        blackStone.putStone(Position(12, 11))
+        blackStone.putStone(Position(9, 8)) // J9 (9, 8)
+        blackStone.putStone(Position(13, 8)) // N9 (13, 8)
+        blackStone.putStone(Position(12, 9)) // M10 (12, 9)
+        blackStone.putStone(Position(12, 11)) // M12 (12, 11)
 
-        val actual3 = OmokRule(Board.board).checkRule(11, 10)
+        val actual3 = ThreeThreeChecker.checkThreeThree(11, 10) // L11 (11, 10)
         assertThat(actual3).isEqualTo(true)
     }
 
@@ -48,12 +51,12 @@ class OmokRuleTest {
     fun `3-3 정의 4`() {
         val blackStone = BlackStone()
 
-        blackStone.putStone(Position(10, 2))
-        blackStone.putStone(Position(10, 5))
-        blackStone.putStone(Position(12, 3))
-        blackStone.putStone(Position(13, 3))
+        blackStone.putStone(Position(10, 2)) // K3 (10, 2)
+        blackStone.putStone(Position(10, 5)) // K6 (10, 5)
+        blackStone.putStone(Position(12, 3)) // M4 (12, 3)
+        blackStone.putStone(Position(13, 3)) // N4 (13, 3)
 
-        val actual4 = OmokRule(Board.board).checkRule(10, 3)
+        val actual4 = ThreeThreeChecker.checkThreeThree(10, 3) // K4 (10, 3)
         assertThat(actual4).isEqualTo(true)
     }
 
@@ -88,22 +91,22 @@ class OmokRuleTest {
 
         OutputView().drawBoard(Board)
 
-        val actual1 = OmokRule(Board.board).checkRule(2, 7)
+        val actual1 = FourFourChecker.checkFourFour(2, 7)
         assertThat(actual1).isEqualTo(true)
 
-        val actual2 = OmokRule(Board.board).checkRule(2, 12)
+        val actual2 = ExceedFiveChecker.checkMoreThanFive(2, 12)
         assertThat(actual2).isEqualTo(true)
 
-        val actual3 = OmokRule(Board.board).checkRule(5, 11)
+        val actual3 = FourFourChecker.checkFourFour(5, 11)
         assertThat(actual3).isEqualTo(true)
 
-        val actual4 = OmokRule(Board.board).checkRule(7, 4)
+        val actual4 = FourFourChecker.checkFourFour(7, 4)
         assertThat(actual4).isEqualTo(true)
 
-        val actual5 = OmokRule(Board.board).checkRule(8, 7)
+        val actual5 = FourFourChecker.checkFourFour(8, 7)
         assertThat(actual5).isEqualTo(true)
 
-        val actual6 = OmokRule(Board.board).checkRule(9, 9)
+        val actual6 = FourFourChecker.checkFourFour(9, 9)
         assertThat(actual6).isEqualTo(true)
     }
 }
