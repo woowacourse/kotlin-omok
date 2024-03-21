52a79a33021d1867d7df59ff656323b0c097a9a5
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-03-21 11:13:39 +0900

    feat: 흑의 차례에만 렌주룰 적용 구현

--- src/main/kotlin/omok/model/GameState.kt
+++ src/main/kotlin/omok/model/GameState.kt
@@ -8,54 +8,8 @@ sealed class GameState(val board: Board) {
     ): GameState
 
     sealed class Running(board: Board) : GameState(board) {
-        open fun checkOmok(lastPosition: Pair<Int, Int>): Boolean {
-            val lastStoneType = board.layout[lastPosition.first][lastPosition.second]
-
-            for (dir in directions) {
-                var count = 1 // 마지막으로 놓인 돌 포함
-                // 해당 방향으로 연속된 돌 확인
-                count += countStones(lastPosition, dir, lastStoneType)
-                // 반대 방향으로 연속된 돌 확인
-                count += countStones(lastPosition, Pair(-dir.first, -dir.second), lastStoneType)
-
-                if (count >= 5) return true // 오목이면 true 반환
-            }
-
-            return false // 오목이 아니면 false 반환
-        }
-
-        open fun countStones(
-            start: Pair<Int, Int>,
-            direction: Pair<Int, Int>,
-            stoneType: StoneType,
-        ): Int {
-            var count = 0
-            var pos = Pair(start.first + direction.first, start.second + direction.second)
-
-            while (pos.first in 0
-                until BOARD_SIZE && pos.second in 0
-                until BOARD_SIZE && board.layout[pos.first][pos.second] == stoneType
-            ) {
-                count++
-                pos = Pair(pos.first + direction.first, pos.second + direction.second)
-            }
-
-            return count
-        }
-
         companion object {
             private const val BOARD_SIZE = 15
-            private val directions =
-                arrayOf(
-                    Pair(-1, 0),
-                    Pair(1, 0),
-                    Pair(0, -1),
-                    Pair(0, 1),
-                    Pair(-1, -1),
-                    Pair(-1, 1),
-                    Pair(1, -1),
-                    Pair(1, 1),
-                )
         }
 
         class BlackTurn(board: Board) : Running(board) {
@@ -70,9 +24,21 @@ sealed class GameState(val board: Board) {
                 board.placeStone(position, StoneType.BLACK_STONE)
                 onShow(board)
 
-                if (checkOmok(Pair(position.coordinate.x, position.coordinate.y))) {
+                val omokRule =
+                    OmokRule(
+                        board.layout,
+                        currentStone = StoneType.BLACK_STONE,
+                        otherStone = StoneType.WHITE_STONE,
+                        boardSize = 15,
+                    )
+                if (omokRule.validateOmok(
+                        position.coordinate.x, position.coordinate.y,
+                    ) &&
+                    omokRule.checkMoreThanFive(position.coordinate.x, position.coordinate.y)
+                ) {
                     return Finish(board)
                 }
+
                 return WhiteTurn(board)
             }
         }
@@ -87,11 +53,33 @@ sealed class GameState(val board: Board) {
                 val position = onRead()
 
                 board.placeStone(position, StoneType.WHITE_STONE)
-                onShow(board)
 
-                if (checkOmok(Pair(position.coordinate.x, position.coordinate.y))) {
+                val blockRule =
+                    OmokRule(
+                        board.layout,
+                        currentStone = StoneType.BLACK_STONE,
+                        otherStone = StoneType.WHITE_STONE,
+                        boardSize = 15,
+                    )
+                val blockBoard =
+                    board.generateBlock(
+                        blockRule::checkThreeThree,
+                        blockRule::countFourFour,
+                        blockRule::checkMoreThanFive,
+                    )
+                onShow(blockBoard)
+
+                val omokRule =
+                    OmokRule(
+                        board.layout,
+                        currentStone = StoneType.WHITE_STONE,
+                        otherStone = StoneType.BLACK_STONE,
+                        boardSize = 15,
+                    )
+                if (omokRule.validateOmok(position.coordinate.x, position.coordinate.y)) {
                     return Finish(board)
                 }
+
                 return BlackTurn(board)
             }
         }
