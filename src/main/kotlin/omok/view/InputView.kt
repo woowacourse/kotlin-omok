38c1b3be4ad084e471deefcb9cfc9a548414f432
sh1mj1 <7wlgns@gmail.com>
2024-03-21 13:08:50 +0900

    style(InputView): readStonePosition 의 출력 형식 변경

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -5,8 +5,8 @@ import omok.model.Stone
 
 class InputView {
     fun readStonePosition(stone: Stone, recentPosition: Position?): Position {
-        println("${stone.output()}의 차례입니다.")
-        recentPosition?.run { println("마지막 돌의 위치: ${this.output()}") }
+        print("${stone.output()}의 차례입니다. ")
+        recentPosition?.run { println("(마지막 돌의 위치: ${this.output()})") } ?: println()
         print("위치를 입력하세요: ")
         return readln().toPosition()
     }
