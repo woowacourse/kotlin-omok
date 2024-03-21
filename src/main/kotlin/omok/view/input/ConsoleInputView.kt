49324fd0904789485810c788e17f709fa89d6d67
aprilgom <aprilgom@gmail.com>
2024-03-21 14:18:46 +0900

    feat: 입력 유효성 검증 로직 추가

--- src/main/kotlin/omok/view/input/ConsoleInputView.kt
+++ src/main/kotlin/omok/view/input/ConsoleInputView.kt
@@ -3,11 +3,22 @@ package omok.view.input
 import omok.model.entity.Point
 
 class ConsoleInputView : InputView {
-    override fun getStonePoint(): Point {
+    override tailrec fun getStonePoint(): Point {
         print("위치를 입력하세요: ")
         val input = readln()
-        val x = alphabetToInt(input[0])
-        val y = input.substring(1).toInt()
+        val inputX = input[0]
+        val isInputXValid = inputX in 'A'..'O'
+
+        val inputY = input.substring(1).toIntOrNull() ?: return getStonePoint()
+        val isInputYValid = inputY in 1..15
+
+        if (!(isInputXValid && isInputYValid)) {
+            return getStonePoint()
+        }
+
+        val x = alphabetToInt(inputX)
+        val y = inputY
+
         return Point(x, y)
     }
 
