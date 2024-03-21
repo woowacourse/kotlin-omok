200ad38433a118c18437cd048b1ffc0a37daf3e0
Hogu59 <dbstjdguswkd@naver.com>
2024-03-21 14:02:20 +0900

    test(fixture): Implement Stone Fixture

--- src/test/kotlin/omok/fixture/StoneFixture.kt
+++ src/test/kotlin/omok/fixture/StoneFixture.kt
@@ -1,6 +1,8 @@
 package omok.fixture
 
+import omok.model.Color
+import omok.model.Point
 import omok.model.Stone
 
-val FIRST_ROW_FIRST_COL = Stone(1, 1)
-val FIRST_ROW_SECOND_COL = Stone(1, 2)
+val FIRST_ROW_FIRST_COL = Stone(Point(1, 1), Color.BLACK)
+val FIRST_ROW_SECOND_COL = Stone(Point(1, 2), Color.BLACK)
