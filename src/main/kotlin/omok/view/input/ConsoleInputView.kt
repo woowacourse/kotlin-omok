062b6b1ccc9a011ca9b5d0d70314e80b79e303c5
aprilgom <aprilgom@gmail.com>
2024-03-21 14:48:58 +0900

    refactor(ConsoleOutputView): 상수 제거

--- src/main/kotlin/omok/view/input/ConsoleInputView.kt
+++ src/main/kotlin/omok/view/input/ConsoleInputView.kt
@@ -7,10 +7,10 @@ class ConsoleInputView : InputView {
         print("위치를 입력하세요: ")
         val input = readln()
         val inputX = input[0]
-        val isInputXValid = inputX in 'A'..'O'
+        val isInputXValid = inputX in INPUTX_SIZE_RANGE
 
         val inputY = input.substring(1).toIntOrNull() ?: return getStonePoint()
-        val isInputYValid = inputY in 1..15
+        val isInputYValid = inputY in INPUTY_SIZE_RANGE
 
         if (!(isInputXValid && isInputYValid)) {
             return getStonePoint()
@@ -23,4 +23,15 @@ class ConsoleInputView : InputView {
     }
 
     private fun alphabetToInt(alphabet: Char): Int = alphabet - 'A' + 1
+
+    companion object {
+
+        private const val INPUTY_MIN_SIZE = 1
+        private const val INPUTY_MAX_SIZE = 15
+        private val INPUTY_SIZE_RANGE = INPUTY_MIN_SIZE..INPUTY_MAX_SIZE
+
+        private const val INPUTX_MIN_SIZE = 'A'
+        private const val INPUTX_MAX_SIZE = 'O'
+        private val INPUTX_SIZE_RANGE = INPUTX_MIN_SIZE..INPUTX_MAX_SIZE
+    }
 }
