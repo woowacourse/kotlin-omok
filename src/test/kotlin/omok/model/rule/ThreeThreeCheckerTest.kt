260f9993b531e34bdee568dbf6847ed273e36f24
hxeyexn <kck85075@kumoh.ac.kr>
2024-03-23 01:41:17 +0900

    test(ThreeThreeCheckerTest): 열린 3-3 금수 테스트 추가 및 테스트명 변경
    - 추가한 테스트: 열린 3, 닫힌 3은 3-3 금수가 아니다

--- src/test/kotlin/omok/model/rule/ThreeThreeCheckerTest.kt
+++ src/test/kotlin/omok/model/rule/ThreeThreeCheckerTest.kt
@@ -4,7 +4,6 @@ import omok.model.board.Board
 import omok.model.position.Position
 import omok.model.stone.BlackStone
 import omok.model.stone.Stone
-import omok.model.stone.WhiteStone
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -20,7 +19,7 @@ class ThreeThreeCheckerTest {
     }
 
     @Test
-    fun `3-3 케이스 1`() {
+    fun `3-3 금수 케이스 1`() {
         val blackStone = BlackStone()
 
         blackStone.putStone(Position.of('C', 12))
@@ -33,7 +32,7 @@ class ThreeThreeCheckerTest {
     }
 
     @Test
-    fun `3-3 케이스 2`() {
+    fun `3-3 금수 케이스 2`() {
         val blackStone = BlackStone()
 
         blackStone.putStone(Position.of('B', 6))
@@ -46,7 +45,7 @@ class ThreeThreeCheckerTest {
     }
 
     @Test
-    fun `3-3 케이스 3`() {
+    fun `3-3 금수 케이스 3`() {
         val blackStone = BlackStone()
 
         blackStone.putStone(Position.of('J', 9))
@@ -59,7 +58,7 @@ class ThreeThreeCheckerTest {
     }
 
     @Test
-    fun `3-3 케이스 4`() {
+    fun `3-3 금수 케이스 4`() {
         val blackStone = BlackStone()
 
         blackStone.putStone(Position.of('K', 3))
@@ -72,50 +71,15 @@ class ThreeThreeCheckerTest {
     }
 
     @Test
-    fun `4-4 케이스`() {
+    fun `열린 3, 닫힌 3은 3-3 금수가 아니다`() {
         val blackStone = BlackStone()
-        val whiteStone = WhiteStone()
-        blackStone.putStone(Position.of('C', 10))
-        blackStone.putStone(Position.of('C', 11))
-        blackStone.putStone(Position.of('C', 12))
+
         blackStone.putStone(Position.of('C', 14))
-        blackStone.putStone(Position.of('C', 15))
-        blackStone.putStone(Position.of('D', 12))
-        blackStone.putStone(Position.of('G', 12))
-        blackStone.putStone(Position.of('I', 12))
-        blackStone.putStone(Position.of('J', 12))
-        blackStone.putStone(Position.of('E', 5))
-        blackStone.putStone(Position.of('E', 6))
-        blackStone.putStone(Position.of('F', 5))
-        blackStone.putStone(Position.of('G', 5))
-        blackStone.putStone(Position.of('G', 4))
-        blackStone.putStone(Position.of('H', 6))
-        blackStone.putStone(Position.of('H', 7))
-        blackStone.putStone(Position.of('H', 8))
-        blackStone.putStone(Position.of('J', 8))
-        blackStone.putStone(Position.of('K', 8))
-        blackStone.putStone(Position.of('J', 9))
-        blackStone.putStone(Position.of('J', 6))
+        blackStone.putStone(Position.of('C', 13))
+        blackStone.putStone(Position.of('B', 12))
+        blackStone.putStone(Position.of('A', 12))
 
-        whiteStone.putStone(Position.of('D', 5))
-        whiteStone.putStone(Position.of('H', 9))
-
-        val actual1 = FourFourChecker.checkFourFour(2, 7)
-        assertThat(actual1).isEqualTo(true)
-
-        val actual2 = ExceedFiveChecker.checkMoreThanFive(2, 12)
-        assertThat(actual2).isEqualTo(true)
-
-        val actual3 = FourFourChecker.checkFourFour(5, 11)
-        assertThat(actual3).isEqualTo(true)
-
-        val actual4 = FourFourChecker.checkFourFour(7, 4)
-        assertThat(actual4).isEqualTo(true)
-
-        val actual5 = FourFourChecker.checkFourFour(8, 7)
-        assertThat(actual5).isEqualTo(true)
-
-        val actual6 = FourFourChecker.checkFourFour(9, 9)
-        assertThat(actual6).isEqualTo(true)
+        val actual = ThreeThreeChecker.checkThreeThree(2, 11) // C12 (2, 11)
+        assertThat(actual).isEqualTo(false)
     }
 }
