61aa133094336f5f4d57376c8adebccab377abb1
murjune <sis9221302@gmail.com>
2024-03-24 05:08:05 +0900

    feat(Inputview): showInvalidInputPosition

--- src/main/kotlin/omok/view/ConsoleOmokInputView.kt
+++ src/main/kotlin/omok/view/ConsoleOmokInputView.kt
@@ -6,10 +6,15 @@ class ConsoleOmokInputView : OmokInputView {
     override fun readPosition(): PositionUiModel {
         print("위치를 입력하세요 : ")
         val input = readln().trim()
-        val inputX = input.firstOrNull()?.formatToPositionX() ?: return readPosition()
-        val inputY = input.substring(1).toIntOrNull() ?: return readPosition()
+        val inputX = input.firstOrNull()?.formatToPositionX() ?: return showInvalidInputPosition(::readPosition)
+        val inputY = input.substring(1).toIntOrNull() ?: return showInvalidInputPosition(::readPosition)
         val (x, y) = inputX to inputY
         return PositionUiModel(x, y)
+    }
+
+    private inline fun showInvalidInputPosition(onError: () -> PositionUiModel): PositionUiModel {
+        println("잘못된 입력입니다. 다시 입력해주세요. 예시 입력) A1")
+        return onError()
     }
 
     private fun Char.formatToPositionX(): Int = uppercase().first() - 'A' + 1
