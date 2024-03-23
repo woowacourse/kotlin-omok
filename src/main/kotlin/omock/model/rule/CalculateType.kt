287194dceea2a1b6374a1260e53923b199b08a33
JunJangE <wnswkd486@gmail.com>
2024-03-23 17:55:08 +0900

    refactor: CalculateType 확장함수를 통해 에러 메시지 핸들링

--- src/main/kotlin/omock/model/rule/CalculateType.kt
+++ src/main/kotlin/omock/model/rule/CalculateType.kt
@@ -8,21 +8,19 @@ sealed interface CalculateType {
     data object IsReverseTwoAndThree : CalculateType
 
     data object IsClearFourToFourCount : CalculateType
+}
 
-    companion object {
-        inline fun CalculateType.checkCalculateType(action: () -> Boolean) {
-            if (action()) {
-                throw IllegalArgumentException(getCalculateMessage(this))
-            }
-        }
+inline fun CalculateType.checkCalculateType(action: () -> Boolean) {
+    if (action()) {
+        throw IllegalArgumentException(getCalculateMessage(this))
+    }
+}
 
-        fun getCalculateMessage(calculateType: CalculateType): String {
-            return when (calculateType) {
-                FourToFourCount -> "4-4 금수를 어겼습니다."
-                IsClearFourToFourCount -> "열린 4-4 금수를 어겼습니다."
-                IsReverseTwoAndThree -> "장목 금수를 어겼습니다."
-                ThreeToThreeCount -> "3-3 금수를 어겼습니다."
-            }
-        }
+fun getCalculateMessage(calculateType: CalculateType): String {
+    return when (calculateType) {
+        CalculateType.FourToFourCount -> "4-4 금수를 어겼습니다."
+        CalculateType.IsClearFourToFourCount -> "열린 4-4 금수를 어겼습니다."
+        CalculateType.IsReverseTwoAndThree -> "장목 금수를 어겼습니다."
+        CalculateType.ThreeToThreeCount -> "3-3 금수를 어겼습니다."
     }
 }
