8490247d9710832c3a08050a9a49625a9d8d42ed
kimhm0728 <kimhm0728@naver.com>
2024-03-24 01:15:21 +0900

    refactor: 문자열을 상수로 분리

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -16,9 +16,10 @@ class Board(private val _board: MutableMap<Position, Stone> = initBoard()) {
         _board[position] = player.stone
     }
 
-    fun find(position: Position): Stone = _board[position] ?: throw IllegalArgumentException("올바르지 않은 위치입니다.")
+    fun find(position: Position): Stone = _board[position] ?: throw IllegalArgumentException(INVALID_POSITION_MESSAGE)
 
     companion object {
+        private const val INVALID_POSITION_MESSAGE = "올바르지 않은 위치입니다."
         private const val DUPLICATION_POSITION_MESSAGE = "이미 바둑돌이 있는 위치입니다."
         private const val FORBIDDEN_POSITION_MESSAGE = "바둑돌을 놓을 수 없는 위치입니다."
 
