b5329ca8a8f08d5a0130af8a200ab5d8b34f8f34
murjune <sis9221302@gmail.com>
2024-03-24 04:51:02 +0900

    feat: BoardSize 최소 사이즈 5이상으로 변경

--- src/main/kotlin/omok/model/board/BoardSize.kt
+++ src/main/kotlin/omok/model/board/BoardSize.kt
@@ -6,15 +6,18 @@ value class BoardSize(val width: Int) {
         require(width >= MIN_SIZE) { "input: width - BoardSize 는 $MIN_SIZE 보다 커야함" }
     }
 
+    fun range(): IntRange = MIN_RANGE..width
+
     fun isInBounds(
         x: Int,
         y: Int,
     ): Boolean {
-        val range = MIN_SIZE..width
+        val range = MIN_RANGE..width
         return x in range && y in range
     }
 
     companion object {
-        private const val MIN_SIZE = 1
+        private const val MIN_RANGE = 1
+        private const val MIN_SIZE = 5
     }
 }
--- src/test/kotlin/omok/model/board/BoardSizeTest.kt
+++ src/test/kotlin/omok/model/board/BoardSizeTest.kt
@@ -9,16 +9,16 @@ import org.junit.jupiter.api.Test
 
 class BoardSizeTest {
     @Test
-    fun `BoardSize 의 크기는 1 이상이다`() {
+    fun `BoardSize 의 크기는 5 이상이다`() {
         assertSoftly {
-            shouldThrow<IllegalArgumentException> { BoardSize(0) }
-            shouldNotThrow<IllegalArgumentException> { BoardSize(1) }
+            shouldThrow<IllegalArgumentException> { BoardSize(4) }
+            shouldNotThrow<IllegalArgumentException> { BoardSize(5) }
         }
     }
 
     @Test
     fun `BoardSize 의 Bound 안에 있는지 확인할 수 있다`() {
-        val boardSize = BoardSize(3)
+        val boardSize = BoardSize(5)
         assertSoftly {
             boardSize.isInBounds(0, 0).shouldBeFalse()
             boardSize.isInBounds(1, 1).shouldBeTrue()
