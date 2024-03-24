1b957d0757d17049a05aa26cf6cc563485a6de4a
김진우 <jinwoo3661@naver.com>
2024-03-25 03:20:08 +0900

    refactor: BlackPlayer 함수 분리

--- src/main/kotlin/omock/model/BlackPlayer.kt
+++ src/main/kotlin/omock/model/BlackPlayer.kt
@@ -19,28 +19,53 @@ data class BlackPlayer(
                 directionResult = result,
             ) { calculateType ->
                 when (calculateType) {
-                    CalculateType.FourToFourCount ->
-                        CalculateType.FourToFourCount.checkCalculateType {
-                            ++fourToFourCount >= MIN_FOUR_TO_FOUR_COUNT
-                        }
-                    CalculateType.IsClearFourToFourCount ->
-                        CalculateType.IsClearFourToFourCount.checkCalculateType {
-                            ++isClearFourToFourCount >= MIN_IS_CLEAR_FOUR_TO_FOUR_COUNT
-                        }
-                    CalculateType.IsReverseTwoAndThree ->
-                        CalculateType.IsReverseTwoAndThree.checkCalculateType {
-                            isReverseTwoAndThree = true
-                            true
-                        }
-
-                    CalculateType.ThreeToThreeCount ->
-                        CalculateType.ThreeToThreeCount.checkCalculateType { ++threeToThreeCount >= MIN_THREE_TO_THREE_COUNT }
+                    CalculateType.FourToFourCount -> checkFourToFour(++fourToFourCount)
+                    CalculateType.IsClearFourToFourCount -> checkIsClearFourToFour(++isClearFourToFourCount)
+                    CalculateType.IsReverseTwoAndThree -> isReverseTwoAndThree = checkIsReverseTwoAndThree()
+                    CalculateType.ThreeToThreeCount -> checkThreeToThreeCount(++threeToThreeCount)
                 }
             }
-            if (!isReverseTwoAndThree && (reverseResultCount + result.count >= MIN_O_MOCK_COUNT)) return true
+            if (isOMockWinState(!isReverseTwoAndThree ,reverseResultCount + result.count)) return true
         }
         return false
     }
+
+    private fun isOMockWinState(
+        isNotReverseTwoAndThree: Boolean,
+        totalCount: Int,
+    ): Boolean {
+        return isNotReverseTwoAndThree && isOverMinOMockCount(totalCount)
+    }
+
+    private fun isOverMinOMockCount(count: Int): Boolean {
+        return count >= MIN_O_MOCK_COUNT
+    }
+
+    private fun checkFourToFour(fourToFourCount: Int) {
+        CalculateType.FourToFourCount.checkCalculateType {
+            fourToFourCount >= MIN_FOUR_TO_FOUR_COUNT
+        }
+    }
+
+    private fun checkIsClearFourToFour(isClearFourToFourCount: Int) {
+        CalculateType.IsClearFourToFourCount.checkCalculateType {
+            isClearFourToFourCount >= MIN_IS_CLEAR_FOUR_TO_FOUR_COUNT
+        }
+    }
+
+    private fun checkIsReverseTwoAndThree(): Boolean {
+        var isReverseTwoAndThree = false
+        CalculateType.IsReverseTwoAndThree.checkCalculateType {
+            isReverseTwoAndThree = true
+            true
+        }
+        return isReverseTwoAndThree
+    }
+
+    private fun checkThreeToThreeCount(threeToThreeCount: Int) {
+        CalculateType.ThreeToThreeCount.checkCalculateType { threeToThreeCount >= MIN_THREE_TO_THREE_COUNT }
+    }
+
 
     private inline fun getCalculateType(
         isReverseResultFirstClear: Boolean,
@@ -48,27 +73,65 @@ data class BlackPlayer(
         directionResult: DirectionResult,
         type: (CalculateType) -> Unit,
     ) {
-        if (!directionResult.isLastClear) return
-        if (!directionResult.isFirstClear) {
-            if (directionResult.count + reverseResultCount == EDGE_THREE_TO_THREE_COUNT) {
-                type(CalculateType.ThreeToThreeCount)
-            } else if (directionResult.count + reverseResultCount == EDGE_FOUR_TO_FOUR_COUNT) {
-                type(CalculateType.FourToFourCount)
-            } else if (directionResult.count == EDGE_THREE_TO_THREE_COUNT &&
-                reverseResultCount == EDGE_FOUR_TO_FOUR_COUNT && !isReverseResultFirstClear
-            ) {
-                type(
-                    CalculateType.IsReverseTwoAndThree,
-                )
-            }
-        } else {
-            if (isReverseResultFirstClear && directionResult.count == EDGE_THREE_TO_THREE_COUNT) {
-                type(CalculateType.IsClearFourToFourCount)
-            } else {
-                if (directionResult.count + reverseResultCount == EDGE_THREE_TO_THREE_COUNT) type(CalculateType.IsClearFourToFourCount)
-                if (reverseResultCount == EDGE_THREE_TO_THREE_COUNT) type(CalculateType.ThreeToThreeCount)
-                if (reverseResultCount == EDGE_FOUR_TO_FOUR_COUNT) type(CalculateType.FourToFourCount)
-            }
+        if (isNotLastClearResult(directionResult)) return
+        if (isNotFirstClearResult(directionResult)) provideFirstClearResult(
+            isReverseResultFirstClear = isReverseResultFirstClear,
+            reverseResultCount = reverseResultCount,
+            directionResult = directionResult,
+            type = type,
+        ) else provideNotFirstClearResult(
+            isReverseResultFirstClear = isReverseResultFirstClear,
+            reverseResultCount = reverseResultCount,
+            directionResult = directionResult,
+            type = type,
+        )
+    }
+
+    private inline fun provideFirstClearResult(
+        isReverseResultFirstClear: Boolean,
+        reverseResultCount: Int,
+        directionResult: DirectionResult,
+        type: (CalculateType) -> Unit,
+    ) {
+        if (isThreeToThreeCount(directionResult.count + reverseResultCount)) {
+            type(CalculateType.ThreeToThreeCount)
+        } else if (isFourToFourCount(directionResult.count + reverseResultCount)) {
+            type(CalculateType.FourToFourCount)
+        } else if (isThreeToThreeCount(directionResult.count) &&
+            isFourToFourCount(reverseResultCount) && !isReverseResultFirstClear
+        ) {
+            type(CalculateType.IsReverseTwoAndThree)
         }
+    }
+
+    private inline fun provideNotFirstClearResult(
+        isReverseResultFirstClear: Boolean,
+        reverseResultCount: Int,
+        directionResult: DirectionResult,
+        type: (CalculateType) -> Unit,
+    ) {
+        if (isReverseResultFirstClear && isThreeToThreeCount(directionResult.count)) {
+            type(CalculateType.IsClearFourToFourCount)
+        } else {
+            if (isThreeToThreeCount(directionResult.count + reverseResultCount)) type(CalculateType.IsClearFourToFourCount)
+            if (isThreeToThreeCount(reverseResultCount)) type(CalculateType.ThreeToThreeCount)
+            if (isFourToFourCount(reverseResultCount)) type(CalculateType.FourToFourCount)
+        }
+    }
+
+    private fun isNotLastClearResult(directionResult: DirectionResult): Boolean {
+        return !directionResult.isLastClear
+    }
+
+    private fun isNotFirstClearResult(directionResult: DirectionResult): Boolean {
+        return !directionResult.isFirstClear
+    }
+
+    private fun isThreeToThreeCount(count: Int): Boolean {
+        return count == EDGE_THREE_TO_THREE_COUNT
+    }
+
+    private fun isFourToFourCount(count: Int): Boolean {
+        return count == EDGE_FOUR_TO_FOUR_COUNT
     }
 }
