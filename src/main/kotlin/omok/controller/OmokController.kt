204dc78b8f0ce07833b86dbced2a2312e496211f
kkosang <eiwoqp77@gmail.com>
2024-03-24 18:37:30 +0900

    refactor: OmokController 수정

--- src/main/kotlin/omok/controller/OmokController.kt
+++ src/main/kotlin/omok/controller/OmokController.kt
@@ -5,6 +5,7 @@ import omok.model.Color
 import omok.model.OmokGame
 import omok.model.Player
 import omok.model.Stones
+import omok.view.InputView
 import omok.view.OutputView
 
 class OmokController {
@@ -12,9 +13,35 @@ class OmokController {
         OutputView.printStart()
         val players = Player(Color.BLACK) to Player(Color.WHITE)
         val board = Board(Stones())
-        val omokGame = OmokGame(players, board)
-        omokGame.playGame()
+        startGame(players, board)
         displayWinner(players)
+    }
+
+    private fun startGame(
+        players: Pair<Player, Player>,
+        board: Board,
+    ) {
+        val omokGame = OmokGame(players, board)
+        while (omokGame.isRunning()) {
+            for (player in players.toList()) {
+                OutputView.printTurnName(player.color)
+                OutputView.printLastStone(board.stones.getLastStoneCoordinate())
+                retryPlayTurnUntilSuccess(omokGame, player)
+                OutputView.printBoard(board.stones)
+            }
+        }
+    }
+
+    private fun retryPlayTurnUntilSuccess(
+        omokGame: OmokGame,
+        player: Player,
+    ) {
+        val coordinate = InputView.inputStoneCoordinate()
+        val isFinishTurn = omokGame.playTurn(player, coordinate)
+
+        if (!isFinishTurn) {
+            retryPlayTurnUntilSuccess(omokGame, player)
+        }
     }
 
     private fun displayWinner(players: Pair<Player, Player>) {
