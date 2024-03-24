ad3ec96c8b7c5c316eefdbf241ed9b4fe7b98dc5
songpink <jejeje04@naver.com>
2024-03-24 22:10:14 +0900

    test: 오목이 완성되면 승자를 반환하는 테스트 작성

--- src/test/kotlin/omok/model/BoardTest.kt
+++ src/test/kotlin/omok/model/BoardTest.kt
@@ -1,8 +1,11 @@
 package omok.model
 
+import omok.model.fixture.createGameOverBoard
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class BoardTest {
     @Test
@@ -28,5 +31,24 @@ class BoardTest {
         board.place(Position(Row.ONE, Column.A))
         // then
         assertThat(board.stones.last().color).isEqualTo(Color.WHITE)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "3,B,WINNER_BLACK",
+        "9,D,WINNER_WHITE",
+        "1,O,PROCEEDING",
+    )
+    fun `게임 도중 오목이 완성되면 게임을 종료하고 승자를 반환해야 한다`(
+        row: Int,
+        col: Char,
+        gameResult: GameResult,
+    ) {
+        // given
+        val board = Board(_status = createGameOverBoard())
+        // when
+        val result = board.getGameResult(Position.of(row, col))
+        // then
+        assertThat(result).isEqualTo(gameResult)
     }
 }
