cdb996189dba24df2bae418d5e9b09fdbcf09de0
김진우 <jinwoo3661@naver.com>
2024-03-25 00:53:25 +0900

    refactor: 하드코딩 상수화

--- src/main/kotlin/omock/model/CalculateType.kt
+++ src/main/kotlin/omock/model/CalculateType.kt
@@ -18,11 +18,16 @@ sealed interface CalculateType {
 
         fun getCalculateMessage(calculateType: CalculateType): String {
             return when (calculateType) {
-                FourToFourCount -> "4-4 금수를 어겼습니다."
-                IsClearFourToFourCount -> "열린 4-4 금수를 어겼습니다."
-                IsReverseTwoAndThree -> "장목 금수를 어겼습니다."
-                ThreeToThreeCount -> "3-3 금수를 어겼습니다."
+                FourToFourCount -> FOUR_TO_FOUR_COUNT_MESSAGE
+                IsClearFourToFourCount -> IS_CLEAR_FOUR_TO_FOUR_COUNT_MESSAGE
+                IsReverseTwoAndThree -> IS_REVERSE_TWO_AND_THREE_MESSAGE
+                ThreeToThreeCount -> THREE_TO_THREE_COUNT_MESSAGE
             }
         }
+
+        private const val FOUR_TO_FOUR_COUNT_MESSAGE = "4-4 금수를 어겼습니다."
+        private const val IS_CLEAR_FOUR_TO_FOUR_COUNT_MESSAGE = "열린 4-4 금수를 어겼습니다."
+        private const val IS_REVERSE_TWO_AND_THREE_MESSAGE = "장목 금수를 어겼습니다."
+        private const val THREE_TO_THREE_COUNT_MESSAGE = "3-3 금수를 어겼습니다."
     }
 }
