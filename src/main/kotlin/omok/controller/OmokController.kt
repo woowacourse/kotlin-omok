85c85f3659795089963fbbd69459008261e9a86e
Hogu59 <dbstjdguswkd@naver.com>
2024-03-26 01:12:41 +0900

    refactor(OmokController): divide functions

--- src/main/kotlin/omok/controller/OmokController.kt
+++ src/main/kotlin/omok/controller/OmokController.kt
@@ -1,13 +1,14 @@
 package omok.controller
 
+import omok.ExceptionHandler
 import omok.model.BlackStonePlayer
 import omok.model.Board
 import omok.model.Color
 import omok.model.Player
+import omok.model.Point
 import omok.model.Stone
+import omok.model.Turn
 import omok.model.WhiteStonePlayer
-import omok.model.change
-import omok.retryWhileNotException
 import omok.view.InputView
 import omok.view.OutputView
 
@@ -20,18 +21,18 @@ class OmokController {
 
     fun start() {
         outputView.showGameStartHeader()
-        outputView.showBoard(blackPlayer, whitePlayer)
+        outputView.showBoard(board.customBoard)
 
-        var turn = Color.BLACK
+        var turn = Turn(Color.BLACK)
         while (true) {
-            if (turn == Color.BLACK) {
-                val stone = putStone(blackPlayer, turn)
+            if (turn.isBlack()) {
+                val stone = putStone(blackPlayer, Color.BLACK)
                 if (blackPlayer.checkContinuity(stone)) break
             } else {
-                val stone = putStone(whitePlayer, turn)
+                val stone = putStone(whitePlayer, Color.WHITE)
                 if (whitePlayer.checkContinuity(stone)) break
             }
-            turn = turn.change()
+            turn.next()
         }
         outputView.showGameResult(turn)
     }
@@ -39,16 +40,32 @@ class OmokController {
     private fun putStone(
         player: Player,
         turn: Color,
-    ): Stone =
-        retryWhileNotException {
-            val stone = inputView.getStone(board, turn, board.lastStone())
-            player.add(stone)
-            board.add(stone)
-            showBoard()
-            stone
-        }
+    ): Stone {
+        val point = validPoint(turn)
+        val stone = Stone(point = point, color = turn)
+        player.add(stone)
+        board.add(stone)
+        printBoard()
 
-    private fun showBoard() {
-        outputView.showBoard(blackPlayer = blackPlayer, whitePlayer = whitePlayer)
+        return stone
+    }
+
+    private fun validPoint(turn: Color): Point {
+        return ExceptionHandler.handleInputValue {
+            val point = readPoint(turn)
+            require(board.pointEmpty(point)) { "중복된 위치입니다. 비워진 위치에 입력해주세요!" }
+            if (turn == Color.BLACK) require(blackPlayer.isValidPoint(point)) { "룰 위반입니다. 해당 자리에는 수를 둘 수 없습니다. 다른 위치를 시도해주세요." }
+            point
+        }
+    }
+
+    private fun readPoint(turn: Color): Point {
+        return ExceptionHandler.handleInputValue {
+            inputView.getPointOrNull(turn, board.lastStone())
+        } ?: readPoint(turn)
+    }
+
+    private fun printBoard() {
+        outputView.showBoard(board.customBoard)
     }
 }
