1939a20126cd2fffdf73fde1841469bfe7789bc7
Hogu59 <dbstjdguswkd@naver.com>
2024-03-26 01:11:13 +0900

    refactor(InputView): change name and constantize

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -1,46 +1,44 @@
 package omok.view
 
-import omok.model.Board
 import omok.model.Color
 import omok.model.Point
 import omok.model.Stone
-import omok.retryWhileNotException
 
 class InputView {
-    fun getStone(
-        board: Board,
+    fun getPointOrNull(
         color: Color,
         lastStone: Stone?,
-    ): Stone =
-        retryWhileNotException {
-            println("${colorToString(color)}의 차례입니다.${convertPosition(lastStone)}")
-            print("위치를 입력하세요 : ")
-            val input = readln()
-            val x: Int = convertX(input.first()) ?: throw IllegalArgumentException("행 값 오류")
-            val y: Int = convertY(input.substring(1, input.length)) ?: throw IllegalArgumentException("열 값 오류")
-            if (board.stones.stones.any { it.point == Point(x, y) }) throw IllegalArgumentException("중복 오류")
-            Stone(x, y, color)
-        }
+    ): Point? {
+        println(HEADER_TURN.format(colorToString(color), convertPoint(lastStone)))
+        print(HEADER_INPUT_POSITION)
+        val input = readln().validation() ?: return null
+        return input.toPointOrNull()
+    }
+
+    private fun String.validation(): String? = if (this.length !in 1..3) null else this
+
+    private fun String.toPointOrNull(): Point? {
+        val x: Int = convertX(this.first()) ?: return null
+        val y: Int = convertY(this.substring(1, this.length)) ?: return null
+        return Point(x, y)
+    }
 
     private fun colorToString(color: Color): String {
         return when (color) {
-            Color.BLACK -> "흑"
-            Color.WHITE -> "백"
+            Color.BLACK -> COLOR_BLACK
+            Color.WHITE -> COLOR_WHITE
+            else -> COLOR_NONE
         }
     }
 
-    private fun convertPosition(stone: Stone?): String {
+    private fun convertPoint(stone: Stone?): String {
         if (stone == null) return ""
         return " (마지막 돌의 위치: ${('A'.code + stone.point.row).toChar()}${stone.point.col + 1})"
     }
 
     private fun convertX(x: Char): Int? {
         val res = x.uppercaseChar().code - 'A'.code
-        return if (res in 0..14) {
-            res
-        } else {
-            null
-        }
+        return if (res in 0..14) res else null
     }
 
     private fun convertY(y: String): Int? {
@@ -50,5 +48,18 @@ class InputView {
             y.toInt() !in 1..15 -> null
             else -> y.toInt() - 1
         }
+    }
+
+    companion object {
+        private const val HEADER_TURN = "%s의 차례입니다.%s"
+        private const val HEADER_INPUT_POSITION = "위치를 입력하세요 : "
+
+        private const val COLOR_BLACK = "흑"
+        private const val COLOR_WHITE = "백"
+        private const val COLOR_NONE = "NONE"
+
+        private const val ERROR_INVALID_INPUT = "유효하지 않은 입력입니다. 행과 열의 값을 공백없이 입력해주세요! (ex. E14)"
+        private const val ERROR_ROW_VALUE = "행 값은 알파벳 A~O 사이의 값이어야 합니다."
+        private const val ERROR_COL_VALUE = "열 값은 숫자 1~15 사이의 값이어야 합니다."
     }
 }
