e5d9310846b3d3bef7e0249390262f7421f19f06
Hogu59 <dbstjdguswkd@naver.com>
2024-03-26 00:57:34 +0900

    remove(Rule): remove detail rule in Rule interface

--- src/main/kotlin/omok/model/Rule.kt
+++ src/main/kotlin/omok/model/Rule.kt
@@ -5,19 +5,4 @@ interface Rule {
         stones: Stones,
         lastPlacedStone: Stone,
     ): Boolean
-
-    fun isThreeThree(
-        board: List<List<Int>>,
-        stone: Stone,
-    ): Boolean
-
-    fun isFourFour(
-        board: List<List<Int>>,
-        stone: Stone,
-    ): Boolean
-
-    fun isMoreThanFive(
-        board: List<List<Int>>,
-        stone: Stone,
-    ): Boolean
 }
