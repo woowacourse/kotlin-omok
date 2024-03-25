2fef7fd43f1f9919f6e565b36d57b5dec1f7e5dc
haeum808 <haeum808@gmail.com>
2024-03-25 11:32:40 +0900

    refactor: 상수 네이밍 더 명시적으로 변경

--- src/main/kotlin/omok/view/input/ConsoleInputView.kt
+++ src/main/kotlin/omok/view/input/ConsoleInputView.kt
@@ -7,10 +7,10 @@ class ConsoleInputView : InputView {
         print("위치를 입력하세요: ")
         val input = readln()
         val inputX = input[0]
-        val isInputXValid = inputX in INPUTX_SIZE_RANGE
+        val isInputXValid = inputX in INPUT_X_SIZE_RANGE
 
         val inputY = input.substring(1).toIntOrNull() ?: return getStonePoint()
-        val isInputYValid = inputY in INPUTY_SIZE_RANGE
+        val isInputYValid = inputY in INPUT_Y_SIZE_RANGE
 
         if (!(isInputXValid && isInputYValid)) {
             return getStonePoint()
@@ -25,12 +25,12 @@ class ConsoleInputView : InputView {
     private fun alphabetToInt(alphabet: Char): Int = alphabet - 'A' + 1
 
     companion object {
-        private const val INPUTY_MIN_SIZE = 1
-        private const val INPUTY_MAX_SIZE = 15
-        private val INPUTY_SIZE_RANGE = INPUTY_MIN_SIZE..INPUTY_MAX_SIZE
+        private const val INPUT_Y_MIN_SIZE = 1
+        private const val INPUT_Y_MAX_SIZE = 15
+        private val INPUT_Y_SIZE_RANGE = INPUT_Y_MIN_SIZE..INPUT_Y_MAX_SIZE
 
-        private const val INPUTX_MIN_SIZE = 'A'
-        private const val INPUTX_MAX_SIZE = 'O'
-        private val INPUTX_SIZE_RANGE = INPUTX_MIN_SIZE..INPUTX_MAX_SIZE
+        private const val INPUT_X_MIN_SIZE = 'A'
+        private const val INPUT_X_MAX_SIZE = 'O'
+        private val INPUT_X_SIZE_RANGE = INPUT_X_MIN_SIZE..INPUT_X_MAX_SIZE
     }
 }
