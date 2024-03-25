f88a69dd58cb058d76357f506cc140c37a581d30
kimhm0728 <kimhm0728@naver.com>
2024-03-25 20:42:49 +0900

    remove: 사용하지 않는 함수 삭제

--- src/main/kotlin/omok/model/OmokPlayers.kt
+++ src/main/kotlin/omok/model/OmokPlayers.kt
@@ -6,8 +6,4 @@ class OmokPlayers(val blackStonePlayer: Player, val whiteStonePlayer: Player) {
     fun next(recentPlayer: Player): Player {
         return if (recentPlayer == blackStonePlayer) whiteStonePlayer else blackStonePlayer
     }
-
-    fun winningFinishType(winningPlayer: Player): FinishType {
-        return if (winningPlayer == blackStonePlayer) FinishType.BLACK_PLAYER_WIN else FinishType.WHITE_PLAYER_WIN
-    }
 }
