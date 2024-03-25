96836357edde7cadd7cca257904f5c3c0c7db60a
Hogu59 <dbstjdguswkd@naver.com>
2024-03-26 00:53:56 +0900

    test(StonesTest): test add function
    add function was moved from Board to Stones.
    So, test code was also moved.

--- src/test/kotlin/omok/model/StonesTest.kt
+++ src/test/kotlin/omok/model/StonesTest.kt
@@ -20,6 +20,21 @@ class StonesTest {
     }
 
     @Test
+    fun `중복된 Point의 Stone을 add 할 경우, Stones의 사이즈는 증가하지 않는다`() {
+        // when
+        val stone = BLACK_A_1
+        val stones = Stones()
+        stones.add(stone)
+        val size = stones.stones.size
+
+        // given
+        stones.add(stone)
+
+        // when
+        assertThat(stones.stones.size).isEqualTo(size)
+    }
+
+    @Test
     fun `Stones 의 마지막 스톤을 확인할 수 있다`() {
         val stones = Stones()
         stones.add(FIRST_ROW_FIRST_COL)
