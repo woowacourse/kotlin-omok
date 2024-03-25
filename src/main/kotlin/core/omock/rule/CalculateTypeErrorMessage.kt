06cfbb2e593172ffa7b05a9aeb1e5ae038fa8a22
JunJangE <wnswkd486@gmail.com>
2024-03-25 20:46:01 +0900

    chore: 필요없는 코드 제거

--- src/main/kotlin/core/omock/rule/CalculateTypeErrorMessage.kt
+++
@@ -1,8 +0,0 @@
-package core.omock.rule
-
-enum class CalculateTypeErrorMessage(val message: String) {
-    THREE_TO_THREE_COUNT_ERROR("4-4 금수를 어겼습니다."),
-    FOUR_TO_FOUR_COUNT_ERROR("열린 4-4 금수를 어겼습니다."),
-    IS_RESERVE_TWO_AND_THREE_ERROR("장목 금수를 어겼습니다."),
-    IS_CLEAR_FOUR_TO_FOUR_COUNT_ERROR("3-3 금수를 어겼습니다."),
-}
