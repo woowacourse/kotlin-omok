f516fe26cb118d5085876970d47590178f682103
JoYehyun <jo990714@naver.com>
2024-03-26 01:04:29 +0900

    refactor: 유효하지 않는 입력값에 대한 재입력 처리

--- src/main/kotlin/omok/controller/Controller.kt
+++ src/main/kotlin/omok/controller/Controller.kt
@@ -1,12 +1,15 @@
 package omok.controller
 
 import omok.model.OmokGame
+import omok.model.Point
 import omok.view.InputView
 import omok.view.OutputView
 
 class Controller {
     fun run() {
         OutputView.printGameStart()
-        OmokGame().play(OutputView::printBoard, OutputView::printTurn, InputView::readPoint)
+        OmokGame().play(OutputView::printBoard, OutputView::printTurn, ::getPoint)
     }
+
+    private fun getPoint(): Point = runCatching { InputView.readPoint() }.getOrNull() ?: getPoint()
 }
--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -6,7 +6,7 @@ object InputView {
     fun readPoint(): Point {
         print("위치를 입력하세요 : ")
         val input = readln()
-        val x: Int = (input[0]) - 'A'
+        val x: Int = (input[0].uppercaseChar()) - 'A'
         val y: Int = input.substring(1).toIntOrNull()?.let { it - 1 } ?: -1
         return Point(x, y)
     }
