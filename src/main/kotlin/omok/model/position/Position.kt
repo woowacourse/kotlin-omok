7eb9265180d3c8ebf0b34e788c4fcb4b129a6477
hxeyexn <kck85075@kumoh.ac.kr>
2024-03-26 15:51:28 +0900

    feat(Position): 렌주룰 확인 책임을 RulesAdapter로 위임

--- src/main/kotlin/omok/model/position/Position.kt
+++ src/main/kotlin/omok/model/position/Position.kt
@@ -1,18 +1,9 @@
 package omok.model.position
 
-import omok.model.rule.DoubleFourChecker
-import omok.model.rule.DoubleThreeChecker
-import omok.model.rule.ExceedFiveChecker
+import omok.model.rule.RulesAdapter
 
 data class Position(val row: Row, val column: Column) {
     override fun toString(): String = "${Row.X_AXIS_START + row.value}${column.value + 1}"
 
-    fun checkForbidden(): Boolean =
-        DoubleThreeChecker.isDoubleThree(this) ||
-            DoubleFourChecker.isDoubleFour(this) ||
-            ExceedFiveChecker.isMoreThanFive(this, EXCEED_FIVE_PRECONDITION)
-
-    companion object {
-        private const val EXCEED_FIVE_PRECONDITION = 5
-    }
+    fun checkForbidden(): Boolean = RulesAdapter.checkForbidden(this)
 }
