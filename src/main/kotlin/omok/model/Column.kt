410f4d6d1e934ccd97ce99f67c98110fb9827c74
Junyoung-WON <wjyo60@gmail.com>
2024-03-26 17:45:42 +0900

    refactor: Row, Column과 Board와의 의존성 제거 및 InputView에서 입력의 검증 수행
    - 돌은 Board의 크기와 범위를 알 필요가 없으므로, Row, Column과 Board 사이의 의존성을 제거한다.
    - Column과 InputView의 의존성을 제거한다.
    - 좌표 입력 시 알파벳의 입력은 View에서의 기능이며, Column이 갖는 위치 값과 직접적인 연관이 없어야 한다.

--- src/main/kotlin/omok/model/Column.kt
+++ src/main/kotlin/omok/model/Column.kt
@@ -1,15 +1,3 @@
 package omok.model
 
-data class Column private constructor(val value: Int) {
-    companion object {
-        val COORDINATE_COL_RANGE = 'A'..'O'
-        const val ERROR_COLUMN_OUT_OF_RANGE = "열이 바둑판의 범위를 벗어났습니다."
-
-        fun from(col: String): Column {
-            require(col.first() in COORDINATE_COL_RANGE) {
-                ERROR_COLUMN_OUT_OF_RANGE
-            }
-            return Column(col.first() - 'A' + 1)
-        }
-    }
-}
+data class Column(val value: Int)
--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -6,6 +6,8 @@ import omok.model.Row
 
 object InputView {
     private const val MESSAGE_INPUT_COORDINATE = "위치를 입력하세요: "
+    private const val ERROR_INVALID_COLUMN_INPUT = "열의 입력은 영문자 한 글자여야 합니다."
+    private const val ERROR_INVALID_ROW_INPUT = "행의 입력은 문자가 아닌 정수이어야 합니다."
 
     fun inputStoneCoordinate(): Coordinate {
         print(MESSAGE_INPUT_COORDINATE)
@@ -21,9 +23,21 @@ object InputView {
     fun String.validateCoordinate(): Result<Coordinate> {
         return runCatching {
             Coordinate(
-                Row.from(this.substring(1)),
-                Column.from(this.substring(0, 1)),
+                Row(validateStringToInt(this.substring(1))),
+                Column(changeAlphabetToInt(this.substring(0, 1))),
             )
         }
+    }
+
+    fun validateStringToInt(input: String): Int {
+        requireNotNull(input.toIntOrNull()) { ERROR_INVALID_ROW_INPUT }
+        return input.toInt()
+    }
+
+    fun changeAlphabetToInt(input: String): Int {
+        require(input.length == 1) { ERROR_INVALID_COLUMN_INPUT }
+        require(input.matches("^[a-zA-Z]$".toRegex())) { ERROR_INVALID_COLUMN_INPUT }
+        return if (input.matches("^[a-z]$".toRegex())) input.first() - 'a' + 1
+        else input.first() - 'A' + 1
     }
 }
