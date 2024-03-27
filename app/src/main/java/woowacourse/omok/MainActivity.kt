dd21e30ad831e450256df8d2ed76f5e202308a33
Namyunsuk <yunsuk0328@naver.com>
2024-03-27 10:41:40 +0900

    feat: 돌을 놓을 수 있는 기능 및 승리 판단 기능 구현

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -6,23 +6,66 @@ import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import com.google.android.material.snackbar.Snackbar
 import omok.model.position.Position
 import omok.model.stone.BlackStone
 import omok.model.stone.GoStone
+import omok.model.stone.StoneType
 import omok.model.stone.WhiteStone
 
 class MainActivity : AppCompatActivity() {
+    private var stone: GoStone = BlackStone
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val board = findViewById<TableLayout>(R.id.board)
-        board
-            .children
+        startOmokGame(board)
+    }
+
+    private fun startOmokGame(board: TableLayout) {
+        board.children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEachIndexed { index, view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { index, view ->
+                view.setOnClickListener {
+                    handleStonePlacement(board, index, view)
+                }
+            }
+    }
+
+    private fun handleStonePlacement(
+        board: TableLayout,
+        index: Int,
+        view: ImageView,
+    ) {
+        val currentStone = stone.putStone(indexAdapter(index))
+        currentStone?.let {
+            view.setImageResource(stone.imageView())
+            if (checkOmok(board, index, view)) return
+            stone = changeStone(currentStone)
+        }
+    }
+
+    private fun checkOmok(
+        board: TableLayout,
+        index: Int,
+        view: ImageView,
+    ): Boolean {
+        if (stone.findOmok(indexAdapter(index))) {
+            val snackBar = Snackbar.make(view, "${stone.value()} 승리", Snackbar.LENGTH_INDEFINITE)
+            snackBar.setAction("확인") {
+            }
+            snackBar.show()
+            return true
+        }
+        return false
+    }
+
+    private fun changeStone(currentStone: StoneType): GoStone {
+        return if (currentStone == StoneType.BLACK_STONE) BlackStone else WhiteStone
     }
 
     private fun indexAdapter(index: Int): Position {
