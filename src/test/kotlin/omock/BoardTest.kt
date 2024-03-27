2abb39161a28d6a9c95196630b525af86d3e5028
김진우 <jinwoo3661@naver.com>
2024-03-27 16:08:32 +0900

    test: 실패하는 테스트 생성

--- src/test/kotlin/omock/BoardTest.kt
+++ src/test/kotlin/omock/BoardTest.kt
@@ -9,6 +9,7 @@ import omock.model.search.Direction
 import omock.model.stone.Stone
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class BoardTest {
     @Test
@@ -19,7 +20,7 @@ class BoardTest {
         board.makeStones(
             player = player,
             coordinates =
-                arrayOf("2B", "1B", "2A", "3A", "4A", "5A"),
+            arrayOf("2B", "1B", "2A", "3A", "4A", "5A"),
         )
 
         val stone = Stone.from(Row("1"), Column("A"))
@@ -33,5 +34,35 @@ class BoardTest {
         assertThat(visited[Direction.BOTTOM_LEFT]?.count).isEqualTo(0)
         assertThat(visited[Direction.LEFT]?.count).isEqualTo(0)
         assertThat(visited[Direction.LEFT_TOP]?.count).isEqualTo(0)
+    }
+
+    @Test
+    fun `플레이어가 이미 돌이 놓여 있는 위치에 돌을 놓으려 할 때, 예외를 던진다`() {
+        val player = WhitePlayer()
+        val board = Board.from()
+        board.makeStones(
+            player = player,
+            coordinates =
+                arrayOf("1A")
+        )
+
+        val stone = Stone.from(Row("1"), Column("A"))
+        assertThrows<IllegalArgumentException> { board.setStoneState(player, stone)}
+    }
+
+
+    @Test
+    fun `플레이어가 인덱스를 벗어난 위치에 돌을 놓을 때, 예외를 던진다`() {
+        val player = WhitePlayer()
+        val board = Board.from()
+        board.makeStones(
+            player = player,
+            coordinates =
+            arrayOf("1A")
+        )
+        assertThrows<IllegalArgumentException> {
+            val stone = Stone.from(Row("16"), Column("A"))
+            board.setStoneState(player, stone)
+        }
     }
 }
