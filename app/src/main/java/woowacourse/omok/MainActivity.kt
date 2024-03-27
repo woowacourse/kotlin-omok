14ea6332b02c8aa7d1fc2398e97ccad3e0897a00
JunJangE <wnswkd486@gmail.com>
2024-03-27 15:54:14 +0900

    chore: 안드로이드 스튜디오에서 오목 세팅

---
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -0,0 +1,25 @@
+package woowacourse.omok
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.TableRow
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import core.omock.rule.OMockRule
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val board = findViewById<TableLayout>(R.id.board)
+        OMockRule
+        board
+            .children
+            .filterIsInstance<TableRow>()
+            .flatMap { it.children }
+            .filterIsInstance<ImageView>()
+            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+    }
+}
