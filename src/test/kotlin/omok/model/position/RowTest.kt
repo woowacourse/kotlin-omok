415e7ee4543e884828b4b3885d84c9215a4732da
hxeyexn <kck85075@kumoh.ac.kr>
2024-03-27 11:42:49 +0900

    refactor: Row, Column 유효성 검사 테스트명 수정

--- src/test/kotlin/omok/model/position/RowTest.kt
+++ src/test/kotlin/omok/model/position/RowTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 
 class RowTest {
     @Test
-    fun `플레이어가 유효하지 않은 x축 위치를 입력한다면 예외가 발생한다`() {
+    fun `플레이어가 유효하지 않은 Row 값을 입력한다면 예외가 발생한다`() {
         assertThatThrownBy { Row('P') }
             .isExactlyInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("유효하지 않은 x축입니다. 현재 입력 값: P\n")
