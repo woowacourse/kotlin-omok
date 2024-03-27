92229cabd38758221755789eb9a1bfa69ced138d
songpink <jejeje04@naver.com>
2024-03-27 23:44:13 +0900

    feat: 오목 게임을 MainActivity에 구현

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -4,20 +4,44 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import woowacourse.omok.domain.omok.model.Board
+import woowacourse.omok.domain.omok.model.Color
+import woowacourse.omok.domain.omok.model.GameResult
+import woowacourse.omok.domain.omok.model.Position
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        lateinit var eachPlacedPosition: Position
         val board = findViewById<TableLayout>(R.id.board)
+        val androidBoard = Board()
+        val explainMessage = findViewById<TextView>(R.id.expalin_message)
         board
             .children
             .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { rowIndex, tableRow ->
+                tableRow.children
+                    .filterIsInstance<ImageView>()
+                    .forEachIndexed { colIndex, imageView ->
+                        imageView.setOnClickListener {
+                            when (androidBoard.turn) {
+                                Color.BLACK -> imageView.setImageResource(R.drawable.black_stone)
+                                Color.WHITE -> imageView.setImageResource(R.drawable.white_stone)
+                                Color.NONE -> return@setOnClickListener
+                            }
+                            eachPlacedPosition =
+                                Position.of(rowIndex + 1, colIndex.toChar() + 'A'.code)
+                            androidBoard.place(eachPlacedPosition)
+                            if (androidBoard.getGameResult(eachPlacedPosition) != GameResult.PROCEEDING) {
+                                explainMessage.text = "게임끝"
+                            }
+                        }
+                    }
+            }
     }
 }
