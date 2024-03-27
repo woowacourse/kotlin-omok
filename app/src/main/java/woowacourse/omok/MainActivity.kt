a05b2ebef422146597c1bab0f33a484edeae4727
Namyunsuk <yunsuk0328@naver.com>
2024-03-27 10:34:31 +0900

    feat: 판의 인덱스를 좌표로 바꾸는 기능 구현
    - index를 domain의 Position으로 변환

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import omok.model.position.Position
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +19,17 @@ class MainActivity : AppCompatActivity() {
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { index, view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+    }
+
+    private fun indexAdapter(index: Int): Position {
+        val row = FIRST_COLUMN + (index % BOARD_SIZE)
+        val column = BOARD_SIZE - (index / BOARD_SIZE)
+        return Position.of(row, column)
+    }
+
+    companion object {
+        private const val BOARD_SIZE = 15
+        private const val FIRST_COLUMN = 'A'
     }
 }
