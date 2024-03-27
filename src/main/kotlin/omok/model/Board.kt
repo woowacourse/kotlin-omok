ff2b8d4e72c1f5403c1843290393946b6766d0c8
gaeun <geun5744@gmail.com>
2024-03-27 22:16:46 +0900

    refactor: change property use

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -9,7 +9,7 @@ class Board {
         get() = RenjuRuleAdapter()
 
     fun add(stone: Stone) {
-        require(!rule.isInValid(stones, stone)) { "렌주룰을 어겼습니다." }
+        require(!rule.isInValid(_stones, stone)) { "렌주룰을 어겼습니다." }
         checkDuplicate(stone)
 
         _stones.add(stone)
@@ -17,11 +17,11 @@ class Board {
 
     private fun checkDuplicate(stone: Stone) {
         require(
-            stones.all { it.point != stone.point }
+            _stones.all { it.point != stone.point }
         ) { "중복된 위치입니다." }
     }
 
-    fun lastStone(): Stone? = stones.lastOrNull()
+    fun lastStone(): Stone? = _stones.lastOrNull()
 
     companion object {
         private const val BOARD_SIZE = 15
