8e14cbe38f7551ea6e27714eaa01c3aa8a925f31
Namyunsuk <yunsuk0328@naver.com>
2024-03-27 14:13:29 +0900

    feat: DB 데이터들을 가져오는 기능 구현

--- app/src/main/java/woowacourse/omok/OmokDbHelper.kt
+++ app/src/main/java/woowacourse/omok/OmokDbHelper.kt
@@ -1,5 +1,6 @@
 package woowacourse.omok
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
@@ -31,5 +32,23 @@ class OmokDbHelper(context: Context) : SQLiteOpenHelper(context, "omok.db", null
         values.put("${OmokContract.COLUMN_INDEX}", index)
         values.put("${OmokContract.COLUMN_COLOR}", stoneColor)
         db.insert(OmokContract.TABLE_NAME, null, values)
+    }
+
+    @SuppressLint("Range")
+    fun selectStonesInfo(): List<Pair<Int, String>> {
+        val db = this.readableDatabase
+        val dataList = mutableListOf<Pair<Int, String>>()
+        db?.let {
+            val cursor = it.rawQuery("SELECT * FROM ${OmokContract.TABLE_NAME}", null)
+
+            while (cursor.moveToNext()) {
+                val index = cursor.getInt(cursor.getColumnIndex(OmokContract.COLUMN_INDEX))
+                val color = cursor.getString(cursor.getColumnIndex(OmokContract.COLUMN_COLOR))
+                dataList.add(Pair(index, color))
+            }
+            cursor.close()
+        }
+
+        return dataList
     }
 }
