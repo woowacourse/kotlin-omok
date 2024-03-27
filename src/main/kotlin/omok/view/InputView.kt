bd255e1ab688442a1d4da776921fadd19710a52c
Hogu59 <dbstjdguswkd@naver.com>
2024-03-27 16:56:25 +0900

    refactor(InputView): Change nullable return type

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -1,5 +1,6 @@
 package omok.view
 
+import omok.model.Board
 import omok.model.Color
 import omok.model.Point
 import omok.model.Stone
@@ -7,19 +8,22 @@ import omok.model.Stone
 class InputView {
     fun getPointOrNull(
         color: Color,
-        lastStone: Stone?,
-    ): Point? {
-        println(HEADER_TURN.format(colorToString(color), convertPoint(lastStone)))
+        board: Board,
+    ): Point {
+        println(HEADER_TURN.format(colorToString(color), convertPoint(board.lastStone())))
         print(HEADER_INPUT_POSITION)
-        val input = readln().validation() ?: return null
-        return input.toPointOrNull()
+        val input = readln().validation()
+        return input.toPointOrNull(board.size)
     }
 
-    private fun String.validation(): String? = if (this.length !in 1..3) null else this
+    private fun String.validation(): String {
+        require(this.length in 1..3) { ERROR_INVALID_INPUT }
+        return this
+    }
 
-    private fun String.toPointOrNull(): Point? {
-        val x: Int = convertX(this.first()) ?: return null
-        val y: Int = convertY(this.substring(1, this.length)) ?: return null
+    private fun String.toPointOrNull(max: Int): Point {
+        val x: Int = convertX(this.first(), max)
+        val y: Int = convertY(this.substring(1, this.length), max)
         return Point(x, y)
     }
 
@@ -27,7 +31,6 @@ class InputView {
         return when (color) {
             Color.BLACK -> COLOR_BLACK
             Color.WHITE -> COLOR_WHITE
-            else -> COLOR_NONE
         }
     }
 
@@ -36,18 +39,21 @@ class InputView {
         return " (마지막 돌의 위치: ${('A'.code + stone.point.row).toChar()}${stone.point.col + 1})"
     }
 
-    private fun convertX(x: Char): Int? {
+    private fun convertX(
+        x: Char,
+        max: Int,
+    ): Int {
         val res = x.uppercaseChar().code - 'A'.code
-        return if (res in 0..14) res else null
+        require(res in MIN_POINT until max) { ERROR_ROW_VALUE }
+        return res
     }
 
-    private fun convertY(y: String): Int? {
-        return when {
-            y.length !in 1..2 -> null
-            y.toIntOrNull() == null -> null
-            y.toInt() !in 1..15 -> null
-            else -> y.toInt() - 1
-        }
+    private fun convertY(
+        y: String,
+        max: Int,
+    ): Int {
+        require(y.toIntOrNull() != null && y.toInt() - 1 in MIN_POINT until max) { ERROR_COL_VALUE }
+        return y.toInt() - 1
     }
 
     companion object {
@@ -56,7 +62,8 @@ class InputView {
 
         private const val COLOR_BLACK = "흑"
         private const val COLOR_WHITE = "백"
-        private const val COLOR_NONE = "NONE"
+
+        private const val MIN_POINT = 0
 
         private const val ERROR_INVALID_INPUT = "유효하지 않은 입력입니다. 행과 열의 값을 공백없이 입력해주세요! (ex. E14)"
         private const val ERROR_ROW_VALUE = "행 값은 알파벳 A~O 사이의 값이어야 합니다."
