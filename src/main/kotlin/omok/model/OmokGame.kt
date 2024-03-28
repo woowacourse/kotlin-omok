60c351b958affa24437fa7ac4fd0c39765bc12fa
sh1mj1 <7wlgns@gmail.com>
2024-03-28 17:55:30 +0900

    feat(OmokGame): validPosition 을 생성자 파라미터로 가지는 새로운 오목 게임

--- src/main/kotlin/omok/model/OmokGame.kt
+++ src/main/kotlin/omok/model/OmokGame.kt
@@ -1,5 +1,45 @@
 package omok.model
 
+import omok.controller.ValidPosition
+
+class OmokGame2(
+    private val board: Board,
+    private val players: Players,
+    private val validPosition: List<ValidPosition>,
+) {
+    fun gameWinner(
+        nextStonePosition: (Player, Position?) -> Position,
+        nextStonePositionResult: () -> Unit,
+    ): Player {
+        var recentPlayer = players.firstOrderedPlayer()
+        var recentPosition: Position? = null
+
+        while (true) {
+            val next = recentPosition.next(recentPlayer, nextStonePosition)
+            if (next == recentPosition || next == null) {
+                continue
+            }
+            recentPosition = next
+            nextStonePositionResult()
+            if (recentPlayer.isWin(board, recentPosition)) break
+            recentPlayer = players.nextOrder(recentPlayer)
+        }
+        return recentPlayer
+    }
+
+    private fun Position?.next(
+        recentPlayer: Player,
+        nextStonePosition: (Player, Position?) -> Position,
+    ): Position? {
+        val nextPosition = nextStonePosition(recentPlayer, this)
+        if (validPosition.any { !it.valid(board, nextPosition, recentPlayer) }) {
+            return this // 현 위치ㅣ 리턴
+        }
+        board.place2(nextPosition, recentPlayer)
+        return nextPosition
+    }
+}
+
 class OmokGame(private val board: Board, private val players: Players) {
     fun gameWinner(
         nextStonePosition: (Player, Position?) -> Position,
