9abcd227555d3189481d7aaf0c25c6a4d941cbe1
Namyunsuk <yunsuk0328@naver.com>
2024-03-29 06:48:01 +0900

    test: Board테스트 작성
    - 리팩토링 과정에서 통과하지 못하는 테스트 수정
    - 새로 만든 기능 테스트 작성

--- model/src/test/kotlin/omok/model/board/BoardTest.kt
+++ model/src/test/kotlin/omok/model/board/BoardTest.kt
@@ -4,6 +4,7 @@ import omok.model.position.Position
 import omok.model.resetBoard
 import omok.model.resetPosition
 import omok.model.stone.BlackStone
+import omok.model.stone.StoneType
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -16,18 +17,88 @@ class BoardTest {
     }
 
     @Test
-    fun `마지막 돌이 없다면 null을 반환한다`() {
-        val actual = Board.getLastStonePosition()
-        val expected = null
+    fun `마지막 돌이 없다면 false를 반환한다`() {
+        val actual = Board.isLastPositionExist()
+        val expected = false
         assertThat(actual).isEqualTo(expected)
     }
 
     @Test
-    fun `마지막 돌이 있다면 돌의 위치를 반환한다`() {
+    fun `마지막 돌이 있다면 true를 반환한다`() {
+        val stone = BlackStone
+        stone.putStone(Position(1, 5))
+        val actual = Board.isLastPositionExist()
+        val expected = true
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `돌의 마지막 위치를 반환한다`() {
         val stone = BlackStone
         stone.putStone(Position(1, 5))
         val actual = Board.getLastStonePosition()
         val expected = Position(1, 5)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `마지막 돌의 위치를 변경한다`() {
+        val stone = BlackStone
+        stone.putStone(Position(1, 5))
+        Board.changeLstStonePosition(Position(2, 5))
+        val actual = Board.getLastStonePosition()
+        val expected = Position(2, 5)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `돌이 보드의 범위에 있는 경우 true를 반환한다`() {
+        val actual = Board.isPositionInRange(1, 5)
+        val expected = true
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `돌이 보드의 범위를 벗어난 경우 false를 반환한다`() {
+        val actual = Board.isPositionInRange(1, 16)
+        val expected = false
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `특정 위치의 돌의 색이 같은 경우 true를 반환한다`() {
+        val stone = BlackStone
+        stone.putStone(Position(1, 5))
+        val actual = Board.isSameStone(5, 1, StoneType.BLACK_STONE)
+        val expected = true
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `특정 위치의 돌의 색이 다른 경우 false를 반환한다`() {
+        val stone = BlackStone
+        stone.putStone(Position(1, 5))
+        val actual = Board.isSameStone(5, 1, StoneType.WHITE_STONE)
+        val expected = false
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `특정 위치의 돌의 색을 가져온다`() {
+        val stone = BlackStone
+        stone.putStone(Position(1, 5))
+        val actual = Board.getStoneType(5, 1)
+        val expected = StoneType.BLACK_STONE
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `판을 초기화 시킨다`() {
+        val stone = BlackStone
+        stone.putStone(Position(1, 5))
+        Board.resetBoard()
+        val actual = Board.getStoneType(5, 1)
+        val expected = StoneType.NONE
         assertThat(actual).isEqualTo(expected)
     }
 }
