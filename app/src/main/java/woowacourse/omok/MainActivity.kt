9a0860065cbc792878fb656f03e5dd43d9be0d80
ii2001 <iivks2001@gmail.com>
2024-03-28 18:37:55 +0900

    :recycle: :truck: 데이터 베이스 로드 and 세이브 완성

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -1,47 +1,48 @@
 package woowacourse.omok
 
-import Controller
+import android.content.ContentValues
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
-import woowacourse.omok.omok.model.board.CoordsNumber
-import woowacourse.omok.omok.model.board.Position
-import woowacourse.omok.omok.model.board.Stone
-import woowacourse.omok.omok.model.omokGame.OmokGame
-import woowacourse.omok.omok.model.omokGame.OmokGameState
-import woowacourse.omok.omok.view.OutputView
+import woowacourse.omok.R
+import woowacourse.omok.model.board.CoordsNumber
+import woowacourse.omok.model.board.Position
+import woowacourse.omok.model.board.Stone
+import woowacourse.omok.model.omokGame.Board.Companion.BOARD_SIZE
+import woowacourse.omok.model.omokGame.OmokGame
+import woowacourse.omok.view.OutputView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dbHelper: DatabaseHelper
     private val outputView = OutputView()
-    val omok = OmokGame(outputView)
-
+    val omok = OmokGame(this.outputView)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val board = findViewById<TableLayout>(R.id.board)
+        dbHelper = DatabaseHelper(this)
+        loadGame()
         board.children.filterIsInstance<TableRow>().forEachIndexed { rowIndex, rows ->
             rows.children.filterIsInstance<ImageView>().forEachIndexed { columIndex, view ->
-                    view.setOnClickListener {
-                        if (omok.board.isRunning()) {
+                view.setOnClickListener {
+                    if (omok.board.isRunning()) {
                         if (requestPlayerMove(
                                 omok.currentStone, CoordsNumber(rowIndex), CoordsNumber(columIndex)
                             )
                         ) {
-                            if (omok.currentStone != Stone.BLACK) view.setImageResource(R.drawable.black_stone)
-                            else view.setImageResource(R.drawable.white_stone)
+                            val resId =
+                                if (omok.currentStone == Stone.BLACK) R.drawable.black_stone else R.drawable.white_stone
+                            view.setImageResource(resId)
                         }
                     }
+                    saveGame()
                 }
             }
         }
-    }
-
-    fun start(currentStone: Stone) {
-        omok.endGame(omok.startGame(currentStone))
     }
 
     fun requestPlayerMove(
@@ -67,4 +68,59 @@ class MainActivity : AppCompatActivity() {
             return false
         }
     }
+
+    private fun saveGame() {
+        val db = dbHelper.writableDatabase
+        db.beginTransaction()
+        try {
+            db.delete("GameBoard", null, null) // 기존 데이터 삭제
+            for (i in omok.board.gameBoard.indices) {
+                for (j in omok.board.gameBoard[i].indices) {
+                    val stoneType = when (omok.board.gameBoard[i][j]) {
+                        Stone.EMPTY -> 0
+                        Stone.BLACK -> 1
+                        Stone.WHITE -> 2
+                    }
+                    val values = ContentValues().apply {
+                        put("rowIndex", i)
+                        put("columnIndex", j)
+                        put("stoneType", stoneType)
+                    }
+                    db.insert("GameBoard", null, values)
+                }
+            }
+            db.setTransactionSuccessful()
+        } finally {
+            db.endTransaction()
+        }
+    }
+
+    private fun loadGame(): Array<Array<Stone>> {
+        val db = dbHelper.readableDatabase
+        val cursor = db.query(
+            "GameBoard",
+            arrayOf("rowIndex", "columnIndex", "stoneType"),
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+        val board = Array(BOARD_SIZE) { Array(BOARD_SIZE) { Stone.EMPTY } }
+        with(cursor) {
+            while (moveToNext()) {
+                val rowIndex = getInt(getColumnIndexOrThrow("rowIndex"))
+                val columnIndex = getInt(getColumnIndexOrThrow("columnIndex"))
+                val stoneType = getInt(getColumnIndexOrThrow("stoneType"))
+                board[rowIndex][columnIndex] = when (stoneType) {
+                    1 -> Stone.BLACK
+                    2 -> Stone.WHITE
+                    else -> Stone.EMPTY
+                }
+            }
+            close()
+        }
+        return board
+    }
+
 }
