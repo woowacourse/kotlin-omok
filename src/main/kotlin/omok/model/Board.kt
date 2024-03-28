461ce0c909eb5f165c52e0ab903522dec0031b33
sh1mj1 <7wlgns@gmail.com>
2024-03-28 15:32:36 +0900

    feat(Board): Player2 가 보드에 돌을 둔다

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -16,6 +16,16 @@ class Board(private val _board: MutableMap<Position, Stone> = initBoard()) {
         _board[position] = player.stone
     }
 
+    fun place(
+        position: Position,
+        player: Player2,
+    ) {
+        check(find(position) == Stone.NONE) { "이미 바둑돌이 있는 위치입니다." }
+        check(player.canPlace(this, position)) { "바둑돌을 놓을 수 없는 위치입니다." }
+
+        _board[position] = player.stone
+    }
+
     fun find(position: Position): Stone = _board[position] ?: throw IllegalArgumentException("올바르지 않은 위치입니다.")
 
     companion object {
--- src/test/kotlin/omok/model/BoardTest.kt
+++ src/test/kotlin/omok/model/BoardTest.kt
@@ -1,7 +1,9 @@
 package omok.model
 
 import omok.model.rule.ContinualStonesStandard
+import omok.model.rule.ForbiddenRules
 import omok.model.rule.GamePlayingRules
+import omok.model.rule.RuleAdapter2
 import omok.model.rule.ban.ForbiddenPlaces
 import omok.model.rule.winning.ContinualStonesWinningCondition
 import org.assertj.core.api.Assertions.assertThat
@@ -12,6 +14,7 @@ import org.junit.jupiter.api.assertThrows
 class BoardTest {
     private lateinit var emptyBoard: Board
     private lateinit var player: Player
+    private lateinit var player2: Player2
     private val playerStone = Stone.BLACK
 
     @BeforeEach
@@ -25,11 +28,27 @@ class BoardTest {
                     ForbiddenPlaces(),
                 ),
             )
+        player2 =
+            Player2(
+                playerStone,
+                RuleAdapter2(
+                    ContinualStonesWinningCondition(ContinualStonesStandard(5), ContinualStonesCondition.EXACT),
+                    ForbiddenRules(),
+                ),
+            )
     }
 
     @Test
     fun `오목판에 돌을 놓는다`() {
         emptyBoard.place(Position(3, 3), player)
+
+        val actual = emptyBoard.find(Position(3, 3))
+        assertThat(actual).isEqualTo(playerStone)
+    }
+
+    @Test
+    fun `오목판에 돌을 놓는다2`() {
+        emptyBoard.place(Position(3, 3), player2)
 
         val actual = emptyBoard.find(Position(3, 3))
         assertThat(actual).isEqualTo(playerStone)
