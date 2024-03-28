c7c2e137ae5c301ee2feb5c61d2450f507d8e44b
김진우 <jinwoo3661@naver.com>
2024-03-28 20:50:53 +0900

    feat: ui 게임을 구현할 컨트롤러 구현

---
+++ app/src/main/java/woowacourse/omok/controller/OMockGameController.kt
@@ -0,0 +1,52 @@
+package woowacourse.omok.controller
+
+import android.content.Context
+import android.widget.ImageView
+import android.widget.Toast
+import woowacourse.omok.model.player.BlackPlayer
+import woowacourse.omok.model.player.Player
+import woowacourse.omok.model.player.WhitePlayer
+import woowacourse.omok.model.stone.Stone
+import woowacourse.omok.view.OutputView
+import woowacourse.omok.R
+
+class OMockGameController(private val context: Context) : OMockGame() {
+    override fun executePlayerPickFailStep(throwable: Throwable) {
+        OutputView.outputFailureMessage(throwable)
+        Toast.makeText(context, throwable.message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun executePlayerSuccessStep(playerStone: Stone, player: Player) {
+        setLastPickStone(player)
+    }
+
+    private var lastPickImage: ImageView? = null
+
+    fun setLastPickImage(imageView: ImageView) {
+        lastPickImage = imageView
+    }
+
+    fun userTurnFlow(
+        player: Player,
+        playerPick: Pair<String, String>,
+    ) {
+        OutputView.outputUserTurn(player)
+        OutputView.outputLastStone(player.stoneHistory.lastOrNull())
+        val playerStone = player.turn {
+            playerPick
+        }
+        start(player = player, playerStone)
+    }
+
+    fun finishedGameFlow() {
+        OutputView.outputSuccessOMock()
+        Toast.makeText(context, OutputView.OUTPUT_SUCCESS_MESSAGE, Toast.LENGTH_LONG).show()
+    }
+
+    private fun setLastPickStone(player: Player) {
+        when (player) {
+            is BlackPlayer -> lastPickImage?.setImageResource(R.drawable.black_stone)
+            is WhitePlayer -> lastPickImage?.setImageResource(R.drawable.white_stone)
+        }
+    }
+}
