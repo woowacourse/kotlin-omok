6658f632a2405265073eac08679fd6199d640525
sh1mj1 <7wlgns@gmail.com>
2024-03-28 15:28:41 +0900

    feat(Player2): 돌과 룰을 가지는 플레이어를 생성한다
    - Player2 는 기존 Player 와 다르게 RuleAdapter2 를 규칙으록 가진다

--- src/main/kotlin/omok/model/Player.kt
+++ src/main/kotlin/omok/model/Player.kt
@@ -1,6 +1,19 @@
 package omok.model
 
 import omok.model.rule.GamePlayingRules
+import omok.model.rule.RuleAdapter2
+
+class Player2(val stone: Stone, private val rules: RuleAdapter2) {
+    fun canPlace(
+        board: Board,
+        position: Position,
+    ): Boolean = rules.validPosition(board, position)
+
+    fun isWin(
+        board: Board,
+        position: Position,
+    ): Boolean = rules.isWin(board, position)
+}
 
 class Player(val stone: Stone, private val gamePlayingRules: GamePlayingRules) {
     fun canPlace(
