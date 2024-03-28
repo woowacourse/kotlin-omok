4fb09f0e405bf0466df8285938a4486b048b0257
aprilgom <aprilgom@gmail.com>
2024-03-28 11:45:44 +0900

    refactor: board.place에만 Either 적용

--- src/main/kotlin/omok/model/turn/WhiteTurn.kt
+++ src/main/kotlin/omok/model/turn/WhiteTurn.kt
@@ -2,7 +2,6 @@ package omok.model.turn
 
 import omok.model.Board
 import omok.model.Either
-import omok.model.PlaceStoneError
 import omok.model.entity.Point
 import omok.model.entity.Stone
 import omok.model.entity.StoneColor
@@ -11,20 +10,23 @@ import omok.model.rule.FiveInRowRule
 class WhiteTurn(
     board: Board,
 ) : Turn(board) {
-    override fun placeStone(point: Point): Either<PlaceStoneError, Turn> {
+    override fun placeStone(point: Point, printError: (String) -> Unit): Turn {
         val stone = Stone(point, StoneColor.WHITE)
 
         val nextBoard =
             when (val placeResult = board.place(stone)) {
-                is Either.Left -> return Either.Left(placeResult.value)
+                is Either.Left -> {
+                    printError(placeResult.value.errorMessage)
+                    return this
+                }
                 is Either.Right -> placeResult.value
             }
 
         if (nextBoard.isFull() || FiveInRowRule.check(nextBoard)) {
-            return Either.Right(Finished(nextBoard))
+            return Finished(nextBoard)
         }
 
-        return Either.Right(BlackTurn(nextBoard))
+        return BlackTurn(nextBoard)
     }
 
     override fun color(): StoneColor {
