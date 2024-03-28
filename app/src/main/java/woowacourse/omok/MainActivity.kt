7dc077a0e172a2d76eeca5a2328481e4039cb39d
김진우 <jinwoo3661@naver.com>
2024-03-28 12:16:23 +0900

    faet: index 접근 기능 구현

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -1,11 +1,14 @@
 package woowacourse.omok
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import omock.model.position.Column
+import omock.model.position.Row
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +19,16 @@ class MainActivity : AppCompatActivity() {
         board
             .children
             .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed{ rowIndex , rows ->
+                rows.children.filterIsInstance<ImageView>()
+                    .forEachIndexed { columIndex, view ->
+                        view.setOnClickListener {
+                            Log.d("dlajls",Column.transformIndex(columIndex))
+                            Log.d("sdlfjsld",(Row.transformIndex(rowIndex)))
+                            view.setImageResource(R.drawable.black_stone)
+                        }
+                    }
+            }
+
     }
 }
