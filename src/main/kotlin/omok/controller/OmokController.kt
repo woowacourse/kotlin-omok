7cadf19c429a1030c076eb7b86911ea1f731d3d5
sh1mj1 <7wlgns@gmail.com>
2024-03-28 15:34:48 +0900

    feat(OmockController2): 추가한 ~2 객체들로 오목 서비스를 구현한다

--- src/main/kotlin/omok/controller/OmokController.kt
+++ src/main/kotlin/omok/controller/OmokController.kt
@@ -2,12 +2,45 @@ package omok.controller
 
 import omok.model.Board
 import omok.model.OmokGame
+import omok.model.OmokGame2
 import omok.model.Player
+import omok.model.Player2
 import omok.model.Players
+import omok.model.Players2
 import omok.model.Stone
 import omok.model.rule.GamePlayingRules
+import omok.model.rule.RuleAdapter2
 import omok.view.InputView
 import omok.view.OutputView
+
+class OmokController2(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+    blackStoneGamePlayingRules: RuleAdapter2,
+    whiteStoneGamePlayingRules: RuleAdapter2,
+) {
+    private val players =
+        Players2(
+            blackStonePlayer = Player2(Stone.BLACK, blackStoneGamePlayingRules),
+            whiteStonePlayer = Player2(Stone.WHITE, whiteStoneGamePlayingRules),
+        )
+
+    fun startGame() {
+        val board = initializedBoard()
+        val winner =
+            OmokGame2(board, players).gameWinner(
+                nextStonePosition = { player, position -> inputView.readStonePosition(player.stone, position) },
+                nextStonePositionResult = { outputView.printBoard(board) },
+                handleException = { exception -> outputView.printException(exception) },
+            )
+        outputView.printWinner(winner.stone)
+    }
+
+    private fun initializedBoard(): Board {
+        return Board().apply { outputView.printInitialGuide(this) }
+    }
+}
+
 
 class OmokController(
     private val inputView: InputView,
