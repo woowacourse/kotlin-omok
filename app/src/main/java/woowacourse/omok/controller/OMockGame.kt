0cdffb6248c89929dedc93a7d691abfaaaf45ae6
김진우 <jinwoo3661@naver.com>
2024-03-28 20:50:34 +0900

    feat: OMockGame 추상화 클래스 구현

---
+++ app/src/main/java/woowacourse/omok/controller/OMockGame.kt
@@ -0,0 +1,90 @@
+package woowacourse.omok.controller
+
+import woowacourse.omok.model.board.Board
+import woowacourse.omok.model.player.Player
+import woowacourse.omok.model.rule.LoadMap
+import woowacourse.omok.model.rule.OMockRule
+import woowacourse.omok.model.ruletype.FourToFourCount
+import woowacourse.omok.model.ruletype.IsClearFourToFourCount
+import woowacourse.omok.model.ruletype.IsReverseTwoAndThree
+import woowacourse.omok.model.ruletype.ThreeToThreeCount
+import woowacourse.omok.model.search.VisitedDirectionFirstClearResult
+import woowacourse.omok.model.search.VisitedDirectionResult
+import woowacourse.omok.model.stone.Stone
+import woowacourse.omok.view.OutputView
+
+abstract class OMockGame(
+    val board: Board = Board.from(),
+    private val oMockRule: OMockRule = OMockRule(
+        ruleTypes = listOf(
+            ThreeToThreeCount,
+            FourToFourCount,
+            IsClearFourToFourCount,
+            IsReverseTwoAndThree,
+        )
+    ),
+    private val loadMap: LoadMap = LoadMap(board.stoneStates),
+) {
+
+    init {
+        startGameBoard()
+    }
+
+    abstract fun executePlayerPickFailStep(throwable: Throwable)
+    abstract fun executePlayerSuccessStep(playerStone: Stone, player: Player)
+
+    private fun startGameBoard(){
+        OutputView.outputGameStart()
+    }
+
+    fun start(
+        player: Player,
+        playerStone: Stone,
+    ) {
+        playerPutStone(player, playerStone)
+            .onSuccess {
+                applyPlayerSelected(player, playerStone).onSuccess {
+                    executePlayerSuccessStep(playerStone, player)
+                }.onFailure {
+                    executePlayerTurnFailStep(playerStone, it)
+                }
+            }
+            .onFailure {
+                executePlayerPickFailStep(it)
+            }
+    }
+
+    private fun playerPutStone(
+        player: Player,
+        playerStone: Stone,
+    ): Result<Unit> {
+        return runCatching {
+            board.setStoneState(player, playerStone)
+        }
+    }
+
+    private fun applyPlayerSelected(
+        player: Player,
+        playerStone: Stone,
+    ): Result<Unit> {
+        return runCatching {
+            val visitedDirectionResult = VisitedDirectionResult(loadMap.loadMap(playerStone))
+            val visitedDirectionFirstClearResult =
+                VisitedDirectionFirstClearResult(loadMap.firstClearLoadMap(playerStone))
+            oMockRule.checkPlayerRules(
+                player,
+                visitedDirectionResult,
+                visitedDirectionFirstClearResult
+            )
+            board.applyPlayerJudgement(player, visitedDirectionResult)
+        }
+    }
+
+    private fun executePlayerTurnFailStep(
+        playerStone: Stone,
+        throwable: Throwable,
+    ) {
+        board.rollbackState(playerStone)
+        executePlayerPickFailStep(throwable)
+    }
+}
