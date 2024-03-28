97ae239dd03fd1e0224299fba6ccd3c26af8016e
somin <salth0601@gmail.com>
2024-03-28 14:25:06 +0900

    feat : 오목판이 클릭되면 해당 위치의 바둑돌 이미지를 갱신한다

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import omok.model.Board
@@ -12,7 +13,7 @@ import omok.model.state.BlackTurn
 import omok.model.state.GameState
 
 class MainActivity : AppCompatActivity() {
-    private var state: GameState = BlackTurn(RenjuRule, Board(emptyMap()))
+    private var gameState: GameState = BlackTurn(RenjuRule, Board(emptyMap()))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val board = makeBoardView()
         initBoardView(board)
+//        val boardView = OmokBoardView(makeBoardView(), gameState)
     }
 
     private fun makeBoardView() =
@@ -35,13 +37,36 @@ class MainActivity : AppCompatActivity() {
     private fun initBoardView(boardView: Sequence<ImageView>) {
         boardView.forEachIndexed { index, view ->
             view.setOnClickListener {
-                onPlace(index)
+                placeOmokStone(index, view)
             }
         }
     }
 
-    private fun onPlace(index: Int) {
-        val position = indexToPosition(index)
-        state = state.put(position)
+    private fun placeOmokStone(index: Int, view: ImageView) {
+        if (gameState is GameState.Finish) {
+            Toast.makeText(this, "${gameState.winner?.color}의 승리입니다.", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            onPlace(index, view)
+        }
+    }
+
+    private fun onPlace(
+        index: Int,
+        view: ImageView,
+    ) {
+        val position = convertIndexToPosition(index)
+        gameState = gameState.put(position)
+        updateStoneImage(index, view)
+    }
+
+    private fun updateStoneImage(
+        index: Int,
+        view: ImageView,
+    ) {
+        gameState.board[convertIndexToPosition(index)]?.run {
+            val targetColor = mapStoneColorToDrawable(this.color)
+            view.setImageResource(targetColor)
+        }
     }
 }
--- app/src/main/java/woowacourse/omok/Mapper.kt
+++ app/src/main/java/woowacourse/omok/Mapper.kt
@@ -1,9 +1,17 @@
 package woowacourse.omok
 
 import omok.model.Position
+import omok.model.StoneColor
 
-fun indexToPosition(index: Int): Position {
+fun convertIndexToPosition(index: Int): Position {
     val x = index % 15 + 1
     val y = (224 - index) / 15 + 1
     return Position.of(x, y)
+}
+
+fun mapStoneColorToDrawable(color: StoneColor): Int {
+    return when (color) {
+        StoneColor.BLACK -> R.drawable.black_stone
+        StoneColor.WHITE -> R.drawable.white_stone
+    }
 }
