4d8ed3b80f11620a00977756ba5f190acc59d1b4
sh1mj1 <7wlgns@gmail.com>
2024-03-28 15:30:25 +0900

    feat(OmokGame2): Players2 를 가지고 게임을 진행한다

--- src/main/kotlin/omok/model/OmokGame.kt
+++ src/main/kotlin/omok/model/OmokGame.kt
@@ -1,5 +1,37 @@
 package omok.model
 
+class OmokGame2(private val board: Board, private val players: Players2) {
+    fun gameWinner(
+        nextStonePosition: (Player2, Position?) -> Position,
+        nextStonePositionResult: () -> Unit,
+        handleException: (Exception) -> Unit,
+    ): Player2 {
+        var recentPlayer = players.firstOrderedPlayer()
+        var recentPosition: Position? = null
+
+        while (true) {
+            recentPosition = recentPosition.next(recentPlayer, nextStonePosition, handleException)
+            nextStonePositionResult()
+            if (recentPlayer.isWin(board, recentPosition)) break
+            recentPlayer = players.nextOrder(recentPlayer)
+        }
+        return recentPlayer
+    }
+
+    private fun Position?.next(
+        recentPlayer: Player2,
+        nextStonePosition: (Player2, Position?) -> Position,
+        handleException: (Exception) -> Unit,
+    ) = retryUntilNotException(
+        block = {
+            val nextPosition = nextStonePosition(recentPlayer, this)
+            board.place(nextPosition, recentPlayer)
+            nextPosition
+        },
+        handleException,
+    )
+}
+
 class OmokGame(private val board: Board, private val players: Players) {
     fun gameWinner(
         nextStonePosition: (Player, Position?) -> Position,
