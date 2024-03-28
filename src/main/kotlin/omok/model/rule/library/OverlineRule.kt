ef67633d6b7448adacb5d57f2440d67a77a44c35
sh1mj1 <7wlgns@gmail.com>
2024-03-28 14:31:07 +0900

    feat(OverlineRule2): 라이브러리 장목 규칙을 지키는지 판단한다

--- src/main/kotlin/omok/model/rule/library/OverlineRule.kt
+++ src/main/kotlin/omok/model/rule/library/OverlineRule.kt
@@ -1,5 +1,31 @@
 package omok.model.rule.library
 
+import kotlin.properties.Delegates
+
+class OverlineRule2 : OmokRule() {
+    var count by Delegates.notNull<Int>()
+
+    override fun abide(
+        board: List<List<Int>>,
+        position: Pair<Int, Int>,
+    ): Boolean = directions.all { direction -> !checkOverline(board, position, direction) }
+
+    private fun checkOverline(
+        board: List<List<Int>>,
+        position: Pair<Int, Int>,
+        direction: Pair<Int, Int>,
+    ): Boolean {
+        val oppositeDirection = direction.let { (dx, dy) -> Pair(-dx, -dy) }
+        val (stone1, blink1) = search(board, position, oppositeDirection)
+        val (stone2, blink2) = search(board, position, direction)
+
+        return when {
+            blink1 + blink2 == 0 && stone1 + stone2 >= count -> true
+            else -> false
+        }
+    }
+}
+
 class OverlineRule(private val count: Int) : OmokRule() {
     override fun abide(
         board: List<List<Int>>,
