11c63ba3e3b9ef64f773c5f5fc66e02113a544bd
songpink <jejeje04@naver.com>
2024-03-28 13:24:07 +0900

    refactor: 함수분리

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -16,11 +16,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        playUntilFinish()
+    }
 
-        lateinit var eachPlacedPosition: Position
+    private fun playUntilFinish() {
         val board = findViewById<TableLayout>(R.id.board)
         val androidBoard = Board()
         val explainMessage = findViewById<TextView>(R.id.expalin_message)
+        explainMessage.text = "흑의 차례입니다"
         board
             .children
             .filterIsInstance<TableRow>()
@@ -29,19 +32,24 @@ class MainActivity : AppCompatActivity() {
                     .filterIsInstance<ImageView>()
                     .forEachIndexed { colIndex, imageView ->
                         imageView.setOnClickListener {
-                            when (androidBoard.turn) {
-                                Color.BLACK -> imageView.setImageResource(R.drawable.black_stone)
-                                Color.WHITE -> imageView.setImageResource(R.drawable.white_stone)
-                                Color.NONE -> return@setOnClickListener
+                            runCatching {
+                                val eachPlacedPosition =
+                                    Position.of(rowIndex + 1, colIndex.toChar() + 'A'.code)
+                                androidBoard.place(eachPlacedPosition)
+                                when (androidBoard.lastTurn) {
+                                    Color.BLACK -> imageView.setImageResource(R.drawable.black_stone)
+                                    Color.WHITE -> imageView.setImageResource(R.drawable.white_stone)
+                                    Color.NONE -> return@setOnClickListener
+                                }
+                                if (androidBoard.getGameResult(eachPlacedPosition) != GameResult.PROCEEDING) {
+                                    explainMessage.text =
+                                        "${androidBoard.getGameResult(eachPlacedPosition).label}의 승리"
+                                    return@setOnClickListener
+                                }
+                                explainMessage.text = androidBoard.currentTurn.label + "의 차례입니다"
+                            }.onFailure {
+                                explainMessage.text = it.message
                             }
-                            eachPlacedPosition =
-                                Position.of(rowIndex + 1, colIndex.toChar() + 'A'.code)
-                            androidBoard.place(eachPlacedPosition)
-                            if (androidBoard.getGameResult(eachPlacedPosition) != GameResult.PROCEEDING) {
-                                explainMessage.text = "${androidBoard.getGameResult(eachPlacedPosition).label}의 승리"
-                                return@setOnClickListener
-                            }
-                            explainMessage.text = androidBoard.turn.label + "의 차례입니다"
                         }
                     }
             }
