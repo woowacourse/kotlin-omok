3aecc39902d584f0c5cdce7f071ac8c0745ecfd4
haeum808 <haeum808@gmail.com>
2024-03-29 16:38:51 +0900

    feat: 게임 시작시 스낵바로 문구 출력 기능 구현

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import woowacourse.omok.view.output.AndroidOutputView
+import woowacourse.omok.view.output.OutputView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val board = findViewById<TableLayout>(R.id.board)
+        val outputView: OutputView = AndroidOutputView(board)
+
         board
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEach { view ->
+                view.setOnClickListener {
+                    view.setImageResource(R.drawable.black_stone)
+                }
+            }
+
+        printStartGuide(outputView)
+    }
+
+    private fun printStartGuide(outputView: OutputView) {
+        outputView.printStartGuide()
     }
 }
