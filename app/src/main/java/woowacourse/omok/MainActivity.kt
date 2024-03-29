9fcd22694182c2ac2bac81c89821e770ccb8e807
김진우 <jinwoo3661@naver.com>
2024-03-29 18:00:32 +0900

    feat: 전체 삭제 기능 구현

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import woowacourse.omok.controller.OMockGameController
 import woowacourse.omok.data.OmokDAO
 
 class MainActivity : AppCompatActivity() {
-    private val oMockGameController = OMockGameController(this@MainActivity)
     private lateinit var mainBoard: TableLayout
+    private lateinit var resetButton: TextView
+    private val oMockGameController = OMockGameController(this@MainActivity)
     private val dao = OmokDAO(this@MainActivity)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun initView(){
         mainBoard = findViewById(R.id.board)
+        resetButton = findViewById(R.id.reset_button)
         loadBoard(mainBoard)
         mainBoard
             .children
@@ -34,10 +37,13 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
             }
+        resetButton.setOnClickListener {
+            resetBoard()
+        }
     }
 
     private fun resetBoard(){
-
+        dao.resetAllCoordinates()
     }
 
     private fun loadBoard(mainBoard : TableLayout) {
