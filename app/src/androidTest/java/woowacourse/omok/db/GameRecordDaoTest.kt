340998618f957e0fa121193bda281aba0f802d8c
murjune <sis9221302@gmail.com>
2024-03-30 14:08:55 +0900

    feat: GameRecordDao 구현

---
+++ app/src/androidTest/java/woowacourse/omok/db/GameRecordDaoTest.kt
@@ -0,0 +1,82 @@
+package woowacourse.omok.db
+
+import io.kotest.matchers.shouldBe
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import woowacourse.omok.fake.FakeOmokSQLiteHelper
+import woowacourse.omok.fixtures.context
+import woowacourse.omok.fixtures.gameTurnOf
+
+class GameRecordDaoTest {
+    private lateinit var dao: GameRecordDao
+
+    @BeforeEach
+    fun setUp() {
+        dao = GameRecordDao(FakeOmokSQLiteHelper(context))
+        dao.createTable()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        dao.dropTable()
+    }
+
+    @Test
+    @DisplayName("오목 게임 한 턴을 저장 한다")
+    fun `test`() {
+        // given
+        val gameTurnEntity = gameTurnOf(x = 1, y = 1)
+        val expected = 1L
+        // when
+        val actual = dao.saveTurn(gameTurnEntity)
+        // then
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    @DisplayName("오목 게임 기록을 저장한 후, 저장된 기록을 불러온다")
+    fun test2() {
+        // given
+        val gameTurns =
+            listOf(
+                gameTurnOf(x = 1, y = 1),
+                gameTurnOf(x = 1, y = 2),
+            )
+        val expectedIds = listOf(1L, 2L)
+        val expectedTurns =
+            listOf(
+                gameTurnOf(x = 1, y = 1, id = 1),
+                gameTurnOf(x = 1, y = 2, id = 2),
+            )
+        // when
+        val actualIds = dao.saveGameRecord(gameTurns)
+        val actualTurns = dao.selectAll()
+        // then
+        assertAll(
+            { assertThat(actualIds).isEqualTo(expectedIds) },
+            { assertThat(actualTurns).isEqualTo(expectedTurns) },
+        )
+    }
+
+    @Test
+    @DisplayName("오목 게임 기록을 저장한 후, table 을 초기화 한다")
+    fun test3() {
+        // given
+        val gameTurns =
+            listOf(
+                gameTurnOf(x = 1, y = 1),
+                gameTurnOf(x = 1, y = 2),
+            )
+        dao.saveGameRecord(gameTurns)
+        val expectedDeleteCount = 2
+        // when
+        val id = dao.resetTable()
+        // then
+        id shouldBe expectedDeleteCount
+    }
+}
---
+++ app/src/androidTest/java/woowacourse/omok/fixtures/ContextFixtures.kt
@@ -0,0 +1,6 @@
+package woowacourse.omok.fixtures
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+
+val context: Context get() = ApplicationProvider.getApplicationContext<Context>()
