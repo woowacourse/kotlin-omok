adf48a38eab6093c74b69c6a5bc4a638f3edc0b8
dpcks0509 <dpcks0509@gmail.com>
2024-03-30 14:13:37 +0900

    feat: OmokContract에서 table, column 정의

---
+++ app/src/main/java/woowacourse/omok/db/OmokContract.kt
@@ -0,0 +1,10 @@
+package woowacourse.omok.db
+
+class OmokContract {
+    companion object {
+        const val TABLE_NAME = "omok"
+        const val STONE_TYPE = "stoneType"
+        const val POINT_X = "pointX"
+        const val POINT_Y = "pointY"
+    }
+}
