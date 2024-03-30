f1599783bc0d5cbcfefe95a2afe8192601f2e8a2
somin <salth0601@gmail.com>
2024-03-31 05:15:58 +0900

    feat : 게임 상태 저장 및 초기화 기능 추가

--- app/src/main/java/woowacourse/omok/controller/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/controller/MainActivity.kt
@@ -1,67 +1,102 @@
 package woowacourse.omok.controller
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import woowacourse.omok.R
+import woowacourse.omok.db.OmokEntry
+import woowacourse.omok.db.OmokEntryDao
 import woowacourse.omok.model.Board
 import woowacourse.omok.model.Position
 import woowacourse.omok.model.rule.RenjuRule
 import woowacourse.omok.model.state.BlackTurn
 import woowacourse.omok.model.state.GameState
 import woowacourse.omok.util.convertIndexToPosition
+import woowacourse.omok.util.mapEntriesToBoard
+import woowacourse.omok.util.mapStoneColorToGameState
+import woowacourse.omok.util.mapStoneColorToString
 import woowacourse.omok.util.showToast
-import woowacourse.omok.view.OmokBoardView
-import woowacourse.omok.view.OmokView
+import woowacourse.omok.view.BoardView
 
-class MainActivity : AppCompatActivity(), OmokController {
+class MainActivity : AppCompatActivity() {
     private var gameState: GameState = BlackTurn(RenjuRule, Board(emptyMap()))
+    private val dbDao by lazy { OmokEntryDao(this) }
+    private lateinit var boardView: BoardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initView()
-    }
-
-    private fun initView() {
         setContentView(R.layout.activity_main)
+        boardView = BoardView(setupBoardView())
+        restoreSavedGame()
         initBoardView()
+        initGameResetButton()
     }
 
-    private fun initBoardView() {
-        val boardView = makeBoardView()
-        boardView.forEachIndexed { index, view ->
-            view.setOnClickListener {
-                placeOmokStone(index, view)
-            }
-        }
-    }
-
-    private fun makeBoardView() =
+    private fun setupBoardView(): List<ImageView> =
         findViewById<TableLayout>(R.id.board)
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
+            .toList()
 
-    private fun placeOmokStone(
-        index: Int,
-        view: ImageView,
-    ) {
+    private fun restoreSavedGame() {
+        dbDao.findAll().takeIf { it.isNotEmpty() }?.let { omokEntries ->
+            val savedBoard = mapEntriesToBoard(omokEntries)
+            restoreModelAndView(savedBoard)
+        }
+    }
+
+    private fun restoreModelAndView(board: Board) {
+        board.lastStone?.let { lastStone ->
+            gameState = mapStoneColorToGameState(lastStone.color, board)
+            boardView.updateBoard(gameState.board)
+        }
+    }
+
+    private fun initBoardView() {
+        boardView.spaceViews.forEachIndexed { index, spaceView ->
+            spaceView.setOnClickListener {
+                placeOmokStone(convertIndexToPosition(index))
+            }
+        }
+    }
+
+    private fun placeOmokStone(position: Position) {
         runCatching {
-            onPlace(OmokBoardView(view), convertIndexToPosition(index))
+            val newState = gameState.put(position)
+            if (gameState != newState) {
+                gameState = newState
+                updateViewAndDb(position)
+            }
         }.onFailure {
             showToast(this, "${it.message}")
         }
     }
 
-    override fun onPlace(
-        view: OmokView,
-        position: Position,
-    ) {
-        gameState = gameState.put(position)
-        view.updateBoard(position, gameState.board)
+    private fun updateViewAndDb(position: Position) {
+        gameState.board[position]?.run {
+            boardView.updateSingleSpace(this)
+            dbDao.save(
+                OmokEntry(
+                    x = this.position.x,
+                    y = this.position.y,
+                    color = mapStoneColorToString(this.color),
+                ),
+            )
+        }
+    }
+
+    private fun initGameResetButton() {
+        val resetButton = findViewById<Button>(R.id.btn_game_reset)
+        resetButton.setOnClickListener {
+            boardView.resetView()
+            dbDao.drop()
+            gameState = BlackTurn(RenjuRule, Board(emptyMap()))
+        }
     }
 }
