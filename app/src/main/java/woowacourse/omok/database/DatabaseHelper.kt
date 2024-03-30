88a7c3c109726a9fd608795bca98167bb1139566
ii2001 <iivks2001@gmail.com>
2024-03-30 22:21:56 +0900

    :recycle: DatabaseHelper.kt create Table문 Contract로 분리

--- app/src/main/java/woowacourse/omok/database/DatabaseHelper.kt
+++ app/src/main/java/woowacourse/omok/database/DatabaseHelper.kt
@@ -8,28 +8,26 @@ class DatabaseHelper(context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase) {
         val createGameBoardTable = """
-        CREATE TABLE GameBoard (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            rowIndex INTEGER,
-            columnIndex INTEGER,
-            stoneType INTEGER,
-            UNIQUE (rowIndex, columnIndex)
-        )
-    """.trimIndent()
+    CREATE TABLE ${GameBoardContract.GameBoardEntry.TABLE_NAME} (
+        ${GameBoardContract.GameBoardEntry.COLUMN_ID} INTEGER PRIMARY KEY AUTOINCREMENT,
+        ${GameBoardContract.GameBoardEntry.COLUMN_ROW_INDEX} INTEGER,
+        ${GameBoardContract.GameBoardEntry.COLUMN_COLUMN_INDEX} INTEGER,
+        ${GameBoardContract.GameBoardEntry.COLUMN_STONE_TYPE} INTEGER,
+        UNIQUE (${GameBoardContract.GameBoardEntry.COLUMN_ROW_INDEX}, ${GameBoardContract.GameBoardEntry.COLUMN_COLUMN_INDEX})
+    )""".trimIndent()
         db.execSQL(createGameBoardTable)
 
         val createGameStatusTable = """
-        CREATE TABLE GameStatus (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            currentStone INTEGER
-        )
-    """.trimIndent()
+    CREATE TABLE ${GameBoardContract.GameStatusEntry.TABLE_NAME} (
+        ${GameBoardContract.GameStatusEntry.COLUMN_ID} INTEGER PRIMARY KEY AUTOINCREMENT,
+        ${GameBoardContract.GameStatusEntry.COLUMN_CURRENT_STONE} INTEGER
+    )
+""".trimIndent()
         db.execSQL(createGameStatusTable)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         if (oldVersion < 2) {
-            // GameStatus 테이블 추가
             val createGameStatusTable = """
             CREATE TABLE GameStatus (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
