1218a580ef2da3bafc0a5352956e9e34ddba82f9
김진우 <jinwoo3661@naver.com>
2024-03-30 18:34:19 +0900

    refactor: 애매한 테스트코드명 명확하게 하도록 리팩토링

--- app/src/test/java/woowacourse/omok/StoneTest.kt
+++ app/src/test/java/woowacourse/omok/StoneTest.kt
@@ -1,27 +1,27 @@
 package omock
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import woowacourse.omok.model.position.Column
 import woowacourse.omok.model.position.Row
 import woowacourse.omok.model.stone.Stone
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 
 class StoneTest {
     @Test
     fun `돌은 row, column를 가지고 있다`() {
         val stone = Stone(row = Row("1"), column = Column("A"))
 
-        assertThat(stone.row.comma).isEqualTo("1")
-        assertThat(stone.column.comma).isEqualTo("A")
+        assertThat(stone.getRowComma()).isEqualTo("1")
+        assertThat(stone.getColumnComma()).isEqualTo("A")
     }
 
     @Test
-    fun `row, column을 통해 돌을 재활용 할 수 있다`() {
+    fun `돌을 통해서 Board의 인덱스를 얻을 수 있다`() {
         val row = Row("1")
         val column = Column("A")
         val actual = Stone.from(row = row, column = column)
 
-        assertThat(actual.row).isEqualTo(row)
-        assertThat(actual.column).isEqualTo(column)
+        assertThat(actual.getBoardRowIndex()).isEqualTo(Row.MAX_ROW)
+        assertThat(actual.getColumnIndex()).isEqualTo(Column.MIN_COLUMN_INDEX)
     }
 }
