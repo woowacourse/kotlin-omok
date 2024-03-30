3347001382faed3826aca60f2ae63268b36b4cd9
Gimun Kim <kmkim7575@gmail.com>
2024-03-30 16:02:30 +0900

    feat: 착수에 따라 db에 착수 정보를 추가하는 기능 구현

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -1,5 +1,6 @@
 package woowacourse.omok
 
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
@@ -11,15 +12,23 @@ import woowacourse.omok.model.Board
 import woowacourse.omok.model.GameResult
 import woowacourse.omok.model.Position
 import woowacourse.omok.model.Stone
+import woowacourse.omok.model.database.PlacementContract
+import woowacourse.omok.model.database.PlacementDbHelper
 import woowacourse.omok.model.state.GameState
 
 class MainActivity : AppCompatActivity() {
     private val placementData: Board by lazy { Board() }
     private lateinit var gameState: GameState
+    private lateinit var db: SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dbHelper = PlacementDbHelper(this)
+        db = dbHelper.writableDatabase
+
+        val gameId = intent.getLongExtra(GAME_ID, 0)
         findViewById<TableLayout>(R.id.board).apply {
             children
                 .filterIsInstance<TableRow>()
@@ -27,7 +36,7 @@ class MainActivity : AppCompatActivity() {
                 .filterIsInstance<ImageView>()
                 .forEachIndexed { index, view ->
                     view.setOnClickListener {
-                        markPosition(index, view)
+                        markPosition(index, view, gameId)
                     }
                 }
         }
@@ -36,12 +45,24 @@ class MainActivity : AppCompatActivity() {
     private fun markPosition(
         index: Int,
         view: ImageView,
+        gameId: Long,
     ) {
         val position = getInputPosition(index)
         if (!::gameState.isInitialized || gameState !is GameState.GameOver) {
             gameState = playEachTurn(position)
             showGameStateMessage(gameState)
-            if (gameState !is GameState.Error) setStoneImage(view)
+            if (gameState !is GameState.Error) {
+                setStoneImage(view)
+                db.execSQL(
+                    "INSERT INTO ${PlacementContract.TABLE_NAME} (" +
+                        "${PlacementContract.COLUMN_ROOM_ID}," +
+                        " ${PlacementContract.COLUMN_COLOR}," +
+                        " ${PlacementContract.COLUMN_VERTICAL_COORDINATE}," +
+                        " ${PlacementContract.COLUMN_HORIZONTAL_COORDINATE}" +
+                        ") \n" +
+                        "VALUES ($gameId, ${placementData.lastPlacement?.color?.name}, ${position.second}, ${position.first})",
+                )
+            }
         }
     }
 
@@ -79,5 +100,6 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val BOARD_DISPLAY_SIZE = 15
+        private const val GAME_ID = "game_id"
     }
 }
