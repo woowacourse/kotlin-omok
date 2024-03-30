ec7cdcc6d6c0e794f257a970004b73df6e85d1a1
kkosang <eiwoqp77@gmail.com>
2024-03-30 18:37:42 +0900

    feat: Contract,DbHelper 클래스 추가

---
+++ app/src/main/java/woowacourse/omok/db/OmokContract.kt
@@ -0,0 +1,10 @@
+package woowacourse.omok.db
+
+import android.provider.BaseColumns
+
+object OmokContract : BaseColumns {
+    const val TABLE_NAME = "omok"
+    const val COLUMN_STONE_COLOR = "stoneColor"
+    const val COLUMN_POSITION_X = "positionX"
+    const val COLUMN_POSITION_Y = "positionY"
+}
