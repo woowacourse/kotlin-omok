e9ac77bb494fd7166426dfe553ef09896a14b0c1
Junyoung-WON <wjyo60@gmail.com>
2024-04-01 01:46:33 +0900

    feat: 오목 게임이 동작하도록 기능 구현
    - GameEventListener를 상속하여 금수, 오목인 경우 필요한 동작을 하도록 구현
      - 금수인 경우 금수임을 알리는 메시지를 토스트로 출력
      - 게임 종료 시 승자가 누군지 알리는 메시지를 토스트로 출력
    - 오목판 레이아웃과 OmokController를 프로퍼티로 설정
    - 오목판을 터치한 위치로부터 좌표값을 얻어 착수를 시도
    - 착수에 성공 시 해당 위치에 돌 이미지를 그림

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -1,24 +1,92 @@
 package woowacourse.omok
 
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import woowacourse.omok.controller.OmokController
+import woowacourse.omok.model.Color
+import woowacourse.omok.model.GameEventListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GameEventListener {
+    private lateinit var boardLayout: TableLayout
+    private lateinit var omokController: OmokController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val board = findViewById<TableLayout>(R.id.board)
-        board
+        boardLayout = findViewById(R.id.board)
+        omokController = OmokController()
+        omokController.gameEventListener = this
+        omokController.start()
+
+        boardLayout
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEach { view ->
+                view.setOnClickListener {
+                    if (!omokController.gameEnded)
+                        handleStonePlacement(view)
+                }
+            }
+    }
+
+    override fun onForbiddenStone() {
+        viewToastMessage(FORBIDDEN_STONE_MESSAGE)
+    }
+
+    override fun onGameEnd(winner: Color) {
+        runOnUiThread {
+            Toast.makeText(this, "게임 종료! 승자: ${winner?.name}", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun handleStonePlacement(clickedView: ImageView) {
+        val tableRow = clickedView.parent as? ViewGroup
+        val col = tableRow?.indexOfChild(clickedView)
+        val row = tableRow?.let { tblRow -> boardLayout.indexOfChild(tblRow) }
+
+        if (col != null && row != null) {
+            Log.d("MainActivity", "x: ${col}, y: $row")
+            tryPlaceStone(row + INDEX_ADJUSTMENT, col + INDEX_ADJUSTMENT, clickedView)
+        }
+    }
+
+    private fun tryPlaceStone(
+        row: Int,
+        col: Int,
+        clickedView: ImageView,
+    ) {
+        val color = omokController.getNextTurn()
+        if (omokController.placeStoneAtPosition(row, col)) {
+            viewStone(color, clickedView)
+        }
+    }
+
+    private fun viewStone(
+        color: Color,
+        view: ImageView,
+    ) {
+        when (color) {
+            Color.BLACK -> view.setImageResource(R.drawable.black_stone)
+            Color.WHITE -> view.setImageResource(R.drawable.white_stone)
+        }
+    }
+
+    private fun viewToastMessage(message: String?) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        const val INDEX_ADJUSTMENT = 1
+        const val FORBIDDEN_STONE_MESSAGE = "이 위치는 금수입니다!"
     }
 }
