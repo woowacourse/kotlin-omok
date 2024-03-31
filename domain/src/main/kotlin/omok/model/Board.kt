9dfed854d3813567a946a1dc7325ec722ef647c8
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-04-01 04:56:14 +0900

    feat: 다시 시작하기 기능 구현

--- domain/src/main/kotlin/omok/model/Board.kt
+++ domain/src/main/kotlin/omok/model/Board.kt
@@ -4,6 +4,10 @@ class Board(private val boardSize: Int = BOARD_SIZE) {
     private var boardLayout = BoardLayout(boardSize)
     var lastCoordinate: Coordinate? = null
         private set
+    
+    fun clear() {
+        boardLayout = BoardLayout(boardSize)
+    }
 
     fun getBoardLayout(): Array<Array<PositionType>> {
         return boardLayout.deepCopy()
