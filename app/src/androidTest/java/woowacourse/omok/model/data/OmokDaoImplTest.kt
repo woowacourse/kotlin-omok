ebca4222793a14265cb3eac6de7bbc6ca78de4e0
kimhm0728 <kimhm0728@naver.com>
2024-03-31 14:45:27 +0900

    test: dao 테스트 추가

---
+++ app/src/androidTest/java/woowacourse/omok/model/data/OmokDaoImplTest.kt
@@ -0,0 +1,75 @@
+package woowacourse.omok.model.data
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class OmokDaoImplTest {
+    private lateinit var omokDaoImpl: OmokDaoImpl
+
+    @Before
+    fun setUp() {
+        omokDaoImpl = OmokDaoImpl(ApplicationProvider.getApplicationContext())
+    }
+
+    @After
+    fun tearDown() {
+        omokDaoImpl.drop()
+    }
+
+    @Test
+    fun `데이터를_저장한다`() {
+        // given
+        val omokEntity = OmokEntity(10, 10, STONE_BLACK)
+
+        // when
+        val actual = omokDaoImpl.save(omokEntity)
+
+        // then
+        assertThat(actual.id).isGreaterThan(-1)
+        assertThat(actual.row).isEqualTo(10)
+        assertThat(actual.col).isEqualTo(10)
+        assertThat(actual.stone).isEqualTo(STONE_BLACK)
+    }
+
+    @Test
+    fun `모든_데이터를_조회한다`() {
+        // given
+        val omokEntity = OmokEntity(5, 5, STONE_WHITE)
+        omokDaoImpl.save(omokEntity)
+
+        // when
+        val actual = omokDaoImpl.findAll()
+
+        // then
+        assertThat(actual[0].id).isGreaterThan(-1)
+        assertThat(actual[0].row).isEqualTo(5)
+        assertThat(actual[0].col).isEqualTo(5)
+        assertThat(actual[0].stone).isEqualTo(STONE_WHITE)
+    }
+
+    @Test
+    fun `모든_데이터를_삭제한다`() {
+        // given
+        val omokEntity = OmokEntity(7, 7, STONE_BLACK)
+        omokDaoImpl.save(omokEntity)
+        omokDaoImpl.save(omokEntity)
+
+        // when
+        omokDaoImpl.drop()
+        val actual = omokDaoImpl.findAll()
+
+        // then
+        assertThat(actual).isEmpty()
+    }
+
+    companion object {
+        private const val STONE_BLACK = "black"
+        private const val STONE_WHITE = "white"
+    }
+}
--- app/src/main/java/woowacourse/omok/model/data/OmokDbHelper.kt
+++ app/src/main/java/woowacourse/omok/model/data/OmokDbHelper.kt
@@ -7,13 +7,12 @@ import android.database.sqlite.SQLiteOpenHelper
 class OmokDbHelper(context: Context) : SQLiteOpenHelper(context, "omok.db", null, 1) {
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(
-            """CREATE TABLE ${OmokContract.TABLE_NAME} (
-                    ${OmokContract.COLUMN_ROW} int not null,
-                    ${OmokContract.COLUMN_COL} int not null,
-                    ${OmokContract.COLUMN_STONE} varchar(50),
-                    ${OmokContract.COLUMN_ID} int not null
-                )
-                """,
+            "CREATE TABLE ${OmokContract.TABLE_NAME} (\n" +
+                " ${OmokContract.COLUMN_ROW} int,\n" +
+                " ${OmokContract.COLUMN_COL} int,\n" +
+                " ${OmokContract.COLUMN_STONE} varchar(50),\n" +
+                " ${OmokContract.COLUMN_ID} int\n" +
+                ")",
         )
     }
 
