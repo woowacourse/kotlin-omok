ab8f03ba6293caa440262e0168e98d1689b20e0b
kimhm0728 <kimhm0728@naver.com>
2024-03-31 21:23:19 +0900

    remove: app 모듈 밖의 src 패키지 삭제

--- src/main/kotlin/omok/model/board/Position.kt
+++
@@ -1,7 +0,0 @@
-package omok.model.board
-
-data class Position(val row: Int, val col: Int) {
-    fun move(direction: Direction): Position {
-        return Position(row + direction.row, col + direction.col)
-    }
-}
