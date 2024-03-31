ec5b574532be5bae5c32ad162f84b9c62c23acc6
murjune <sis9221302@gmail.com>
2024-03-31 19:22:43 +0900

    feat: OmokRepository 수정

--- app/src/main/java/woowacourse/omok/data/OmokRepository.kt
+++ app/src/main/java/woowacourse/omok/data/OmokRepository.kt
@@ -1,37 +1,49 @@
 package woowacourse.omok.data
 
-import omok.model.OmokStone
-import omok.model.Position
-import omok.model.StoneColor
+import omock.model.Position
+import omock.model.board.Block
+import omock.model.board.BlockState
+import omock.model.board.buildOmokBoard
+import omock.model.game.OmokGame
+import omock.model.rule.RenjuRule
 import woowacourse.omok.db.GameRecordDao
 import woowacourse.omok.db.GameTurnEntity
 
 class OmokRepository(private val dao: GameRecordDao) {
-    fun saveGameRecord(stones: List<OmokStone>) {
-        val gameTurns = stones.map { it.toGameTurnEntity() }
+    fun saveGame(block: Block) {
+        val gameTurns = listOf(block.toGameTurnEntity())
         dao.saveGameRecord(gameTurns)
     }
 
-    fun selectAll(): List<OmokStone> {
-        return dao.selectAll().map { it.toOmokStone() }
+    fun fetchGame(): OmokGame {
+        val blocks = dao.selectAll().map { it.toOmokStone() }
+        val board =
+            buildOmokBoard {
+                size(DEFAULT_BOARD_SIZE)
+                blocks(blocks)
+                rule(DEFAULT_RULE)
+            }
+        return OmokGame(board)
     }
 
-    fun resetTable() {
+    fun resetGame() {
         dao.resetTable()
     }
 
-    private fun GameTurnEntity.toOmokStone(): OmokStone {
+    private fun GameTurnEntity.toOmokStone(): Block {
         val (id, x, y, color) = this
-        return OmokStone(Position.of(x, y), StoneColor.valueOf(color))
+        return Block(Position(x, y), BlockState.valueOf(color))
     }
 
-    private fun OmokStone.toGameTurnEntity(): GameTurnEntity {
+    private fun Block.toGameTurnEntity(): GameTurnEntity {
         val (position, color) = this
-        val (x, y) = position.x to position.y
+        val (x, y) = position
         return GameTurnEntity(NO_ID, x, y, color.name)
     }
 
     companion object {
         private const val NO_ID = -1L
+        private const val DEFAULT_BOARD_SIZE = 15
+        private val DEFAULT_RULE = RenjuRule
     }
 }
