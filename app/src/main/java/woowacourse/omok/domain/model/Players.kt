d9c0ce44c8b75d5154f33e123ee63c1414dedb3d
sh1mj1 <7wlgns@gmail.com>
2024-04-01 03:31:13 +0900

    feat(Players): 돌 색깔로부터 플레이어를 구한다

--- app/src/main/java/woowacourse/omok/domain/model/Players.kt
+++ app/src/main/java/woowacourse/omok/domain/model/Players.kt
@@ -4,4 +4,6 @@ class Players(private val blackStonePlayer: Player, private val whiteStonePlayer
     fun firstOrderedPlayer(): Player = blackStonePlayer
 
     fun nextOrder(player: Player): Player = if (player == blackStonePlayer) whiteStonePlayer else blackStonePlayer
+
+    fun find(stone: Stone): Player = if (stone == Stone.WHITE) whiteStonePlayer else blackStonePlayer
 }
