93e27de04fbceddbba1a3016fc1a2b660f71a02f
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-04-01 04:52:40 +0900

    feat: OmokContract 구현

---
+++ app/src/main/java/woowacourse/omok/local/db/OmokContract.kt
@@ -0,0 +1,11 @@
+package woowacourse.omok.local.db
+
+object OmokContract {
+    const val DATABASE_VERSION = 1
+    const val DATABASE_NAME = "omok.db"
+    const val TABLE_NAME = "t_stone"
+    const val COLUMN_ID = "id"
+    const val COORDINATE_X = "x"
+    const val COORDINATE_Y = "y"
+    const val POSITION_TYPE = "position_type"
+}
