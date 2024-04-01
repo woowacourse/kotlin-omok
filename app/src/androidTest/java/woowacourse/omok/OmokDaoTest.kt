caa5d268c8fe2e7eecb8669359c6ef8dae9ec703
Namyunsuk <yunsuk0328@naver.com>
2024-04-01 15:08:04 +0900

    feat: id값에 대한 데이터를 가져오는 기능 구현

--- app/src/androidTest/java/woowacourse/omok/OmokDaoTest.kt
+++ app/src/androidTest/java/woowacourse/omok/OmokDaoTest.kt
@@ -3,6 +3,7 @@ package woowacourse.omok
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
 import org.junit.Test
 import org.junit.jupiter.api.BeforeEach
 import org.junit.runner.RunWith
@@ -18,38 +19,57 @@ class OmokDaoTest {
         dao.reset()
     }
 
+    @After
+    fun tearDown() {
+        dao.reset()
+    }
+
+    @Test
+    fun `id값에_해당하는_데이터를_가져온다`() {
+        // given
+        val omokEntry1 = OmokEntry("A", 5, "흑", 1)
+        val omokEntry2 = OmokEntry("A", 6, "흑", 2)
+        dao.insert(omokEntry1)
+        dao.insert(omokEntry2)
+
+        // when
+        val actual = dao.findEntryById(2)
+
+        // then
+        assertThat(actual).isEqualTo(omokEntry2)
+    }
+
     @Test
     fun `돌을_놓을_경우_돌의_위치와_색깔이_저장된다`() {
         // given
-        val omokEntry = OmokEntry("A", 5, "흑")
+        val omokEntry = OmokEntry("A", 5, "흑", 1)
 
         // when
         val actual = dao.insert(omokEntry)
+        val expected = dao.findEntryById(1)
+        println(actual)
+        println(expected)
 
         // then
-        assertThat(actual.row).isEqualTo("A")
-        assertThat(actual.column).isEqualTo(5)
-        assertThat(actual.color).isEqualTo("흑")
+        assertThat(actual).isEqualTo(expected)
     }
 
     @Test
     fun `돌을_놓은_모든_경우의_값을_가지고_온다`() {
         // given
-        val omokEntry1 = OmokEntry("A", 1, "흑")
-        val omokEntry2 = OmokEntry("A", 2, "백")
-
-        // when
+        val omokEntry1 = OmokEntry("A", 1, "흑", 1)
+        val omokEntry2 = OmokEntry("A", 2, "백", 2)
         dao.insert(omokEntry1)
         dao.insert(omokEntry2)
+
+        // when
         val actual = dao.findAll()
+        val expected1 = dao.findEntryById(1)
+        val expected2 = dao.findEntryById(2)
 
         // then
-        assertThat(actual[0].row).isEqualTo("A")
-        assertThat(actual[0].column).isEqualTo(1)
-        assertThat(actual[0].color).isEqualTo("흑")
-        assertThat(actual[1].row).isEqualTo("A")
-        assertThat(actual[1].column).isEqualTo(2)
-        assertThat(actual[1].color).isEqualTo("백")
+        assertThat(actual[0]).isEqualTo(expected1)
+        assertThat(actual[1]).isEqualTo(expected2)
     }
 
     @Test
--- app/src/main/java/woowacourse/omok/omokdb/OmokDao.kt
+++ app/src/main/java/woowacourse/omok/omokdb/OmokDao.kt
@@ -8,6 +8,26 @@ import androidx.core.content.contentValuesOf
 class OmokDao(context: Context) {
     private val dbHelper = OmokDbHelper(context)
 
+    fun findEntryById(id: Long): OmokEntry {
+        val db = dbHelper.readableDatabase
+        val cursor =
+            db.query(
+                OmokContract.TABLE_NAME,
+                arrayOf(
+                    OmokContract.COLUMN_ROW,
+                    OmokContract.COLUMN_COLUMN,
+                    OmokContract.COLUMN_COLOR,
+                    BaseColumns._ID,
+                ),
+                "${BaseColumns._ID} = ?",
+                arrayOf(id.toString()),
+                null,
+                null,
+                null,
+            )
+        return findOmokEntry(cursor)
+    }
+
     fun insert(omokEntry: OmokEntry): OmokEntry {
         val db = dbHelper.writableDatabase
         val id =
@@ -43,18 +63,34 @@ class OmokDao(context: Context) {
         return omokEntries(cursor)
     }
 
+    private fun findOmokEntry(cursor: Cursor): OmokEntry {
+        val omokEntries: MutableList<OmokEntry> = mutableListOf()
+
+        while (cursor.moveToNext()) {
+            val omokEntry = createOmokEntry(cursor)
+            omokEntries.add(omokEntry)
+        }
+        cursor.close()
+        return omokEntries[0]
+    }
+
     private fun omokEntries(cursor: Cursor): MutableList<OmokEntry> {
         val omokEntries: MutableList<OmokEntry> = mutableListOf()
 
         while (cursor.moveToNext()) {
-            val id = cursor.getLong(cursor.getColumnIndexOrThrow(BaseColumns._ID))
-            val row = cursor.getString(cursor.getColumnIndexOrThrow(OmokContract.COLUMN_ROW))
-            val column = cursor.getInt(cursor.getColumnIndexOrThrow(OmokContract.COLUMN_COLUMN))
-            val color = cursor.getString(cursor.getColumnIndexOrThrow(OmokContract.COLUMN_COLOR))
-            omokEntries.add(OmokEntry(row, column, color, id))
+            val omokEntry = createOmokEntry(cursor)
+            omokEntries.add(omokEntry)
         }
         cursor.close()
         return omokEntries
+    }
+
+    private fun createOmokEntry(cursor: Cursor): OmokEntry {
+        val id = cursor.getLong(cursor.getColumnIndexOrThrow(BaseColumns._ID))
+        val row = cursor.getString(cursor.getColumnIndexOrThrow(OmokContract.COLUMN_ROW))
+        val column = cursor.getInt(cursor.getColumnIndexOrThrow(OmokContract.COLUMN_COLUMN))
+        val color = cursor.getString(cursor.getColumnIndexOrThrow(OmokContract.COLUMN_COLOR))
+        return OmokEntry(row, column, color, id)
     }
 
     fun reset() {
