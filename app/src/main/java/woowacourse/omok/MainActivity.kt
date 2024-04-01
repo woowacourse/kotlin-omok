b1c5226d165d675bc6dd13c636cbf4391eff7176
Junyoung-WON <wjyo60@gmail.com>
2024-04-01 16:52:41 +0900

    feat: MainActivity와 DB 연결 및 관련 기능 구현
    - StonesDao 활용하여 DB에 착수된 돌의 정보의 삽입, 삭제, 추가 기능 구현
    - UI를 그리는 부분은 Thread 또는 Coroutine을 활용하여 백그라운드에서 draw하도록 구현
    - 게임 종료 버튼 구현, 종료 시 앱을 백그라운드로 보냄
    - 메서드 리팩터링 진행
    - ktlint 체크

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -10,11 +10,19 @@ import android.widget.TableRow
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import woowacourse.omok.controller.OmokController
+import woowacourse.omok.db.StoneEntity
+import woowacourse.omok.db.StoneMapper
+import woowacourse.omok.db.StonesDao
 import woowacourse.omok.model.Color
 import woowacourse.omok.model.GameEventListener
 
 class MainActivity : AppCompatActivity(), GameEventListener {
+    private lateinit var stonesDao: StonesDao
     private lateinit var boardLayout: TableLayout
     private lateinit var omokController: OmokController
 
@@ -22,11 +30,50 @@ class MainActivity : AppCompatActivity(), GameEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        stonesDao = StonesDao(this)
         boardLayout = findViewById(R.id.board)
-        omokController = OmokController()
-        omokController.gameEventListener = this
-        omokController.start()
+        omokController = OmokController(this)
+        loadStonesFromDb()
 
+        setOnBoardTouch()
+        omokController.printStart()
+
+        val restartButton: Button = findViewById(R.id.restartButton)
+        restartButton.setOnClickListener {
+            omokController.restartGame()
+            resetBoardView()
+            clearDb()
+        }
+
+        val exitButton: Button = findViewById(R.id.exitButton)
+        exitButton.setOnClickListener {
+            moveTaskToBack(true)
+        }
+    }
+
+    override fun onForbiddenStone() {
+        runOnUiThread {
+            viewToastMessage(FORBIDDEN_STONE_MESSAGE, SHORT_DURATION)
+        }
+    }
+
+    override fun onGameEnd(winner: Color) {
+        runOnUiThread {
+            viewToastMessage(WINNER_MESSAGE.format(getPlayerColor(winner)), LONG_DURATION)
+        }
+        clearDb()
+    }
+
+    private fun loadStonesFromDb() = CoroutineScope(Dispatchers.IO).launch {
+        val stoneEntities = stonesDao.getAllStones()
+        val stones = stoneEntities.map { StoneMapper.toStone(it) }
+        omokController.setStonesOnBoard(stones)
+        withContext(Dispatchers.Main) {
+            updateUI(stoneEntities)
+        }
+    }
+
+    private fun setOnBoardTouch() {
         boardLayout
             .children
             .filterIsInstance<TableRow>()
@@ -34,25 +81,45 @@ class MainActivity : AppCompatActivity(), GameEventListener {
             .filterIsInstance<ImageView>()
             .forEach { view ->
                 view.setOnClickListener {
-                    if (!omokController.gameEnded)
+                    if (!omokController.gameEnded) {
                         handleStonePlacement(view)
+                    }
                 }
             }
-
-        val restartButton: Button = findViewById(R.id.restartButton)
-        restartButton.setOnClickListener {
-            omokController.restartGame()
-            resetBoardView()
-        }
     }
 
-    override fun onForbiddenStone() {
-        viewToastMessage(FORBIDDEN_STONE_MESSAGE, SHORT_DURATION)
+    private fun insertStoneToDb(
+        color: Color,
+        row: Int,
+        col: Int,
+        order: Int,
+    ) {
+        val stoneEntity =
+            StoneEntity(
+                color = color.name.lowercase(),
+                row = row,
+                column = col,
+                order = order,
+            )
+        stonesDao.insertStone(stoneEntity)
     }
 
-    override fun onGameEnd(winner: Color) {
-        runOnUiThread {
-            viewToastMessage(WINNER_MESSAGE.format(getPlayerColor(winner)), LONG_DURATION)
+    private fun clearDb() {
+        stonesDao.deleteAllStones()
+    }
+
+    private fun updateUI(stoneEntities: List<StoneEntity>) {
+        stoneEntities.forEach { stoneEntity ->
+            val rowIndex = stoneEntity.row - INDEX_ADJUSTMENT
+            val columnIndex = stoneEntity.column - INDEX_ADJUSTMENT
+
+            val row = boardLayout.getChildAt(rowIndex) as? TableRow
+            val cell = row?.getChildAt(columnIndex) as? ImageView
+
+            cell?.let {
+                val color = Color.valueOf(stoneEntity.color.uppercase())
+                viewStone(color, it)
+            }
         }
     }
 
@@ -74,8 +141,21 @@ class MainActivity : AppCompatActivity(), GameEventListener {
     ) {
         val color = omokController.getNextTurn()
         if (omokController.placeStoneAtPosition(row, col)) {
-            viewStone(color, clickedView)
+            placeStone(color, row, col, clickedView)
         }
+    }
+
+    private fun placeStone(
+        color: Color,
+        row: Int,
+        col: Int,
+        clickedView: ImageView,
+    ) {
+        val order = omokController.getStoneOrder()
+        if (!omokController.gameEnded) {
+            insertStoneToDb(color, row, col, order)
+        }
+        viewStone(color, clickedView)
     }
 
     private fun viewStone(
@@ -98,7 +178,10 @@ class MainActivity : AppCompatActivity(), GameEventListener {
         }
     }
 
-    private fun viewToastMessage(message: String?, duration: Int) {
+    private fun viewToastMessage(
+        message: String?,
+        duration: Int,
+    ) {
         Toast.makeText(this, message, duration).show()
     }
 
