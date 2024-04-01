a4c5750e7ca5af385c73c2232ccce2f01ab0fec9
kkosang <eiwoqp77@gmail.com>
2024-04-01 15:22:38 +0900

    feat: 앱 재실행시, DB 에서 stone 불러오는 기능 구현

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -11,6 +11,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import woowacourse.omok.db.OmokDao
+import woowacourse.omok.db.OmokEntity
 import woowacourse.omok.model.Board
 import woowacourse.omok.model.Color
 import woowacourse.omok.model.Coordinate
@@ -18,16 +20,18 @@ import woowacourse.omok.model.OmokGame
 import woowacourse.omok.model.Player
 import woowacourse.omok.model.PositionX
 import woowacourse.omok.model.PositionY
+import woowacourse.omok.model.Stone
 import woowacourse.omok.model.StoneState
 import woowacourse.omok.model.Stones
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textView: TextView
-    private lateinit var viewBoard: TableLayout
+    private val textView: TextView by lazy { findViewById(R.id.playerTurn_textView) }
+    private val boardView: TableLayout by lazy { findViewById(R.id.board) }
     private val players = listOf(Player(Color.BLACK), Player(Color.WHITE))
     private var currentPlayerIndex = 0
     private lateinit var currentPlayer: Player
     private lateinit var omokGame: OmokGame
+    private val omokDao = OmokDao(this@MainActivity)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,36 +39,59 @@ class MainActivity : AppCompatActivity() {
 
         initializeViews()
         setupGame()
+        setupOmokGame()
+        playOmokGame()
         setupResetButton()
         setupBoardClickListeners()
     }
 
-    private fun initializeViews() {
-        textView = findViewById(R.id.playerTurn_textView)
-        viewBoard = findViewById(R.id.board)
-    }
+    private fun setupOmokGame() {
+        val dbStones = omokDao.findAllOmok()
+        val stonesList = mutableListOf<Stone>()
 
-    private fun setupGame() {
-        val stones = Stones()
-        val board = Board(stones, RuleAdaptor(stones))
-        currentPlayerIndex = 0
-        currentPlayer = players[currentPlayerIndex]
+        dbStones.forEach {
+            val color =
+                when (it.stoneColor) {
+                    Color.BLACK.name -> Color.BLACK
+                    else -> Color.WHITE
+                }
+            val coordinate = Coordinate(PositionX(it.positionX), PositionY(it.positionY))
+            stonesList.add(
+                Stone(
+                    color,
+                    coordinate,
+                ),
+            )
+
+            val resourceId =
+                when (it.stoneColor) {
+                    Color.BLACK.name -> R.drawable.black_stone
+                    Color.WHITE.name -> R.drawable.white_stone
+                    else -> 0
+                }
+            setStoneImage(coordinate.x.value - 1, coordinate.y.value - 1, resourceId)
+        }
+
+        currentPlayerIndex = stonesList.size
+        currentPlayer = players[(currentPlayerIndex) % players.size]
+        val board = Board(Stones(stonesList), RuleAdaptor(Stones(stonesList)))
         omokGame = OmokGame(board)
 
-        updateText("BLACK 플레이어부터 시작합니다.")
+        updateText("${currentPlayer.color.name} 플레이어부터 시작합니다.")
     }
 
-    private fun setupResetButton() {
-        val resetButton = findViewById<Button>(R.id.reset_button)
-        resetButton.setOnClickListener {
-            setupGame()
-            resetBoard()
-            setupBoardClickListeners()
-        }
+    private fun setStoneImage(
+        x: Int,
+        y: Int,
+        resourceId: Int,
+    ) {
+        val row = boardView.getChildAt(x) as TableRow
+        val imageView = row.getChildAt(y) as ImageView
+        imageView.setImageResource(resourceId)
     }
 
-    private fun setupBoardClickListeners() {
-        viewBoard
+    private fun playOmokGame() {
+        boardView
             .children
             .filterIsInstance<TableRow>()
             .forEachIndexed { rowIndex, rows ->
@@ -72,7 +99,10 @@ class MainActivity : AppCompatActivity() {
                     .forEachIndexed { columnIndex, view ->
                         view.setOnClickListener {
                             val coordinate =
-                                Coordinate(PositionX(rowIndex + 1), PositionY(columnIndex + 1))
+                                Coordinate(
+                                    PositionX(rowIndex + 1),
+                                    PositionY(columnIndex + 1),
+                                )
                             progressGameTurn(currentPlayer, coordinate) {
                                 when (currentPlayer.color) {
                                     Color.BLACK -> view.setImageResource(R.drawable.black_stone)
@@ -97,6 +127,13 @@ class MainActivity : AppCompatActivity() {
             )
         when (stoneState) {
             is StoneState.SuccessfulPlaced -> {
+                omokDao.insertOmok(
+                    OmokEntity(
+                        currentPlayer.color.name,
+                        coordinate.x.value,
+                        coordinate.y.value,
+                    ),
+                )
                 if (!omokGame.isRunning()) {
                     showToast(this, "${currentPlayer.color}플레이어 승리!!!")
                     removeClickListeners()
@@ -110,7 +147,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun removeClickListeners() {
-        viewBoard
+        boardView
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
@@ -120,13 +157,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun changePlayerTurn(coordinate: Coordinate) {
         updateText("${currentPlayer.color}플레이어가 착수 했습니다.\n 마지막 돌의 위치: (${coordinate.x.value},${coordinate.y.value})")
-
         currentPlayer = players[(currentPlayerIndex + 1) % players.size]
         currentPlayerIndex++
     }
 
     private fun resetBoard() {
-        viewBoard
+        boardView
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
