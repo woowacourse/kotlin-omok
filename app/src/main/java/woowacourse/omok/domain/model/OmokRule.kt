a292478abcd067e9e455dcf27b9c950128139fad
JoYehyun <jo990714@naver.com>
2024-04-02 10:52:07 +0900

    chore: 파일 제거

--- app/src/main/java/woowacourse/omok/domain/model/OmokRule.kt
+++
@@ -1,9 +0,0 @@
-package woowacourse.omok.domain.model
-
-// interface OmokRule {
-//    fun validPosition(
-//        board: List<List<Int>>,
-//        x: Int,
-//        y: Int,
-//    ): Boolean
-// }
