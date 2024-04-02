fbdcb3b3c0977a64968aed4bc6c822f3d03d47d7
hxeyexn <kck85075@kumoh.ac.kr>
2024-04-02 23:15:25 +0900

    feat: putStone 반환값 변경 및 PutResult 객체 처리 로직 수정
    - 이전: StoneType 반환, OmokGame에서 PutResult 처리
    - 이후: PutResult 반환, GoStone에서 PutResult 처리

--- domain/src/main/kotlin/omok/model/OmokGame.kt
+++ domain/src/main/kotlin/omok/model/OmokGame.kt
@@ -2,12 +2,10 @@ package omok.model
 
 import omok.model.board.Board
 import omok.model.position.Position
-import omok.model.result.PutResult
 import omok.model.stone.BlackStone
 import omok.model.stone.GoStone
-import omok.model.stone.StoneType
 import omok.model.stone.WhiteStone
-import omok.utils.HandlingPutResultUtils
+import omok.utils.PutResultUtils.isRunning
 
 class OmokGame(private val blackStone: BlackStone, private val whiteStone: WhiteStone) {
     fun start(
@@ -16,52 +14,18 @@ class OmokGame(private val blackStone: BlackStone, private val whiteStone: White
         printWinner: (GoStone) -> Unit,
     ) {
         var stone: GoStone = blackStone
+        var isOmok = false
 
         do {
-            val (position, currentStone) = putStone(readPosition, stone)
-            val isOmok = stone.findOmok(position)
+            val position = readPosition(stone)
+            val putResult = stone.putStone(position)
+            if (isRunning(putResult).not()) continue
+            isOmok = stone.findOmok(position)
             if (isOmok) printWinner(stone)
-            stone = changeStone(currentStone)
+            stone = changeStone(stone)
             drawBoard(Board)
         } while (!isOmok)
     }
 
-    fun changeStone(currentStone: StoneType) = if (currentStone == StoneType.BLACK_STONE) whiteStone else blackStone
-
-    private fun putStone(
-        readPosition: (GoStone) -> Position,
-        stone: GoStone,
-    ): Pair<Position, StoneType> {
-        val position = readPosition(stone)
-        handleInvalidPosition(position, readPosition, stone)
-        handleForbidden(position, readPosition, stone)
-        val currentStone = stone.putStone(position)
-        return Pair(position, currentStone)
-    }
-
-    private fun handleInvalidPosition(
-        position: Position,
-        readPosition: (GoStone) -> Position,
-        stone: GoStone,
-    ) {
-        val putResult = position.validatePosition()
-
-        if (putResult == PutResult.Failure) {
-            HandlingPutResultUtils.displayPutStatus(putResult)
-            this.putStone(readPosition, stone)
-        }
-    }
-
-    private fun handleForbidden(
-        position: Position,
-        readPosition: (GoStone) -> Position,
-        stone: GoStone,
-    ) {
-        val putResult = position.checkForbidden()
-
-        if (putResult != PutResult.Running) {
-            HandlingPutResultUtils.displayPutStatus(putResult)
-            this.putStone(readPosition, stone)
-        }
-    }
+    fun changeStone(currentStone: GoStone) = if (currentStone == blackStone) whiteStone else blackStone
 }
--- domain/src/main/kotlin/omok/model/stone/GoStone.kt
+++ domain/src/main/kotlin/omok/model/stone/GoStone.kt
@@ -2,15 +2,26 @@ package omok.model.stone
 
 import omok.model.board.Board
 import omok.model.position.Position
+import omok.model.result.PutResult
 import omok.model.rule.OmokChecker
+import omok.utils.PutResultUtils.isOccupiedOrForbidden
+import omok.utils.PutResultUtils.isRunning
 
 abstract class GoStone {
     abstract val stoneType: StoneType
 
-    fun putStone(position: Position): StoneType {
-        Board.board[position.column.value][position.row.value] = stoneType
-        Board.updateLastPosition(position)
-        return stoneType
+    fun putStone(position: Position): PutResult {
+        var putResult = position.validatePosition()
+        if (isOccupiedOrForbidden(putResult)) return putResult
+
+        putResult = position.checkForbidden()
+        if (isOccupiedOrForbidden(putResult)) return putResult
+
+        if (isRunning(putResult)) {
+            Board.board[position.column.value][position.row.value] = stoneType
+            Board.updateLastPosition(position)
+        }
+        return putResult
     }
 
     fun findOmok(position: Position): Boolean = OmokChecker.findOmok(position, stoneType)
