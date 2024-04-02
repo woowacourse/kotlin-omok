2a822e2d2793ccd7d80f3f61667fd5fb5cebf1fb
sh1mj1 <7wlgns@gmail.com>
2024-04-02 12:46:51 +0900

    feat(Finished): 완료 상태가 된다

---
+++ app/src/main/java/woowacourse/omok/domain/model/state/Finished.kt
@@ -0,0 +1,18 @@
+package woowacourse.omok.domain.model.state
+
+import woowacourse.omok.domain.model.Board
+import woowacourse.omok.domain.model.Position
+import woowacourse.omok.domain.model.StonePosition
+
+data class Finished(val latestStonePosition: StonePosition) : GameState {
+    override fun place(
+        board: Board,
+        position: Position,
+    ): GameState {
+        throw IllegalStateException("게임이 종료되었습니다.")
+    }
+
+    override fun handleInvalidPosition(handling: (StonePosition, String) -> Unit): GameState {
+        throw IllegalStateException("게임이 종료되었습니다.")
+    }
+}
---
+++ app/src/test/java/woowacourse/omok/model/state/FinishedTest.kt
@@ -0,0 +1,30 @@
+package woowacourse.omok.model.state
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import woowacourse.omok.domain.model.Board
+import woowacourse.omok.domain.model.Position
+import woowacourse.omok.domain.model.Stone
+import woowacourse.omok.domain.model.StonePosition
+import woowacourse.omok.domain.model.state.Finished
+import woowacourse.omok.model.initBoard
+
+class FinishedTest {
+    private val emptyBoard: Board = initBoard()
+
+    @Test
+    fun `종료 상태에서는 돌을 더 둘 수 없다`() {
+        val finished = Finished(StonePosition(Position(1, 1), Stone.BLACK))
+        assertThrows<IllegalStateException> {
+            finished.place(emptyBoard, Position(1, 1))
+        }
+    }
+
+    @Test
+    fun `종료 상태에서는 유효하지 않은 위치에 대해 다룰 수 없다`() {
+        val finished = Finished(StonePosition(Position(1, 1), Stone.BLACK))
+        assertThrows<IllegalStateException> {
+            finished.handleInvalidPosition { _, _ -> }
+        }
+    }
+}
