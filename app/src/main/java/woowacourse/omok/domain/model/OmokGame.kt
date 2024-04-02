69ea37dc891d3ee0c7d594075c6d1d61d53a6dfb
sh1mj1 <7wlgns@gmail.com>
2024-04-02 18:34:29 +0900

    feat(OmokGame): 게임을 진행한다

--- app/src/main/java/woowacourse/omok/domain/model/OmokGame.kt
+++ app/src/main/java/woowacourse/omok/domain/model/OmokGame.kt
@@ -1,12 +1,42 @@
 package woowacourse.omok.domain.model
 
 import woowacourse.omok.domain.controller.ValidPosition
+import woowacourse.omok.domain.model.state.Finished
+import woowacourse.omok.domain.model.state.GameState
+import woowacourse.omok.domain.model.state.InitialGameTurn
+import woowacourse.omok.domain.model.state.InvalidPosition
+import woowacourse.omok.domain.model.state.Turn
 
 class OmokGame(
     private val board: Board,
     private val players: Players,
     private val validPosition: List<ValidPosition>,
 ) {
+    private val initialTurn: GameState = InitialGameTurn()
+
+    fun runGame(
+        firstPosition: () -> Position,
+        nextPosition: (GameState) -> Position,
+        handling: (StonePosition, String) -> Unit,
+        nextStonePositionResult: () -> Unit,
+    ): GameState {
+        var gameTurn: GameState = runFirstTurn(firstPosition)
+        nextStonePositionResult()
+        while (!gameTurn.isFinished()) {
+            gameTurn =
+                when (gameTurn) {
+                    is InitialGameTurn -> gameTurn.place(board, nextPosition(gameTurn))
+                    is Turn -> gameTurn.place(board, nextPosition(gameTurn))
+                    is InvalidPosition -> gameTurn.handleInvalidPosition(handling)
+                    is Finished -> throw IllegalStateException("게임이 종료되었습니다.")
+                }
+            nextStonePositionResult()
+        }
+        return gameTurn
+    }
+
+    private fun runFirstTurn(firstPosition: () -> Position): GameState = initialTurn.place(board, firstPosition())
+
     fun gameWinner(
         nextStonePosition: (Player, Position?) -> Position,
         nextStonePositionResult: () -> Unit,
