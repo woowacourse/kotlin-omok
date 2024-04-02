20f80369b030df7130999829250546421fa75606
Namyunsuk <yunsuk0328@naver.com>
2024-04-02 13:03:10 +0900

    feat: 돌을 놓을 때의 결과에 따른 행동을 하는 객체 작성

---
+++ model/src/main/kotlin/omok/model/result/ResultHandler.kt
@@ -0,0 +1,18 @@
+package omok.model.result
+
+object ResultHandler {
+    private const val ALREADY_STONE_PLACE_MESSAGE = "이미 놓여진 자리입니다."
+    private const val DOUBLE_THREE = "(33)"
+    private const val DOUBLE_FOUR = "(44)"
+    private const val EXCEED_FIVE = "(장목)"
+    private const val FORBIDDEN_MOVE_MESSAGE = "금수입니다.\n"
+
+    fun handleResult(putResult: PutResult) =
+        when (putResult) {
+            PutResult.Running -> {}
+            PutResult.Failure -> println(ALREADY_STONE_PLACE_MESSAGE)
+            PutResult.DoubleThree -> println("$FORBIDDEN_MOVE_MESSAGE$DOUBLE_THREE")
+            PutResult.DoubleFour -> println("$FORBIDDEN_MOVE_MESSAGE$DOUBLE_FOUR")
+            PutResult.ExceedFive -> println("$FORBIDDEN_MOVE_MESSAGE$EXCEED_FIVE")
+        }
+}
