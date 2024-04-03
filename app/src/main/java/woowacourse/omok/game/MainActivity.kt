157089da38f3efa60f9acf5b82557b05ac409f7c
Hogu59 <dbstjdguswkd@naver.com>
2024-04-03 20:34:06 +0900

    refactor(MainActivity): refactor
    - method seperation
    - move domain logic to domain
    - move board(TableLayout) to class global scope

--- app/src/main/java/woowacourse/omok/game/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/game/MainActivity.kt
@@ -1,185 +1,140 @@
-package woowacourse.omok.game.controller
+package woowacourse.omok.game
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import com.google.android.material.snackbar.Snackbar
-import omok.model.Board
-import omok.model.Color
-import omok.model.Point
-import omok.model.RenjuRuleAdapter
-import omok.model.Stone
-import omok.model.Turn
 import woowacourse.omok.R
 import woowacourse.omok.game.database.OmokEntry
 import woowacourse.omok.game.database.OmokEntryDao
+import woowacourse.omok.game.model.Board
+import woowacourse.omok.game.model.Color
+import woowacourse.omok.game.model.Point
+import woowacourse.omok.game.model.RenjuRuleAdapter
+import woowacourse.omok.game.model.Stone
+import woowacourse.omok.game.model.Turn
+import woowacourse.omok.game.model.isBlack
 
 class MainActivity : AppCompatActivity() {
-    private val board2 = Board(15, RenjuRuleAdapter())
+    private val logicBoard = Board(15, RenjuRuleAdapter())
+    private lateinit var board: TableLayout
     private val dao by lazy { OmokEntryDao(this) }
-    private val bannedPoints = mutableListOf<Point>()
-    private lateinit var textView: TextView
-    private lateinit var button: Button
+    private lateinit var turn: Turn
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        board = findViewById(R.id.board)
 
-        val board = findViewById<TableLayout>(R.id.board)
-        textView = findViewById(R.id.textView)
-        button = findViewById(R.id.putBtn)
+        fetchSavedData()
+        turn = Turn(logicBoard.lastColor()).currentTurn()
 
-        fetchSavedData(board)
-        val turn = Turn(board2.lastColor())
-        turn.next()
+        startGame()
+    }
 
+    private fun startGame() {
         board
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
             .forEachIndexed { index, view ->
-                val (row, col) = listOf(index / 15, index % 15)
-                val point = Point(row, col)
-                putStone(view, point, turn, board)
+                play(index, view)
             }
     }
 
-    private fun putStone(
+    private fun play(
+        index: Int,
         view: ImageView,
-        point: Point,
-        turn: Turn,
-        board: TableLayout,
     ) {
         view.setOnClickListener {
-            if (board2.pointEmpty(point) && board2.isValidPoint(turn, point)) {
-                view.setImageResource(turnToStone(turn))
-                board2.add(Stone(point, turn.color()))
-                saveStoneData(Stone(point, turn.color()))
-                resetBanned(board)
-                if (board2.checkEndCondition()) {
-                    resetAll(board, view, turn)
-                }
-                turn.next()
-            } else {
-                handleInvalidPoint(turn, point, view)
-            }
+            validatePlaceable(currentStone(index), view)
         }
     }
 
-    private fun handleInvalidPoint(
-        turn: Turn,
-        point: Point,
-        view: ImageView
+    private fun currentStone(index: Int): Stone = Stone(Point.from(index), turn.color())
+
+    private fun validatePlaceable(
+        stone: Stone,
+        view: ImageView,
     ) {
-        if (!board2.isValidPoint(turn, point)) {
-            // 렌주룰 위반
-            // Activity 가 룰 위반인 것을 알고 있어도 되는가? -> 모델의 역할 아닌가?
-            view.setImageResource(R.drawable.unavailable_point)
-            Toast.makeText(this, "둘 수 없는 자리입니다.", Toast.LENGTH_SHORT).show()
-            bannedPoints.add(point)
+        if (placeable(stone)) {
+            putStone(stone, view)
         } else {
-            Toast.makeText(this, "둘 수 없는 자리입니다.", Toast.LENGTH_SHORT).show()
+            handleInvalidPoint(stone, view)
         }
     }
 
-    private fun resetBanned(board: TableLayout) {
-        while (bannedPoints.size > 0) {
-            val currentPoint = bannedPoints.removeAt(0)
-            val (row, col) = listOf(currentPoint.row, currentPoint.col)
-            board
-                .children
-                .filterIsInstance<TableRow>()
-                .flatMap { it.children }
-                .filterIsInstance<ImageView>()
-                .forEachIndexed { index, view ->
-                    if (index == row * 15 + col) view.setImageDrawable(null)
-                }
+    private fun placeable(stone: Stone): Boolean = logicBoard.pointEmpty(stone.point) && logicBoard.isValid(stone)
+
+    private fun putStone(
+        stone: Stone,
+        view: ImageView,
+    ) {
+        view.setImageResource(imageResource(stone))
+        logicBoard.add(stone)
+        saveStoneData(stone)
+        resetBanned()
+        checkEndState()
+        turn.next()
+    }
+
+    private fun resetBanned() {
+        while (logicBoard.bannedPointCount() > 0) {
+            resetOneBannedPoint()
         }
     }
 
-    private fun resetAll(
-        board: TableLayout,
-        view: ImageView,
-        turn: Turn,
-    ) {
-        resetBoardView(board)
-        board2.resetCustomBoard()
-        deleteAllData()
-        popUpSnackBar(view, turn, board)
-        turn.reset()
+    private fun resetOneBannedPoint() {
+        val currentPoint = logicBoard.fetchBannedPoint()
+        board
+            .children
+            .filterIsInstance<TableRow>()
+            .flatMap { it.children }
+            .filterIsInstance<ImageView>()
+            .filterIndexed { index, _ ->
+                index == currentPoint.index()
+            }.forEach { imageView ->
+                imageView.setImageDrawable(null)
+            }
     }
 
-    private fun popUpSnackBar(
-        view: ImageView,
-        turn: Turn,
-        board: TableLayout
-    ) {
+    private fun checkEndState() {
+        if (logicBoard.checkEndCondition()) {
+            showSnackBarAndReset()
+        }
+    }
+
+    private fun showSnackBarAndReset() {
+        popUpSnackBar(turn)
+    }
+
+    private fun popUpSnackBar(turn: Turn) {
         val snackBar =
             Snackbar.make(
-                view,
-                "게임 종료!  ${turnToString(turn)}의 승리입니다!",
+                board,
+                "게임 종료!  ${turnToString(turn)}의 승리입니다!\n확인을 누르면 게임이 다시 시작됩니다.",
                 Snackbar.LENGTH_INDEFINITE,
             )
-        board.setOnClickListener(null)
         snackBar.setAction("확인") {
-            //resetBoardView(board)
+            resetData()
             snackBar.dismiss()
         }
         snackBar.show()
     }
 
-    private fun turnToStone(turn: Turn): Int {
-        return if (turn.isBlack()) {
-            R.drawable.black_stone
-        } else {
-            R.drawable.white_stone
-        }
+    private fun resetData() {
+        resetBoardView()
+        logicBoard.reset()
+        deleteDbData()
+        turn.reset()
     }
 
-    private fun turnToString(turn: Turn): String = colorToString(turn.color())
-
-    private fun colorToString(color: Color): String = if (color == Color.BLACK) "흑" else "백"
-
-    private fun stringToTurn(color: String): Color = if (color == "흑") Color.BLACK else Color.WHITE
-
-    private fun saveStoneData(stone: Stone) {
-        dao.save(OmokEntry(null, stone.point.row, stone.point.col, colorToString(stone.color)))
-    }
-
-    private fun fetchSavedData(board: TableLayout) {
-        val data = dao.findAll()
-        setUpCustomBoard(board, data)
-    }
-
-    private fun setUpCustomBoard(
-        board: TableLayout,
-        omokData: List<OmokEntry>,
-    ) {
-        omokData.forEach { it ->
-            val color = stringToTurn(it.color)
-            board2.add(Stone(Point(it.row, it.col), color))
-
-            val currentIndex = it.row * 15 + it.col
-            board.children
-                .filterIsInstance<TableRow>()
-                .flatMap { it.children }
-                .filterIsInstance<ImageView>()
-                .forEachIndexed { index, imageView ->
-                    if (index == currentIndex) {
-                        imageView.setImageResource(turnToStone(Turn(color)))
-                    }
-                }
-        }
-    }
-
-    private fun resetBoardView(board: TableLayout) {
+    private fun resetBoardView() {
         board
             .children
             .filterIsInstance<TableRow>()
@@ -190,7 +145,69 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    private fun deleteAllData() {
+    private fun handleInvalidPoint(
+        stone: Stone,
+        view: ImageView,
+    ) {
+        if (!logicBoard.isValid(stone)) {
+            view.setImageResource(R.drawable.unavailable_point)
+            logicBoard.addBannedPoint(stone.point)
+            Toast.makeText(this, "둘 수 없는 자리입니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "둘 수 없는 자리입니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun imageResource(turn: Turn): Int {
+        return if (turn.isBlack()) {
+            R.drawable.black_stone
+        } else {
+            R.drawable.white_stone
+        }
+    }
+
+    private fun imageResource(stone: Stone): Int {
+        return if (stone.color.isBlack()) {
+            R.drawable.black_stone
+        } else {
+            R.drawable.white_stone
+        }
+    }
+
+    private fun turnToString(turn: Turn): String = colorToString(turn.color())
+
+    private fun colorToString(color: Color): String = if (color == Color.BLACK) "흑" else "백"
+
+    private fun saveStoneData(stone: Stone) {
+        dao.save(OmokEntry(stone.point.index(), colorToString(stone.color)))
+    }
+
+    private fun fetchSavedData() {
+        val data = dao.findAll()
+        setUpLogicBoard(data)
+    }
+
+    private fun setUpLogicBoard(omokData: List<OmokEntry>) {
+        omokData.forEach { it ->
+            val color = stringToColor(it.color)
+            logicBoard.add(Stone(Point.from(it.index), color))
+
+            val currentIndex = it.index
+            board.children
+                .filterIsInstance<TableRow>()
+                .flatMap { it.children }
+                .filterIsInstance<ImageView>()
+                .forEachIndexed { index, imageView ->
+                    if (index == currentIndex) {
+                        imageView.setImageResource(imageResource(Turn(color)))
+                    }
+                }
+        }
+    }
+
+    private fun stringToColor(color: String): Color = if (color == "흑") Color.BLACK else Color.WHITE
+
+    private fun deleteDbData() {
         dao.drop()
     }
 }
