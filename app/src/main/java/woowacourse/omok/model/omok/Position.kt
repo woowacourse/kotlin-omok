550bd3dbe95e1c9549654114cb392fd6e6a2961e
somin <salth0601@gmail.com>
2024-04-04 04:22:36 +0900

    chore : ktlint

--- app/src/main/java/woowacourse/omok/model/omok/Position.kt
+++ app/src/main/java/woowacourse/omok/model/omok/Position.kt
@@ -1,11 +1,9 @@
 package woowacourse.omok.model.omok
 
 data class Position(val x: Int, val y: Int) {
-    operator fun plus(other: Vector): Position =
-        Position(x + other.position.x, y + other.position.y)
+    operator fun plus(other: Vector): Position = Position(x + other.position.x, y + other.position.y)
 
-    operator fun minus(other: Vector): Position =
-        Position(x - other.position.x, y - other.position.y)
+    operator fun minus(other: Vector): Position = Position(x - other.position.x, y - other.position.y)
 
     constructor(pair: Pair<Int, Int>) : this(pair.first, pair.second)
 
