84ba012faebc0c89d9c1eb74034e335fcecacadd
sh1mj1 <7wlgns@gmail.com>
2024-04-03 13:39:14 +0900

    feat(OmokController2): 사용하지 않은 클래스를 제거한다

--- app/src/main/java/woowacourse/omok/domain/Application.kt
+++ app/src/main/java/woowacourse/omok/domain/Application.kt
@@ -1,12 +1,12 @@
 package woowacourse.omok.domain
 
-import woowacourse.omok.domain.controller.OmokController2
+import woowacourse.omok.domain.controller.OmokController
 import woowacourse.omok.domain.view.InputView
 import woowacourse.omok.domain.view.OutputView
 
 fun main() {
-    OmokController2(
+    OmokController(
         InputView(),
         OutputView()
-    ).startGame2()
+    ).startGame()
 }
--- app/src/main/java/woowacourse/omok/domain/controller/OmokController.kt
+++ app/src/main/java/woowacourse/omok/domain/controller/OmokController.kt
@@ -1,46 +1,16 @@
 package woowacourse.omok.domain.controller
 
 import woowacourse.omok.domain.model.Board
-import woowacourse.omok.domain.model.OmokGame
 import woowacourse.omok.domain.model.OmokGame2
-import woowacourse.omok.domain.model.Player
-import woowacourse.omok.domain.model.Players
 import woowacourse.omok.domain.model.Stone
-import woowacourse.omok.domain.model.rule.RuleAdapter
 import woowacourse.omok.domain.view.InputView
 import woowacourse.omok.domain.view.OutputView
 
 class OmokController(
     private val inputView: InputView,
     private val outputView: OutputView,
-    blackStoneGamePlayingRules: RuleAdapter,
-    whiteStoneGamePlayingRules: RuleAdapter,
 ) {
-    private val players =
-        Players(
-            blackStonePlayer = Player(Stone.BLACK, blackStoneGamePlayingRules),
-            whiteStonePlayer = Player(Stone.WHITE, whiteStoneGamePlayingRules),
-        )
-
     fun startGame() {
-        val board = initializedBoard()
-        val winner =
-            OmokGame(
-                board,
-                players,
-                listOf(
-                    EmptyPosition { player, position, message -> outputView.printInvalidPosition(player, position, message) },
-                    AbideForbiddenRules { player, position, message -> outputView.printInvalidPosition(player, position, message) },
-                ),
-            ).gameWinner(
-                nextStonePosition = { player, position -> inputView.readStonePosition(player.stone, position) },
-                nextStonePositionResult = { outputView.printBoard(board) },
-            )
-
-        outputView.printWinner(winner.stone)
-    }
-
-    fun startGame2() {
         val board = initializedBoard()
         val omokGame2 = OmokGame2(board)
 
