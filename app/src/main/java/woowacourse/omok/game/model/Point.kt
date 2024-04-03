22e4ec8e014caf17178c85778d5e94d58c53e958
Hogu59 <dbstjdguswkd@naver.com>
2024-04-03 20:29:39 +0900

    feat(Point): Implement function and factory method
    function returns index, factory method makes an instance with index.

--- app/src/main/java/woowacourse/omok/game/model/Point.kt
+++ app/src/main/java/woowacourse/omok/game/model/Point.kt
@@ -1,3 +1,11 @@
-package omok.model
+package woowacourse.omok.game.model
 
-data class Point(val row: Int, val col: Int)
+data class Point(val row: Int, val col: Int) {
+    fun index(): Int = row * 15 + col
+
+    companion object {
+        fun from(index: Int): Point {
+            return Point(row = index / 15, col = index % 15)
+        }
+    }
+}
