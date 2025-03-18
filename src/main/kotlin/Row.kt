eabb3fb8303231ae1b455646cd1c75edd555ce4b
leeyerin0210 <lyl8661@gmail.com>
2025-03-18 14:46:07 +0900

    refactor: 팩토리 함수 생성

--- src/main/kotlin/Row.kt
+++ src/main/kotlin/Row.kt
@@ -1,5 +1,5 @@
-class Row(
-    val value: Int
+class Row private constructor(
+    val value: Int,
 ) {
     init {
         require(value in MIN_VALUE..MAX_VALUE) { ERROR_OUT_OF_BOUND }
@@ -9,6 +9,7 @@ class Row(
         private const val MIN_VALUE = 1
         private const val MAX_VALUE = 15
         private const val ERROR_OUT_OF_BOUND = "입력한 행이 범위를 벗어났습니다."
-    }
 
+        fun from(value: Int): Row = Row(value)
+    }
 }
--- src/test/kotlin/RowTest.kt
+++ src/test/kotlin/RowTest.kt
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.assertThrows
 class RowTest {
     @Test
     fun `유효한 행이 아니라면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> { Row(0) }
+        assertThrows<IllegalArgumentException> { Row.from(0) }
     }
 
     @Test
     fun `행이 유효하면 생성한다`() {
-        assertDoesNotThrow { Row(1) }
+        assertDoesNotThrow { Row.from(1) }
     }
 }
