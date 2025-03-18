f3c2df2540b7039eb6625b7d286ba4781e65a5f0
ijh1298 <imjh0417@gmail.com>
2025-03-18 19:20:04 +0900

    feat(Board): 바둑판은 오목 상태를 판단한다

--- src/main/kotlin/omok/Board.kt
+++ src/main/kotlin/omok/Board.kt
@@ -6,4 +6,52 @@ class Board {
     fun place(intersection: Intersection) {
         board[intersection.position.row.value][intersection.position.column.value] = intersection.state
     }
+
+    fun check(intersection: Intersection): BoardState {
+        val verticalCheck: Boolean = checkLine(intersection, 0)
+        val horizontalCheck = checkLine(intersection, 2)
+        val diagonalDown = checkLine(intersection, 4)
+        val diagonalUp = checkLine(intersection, 6)
+
+        if (verticalCheck || horizontalCheck || diagonalDown || diagonalUp) {
+            if (intersection.state == IntersectionState.BLACK)
+                return BoardState.BLACK_OMOK
+            if (intersection.state == IntersectionState.WHITE)
+                return BoardState.WHITE_OMOK
+        }
+        return BoardState.PLAYING
+    }
+
+    private fun checkLine(intersection: Intersection, startDir: Int): Boolean {
+        return 1 + checkDirection(intersection, startDir) + checkDirection(intersection, startDir + 1) >= 5
+    }
+
+    private fun checkDirection(intersection: Intersection, dir: Int): Int {
+        var curX = intersection.position.row.value
+        var curY = intersection.position.column.value
+        val curColor = intersection.state
+
+        val direction = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1, -1 to 1, 1 to -1, -1 to -1, 1 to 1)
+
+        var count = 0
+        while (true) {
+            if (count >= 5)
+                return 5
+
+            val nextX = curX + direction[dir].first
+            val nextY = curY + direction[dir].second
+
+            if (nextX <= 0 || nextX >= 16 || nextY <= 0 || nextY >= 16)
+                break
+
+            if (board[nextX][nextY] == curColor)
+                count++
+            else
+                break
+
+            curX = nextX
+            curY = nextY
+        }
+        return count
+    }
 }
--- src/test/kotlin/omok/GoBoardTest.kt
+++ src/test/kotlin/omok/GoBoardTest.kt
@@ -16,4 +16,34 @@ class GoBoardTest {
 
         assertThat(actual).isEqualTo(expected)
     }
+
+    @Test
+    fun `가로로 완성된 오목을 확인할 수 있다`() {
+        val board = Board().apply {
+            place(Intersection(Position.of(1, 1), IntersectionState.WHITE))
+            place(Intersection(Position.of(1, 2), IntersectionState.WHITE))
+            place(Intersection(Position.of(1, 3), IntersectionState.WHITE))
+            place(Intersection(Position.of(1, 4), IntersectionState.WHITE))
+            place(Intersection(Position.of(1, 5), IntersectionState.WHITE))
+        }
+        val actual: BoardState = board.check(Intersection(Position.of(1, 5), IntersectionState.WHITE))
+        val expected: BoardState = BoardState.WHITE_OMOK
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `대각선으로 완성된 오목을 확인할 수 있다`() {
+        val board = Board().apply {
+            place(Intersection(Position.of(13, 5), IntersectionState.WHITE))
+            place(Intersection(Position.of(11, 7), IntersectionState.WHITE))
+            place(Intersection(Position.of(10, 8), IntersectionState.WHITE))
+            place(Intersection(Position.of(9, 9), IntersectionState.WHITE))
+            place(Intersection(Position.of(12, 6), IntersectionState.WHITE))
+        }
+        val actual: BoardState = board.check(Intersection(Position.of(12, 6), IntersectionState.WHITE))
+        val expected: BoardState = BoardState.WHITE_OMOK
+
+        assertThat(actual).isEqualTo(expected)
+    }
 }
