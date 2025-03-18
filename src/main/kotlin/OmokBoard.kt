d725446a6fa25eb222491fa0e9b2ed6152aed7a2
junseo511 <prime54@g.skku.edu>
2025-03-18 16:15:59 +0900

    refactor: 오목판 크기 변수명 변경

--- src/main/kotlin/OmokBoard.kt
+++ src/main/kotlin/OmokBoard.kt
@@ -3,10 +3,10 @@ value class OmokBoard(
     val board: List<Point> = CACHE_OMOK_BOARD,
 ) {
     companion object {
-        private val OMOK_BOARD_SIZE = (1..15)
+        private val OMOK_BOARD_RANGE = (1..15)
         private val CACHE_OMOK_BOARD: List<Point> =
-            OMOK_BOARD_SIZE.flatMap { row ->
-                OMOK_BOARD_SIZE.map { column ->
+            OMOK_BOARD_RANGE.flatMap { row ->
+                OMOK_BOARD_RANGE.map { column ->
                     Point(Position(RowPosition(row), ColumnPosition(column)))
                 }
             }
