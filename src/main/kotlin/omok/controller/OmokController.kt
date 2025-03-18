79dc859afcf623d70fa59149ac1ed4cb6e736a10
DoableTuple <doabletuple@gmail.com>
2025-03-18 21:19:37 +0900

    feat(OmokController): 컨트롤러 생성

---
+++ src/main/kotlin/omok/controller/OmokController.kt
@@ -0,0 +1,36 @@
+package omok.controller
+
+import omok.model.Board
+import omok.model.BoardState
+import omok.model.Intersection
+import omok.model.IntersectionState
+import omok.model.Position
+import omok.view.InputView
+import omok.view.OutputView
+
+class OmokController(
+    val inputView: InputView,
+    val outputView: OutputView,
+) {
+    fun run() {
+        outputView.printOmokStart()
+        val board = Board()
+        outputView.printBoard(board)
+        val position: Position = inputView.readInitialTurn()
+        board.place(Intersection(position, IntersectionState.BLACK))
+        outputView.printBoard(board)
+        processTurn(board)
+    }
+
+    private tailrec fun processTurn(board: Board) {
+        val position: Position = inputView.readInitialTurn()
+        val stone: IntersectionState = board.lastStone.state.reverse()
+        val intersection = Intersection(position, stone)
+        board.place(intersection)
+        outputView.printBoard(board)
+        val boardState: BoardState = board.check(intersection)
+        if (boardState == BoardState.PLAYING) {
+            processTurn(board)
+        }
+    }
+}
