9e91a2c2400f5f381a74bf7aa891f6b40244411f
DongjooSEO <readyjoo24@gmail.com>
2025-03-18 16:05:26 +0900

    refactor: Board class로 전환

--- src/main/kotlin/omok/board/Board.kt
+++ src/main/kotlin/omok/board/Board.kt
@@ -2,7 +2,7 @@ package omok.board
 
 import omok.stone.Stone
 
-abstract class Board(points: Set<Stone> = setOf()) {
+class Board(points: Set<Stone> = setOf()) {
     private val _points = points
     val points: Set<Stone>
         get() = _points.toSet()
--- src/test/kotlin/omok/board/BoardTest.kt
+++ src/test/kotlin/omok/board/BoardTest.kt
@@ -6,13 +6,11 @@ import omok.stone.StoneColor
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class FakeBoard(stones: Set<Stone>) : Board(stones)
-
 class BoardTest {
     @Test
     fun `보드에는 중복된 위치의 돌이 존재할 수 없다`() {
         val stone = Stone(StoneColor.WHITE, Position(1, 1))
-        val board = FakeBoard(setOf(stone, stone))
+        val board = Board(setOf(stone, stone))
 
         assertThat(board.points.size).isEqualTo(1)
     }
