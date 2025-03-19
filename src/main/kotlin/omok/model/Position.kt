aa413b65e3594198fcc31f1d7e0dd1a158af49eb
gahyunkim <mapledt001@naver.com>
2025-03-19 22:21:52 +0900

    feat: Position을 렌주룰 라이브러리 Point로 변경하는 코드 구현

--- src/main/kotlin/omok/model/Position.kt
+++ src/main/kotlin/omok/model/Position.kt
@@ -1,5 +1,7 @@
 package omok.model
 
+import rule.wrapper.point.Point
+
 data class Position(
     val row: Row,
     val col: Col,
@@ -8,6 +10,8 @@ data class Position(
         require(row.value in 0..14) { ROW_RANGE_ERROR_MESSAGE }
         require(col.value in 0..14) { COL_RANGE_ERROR_MESSAGE }
     }
+
+    fun toPoint(): Point = Point(row.value, col.value)
 
     companion object {
         private const val ROW_RANGE_ERROR_MESSAGE = "가로 좌표는 오목판의 범위를 벗어날 수 없습니다"
