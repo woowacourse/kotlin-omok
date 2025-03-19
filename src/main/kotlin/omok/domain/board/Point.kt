37596482704583cf7d35be2a5d26ed9f6afb279d
chanho0908 <uni070@naver.com>
2025-03-20 03:15:40 +0900

    feat : 좌표 객체 팩토리 메서드 추가

--- src/main/kotlin/omok/domain/board/Point.kt
+++ src/main/kotlin/omok/domain/board/Point.kt
@@ -4,4 +4,16 @@ data class Point(
     val x: OmokColumn,
     val y: OmokRow,
     val stoneStatus: StoneStatus,
-)
+) {
+    companion object {
+        fun of(
+            row: String,
+            column: Char,
+            stoneStatus: StoneStatus,
+        ): Point {
+            val x = OmokColumn.of(column)
+            val y = OmokRow.of(row)
+            return Point(x, y, stoneStatus)
+        }
+    }
+}
