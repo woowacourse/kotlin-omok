927bd2455a6d2a356c613a5c765d391c69115f3e
ijh1298 <imjh0417@gmail.com>
2025-03-19 11:20:15 +0900

    test: LineTest 테스트 코드 수정

--- src/test/kotlin/omok/model/LineTest.kt
+++ src/test/kotlin/omok/model/LineTest.kt
@@ -6,8 +6,8 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class LineTest {
     @ParameterizedTest
-    @ValueSource(ints = [-1, 15])
-    fun `줄은 0부터 14까지의 값을 가진다`(input: Int) {
+    @ValueSource(ints = [-1, 0, 16])
+    fun `줄은 1부터 15까지의 값을 가진다`(input: Int) {
         assertThrows<IllegalArgumentException> { Line(input) }
     }
 }
