8ce53201e0a65ecbc84cbe0a0fb1c10256555125
HamBeomJoon <hbj0802@naver.com>
2025-03-19 16:19:22 +0900

    test: 렌주룰 테스트 추가

---
+++ src/test/kotlin/omok/OmokFixture.kt
@@ -0,0 +1,22 @@
+package omok
+
+import rule.wrapper.point.Point
+
+val POINT_H5 = Point(4, 7)
+val POINT_H6 = Point(5, 7)
+val POINT_H7 = Point(6, 7)
+val POINT_H8 = Point(7, 7)
+val POINT_H9 = Point(8, 7)
+val POINT_H10 = Point(9, 7)
+val POINT_E8 = Point(7, 4)
+val POINT_F8 = Point(7, 5)
+val POINT_G8 = Point(7, 6)
+
+
+fun beforeDoubleThree(): List<Point> = listOf(POINT_H7, POINT_G8, POINT_H6, POINT_F8)
+
+fun toViolation(): Point = POINT_H8
+
+fun beforeDoubleFour(): List<Point> = listOf(POINT_H7, POINT_G8, POINT_H6, POINT_F8, POINT_E8, POINT_H5)
+
+fun beforeOverLine(): List<Point> = listOf(POINT_H5, POINT_H6, POINT_H7, POINT_H9, POINT_H10)
--- src/test/kotlin/omok/domain/OmokGridTest.kt
+++ src/test/kotlin/omok/domain/OmokGridTest.kt
@@ -1,9 +1,14 @@
 package omok.domain
 
+import omok.beforeDoubleFour
+import omok.beforeDoubleThree
+import omok.beforeOverLine
+import omok.toViolation
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import rule.BlackRenjuRule
 import rule.wrapper.point.Point
 
 class OmokGridTest {
@@ -58,5 +63,41 @@ class OmokGridTest {
         val expected = listOf(Point(1, 1), Point(2, 2), Point(3, 3), Point(4, 4))
 
         assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `삼삼일 때 렌주룰을 위반한다`() {
+        val omokGrid = OmokGrid()
+        val beforeDoubleThree = beforeDoubleThree()
+
+        beforeDoubleThree.forEach { point -> omokGrid.putStone(point, StoneState.BLACK) }
+
+        val actual = omokGrid.isViolation(BlackRenjuRule(), toViolation())
+
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `사사일 때 렌주룰을 위반한다`() {
+        val omokGrid = OmokGrid()
+        val beforeDoubleThree = beforeDoubleFour()
+
+        beforeDoubleThree.forEach { point -> omokGrid.putStone(point, StoneState.BLACK) }
+
+        val actual = omokGrid.isViolation(BlackRenjuRule(), toViolation())
+
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `장목일 때 렌주룰을 위반한다`() {
+        val omokGrid = OmokGrid()
+        val beforeDoubleThree = beforeOverLine()
+
+        beforeDoubleThree.forEach { point -> omokGrid.putStone(point, StoneState.BLACK) }
+
+        val actual = omokGrid.isViolation(BlackRenjuRule(), toViolation())
+
+        assertThat(actual).isTrue()
     }
 }
