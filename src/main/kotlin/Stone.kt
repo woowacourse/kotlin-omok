de46aa5c020073a814fc362f1416d0c36e236779
yrsel <xxknu88@gmail.com>
2025-03-19 14:40:30 +0900

    feat: 돌 객체 생성 팩토리 함수 생성

--- src/main/kotlin/Stone.kt
+++ src/main/kotlin/Stone.kt
@@ -5,4 +5,15 @@ class Stone(
     fun isSamePosition(stone: Stone): Boolean = position.isSamePosition(stone.position)
 
     fun isSameColor(stone: Stone): Boolean = color.isSameColor(stone.color)
+
+    companion object {
+        fun of(
+            position: String,
+            color: StoneColor,
+        ): Stone {
+            val r = position.substring(1)
+            val c = position[0]
+            return Stone(Position(Row.from(r.toInt()), Col.from(c)), color)
+        }
+    }
 }
