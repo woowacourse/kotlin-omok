adb7af6cbaa78d81ea9c17abb62c9ec6d1305b9b
gahyunkim <mapledt001@naver.com>
2025-03-19 16:20:12 +0900

    test: BoardTest 테스트 코드 수정

--- src/test/kotlin/omok/model/BoardTest.kt
+++ src/test/kotlin/omok/model/BoardTest.kt
@@ -12,28 +12,17 @@ class BoardTest {
     fun `돌을 원하는 위치에 착수할 수 있다`() {
         val initialBoard = initBoard()
         val position = Position(Row(5), Col(5))
-        val stone = Stone(position, StoneState.BLACK)
 
-        assertDoesNotThrow { initialBoard.placeStone(stone) }
+        assertDoesNotThrow { initialBoard.placeStone(position) }
     }
 
     @Test
     fun `돌이 놓여있는 위치에 착수할 수 없다`() {
         val initialBoard = initBoard()
         val position55 = Position(Row(5), Col(5))
-        val nextBoard = initialBoard.placeStone(Stone(position55, StoneState.BLACK))
+        val nextBoard = initialBoard.placeStone(position55)
 
-        assertThrows<IllegalArgumentException> { nextBoard.placeStone(Stone(position55, StoneState.WHITE)) }
-    }
-
-    @Test
-    fun `같은 색의 돌을 연속하여 착수할 수 없다`() {
-        val initialBoard = initBoard()
-        val position55 = Position(Row(5), Col(5))
-        val position66 = Position(Row(6), Col(6))
-        val nextBoard = initialBoard.placeStone(Stone(position55, StoneState.BLACK))
-
-        assertThrows<IllegalArgumentException> { nextBoard.placeStone(Stone(position66, StoneState.BLACK)) }
+        assertThrows<IllegalArgumentException> { nextBoard.placeStone(position55) }
     }
 
     @Test
