281e0a70182e794515374fbdd75c18f71971f3cd
DoableTuple <doabletuple@gmail.com>
2025-03-19 22:05:15 +0900

    refactor: 흑백 공통 로직을 Player로 이동

--- src/main/kotlin/omok/model/Player.kt
+++ src/main/kotlin/omok/model/Player.kt
@@ -5,7 +5,7 @@ import rule.type.Violation
 import rule.wrapper.point.Point
 
 abstract class Player {
-    abstract val points: List<Point>
+    abstract val points: MutableList<Point>
     abstract val rule: OmokRule
 
     fun isOccupied(
@@ -15,8 +15,16 @@ abstract class Player {
         return newPoint in points || newPoint in otherPoints
     }
 
+    fun place(
+        newPoint: Point,
+        otherPoints: List<Point>,
+    ) {
+        require(!isOccupied(newPoint, otherPoints)) { ERROR_MESSAGE_IS_ALREADY_OCCUPIED }
+        checkViolation(newPoint, otherPoints)
+        points += newPoint
+    }
 
-    fun checkViolation(
+    private fun checkViolation(
         newPoint: Point,
         otherPoints: List<Point>,
     ) {
@@ -31,12 +39,8 @@ abstract class Player {
         }
     }
 
-    abstract fun place(
-        newPoint: Point,
-        otherPoints: List<Point>,
-    )
-
     companion object {
+        private const val ERROR_MESSAGE_IS_ALREADY_OCCUPIED = "이미 돌이 있는 자리입니다."
         private const val ERROR_MESSAGE_DOUBLE_THREE_VIOLATION = "삼삼 금수입니다."
         private const val ERROR_MESSAGE_DOUBLE_FOUR_VIOLATION = "사사 금수입니다."
         private const val ERROR_MESSAGE_OVERLINE_VIOLATION = "장목 금수입니다."
--- src/main/kotlin/omok/model/WhitePlayer.kt
+++ src/main/kotlin/omok/model/WhitePlayer.kt
@@ -2,27 +2,9 @@ package omok.model
 
 import rule.OmokRule
 import rule.WhiteRenjuRule
-import rule.type.Violation
 import rule.wrapper.point.Point
 
 class WhitePlayer : Player() {
     override val points = mutableListOf<Point>()
-
     override val rule: OmokRule = WhiteRenjuRule()
-
-    override fun place(
-        newPoint: Point,
-        otherPoints: List<Point>,
-    ) {
-        require(!isOccupied(newPoint, otherPoints)) { ERROR_MESSAGE_IS_ALREADY_OCCUPIED }
-        checkViolation(newPoint, otherPoints)
-        points += newPoint
-    }
-
-    companion object {
-        private const val ERROR_MESSAGE_IS_ALREADY_OCCUPIED = "이미 돌이 있는 자리입니다."
-        private const val ERROR_MESSAGE_DOUBLE_THREE_VIOLATION = "삼삼 금수입니다."
-        private const val ERROR_MESSAGE_DOUBLE_FOUR_VIOLATION = "사사 금수입니다."
-        private const val ERROR_MESSAGE_OVERLINE_VIOLATION = "장목 금수입니다."
-    }
 }
