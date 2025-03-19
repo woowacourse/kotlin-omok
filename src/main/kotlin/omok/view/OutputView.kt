7dbe8e15a80fad2337d95d7051ec3dae1b06460e
ijh1298 <imjh0417@gmail.com>
2025-03-19 11:17:42 +0900

    refactor: 바둑판 출력 형식 수정

--- src/main/kotlin/omok/view/OutputView.kt
+++ src/main/kotlin/omok/view/OutputView.kt
@@ -22,8 +22,10 @@ class OutputView {
         board: Board,
     ) {
         print("%3d ".format(row))
-        if (board.board[row][1] == IntersectionState.EMPTY) {
-            print("├──")
+        when (board.board[row][1]) {
+            IntersectionState.EMPTY -> print("├──")
+            IntersectionState.BLACK -> print("●──")
+            IntersectionState.WHITE -> print("○──")
         }
 
         for (column in 2..14) {
@@ -34,34 +36,42 @@ class OutputView {
             }
         }
 
-        if (board.board[row][1] == IntersectionState.EMPTY) {
-            println("┤")
+        when (board.board[row][15]) {
+            IntersectionState.EMPTY -> println("┤")
+            IntersectionState.BLACK -> println("●")
+            IntersectionState.WHITE -> println("○")
         }
     }
 
     private fun printBoardHeader(board: Board) {
         print("%3d ".format(15))
-        if (board.board[1][1] == IntersectionState.EMPTY) {
-            print("┌──")
+        when (board.board[15][1]) {
+            IntersectionState.EMPTY -> print("┌──")
+            IntersectionState.BLACK -> print("●──")
+            IntersectionState.WHITE -> print("○──")
         }
 
         for (column in 2..14) {
-            when (board.board[1][column]) {
+            when (board.board[15][column]) {
                 IntersectionState.EMPTY -> print("┬──")
                 IntersectionState.BLACK -> print("●──")
                 IntersectionState.WHITE -> print("○──")
             }
         }
 
-        if (board.board[1][1] == IntersectionState.EMPTY) {
-            println("┐")
+        when (board.board[15][15]) {
+            IntersectionState.EMPTY -> println("┐")
+            IntersectionState.BLACK -> println("●")
+            IntersectionState.WHITE -> println("○")
         }
     }
 
     private fun printBoardFooter(board: Board) {
         print("%3d ".format(1))
-        if (board.board[1][1] == IntersectionState.EMPTY) {
-            print("└──")
+        when (board.board[1][1]) {
+            IntersectionState.EMPTY -> print("└──")
+            IntersectionState.BLACK -> print("●──")
+            IntersectionState.WHITE -> print("○──")
         }
 
         for (column in 2..14) {
@@ -72,8 +82,10 @@ class OutputView {
             }
         }
 
-        if (board.board[1][15] == IntersectionState.EMPTY) {
-            println("┘")
+        when (board.board[1][15]) {
+            IntersectionState.EMPTY -> println("┘")
+            IntersectionState.BLACK -> println("●")
+            IntersectionState.WHITE -> println("○")
         }
 
         println("    A  B  C  D  E  F  G  H  I  J  K  L  M  N  O")
