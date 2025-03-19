f023d9475e2acc2176579a78d1ea55119cb2a2f2
yrsel <xxknu88@gmail.com>
2025-03-19 14:41:51 +0900

    feat: 차례 색상 변경

--- src/main/kotlin/StoneColor.kt
+++ src/main/kotlin/StoneColor.kt
@@ -4,4 +4,11 @@ enum class StoneColor {
     ;
 
     fun isSameColor(color: StoneColor): Boolean = this == color
+
+    fun switch(): StoneColor {
+        return when (this) {
+            WHITE -> BLACK
+            BLACK -> WHITE
+        }
+    }
 }
