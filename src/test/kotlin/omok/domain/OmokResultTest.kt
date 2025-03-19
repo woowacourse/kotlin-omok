b0472d2c26f31387674e8005d7e19c712a99fc4a
HamBeomJoon <hbj0802@naver.com>
2025-03-19 19:42:18 +0900

    test: stoneState -> Player 매개변수 변경

--- src/test/kotlin/omok/domain/OmokResultTest.kt
+++ src/test/kotlin/omok/domain/OmokResultTest.kt
@@ -5,20 +5,20 @@ import org.junit.jupiter.api.Test
 
 class OmokResultTest {
     @Test
-    fun `마지막 턴이 black이면 black가 승리한다`() {
-        val nowTurn = StoneState.BLACK
+    fun `마지막 플레이어가 black이면 black가 승리한다`() {
+        val nowPlayer = BlackPlayer()
 
-        val actual = OmokResult.returnWinner(nowTurn)
+        val actual = OmokResult.returnWinner(nowPlayer)
         val expected = OmokResult.BLACKWIN
 
         assertThat(actual).isEqualTo(expected)
     }
 
     @Test
-    fun `마지막 턴이 white면 white가 승리한다`() {
-        val nowTurn = StoneState.WHITE
+    fun `마지막 플레이어가 white면 white가 승리한다`() {
+        val nowPlayer = WhitePlayer()
 
-        val actual = OmokResult.returnWinner(nowTurn)
+        val actual = OmokResult.returnWinner(nowPlayer)
         val expected = OmokResult.WHITEWIN
 
         assertThat(actual).isEqualTo(expected)
