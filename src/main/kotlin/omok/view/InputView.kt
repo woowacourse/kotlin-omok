39d6682b6c718b4a18028d6eeefd697cb7a139d7
HamBeomJoon <hbj0802@naver.com>
2025-03-19 21:41:52 +0900

    refactor: 정규식 삭제 및 유효성 검증 로직 추가

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -1,6 +1,7 @@
 package omok.view
 
 import omok.domain.BlackPlayer
+import omok.domain.OmokGrid.Companion.DEFAULT_SIZE
 import omok.domain.Player
 import omok.domain.WhitePlayer
 import rule.wrapper.point.Point
@@ -15,7 +16,7 @@ class InputView {
         print(MESSAGE_POSITION_GUIDE)
         val rawInput = readln().trim()
 
-        return validateInput(rawInput) ?: getPoint(player, latestPoint)
+        return parsingInput(rawInput) ?: getPoint(player, latestPoint)
     }
 
     private fun convertToString(point: Point): String {
@@ -23,10 +24,26 @@ class InputView {
         return letter + (point.row).toString()
     }
 
-    private fun validateInput(rawInput: String): Point? {
-        val match: MatchResult = REGEX_PATTERN.matchEntire(rawInput) ?: return null
-        val (letter, number) = match.destructured
-        return Point(number.toInt(), convertLetter(letter))
+    private fun parsingInput(rawInput: String): Point? {
+        if (rawInput.isEmpty()) return null
+
+        val rawRow = rawInput.substring(1)
+        val rawCol = rawInput.substring(0, 1)
+
+        val row = validateRow(rawRow) ?: return null
+        val col = validateCol(rawCol) ?: return null
+        return Point(row, col)
+    }
+
+    private fun validateRow(number: String): Int? {
+        if (number.toIntOrNull() == null) return null
+        if (number.toInt() !in 1..DEFAULT_SIZE) return null
+        return number.toInt()
+    }
+
+    private fun validateCol(col: String): Int? {
+        if (col[0] - 'A' !in 0 until DEFAULT_SIZE) return null
+        return convertLetter(col)
     }
 
     private fun convertLetter(letter: String): Int {
@@ -45,6 +62,5 @@ class InputView {
         private const val MESSAGE_TURN: String = "\n%s의 차례입니다."
         private const val MESSAGE_LATEST_POSITION: String = "(마지막 돌의 위치: %s)"
         private const val MESSAGE_POSITION_GUIDE: String = "\n위치를 입력하세요: "
-        private val REGEX_PATTERN = Regex("^([A-O])([1-9]|1[0-5])$")
     }
 }
--- src/test/kotlin/omok/domain/OmokGridTest.kt
+++ src/test/kotlin/omok/domain/OmokGridTest.kt
@@ -15,12 +15,6 @@ class OmokGridTest {
     }
 
     @Test
-    fun `보드는 15x15 크기를 가진다`() {
-        assertThat(omokGrid.board.size).isEqualTo(15)
-        assertThat(omokGrid.board.first().size).isEqualTo(15)
-    }
-
-    @Test
     fun `좌표와 돌 상태를 받으면 해당 위치에 돌을 놓는다`() {
         val row = 1
         val col = 2
