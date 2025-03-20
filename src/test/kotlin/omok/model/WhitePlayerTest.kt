e9a83d79d12d2d40bac83c402fde95efe49641ca
ijh1298 <imjh0417@gmail.com>
2025-03-20 15:23:05 +0900

    test: 플레이어가 수를 뒀을 때 오목 완성 테스트 케이스 추가

--- src/test/kotlin/omok/model/WhitePlayerTest.kt
+++ src/test/kotlin/omok/model/WhitePlayerTest.kt
@@ -26,4 +26,48 @@ class WhitePlayerTest {
 
         assertThat(actual).isEqualTo(expected)
     }
+
+    @Test
+    fun `가로로 완성된 오목을 확인할 수 있다`() {
+        val whitePlayer = WhitePlayer().apply {
+            place(Point(1, 1), emptyList())
+            place(Point(1, 2), emptyList())
+            place(Point(1, 3), emptyList())
+            place(Point(1, 4), emptyList())
+        }
+        val actual: GameState = whitePlayer.place(Point(1, 5), emptyList())
+
+        val expected: GameState = GameState.WHITE_OMOK
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `대각선으로 완성된 오목을 확인할 수 있다`() {
+        val whitePlayer = WhitePlayer().apply {
+            place(Point(13, 5), emptyList())
+            place(Point(11, 7), emptyList())
+            place(Point(10, 8), emptyList())
+            place(Point(9, 9), emptyList())
+        }
+        val actual: GameState = whitePlayer.place(Point(12, 6), emptyList())
+
+        val expected: GameState = GameState.WHITE_OMOK
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `오목이 완성되지 않았으면 진행 중인 상태이다`() {
+        val whitePlayer = WhitePlayer().apply {
+            place(Point(8, 8), emptyList())
+            place(Point(8, 9), emptyList())
+            place(Point(9, 8), emptyList())
+        }
+        val actual: GameState = whitePlayer.place(Point(9, 9), emptyList())
+
+        val expected: GameState = GameState.PLAYING
+
+        assertThat(actual).isEqualTo(expected)
+    }
 }
