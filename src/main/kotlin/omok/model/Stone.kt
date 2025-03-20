3cc74e259067eed989f1f8f507241e7340a4df91
gahyunkim <mapledt001@naver.com>
2025-03-20 14:38:18 +0900

    feat: Stone에서 Pair의 값을 받아오는 부생성자 구현

--- src/main/kotlin/omok/model/Stone.kt
+++ src/main/kotlin/omok/model/Stone.kt
@@ -3,4 +3,6 @@ package omok.model
 class Stone(
     val position: Position,
     val stoneState: StoneState,
-)
+) {
+    constructor(lastStone: Pair<Position, StoneState>) : this(lastStone.first, lastStone.second)
+}
