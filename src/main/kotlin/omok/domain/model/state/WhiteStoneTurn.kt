f513d9b95acbf3f183c2041c50b3de718872465e
cucumber99 <dlsguq1@gmail.com>
2025-03-20 11:23:07 +0900

    feat: 백돌 차례 구현

---
+++ src/main/kotlin/omok/domain/model/state/WhiteStoneTurn.kt
@@ -0,0 +1,18 @@
+package omok.domain.model.state
+
+import omok.domain.model.Board
+import omok.domain.model.position.Position
+import omok.domain.model.rule.OmokRule
+import omok.domain.model.stone.StoneType
+
+class WhiteStoneTurn(
+    board: Board,
+) : Running(board, WHITE_STONE_RULE) {
+    override fun placeStone(onPlace: () -> Position): OmokState {
+        return super.placeStone(StoneType.WHITE, onPlace, ::BlackStoneTurn)
+    }
+
+    companion object {
+        private val WHITE_STONE_RULE = OmokRule { omokStone, board -> board.canPlace(omokStone.position) }
+    }
+}
