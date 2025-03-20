3599bf95178b7b124751650993b3f0eb64f21a74
DoableTuple <doabletuple@gmail.com>
2025-03-20 16:48:05 +0900

    style: 스타일 컨벤션 준수

--- src/main/kotlin/omok/model/Points.kt
+++ src/main/kotlin/omok/model/Points.kt
@@ -3,7 +3,7 @@ package omok.model
 import rule.wrapper.point.Point
 
 class Points(
-    points: List<Point> = listOf()
+    points: List<Point> = listOf(),
 ) {
     private var _points = points
     val points get() = _points.map { it.copy() }
--- src/test/kotlin/omok/model/WhitePlayerTest.kt
+++ src/test/kotlin/omok/model/WhitePlayerTest.kt
@@ -29,12 +29,13 @@ class WhitePlayerTest {
 
     @Test
     fun `가로로 완성된 오목을 확인할 수 있다`() {
-        val whitePlayer = WhitePlayer().apply {
-            place(Point(1, 1), Points())
-            place(Point(1, 2), Points())
-            place(Point(1, 3), Points())
-            place(Point(1, 4), Points())
-        }
+        val whitePlayer =
+            WhitePlayer().apply {
+                place(Point(1, 1), Points())
+                place(Point(1, 2), Points())
+                place(Point(1, 3), Points())
+                place(Point(1, 4), Points())
+            }
         val actual: GameState = whitePlayer.place(Point(1, 5), Points())
 
         val expected: GameState = GameState.WHITE_OMOK
@@ -44,12 +45,13 @@ class WhitePlayerTest {
 
     @Test
     fun `대각선으로 완성된 오목을 확인할 수 있다`() {
-        val whitePlayer = WhitePlayer().apply {
-            place(Point(13, 5), Points())
-            place(Point(11, 7), Points())
-            place(Point(10, 8), Points())
-            place(Point(9, 9), Points())
-        }
+        val whitePlayer =
+            WhitePlayer().apply {
+                place(Point(13, 5), Points())
+                place(Point(11, 7), Points())
+                place(Point(10, 8), Points())
+                place(Point(9, 9), Points())
+            }
         val actual: GameState = whitePlayer.place(Point(12, 6), Points())
 
         val expected: GameState = GameState.WHITE_OMOK
@@ -59,11 +61,12 @@ class WhitePlayerTest {
 
     @Test
     fun `오목이 완성되지 않았으면 진행 중인 상태이다`() {
-        val whitePlayer = WhitePlayer().apply {
-            place(Point(8, 8), Points())
-            place(Point(8, 9), Points())
-            place(Point(9, 8), Points())
-        }
+        val whitePlayer =
+            WhitePlayer().apply {
+                place(Point(8, 8), Points())
+                place(Point(8, 9), Points())
+                place(Point(9, 8), Points())
+            }
         val actual: GameState = whitePlayer.place(Point(9, 9), Points())
 
         val expected: GameState = GameState.PLAYING
