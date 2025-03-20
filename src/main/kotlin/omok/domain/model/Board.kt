527062fe387dcf18af7d24ad9aba4e75aaae3503
cucumber99 <dlsguq1@gmail.com>
2025-03-20 11:17:56 +0900

    refactor: 오목 확인 로직 개선
    - Board가 오목을 확인합니다

--- src/main/kotlin/omok/domain/model/Board.kt
+++ src/main/kotlin/omok/domain/model/Board.kt
@@ -1,6 +1,32 @@
-package domain.model
+package omok.domain.model
 
-class WinningJudgement(private val stones: Map<Position, Stone>) {
+import omok.domain.model.position.Column
+import omok.domain.model.position.Direction
+import omok.domain.model.position.Position
+import omok.domain.model.position.Row
+import omok.domain.model.stone.OmokStone
+import omok.domain.model.stone.StoneType
+
+class Board(
+    val stones: Map<Position, StoneType> = mapOf(),
+    val size: Int = DEFAULT_BOARD_SIZE,
+) {
+    fun placeStone(
+        position: Position,
+        stoneType: StoneType,
+    ): Board {
+        require(canPlace(position)) { "바둑돌이 있는 곳에는 둘 수 없습니다." }
+        return Board(stones + mapOf(position to stoneType))
+    }
+
+    fun canPlace(position: Position): Boolean = stones[position] == null
+
+    fun lastStoneOrNull(): OmokStone? {
+        val lastKey = stones.keys.lastOrNull() ?: return null
+        val lastValue = stones[lastKey] ?: return null
+        return OmokStone(lastKey, lastValue)
+    }
+
     fun checkWin(): Boolean {
         val directions =
             listOf(
@@ -19,14 +45,14 @@ class WinningJudgement(private val stones: Map<Position, Stone>) {
 
     private fun Direction.countConsecutive(
         position: Position,
-        stone: Stone,
+        stoneType: StoneType,
     ): Int {
-        return countInDirection(position, stone, this) + countInDirection(position, stone, this.inverse()) - 1
+        return countInDirection(position, stoneType, this) + countInDirection(position, stoneType, this.inverse()) - 1
     }
 
     private fun countInDirection(
         position: Position,
-        stone: Stone,
+        stoneType: StoneType,
         direction: Direction,
     ): Int {
         var count = 1
@@ -34,7 +60,7 @@ class WinningJudgement(private val stones: Map<Position, Stone>) {
 
         while (true) {
             val nextPos = currentPos.next(direction) ?: break
-            if (stones[nextPos] == stone) {
+            if (stones[nextPos] == stoneType) {
                 count++
                 currentPos = nextPos
             } else {
@@ -54,5 +80,9 @@ class WinningJudgement(private val stones: Map<Position, Stone>) {
         val nextColumn = Column.from(('A' + nextColIndex))
         val nextRow = Row(nextRowValue)
         return Position(nextColumn, nextRow)
+    }
+
+    companion object {
+        private const val DEFAULT_BOARD_SIZE = 15
     }
 }
