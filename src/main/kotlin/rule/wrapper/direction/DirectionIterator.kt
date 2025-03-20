9ab8560a3604a4b5f8c8711e27370dfd23de1b7b
yrsel <xxknu88@gmail.com>
2025-03-21 05:40:47 +0900

    style: ktlint 오류 수정

--- src/main/kotlin/rule/wrapper/direction/DirectionIterator.kt
+++ src/main/kotlin/rule/wrapper/direction/DirectionIterator.kt
@@ -3,12 +3,12 @@ package rule.wrapper.direction
 import rule.other.Iterator
 
 internal class DirectionIterator(items: List<Direction>) : Iterator<Direction> {
-    private val _items: MutableList<Direction> = items.toMutableList()
+    private val items: MutableList<Direction> = items.toMutableList()
 
-    override fun hasNext(): Boolean = _items.isNotEmpty()
+    override fun hasNext(): Boolean = items.isNotEmpty()
 
     override fun next(): Direction {
-        if (hasNext()) return _items.removeFirst()
+        if (hasNext()) return items.removeFirst()
         throw IllegalStateException("The next direction does not exist.")
     }
 }
