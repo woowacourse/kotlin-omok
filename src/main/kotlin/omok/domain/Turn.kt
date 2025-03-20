fde46dfe11950630aa0327d18d526d6cf1640b1e
devfeijoa <devfeijoa@gmail.com>
2025-03-20 12:58:37 +0900

    refactor: rowType으로 네이밍 변경

--- src/main/kotlin/omok/domain/Turn.kt
+++ src/main/kotlin/omok/domain/Turn.kt
@@ -3,4 +3,6 @@ package omok.domain
 interface Turn {
     val color: StoneType
     fun stone(position: String): Stone
+        val row =
+            RowType.entries.find { it.name == rowString }?.value ?: throw IllegalArgumentException(ERROR_NOT_FIND)
 }
