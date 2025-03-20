1e7624bca3e963b8049ebfdddce642eb8b896142
DoableTuple <doabletuple@gmail.com>
2025-03-20 11:04:42 +0900

    feat(Stone): Stone 클래스 추가

---
+++ src/main/kotlin/omok/model/Stone.kt
@@ -0,0 +1,13 @@
+package omok.model
+
+import rule.wrapper.point.Point
+
+data class Stone(
+    val point: Point,
+    val color: StoneColor,
+)
+
+enum class StoneColor {
+    BLACK,
+    WHITE,
+}
