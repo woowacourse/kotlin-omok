27aa56ff589f41d9c4dd21d2f35dd2f55b86f189
gahyunkim <mapledt001@naver.com>
2025-03-20 14:46:35 +0900

    feat: Parsing 코드 구현

--- src/main/kotlin/omok/model/Position.kt
+++ src/main/kotlin/omok/model/Position.kt
@@ -11,10 +11,26 @@ data class Position(
         require(col.value in 0..14) { COL_RANGE_ERROR_MESSAGE }
     }
 
+    constructor(coordinateText: String) : this(
+        row = Row((coordinateText.substring(1).toIntOrNull() ?: throw IllegalArgumentException("행 번호가 유효하지 않습니다")) - 1),
+        col =
+            Col(
+                coordinateText[0].uppercaseChar().let {
+                    require(it in MIN_COL_CHAR..MAX_COL_CHAR) { "열 문자가 유효하지 않습니다: $it" }
+                    it - 'A'
+                },
+            ),
+    )
+
     fun toPoint(): Point = Point(row.value, col.value)
 
     companion object {
         private const val ROW_RANGE_ERROR_MESSAGE = "가로 좌표는 오목판의 범위를 벗어날 수 없습니다"
         private const val COL_RANGE_ERROR_MESSAGE = "세로 좌표는 오목판의 범위를 벗어날 수 없습니다"
+
+        private const val MIN_COL_CHAR = 'A'
+        private const val MAX_COL_CHAR = 'O'
+        private const val MIN_ROW = 1
+        private const val MAX_ROW = 15
     }
 }
