c4ffa91f09c116affd5a663e3d56bf73e0dab819
woowahan-pjs <jason@woowahan.com>
2025-03-21 15:00:23 +0900

    feat: set up the android project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("jvm") version "2.1.0"
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ktlint) apply false
 }
 
-group = "camp.nextstep.edu"
-version = "1.0-SNAPSHOT"
-
-kotlin {
-    jvmToolchain(21)
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter", "5.11.4")
-    testImplementation("org.assertj", "assertj-core", "3.27.3")
-    testImplementation("io.kotest", "kotest-runner-junit5", "5.9.1")
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-    }
-    ktlint {
-        verbose.set(true)
-    }
+allprojects {
+    apply(plugin = rootProject.libs.plugins.ktlint.get().pluginId)
 }
