4ce5f176ccd811e61ecd07fd578838c1ad23363e
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-21 15:01:21 +0900

    refactor: Y 클래스 변경
    - data class -> value class로 변경
    - test case 추가

--- src/main/kotlin/omok/model/board/Y.kt
+++ src/main/kotlin/omok/model/board/Y.kt
@@ -1,18 +1,16 @@
 package omok.model.board
 
-data class Y(
-    val point: Int,
+@JvmInline
+value class Y(
+    val value: Int,
 ) {
     init {
-        require(point in Y_MIN_RANGE..Y_MAX_RANGE) { "Y좌표는 1에서 15까지이다." }
+        require(value in Y_MIN_RANGE..Y_MAX_RANGE) { "Y 좌표의 범위는 1에서 15까지이다." }
     }
 
-    private val isLeftMost: Boolean = point == Y_MIN_RANGE
-    private val isRightMost: Boolean = point == Y_MAX_RANGE
+    operator fun plus(other: Int) = Y(Y_MIN_RANGE + (this.value - Y_MIN_RANGE) + other)
 
-    operator fun plus(other: Int) = Y(Y_MIN_RANGE + (this.point - Y_MIN_RANGE) + other)
-
-    operator fun minus(other: Int) = Y(Y_MIN_RANGE + (this.point - Y_MIN_RANGE) - other)
+    operator fun minus(other: Int) = Y(Y_MIN_RANGE + (this.value - Y_MIN_RANGE) - other)
 
     companion object {
         private const val Y_MIN_RANGE = 1
