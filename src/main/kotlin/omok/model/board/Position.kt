8bf4928ceca5910e995f5361bfb91070b0aec9b1
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-21 15:36:53 +0900

    docs: 기능 목록 추가

--- src/main/kotlin/omok/model/board/Position.kt
+++ src/main/kotlin/omok/model/board/Position.kt
@@ -1,6 +1,11 @@
 package omok.model.board
 
+import omok.model.stone.StoneState
+
 data class Position(
     val x: X,
     val y: Y,
-)
+    val stoneState: StoneState = StoneState.NONE,
+) {
+    //
+}
