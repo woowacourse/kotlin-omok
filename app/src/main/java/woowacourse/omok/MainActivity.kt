e8bbbf6866fad98ae4d70785cef8ab054e9d6916
woowahan-pjs <jason@woowahan.com>
2025-03-21 15:00:23 +0900

    feat: set up the android project

---
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -0,0 +1,32 @@
+package woowacourse.omok
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.TableRow
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.children
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val board = findViewById<TableLayout>(R.id.board)
+        board
+            .children
+            .filterIsInstance<TableRow>()
+            .flatMap { it.children }
+            .filterIsInstance<ImageView>()
+            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+    }
+}
