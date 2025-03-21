ba5f17e9f46c535729faeea5ee621630ee41de1b
HamBeomJoon <hbj0802@naver.com>
2025-03-21 17:14:43 +0900

    feat: Position 클래스 추가

---
+++ src/main/kotlin/omok/domain/Position.kt
@@ -0,0 +1,19 @@
+package omok.domain
+
+data class Position(val x: Int, val y: Int) {
+    var stoneState: StoneState = StoneState.BLANK
+        private set
+
+    init {
+        require(x in 1..OmokGrid.DEFAULT_SIZE) {}
+        require(y in 1..OmokGrid.DEFAULT_SIZE) {}
+    }
+
+    fun changeState(state: StoneState): Boolean {
+        if (stoneState == StoneState.BLANK) {
+            stoneState = state
+            return true
+        }
+        return false
+    }
+}
