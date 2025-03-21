72f32c1025de6293fc630e20d7ffdcdc60bddf56
DoableTuple <doabletuple@gmail.com>
2025-03-21 14:17:20 +0900

    refactor: StoneColor 클래스 분리

--- src/main/kotlin/omok/model/Stone.kt
+++ src/main/kotlin/omok/model/Stone.kt
@@ -6,16 +6,3 @@ data class Stone(
     val point: Point,
     val color: StoneColor,
 )
-
-enum class StoneColor {
-    BLACK,
-    WHITE,
-    ;
-
-    fun reverse(): StoneColor {
-        return when (this) {
-            BLACK -> WHITE
-            WHITE -> BLACK
-        }
-    }
-}
