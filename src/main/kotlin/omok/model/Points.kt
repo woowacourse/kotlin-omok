00964f9db47acd7d0da2d32e47a2af6647673b06
ijh1298 <imjh0417@gmail.com>
2025-03-21 23:54:07 +0900

    refactor: Points 프로퍼티 getter 반환값 자료구조 수정

--- src/main/kotlin/omok/model/Points.kt
+++ src/main/kotlin/omok/model/Points.kt
@@ -4,7 +4,7 @@ class Points(
     points: Set<Point> = setOf(),
 ) {
     private var _points = points
-    val points get() = _points.map { it.copy() }
+    val points get() = _points.map { it.copy() }.toSet()
 
     fun add(point: Point) {
         _points += point
