f104b02c94eed6e93d518ac96f2c808f581372dd
woowahan-pjs <jason@woowahan.com>
2025-03-21 12:10:04 +0900

    build: upgrade to kotlin 2.1.0 and android gradle plugin 8.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("de.mannodermaus.android-junit5") version "1.10.0.0"
+    id("de.mannodermaus.android-junit5") version "1.12.0.0"
 }
 
 android {
     namespace = "woowacourse.omok"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "woowacourse.omok"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -29,11 +29,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
     packaging {
         resources {
@@ -44,20 +44,20 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    testImplementation("org.assertj:assertj-core:3.25.3")
-    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    androidTestImplementation("org.assertj:assertj-core:3.25.3")
-    androidTestImplementation("io.kotest:kotest-runner-junit5:5.8.0")
-    androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.3.0")
-    androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.3.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.0")
+    testImplementation("org.assertj:assertj-core:3.27.3")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter:5.12.0")
+    androidTestImplementation("org.assertj:assertj-core:3.27.3")
+    androidTestImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.7.0")
+    androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.7.0")
 }
