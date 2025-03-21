40cad019b4bddaa8d8329326fb2ecfd801f9aac0
gahyunkim <mapledt001@naver.com>
2025-03-22 00:37:26 +0900

    test: StoneColor로 변경으로 인한 test 코드 수정

--- src/test/kotlin/omok/model/OmokRuleTest.kt
+++ src/test/kotlin/omok/model/OmokRuleTest.kt
@@ -2,7 +2,7 @@ package omok.model
 
 import omok.model.rule.OmokRule
 import omok.model.stone.Stone
-import omok.model.stone.StoneState
+import omok.model.stone.StoneColor
 import omok.model.stone.position.Col
 import omok.model.stone.position.Position
 import omok.model.stone.position.Row
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test
 class OmokRuleTest {
     @Test
     fun `대각선으로 오목임을 확인할 수 있다`() {
-        val stonesList = List(5) { Position(Row(it), Col(it)) to StoneState.WHITE }
+        val stonesList = List(5) { Position(Row(it), Col(it)) to StoneColor.WHITE }
 
         val lastStone = Stone(stonesList[stonesList.size - 1])
 
@@ -23,7 +23,7 @@ class OmokRuleTest {
 
     @Test
     fun `세로로 오목임을 확인할 수 있다`() {
-        val stonesList = List(5) { Position(Row(it), Col(1)) to StoneState.WHITE }
+        val stonesList = List(5) { Position(Row(it), Col(1)) to StoneColor.WHITE }
 
         val lastStone = Stone(stonesList[stonesList.size - 1])
 
@@ -34,7 +34,7 @@ class OmokRuleTest {
 
     @Test
     fun `가로로 오목임을 확인할 수 있다`() {
-        val stonesList = List(5) { Position(Row(it), Col(1)) to StoneState.WHITE }
+        val stonesList = List(5) { Position(Row(it), Col(1)) to StoneColor.WHITE }
 
         val lastStone = Stone(stonesList[stonesList.size - 1])
 
@@ -45,7 +45,7 @@ class OmokRuleTest {
 
     @Test
     fun `6목 이상의 장목도 착수 가능하며 승리 조건으로 인정한다`() {
-        val stonesList = List(6) { Position(Row(3), Col(it)) to StoneState.WHITE }
+        val stonesList = List(6) { Position(Row(3), Col(it)) to StoneColor.WHITE }
 
         val lastStone = Stone(stonesList[stonesList.size - 3])
 
