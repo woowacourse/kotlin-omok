936b0038fa788a100b1fa73d7a00f92945936312
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-03-22 22:35:51 +0900

    refactor: boardSize 프로퍼티 제거

--- src/main/kotlin/omok/domain/Point.kt
+++ src/main/kotlin/omok/domain/Point.kt
@@ -1,11 +1,8 @@
 package omok.domain
 
-import omok.domain.Board.Companion.DEFAULT_BOARD_SIZE
-
 data class Point(
     val x: Int,
     val y: Int,
-    val boardSize: Int = DEFAULT_BOARD_SIZE
 ) {
     companion object {
         private const val ERROR_INVALID_POSITION = "[ERROR] 바둑판의 크기는 %dx%d입니다."
