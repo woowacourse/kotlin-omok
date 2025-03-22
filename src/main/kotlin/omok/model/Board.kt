7fb4aafbac45f0792e871173a306fdadf54d861f
DoableTuple <doabletuple@gmail.com>
2025-03-22 19:18:37 +0900

    refactor: 금수 확인 로직을 Game으로 이동

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -1,8 +1,5 @@
 package omok.model
 
-import rule.BlackRenjuRule
-import rule.WhiteRenjuRule
-import rule.type.Violation
 import rule.wrapper.point.Point
 
 class Board {
@@ -10,10 +7,7 @@ class Board {
     var lastStone: Stone2? = null
 
     fun add(newStone: Stone2) {
-        require(
-            !stones.map { stone -> stone.position }
-                .contains(newStone.position),
-        ) { ERROR_MESSAGE_POSITION_ALREADY_OCCUPIED }
+        require(!stones.map { stone -> stone.position }.contains(newStone.position)) { ERROR_MESSAGE_POSITION_ALREADY_OCCUPIED }
 
         checkViolation(newStone)
 
@@ -32,28 +26,9 @@ class Board {
                     stone ->
                 stone.color == Color.WHITE
             }.map { stone -> Point(stone.position.x, stone.position.y) }
-        val newPoint = Point(newStone.position.x, newStone.position.y)
-
-        val violation: Violation =
-            when (newStone.color) {
-                Color.BLACK -> BlackRenjuRule().checkAnyFoulCondition(blackPoints, whitePoints, newPoint)
-                Color.WHITE -> WhiteRenjuRule().checkAnyFoulCondition(whitePoints, blackPoints, newPoint)
-            }
-
-        require(violation == Violation.NONE) {
-            when (violation) {
-                Violation.DOUBLE_THREE -> ERROR_MESSAGE_DOUBLE_THREE_VIOLATION
-                Violation.DOUBLE_FOUR -> ERROR_MESSAGE_DOUBLE_FOUR_VIOLATION
-                Violation.OVERLINE -> ERROR_MESSAGE_OVERLINE_VIOLATION
-                Violation.NONE -> throw IllegalStateException()
-            }
-        }
     }
 
     companion object {
         private const val ERROR_MESSAGE_POSITION_ALREADY_OCCUPIED = "이미 돌이 있는 자리입니다."
-        private const val ERROR_MESSAGE_DOUBLE_THREE_VIOLATION = "삼삼 금수입니다."
-        private const val ERROR_MESSAGE_DOUBLE_FOUR_VIOLATION = "사사 금수입니다."
-        private const val ERROR_MESSAGE_OVERLINE_VIOLATION = "장목 금수입니다."
     }
 }
--- src/main/kotlin/omok/model/Game2.kt
+++ src/main/kotlin/omok/model/Game2.kt
@@ -1,14 +1,60 @@
 package omok.model
 
+import rule.BlackRenjuRule
+import rule.WhiteRenjuRule
+import rule.type.Violation
+import rule.wrapper.point.Point
+
 class Game2(val board: Board) {
     val blackPlayer = Player2(Color.BLACK)
     val whitePlayer = Player2(Color.WHITE)
 
     fun processTurn(position: Position) {
         when (board.lastStone?.color) {
-            null -> blackPlayer.makeMove(board, position)
-            Color.BLACK -> whitePlayer.makeMove(board, position)
-            Color.WHITE -> blackPlayer.makeMove(board, position)
+            Color.BLACK -> {
+                checkViolation(position, Color.WHITE)
+                whitePlayer.makeMove(board, position)
+            }
+            Color.WHITE, null -> {
+                checkViolation(position, Color.BLACK)
+                blackPlayer.makeMove(board, position)
+            }
         }
+    }
+
+    private fun checkViolation(
+        position: Position,
+        color: Color,
+    ) {
+        val blackPoints: List<Point> =
+            board.stones
+                .filter { stone -> stone.color == Color.BLACK }
+                .map { stone -> Point(stone.position.x, stone.position.y) }
+        val whitePoints: List<Point> =
+            board.stones
+                .filter { stone -> stone.color == Color.WHITE }
+                .map { stone -> Point(stone.position.x, stone.position.y) }
+
+        val newPoint = Point(position.x, position.y)
+        val violation: Violation =
+            when (color) {
+                Color.BLACK -> BlackRenjuRule().checkAnyFoulCondition(blackPoints, whitePoints, newPoint)
+                Color.WHITE -> WhiteRenjuRule().checkAnyFoulCondition(whitePoints, blackPoints, newPoint)
+            }
+
+        require(violation == Violation.NONE) {
+            when (violation) {
+                Violation.DOUBLE_THREE -> ERROR_MESSAGE_DOUBLE_THREE_VIOLATION
+                Violation.DOUBLE_FOUR -> ERROR_MESSAGE_DOUBLE_FOUR_VIOLATION
+                Violation.OVERLINE -> ERROR_MESSAGE_OVERLINE_VIOLATION
+                Violation.NONE -> throw IllegalStateException()
+            }
+        }
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE_DOUBLE_THREE_VIOLATION = "삼삼 금수입니다."
+        private const val ERROR_MESSAGE_DOUBLE_FOUR_VIOLATION = "사사 금수입니다."
+        private const val ERROR_MESSAGE_OVERLINE_VIOLATION = "장목 금수입니다."
     }
 }
