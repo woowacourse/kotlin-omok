e592df3b5eb008af2aa8df4f246a75e1ff2b21be
HamBeomJoon <hbj0802@naver.com>
2025-03-22 15:09:15 +0900

    style: ktlint 적용

--- src/test/kotlin/omok/domain/OmokBoardTest.kt
+++ src/test/kotlin/omok/domain/OmokBoardTest.kt
@@ -1,10 +1,5 @@
 package omok.domain
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-
 class OmokBoardTest {
 //    private lateinit var omokGrid: OmokGrid
 //
