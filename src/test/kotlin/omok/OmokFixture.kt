67fab11bcc9dcdb53c4bd71c336f6dcc06cc1499
hwannow <vnfdlvgid061@naver.com>
2025-03-22 12:46:08 +0900

    test: 심판 객체 테스트 코드 추가

--- src/test/kotlin/omok/OmokFixture.kt
+++ src/test/kotlin/omok/OmokFixture.kt
@@ -1,21 +1,25 @@
 package omok
 
-import rule.wrapper.point.Point
+import omok.domain.point.Column
+import omok.domain.point.OmokPoint
+import omok.domain.point.Row
 
-val POINT_H5 = Point(4, 7)
-val POINT_H6 = Point(5, 7)
-val POINT_H7 = Point(6, 7)
-val POINT_H8 = Point(7, 7)
-val POINT_H9 = Point(8, 7)
-val POINT_H10 = Point(9, 7)
-val POINT_E8 = Point(7, 4)
-val POINT_F8 = Point(7, 5)
-val POINT_G8 = Point(7, 6)
+val POINT_H5 = OmokPoint(Row(4), Column(7))
+val POINT_H6 = OmokPoint(Row(5), Column(7))
+val POINT_H7 = OmokPoint(Row(6), Column(7))
+val POINT_H8 = OmokPoint(Row(7), Column(7))
+val POINT_H9 = OmokPoint(Row(8), Column(7))
+val POINT_H10 = OmokPoint(Row(9), Column(7))
+val POINT_E8 = OmokPoint(Row(7), Column(4))
+val POINT_F8 = OmokPoint(Row(7), Column(5))
+val POINT_G8 = OmokPoint(Row(7), Column(6))
 
-fun beforeDoubleThree(): List<Point> = listOf(POINT_H7, POINT_G8, POINT_H6, POINT_F8)
+fun beforeDoubleThree(): List<OmokPoint> = listOf(POINT_H7, POINT_G8, POINT_H6, POINT_F8)
 
-fun beforeDoubleFour(): List<Point> = listOf(POINT_H7, POINT_G8, POINT_H6, POINT_F8, POINT_E8, POINT_H5)
+fun beforeDoubleFour(): List<OmokPoint> = listOf(POINT_H7, POINT_G8, POINT_H6, POINT_F8, POINT_E8, POINT_H5)
 
-fun beforeOverLine(): List<Point> = listOf(POINT_H5, POINT_H6, POINT_H7, POINT_H9, POINT_H10)
+fun beforeOverLine(): List<OmokPoint> = listOf(POINT_H5, POINT_H6, POINT_H7, POINT_H9, POINT_H10)
 
-fun getFoulPoint(): Point = POINT_H8
+fun getFoulPoint(): OmokPoint = POINT_H8
+
+fun omokPoints(): List<OmokPoint> = listOf(POINT_H5, POINT_H6, POINT_H7, POINT_H8, POINT_H9)
