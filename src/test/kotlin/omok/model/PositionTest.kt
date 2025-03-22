96df583542fc50feae50d7af95dd259a5a663ed5
gahyunkim <mapledt001@naver.com>
2025-03-22 14:50:39 +0900

    test: PositionTest 테스트 코드 삭제

--- src/test/kotlin/omok/model/PositionTest.kt
+++ src/test/kotlin/omok/model/PositionTest.kt
@@ -3,10 +3,8 @@ package omok.model
 import omok.model.stone.position.Col
 import omok.model.stone.position.Position
 import omok.model.stone.position.Row
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class PositionTest {
@@ -20,22 +18,5 @@ class PositionTest {
     @ValueSource(ints = [-1, 15, 18])
     fun `Col의 범위를 벗어날 수 없다`(col: Int) {
         assertThrows<IllegalArgumentException> { Position(Row(1), Col(col)) }
-    }
-
-    @ParameterizedTest
-    @CsvSource(
-        "H8, 7, 7",
-        "A1, 0, 0",
-        "O15, 14, 14",
-    )
-    fun `사용자로부터 돌의 좌표 텍스트로 포지션을 생성할 수 있다`(
-        coordinateText: String,
-        rowIndex: Int,
-        colIndex: Int,
-    ) {
-        val userPosition = Position(coordinateText)
-        val expected = Position(Row(rowIndex), Col(colIndex))
-
-        assertThat(userPosition).isEqualTo(expected)
     }
 }
