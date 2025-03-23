03780c7db1fbb37eafe8ac180bfdbd199eb3554e
DoableTuple <doabletuple@gmail.com>
2025-03-23 13:18:47 +0900

    refactor: 중복되는 금수 확인 로직 제거

--- src/main/kotlin/omok/model/Board.kt
+++ src/main/kotlin/omok/model/Board.kt
@@ -1,31 +1,13 @@
 package omok.model
 
-import rule.wrapper.point.Point
-
 class Board {
     val stones: MutableSet<Stone2> = mutableSetOf()
     var lastStone: Stone2? = null
 
     fun add(newStone: Stone2) {
         require(!stones.map { stone -> stone.position }.contains(newStone.position)) { ERROR_MESSAGE_POSITION_ALREADY_OCCUPIED }
-
-        checkViolation(newStone)
-
         stones.add(newStone)
         lastStone = newStone
-    }
-
-    private fun checkViolation(newStone: Stone2) {
-        val blackPoints: List<Point> =
-            stones.filter {
-                    stone ->
-                stone.color == Color.BLACK
-            }.map { stone -> Point(stone.position.x, stone.position.y) }
-        val whitePoints: List<Point> =
-            stones.filter {
-                    stone ->
-                stone.color == Color.WHITE
-            }.map { stone -> Point(stone.position.x, stone.position.y) }
     }
 
     companion object {
