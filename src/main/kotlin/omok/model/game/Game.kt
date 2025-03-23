1c5a2be822fe655f77d4ff283d372f2812dcd0ae
gahyunkim <mapledt001@naver.com>
2025-03-23 23:05:38 +0900

    refactor: Game 내부의 place() 및 세부 코드 수종

--- src/main/kotlin/omok/model/game/Game.kt
+++ src/main/kotlin/omok/model/game/Game.kt
@@ -1,22 +1,24 @@
 package omok.model.game
 
-import omok.external.mapper.toPoint
+import omok.mapper.BlackRuleChecker
 import omok.model.board.Board
 import omok.model.board.Board.Companion.initBoard
-import omok.model.rule.OmokRule
+import omok.model.rule.BlackOmokRule
+import omok.model.rule.Rule
+import omok.model.rule.WhiteOmokRule
 import omok.model.stone.Stone
 import omok.model.stone.StoneColor
 import omok.model.stone.position.Position
-import rule.BlackRenjuRule
-import rule.type.Violation
 
-class Game {
+class Game(
+    blackRuleChecker: BlackRuleChecker,
+) {
     private var board: Board = initBoard()
     private var turn: StoneColor = StoneColor.BLACK
     private var lastStone: Stone? = null
 
-    private val rule = OmokRule(BOARD_SIZE)
-    private val foulRule = BlackRenjuRule(BOARD_SIZE, BOARD_SIZE)
+    private val whiteOmokRule = WhiteOmokRule(BOARD_SIZE)
+    private val blackOmokRule = BlackOmokRule(blackRuleChecker)
 
     fun getBoard(): Board = board
 
@@ -25,36 +27,30 @@ class Game {
     fun getLastStone(): Stone? = lastStone
 
     fun place(position: Position): Board {
-        if (turn == StoneColor.BLACK) {
-            val blackPoints = board.stonesMap.filter { it.value == StoneColor.BLACK }.map { it.key.toPoint() }
-            val whitePoints = board.stonesMap.filter { it.value == StoneColor.WHITE }.map { it.key.toPoint() }
-
-            val violation = foulRule.checkAnyFoulCondition(blackPoints, whitePoints, position.toPoint())
-            when (violation) {
-                Violation.DOUBLE_THREE -> throw Exception(ERROR_DOUBLE_THREE)
-                Violation.DOUBLE_FOUR -> throw Exception(ERROR_DOUBLE_FOUR)
-                Violation.OVERLINE -> throw Exception(ERROR_OVERLINE)
-                Violation.NONE -> {}
-            }
-        }
-
-        board = board.positionAt(position, turn)
-        lastStone = Stone(position, turn)
-        turn = turn.next()
-
+        validateMove(position)
+        applyMove(position)
         return board
     }
 
-    fun isOmok(): Boolean =
-        lastStone?.let {
-            rule.isLastStoneOmok(board.stonesMap, it)
-        } ?: false
+    private fun validateMove(position: Position) {
+        currentRule().validate(board.stonesMap, position, turn)
+    }
+
+    private fun applyMove(position: Position) {
+        board = board.positionAt(position, turn)
+        lastStone = Stone(position, turn)
+        turn = turn.next()
+    }
+
+    fun isOmok(): Boolean = lastStone?.let { currentRule().isWin(board.stonesMap, it) } ?: false
+
+    private fun currentRule(): Rule =
+        when (turn) {
+            StoneColor.BLACK -> blackOmokRule
+            StoneColor.WHITE -> whiteOmokRule
+        }
 
     companion object {
         private const val BOARD_SIZE = 15
-
-        private const val ERROR_DOUBLE_THREE = "3-3 반칙이 발생했습니다"
-        private const val ERROR_DOUBLE_FOUR = "4-4 반칙이 발생했습니다"
-        private const val ERROR_OVERLINE = "장목 반칙이 발생했습니다"
     }
 }
