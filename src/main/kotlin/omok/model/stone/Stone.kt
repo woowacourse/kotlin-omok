48e82762150c938243f4ae8cd77032b9e510af1c
gahyunkim <mapledt001@naver.com>
2025-03-24 00:12:02 +0900

    refactor: 필요없는 부생성자 삭제

--- src/main/kotlin/omok/model/stone/Stone.kt
+++ src/main/kotlin/omok/model/stone/Stone.kt
@@ -5,6 +5,4 @@ import omok.model.stone.position.Position
 class Stone(
     val position: Position,
     val stoneColor: StoneColor,
-) {
-    constructor(lastStone: Pair<Position, StoneColor>) : this(lastStone.first, lastStone.second)
-}
+)
