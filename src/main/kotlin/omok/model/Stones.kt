c11f89c7c9e75f0a7375f00bd9f0dc2a398515f6
ijh1298 <imjh0417@gmail.com>
2025-03-23 22:18:11 +0900

    feat: Stones 클래스 생성자에 RuleAdapter 주입 및 오목 또는 금수 조건 판단 메서드 추가

--- src/main/kotlin/omok/model/Stones.kt
+++ src/main/kotlin/omok/model/Stones.kt
@@ -1,12 +1,30 @@
 package omok.model
 
+import omok.model.adapter.FoulCondition
+import omok.model.adapter.RuleAdapter
+
 class Stones(
     stones: Set<Stone> = setOf(),
+    private val ruleAdapter: RuleAdapter
 ) {
-    private var _points = stones
-    val points get() = _points.map { it.copy() }.toSet()
+    private var _stones = stones
+    val stones get() = _stones.map { it.copy() }.toSet()
 
     fun add(stone: Stone) {
-        _points += stone
+        _stones += stone
+    }
+
+    fun checkWin(
+        otherStones: Stones,
+        lastStone: Stone,
+    ): Boolean {
+        return ruleAdapter.checkWin(stones, otherStones.stones, lastStone, 5)
+    }
+
+    fun checkAnyFoulCondition(
+        otherStones: Stones,
+        lastStone: Stone,
+    ): FoulCondition {
+        return ruleAdapter.checkAnyFoulCondition(stones, otherStones.stones, lastStone)
     }
 }
