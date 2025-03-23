911b4f44f559d83d51302c235e9626f4a0dd0fe8
HamBeomJoon <hbj0802@naver.com>
2025-03-23 17:31:17 +0900

    refactor: row, col -> x, y로 변경

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -13,39 +13,39 @@ class InputView {
 
     companion object {
         private const val MESSAGE_TURN: String = "\n%s의 차례입니다."
-        private const val MESSAGE_LATEST_POSITION: String = "(마지막 돌의 위치: %s)"
+        private const val MESSAGE_LATEST_POSITION: String = " (마지막 돌의 위치: %s)"
         private const val MESSAGE_POSITION_GUIDE: String = "\n위치를 입력하세요: "
 
         private fun convertToString(position: Position): String {
-            val letter = 'A' + position.y - 1
-            return letter + (position.x).toString()
+            val letter = 'A' + position.x
+            return letter + (position.y + 1).toString()
         }
 
         private fun parsingInput(rawInput: String): Position? {
             if (rawInput.isEmpty()) return null
 
-            val rawRow = rawInput.substring(1)
-            val rawCol = rawInput.substring(0, 1)
+            val rawX = rawInput.substring(0, 1)
+            val rawY = rawInput.substring(1)
 
-            val row = validateRow(rawRow) ?: return null
-            val col = validateCol(rawCol) ?: return null
-            return Position(row, col)
+            val x = validateX(rawX) ?: return null
+            val y = validateY(rawY) ?: return null
+            return Position(x, y)
         }
 
-        private fun validateRow(rawRow: String): Int? {
-            if (rawRow.toIntOrNull() == null) return null
-            if (rawRow.toInt() !in 1..15) return null
-            return rawRow.toInt()
-        }
-
-        private fun validateCol(rawCol: String): Int? {
-            val convertedCol = convertLetter(rawCol)
-            if (convertedCol !in 1..15) return null
+        private fun validateX(rawX: String): Int? {
+            val convertedCol = convertLetter(rawX)
+            if (convertedCol !in 0 .. 15) return null
             return convertedCol
         }
 
+        private fun validateY(rawRow: String): Int? {
+            if (rawRow.toIntOrNull() == null) return null
+            if (rawRow.toInt() !in 0 .. 15) return null
+            return rawRow.toInt() - 1
+        }
+
         private fun convertLetter(letter: String): Int {
-            return letter[0] - 'A' + 1
+            return letter[0] - 'A'
         }
     }
 }
