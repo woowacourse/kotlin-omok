b8f97c99a6363d48fecee340941aa98f7b046ee1
DoableTuple <doabletuple@gmail.com>
2025-03-23 16:15:15 +0900

    refactor: 바둑판 출력 로직 변경

--- src/main/kotlin/omok/view/OutputView.kt
+++ src/main/kotlin/omok/view/OutputView.kt
@@ -1,5 +1,6 @@
 package omok.view
 
+import omok.model.Board
 import omok.model.Color
 import omok.model.Game
 import omok.model.GameState
@@ -52,6 +53,85 @@ class OutputView {
                 Color.WHITE -> WHITE_STONE
             }
         return sb.toString()
+    }
+
+    fun updateBoardString(board: Board): String {
+        val boardString: String = buildBoardString(board.row, board.col)
+        val lines = boardString.lines().toMutableList()
+        board.stones.forEach { stone ->
+            lines[board.row - stone.position.y] = updateLineString(stone.position.x, lines[board.row - stone.position.y], stone.color)
+        }
+        return lines.joinToString("\n")
+    }
+
+    fun updateLineString(
+        col: Int,
+        line: String,
+        color: Color,
+    ): String {
+        val x = col - 1
+        val sb = StringBuilder(line)
+        sb[x + ROW_NUMBER_OFFSET_SIZE + x * COLUMN_NUMBER_OFFSET_SIZE] =
+            when (color) {
+                Color.BLACK -> BLACK_STONE
+                Color.WHITE -> WHITE_STONE
+            }
+        return sb.toString()
+    }
+
+    fun buildBoardString(
+        height: Int,
+        width: Int,
+    ): String {
+        val top: String = buildRow(width, BOARD_TOP_LEFT2, BOARD_TOP_MIDDLE2, BOARD_TOP_RIGHT2)
+        val center: String = buildRow(width, BOARD_CENTER_LEFT2, BOARD_CENTER_MIDDLE2, BOARD_CENTER_RIGHT2)
+        val bottom: String = buildRow(width, BOARD_BOTTOM_LEFT2, BOARD_BOTTOM_MIDDLE2, BOARD_BOTTOM_RIGHT2)
+
+        val rows: List<String> = appendRows(height, top, center, bottom) + buildLastRow(height, width)
+        return rows.joinToString("\n")
+    }
+
+    private fun buildRow(
+        width: Int,
+        left: String,
+        middle: String,
+        right: String,
+    ): String {
+        return StringBuilder().apply {
+            append(left)
+            repeat(width - 2) { append(middle) }
+            append(right)
+        }.toString()
+    }
+
+    private fun buildLastRow(
+        height: Int,
+        width: Int,
+    ): String {
+        return StringBuilder().apply {
+            append(" ".repeat(height.toString().length))
+            for (i in 1..width) {
+                append("  ${(i + 64).toChar()}")
+            }
+        }.toString()
+    }
+
+    private fun appendRows(
+        height: Int,
+        top: String,
+        center: String,
+        bottom: String,
+    ): List<String> {
+        val rows: MutableList<StringBuilder> = MutableList(height) { StringBuilder() }
+        rows.forEachIndexed { i, row ->
+            row.append(" %${height.toString().length}d ".format(height - i))
+            when (i) {
+                0 -> row.append(top)
+                height - 1 -> row.append(bottom)
+                else -> row.append(center)
+            }
+        }
+        return rows.map { row -> row.toString() }
     }
 
     private fun makeInitialBoard(
@@ -118,5 +198,17 @@ class OutputView {
         private const val BOARD_BOTTOM_LEFT = "└─"
         private const val BOARD_BOTTOM_MIDDLE = "─┴─"
         private const val BOARD_BOTTOM_RIGHT = "─┘\n"
+
+        private const val BOARD_TOP_LEFT2 = "┌─"
+        private const val BOARD_TOP_MIDDLE2 = "─┬─"
+        private const val BOARD_TOP_RIGHT2 = "─┐"
+
+        private const val BOARD_CENTER_LEFT2 = "├─"
+        private const val BOARD_CENTER_MIDDLE2 = "─┼─"
+        private const val BOARD_CENTER_RIGHT2 = "─┤"
+
+        private const val BOARD_BOTTOM_LEFT2 = "└─"
+        private const val BOARD_BOTTOM_MIDDLE2 = "─┴─"
+        private const val BOARD_BOTTOM_RIGHT2 = "─┘"
     }
 }
