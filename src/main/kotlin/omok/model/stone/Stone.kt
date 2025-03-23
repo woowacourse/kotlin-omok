4dd13d79369e7794e2aff9580384e7749879b380
medAndro <med4ndro@gmail.com>
2025-03-23 23:01:13 +0900

    refactor: 테스트만을 위한 부 생성자 삭제

--- src/main/kotlin/omok/model/stone/Stone.kt
+++ src/main/kotlin/omok/model/stone/Stone.kt
@@ -5,6 +5,4 @@ import omok.model.stone.position.Position
 data class Stone(
     val position: Position,
     val stoneState: StoneState,
-) {
-    constructor(lastStone: Pair<Position, StoneState>) : this(lastStone.first, lastStone.second)
-}
+)
--- src/test/kotlin/omok/model/rule/NormalOmokRuleTest.kt
+++ src/test/kotlin/omok/model/rule/NormalOmokRuleTest.kt
@@ -13,7 +13,7 @@ class NormalOmokRuleTest {
     fun `대각선으로 오목임을 확인할 수 있다`() {
         val stonesList = List(5) { Position(Row(it), Col(it)) to StoneState.WHITE }
 
-        val lastStone = Stone(stonesList[stonesList.size - 1])
+        val lastStone = pairToStone(stonesList[stonesList.size - 1])
 
         val normalOmokRule = NormalOmokRule(15)
         val whiteStoneResult = normalOmokRule.isPositionOmok(stonesList.toMap(), lastStone.position)
@@ -24,7 +24,7 @@ class NormalOmokRuleTest {
     fun `세로로 오목임을 확인할 수 있다`() {
         val stonesList = List(5) { Position(Row(it), Col(1)) to StoneState.WHITE }
 
-        val lastStone = Stone(stonesList[stonesList.size - 1])
+        val lastStone = pairToStone(stonesList[stonesList.size - 1])
 
         val normalOmokRule = NormalOmokRule(15)
         val whiteStoneResult = normalOmokRule.isPositionOmok(stonesList.toMap(), lastStone.position)
@@ -35,7 +35,7 @@ class NormalOmokRuleTest {
     fun `가로로 오목임을 확인할 수 있다`() {
         val stonesList = List(5) { Position(Row(it), Col(1)) to StoneState.WHITE }
 
-        val lastStone = Stone(stonesList[stonesList.size - 1])
+        val lastStone = pairToStone(stonesList[stonesList.size - 1])
 
         val normalOmokRule = NormalOmokRule(15)
         val whiteStoneResult = normalOmokRule.isPositionOmok(stonesList.toMap(), lastStone.position)
@@ -46,7 +46,7 @@ class NormalOmokRuleTest {
     fun `6목 이상의 장목도 착수 가능하며 승리 조건으로 인정한다`() {
         val stonesList = List(6) { Position(Row(3), Col(it)) to StoneState.WHITE }
 
-        val lastStone = Stone(stonesList[stonesList.size - 3])
+        val lastStone = pairToStone(stonesList[stonesList.size - 3])
 
         val normalOmokRule = NormalOmokRule(15)
         val whiteStoneResult = normalOmokRule.isPositionOmok(stonesList.toMap(), lastStone.position)
@@ -58,11 +58,15 @@ class NormalOmokRuleTest {
     fun `6목이상이 아닌 정확한 오목임을 확인할 수 있다`() {
         val stonesList = List(6) { Position(Row(3), Col(it)) to StoneState.WHITE }
 
-        val lastStone = Stone(stonesList[stonesList.size - 3])
+        val lastStone = pairToStone(stonesList[stonesList.size - 3])
 
         val normalOmokRule = NormalOmokRule(15)
         val whiteStoneResult = normalOmokRule.isPositionOmok(stonesList.toMap(), lastStone.position, true)
 
         assertThat(whiteStoneResult).isFalse()
+    }
+
+    companion object {
+        fun pairToStone(pair: Pair<Position, StoneState>) = Stone(pair.first, pair.second)
     }
 }
