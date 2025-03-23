709ddb485ce100af6d78406bc39ca4bb63c84544
chanho0908 <uni070@naver.com>
2025-03-23 19:09:02 +0900

    feat : 컨트롤러 의존성 주입을 위한 컨테이너 객체 추가

--- src/main/kotlin/omok/Main.kt
+++ src/main/kotlin/omok/Main.kt
@@ -1,18 +1,8 @@
 package omok
 
-import omok.controller.OmokController
-import omok.domain.board.OmokBoard
-import omok.domain.point.OmokPoints
-import omok.domain.service.OmokGame
-import omok.view.InputView
-import omok.view.OutputView
+import omok.di.DependencyInjector
 
 fun main() {
-    val inputView = InputView()
-    val outputView = OutputView()
-    val points = OmokPoints()
-    val omokBoard = OmokBoard(points)
-    val omokGame = OmokGame(omokBoard)
-    val controller = OmokController(outputView, inputView, omokGame)
+    val controller = DependencyInjector().injectController()
     controller.run()
 }
