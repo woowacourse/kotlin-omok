27b6b06bc58bf1560e53741daef076c07de888d6
DoableTuple <doabletuple@gmail.com>
2025-03-23 19:15:52 +0900

    refactor: 입력된 위치의 행/열이 반대로 처리되는 현상 수정

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -11,15 +11,15 @@ class InputView {
         val input: String = readln()
         val col: Int = readCol(input)
         val row: Int = readRow(input)
-        return row to col
+        return col to row
     }
 
     private fun promptInput(game: Game) {
         val lastStone: Stone? = game.lastStone
         val playerName: String =
             when (game.lastStone?.color) {
-                Color.BLACK -> "백"
-                Color.WHITE, null -> "흑"
+                Color.BLACK -> WHITE_PLAYER
+                Color.WHITE, null -> BLACK_PLAYER
             }
         print(MESSAGE_TURN_INDICATOR.format(playerName))
         println(
@@ -28,6 +28,7 @@ class InputView {
                 else -> MESSAGE_LAST_STONE_POSITION.format(lastStone.position.stringRepresentation())
             },
         )
+        println(MESSAGE_ENTER_POSITION)
     }
 
     private fun readCol(input: String): Int {
@@ -45,7 +46,7 @@ class InputView {
     companion object {
         const val MESSAGE_TURN_INDICATOR = "%s의 차례입니다. "
         const val MESSAGE_LAST_STONE_POSITION = "(마지막 돌의 위치: %s)"
-        const val MESSAGE_ENTER_POINT = "위치를 입력하세요: "
+        const val MESSAGE_ENTER_POSITION = "위치를 입력하세요: "
 
         private const val BLACK_PLAYER = "흑"
         private const val WHITE_PLAYER = "백"
