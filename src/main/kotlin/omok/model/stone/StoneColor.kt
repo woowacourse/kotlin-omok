c1baac7cb068246ea155c9561e60523157af50c0
gahyunkim <mapledt001@naver.com>
2025-03-23 20:48:34 +0900

    feat: next()로 다음 턴을 변경하는 코드 구현

--- src/main/kotlin/omok/model/stone/StoneColor.kt
+++ src/main/kotlin/omok/model/stone/StoneColor.kt
@@ -3,4 +3,11 @@ package omok.model.stone
 enum class StoneColor {
     BLACK,
     WHITE,
+    ;
+
+    fun next(): StoneColor =
+        when (this) {
+            BLACK -> WHITE
+            WHITE -> BLACK
+        }
 }
