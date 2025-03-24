c2c1862d0bb9c83a612d948d008a6c60cce090a6
ijh1298 <imjh0417@gmail.com>
2025-03-24 12:55:55 +0900

    feat: Stone 팩토리 생성자 추가

--- src/main/kotlin/omok/model/stone/Stone.kt
+++ src/main/kotlin/omok/model/stone/Stone.kt
@@ -3,4 +3,12 @@ package omok.model.stone
 data class Stone(
     val point: Point,
     val color: StoneColor,
-)
+) {
+    companion object {
+        fun of(
+            row: Int,
+            col: Int,
+            color: StoneColor,
+        ) = Stone(Point(row, col), color)
+    }
+}
