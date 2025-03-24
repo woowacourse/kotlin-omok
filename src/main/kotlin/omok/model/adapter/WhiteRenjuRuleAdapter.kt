0f283aa751b8421797a80cb9dc6920cf094a2f3b
ijh1298 <imjh0417@gmail.com>
2025-03-24 14:10:36 +0900

    refactor: WhiteRenjuRuleAdapter 오목 판단 로직 수정

--- src/main/kotlin/omok/model/adapter/WhiteRenjuRuleAdapter.kt
+++ src/main/kotlin/omok/model/adapter/WhiteRenjuRuleAdapter.kt
@@ -1,14 +1,15 @@
 package omok.model.adapter
 
+import omok.model.Direction
 import omok.model.game.FoulCondition
+import omok.model.stone.Point
 import omok.model.stone.Stone
-import rule.facade.BlackRenjuRule
 
 class WhiteRenjuRuleAdapter(
     boardWidth: Int = 15,
     boardHeight: Int = 15,
 ) : RuleAdapter {
-    private val blackRenjuRule: BlackRenjuRule = BlackRenjuRule(boardWidth, boardHeight)
+    private val boardSize = minOf(boardWidth, boardHeight)
 
     override fun checkAnyFoulCondition(
         blackStones: Set<Stone>,
@@ -21,11 +22,42 @@ class WhiteRenjuRuleAdapter(
         whiteStones: Set<Stone>,
         startStone: Stone,
         sameStoneToCheck: Int,
-    ): Boolean =
-        blackRenjuRule.checkWin(
-            whiteStones.toPairList(),
-            blackStones.toPairList(),
-            startStone.toPair(),
-            sameStoneToCheck,
-        )
+    ): Boolean = isOmok(whiteStones.map { it.point }.toSet(), startStone.point)
+
+    private fun isOmok(
+        points: Set<Point>,
+        lastPoint: Point,
+    ): Boolean = Direction.directionPairs.any { isSerialOmok(points, lastPoint, it) }
+
+    private fun isSerialOmok(
+        points: Set<Point>,
+        lastPoint: Point,
+        directions: Pair<Direction, Direction>,
+    ): Boolean {
+        val forwardCount = count(points, lastPoint, directions.first)
+        val backwardCount = count(points, lastPoint, directions.second)
+        return forwardCount + backwardCount >= OMOK_STONE_COUNT - 1
+    }
+
+    private fun count(
+        points: Set<Point>,
+        point: Point,
+        direction: Direction,
+    ): Int {
+        val boardRange = 1..<boardSize
+        var count = 0
+
+        val (dx, dy) = direction.x to direction.y
+        var (x, y) = point.row + dx to point.col + dy
+        while (x in boardRange && y in boardRange && Point(x, y) in points) {
+            count++
+            x += dx
+            y += dy
+        }
+        return count
+    }
+
+    companion object {
+        private const val OMOK_STONE_COUNT = 5
+    }
 }
