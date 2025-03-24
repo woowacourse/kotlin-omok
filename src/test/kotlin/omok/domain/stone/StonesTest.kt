79d49aa87263e1d2fe07e2de14768c3eee8e90a4
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-03-24 19:55:33 +0900

    test: lastStonePoint 함수 -> 프로퍼티 변경에 따른 테스트 수정

--- src/test/kotlin/omok/domain/stone/StonesTest.kt
+++ src/test/kotlin/omok/domain/stone/StonesTest.kt
@@ -10,6 +10,6 @@ class StonesTest {
     fun `마지막 돌의 위치를 구한다`() {
         val stones = BlackStones(setOf(A1, A2))
         val expected = A2
-        assertThat(stones.lastStonePoint()).isEqualTo(expected)
+        assertThat(stones.lastStonePoint).isEqualTo(expected)
     }
 }
