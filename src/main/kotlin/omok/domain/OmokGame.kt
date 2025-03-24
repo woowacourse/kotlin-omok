4e70a8943f8382dfef754da8912523ffb1524091
HamBeomJoon <hbj0802@naver.com>
2025-03-24 17:36:28 +0900

    feat: game 관련 로직 처리 클래스 생성

---
+++ src/main/kotlin/omok/domain/OmokGame.kt
@@ -0,0 +1,44 @@
+package omok.domain
+
+import omok.domain.turn.Finished
+import omok.domain.turn.NextTurn
+import omok.domain.turn.PutStoneResult
+import omok.domain.turn.TurnManager
+
+class OmokGame(
+    val board: OmokBoard,
+    private val turnManager: TurnManager,
+) {
+    fun putStone(position: Position): PutStoneResult {
+        val nowTurn = turnManager.nowTurn
+        val stone = Stone(position, nowTurn)
+
+        when (nowTurn) {
+            StoneState.BLACK -> {
+                if (board.isStonePlaced(position)) return PutStoneResult.Failure(ERROR_STONE_ALREADY_PUT)
+                if (board.invalidPlace(stone)) return PutStoneResult.Failure(ERROR_INVALID_POSITION)
+            }
+
+            StoneState.WHITE ->
+                if (board.isStonePlaced(position)) {
+                    return PutStoneResult.Failure(
+                        ERROR_STONE_ALREADY_PUT,
+                    )
+                }
+
+            StoneState.BLANK -> throw IllegalStateException()
+        }
+
+        board.putStone(stone)
+
+        if (board.checkOmok(position)) return PutStoneResult.Success(Finished(nowTurn))
+
+        turnManager.changeTurn()
+        return PutStoneResult.Success(NextTurn(turnManager.nowTurn))
+    }
+
+    companion object {
+        const val ERROR_INVALID_POSITION = "잘못된 위치입니다. 다시 입력해주세요."
+        const val ERROR_STONE_ALREADY_PUT = "이미 돌이 있습니다. 다시 입력해주세요."
+    }
+}
