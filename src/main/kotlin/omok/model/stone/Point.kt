97699a258a392787bb2527313cdd0802130d77be
ijh1298 <imjh0417@gmail.com>
2025-03-24 12:54:59 +0900

    feat: Point 클래스 위치 유효성 검증 추가

--- src/main/kotlin/omok/model/stone/Point.kt
+++ src/main/kotlin/omok/model/stone/Point.kt
@@ -1,3 +1,18 @@
 package omok.model.stone
 
-data class Point(val row: Int, val col: Int)
+data class Point(
+    val row: Int,
+    val col: Int,
+) {
+    init {
+        require(row in MIN_BOARD_HEIGHT..MAX_BOARD_HEIGHT && col in MIN_BOARD_WIDTH..MAX_BOARD_WIDTH) { ERROR_MESSAGE_OUT_OF_BOARD }
+    }
+
+    companion object {
+        private const val MIN_BOARD_WIDTH = 1
+        private const val MIN_BOARD_HEIGHT = 1
+        private const val MAX_BOARD_WIDTH = 15
+        private const val MAX_BOARD_HEIGHT = 15
+        private const val ERROR_MESSAGE_OUT_OF_BOARD = "오목판 범위를 벗어났습니다."
+    }
+}
