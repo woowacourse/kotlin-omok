c271c315bcc5f55291f5579e33d891c3a5468599
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-03-24 17:26:03 +0900

    refactor: 돌 정보 관리를 Playing 상태로 이동

--- src/main/kotlin/omok/domain/state/State.kt
+++ src/main/kotlin/omok/domain/state/State.kt
@@ -1,9 +1,3 @@
 package omok.domain.state
 
-import omok.domain.stone.BlackStones
-import omok.domain.stone.WhiteStones
-
-interface State {
-    val blackStones: BlackStones
-    val whiteStones: WhiteStones
-}
+interface State
