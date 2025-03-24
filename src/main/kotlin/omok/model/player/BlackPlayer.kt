b791dfb5c449057b164a9f408cc1685e3c0526e6
박지민 <pjm5489@naver.com>
2025-03-24 16:07:45 +0900

    chore: 클래스 삭제

--- src/main/kotlin/omok/model/player/BlackPlayer.kt
+++
@@ -1,8 +0,0 @@
-package omok.model.player
-
-import omok.model.player.state.BlackPlayerState
-import omok.model.player.state.PlayerState
-
-class BlackPlayer(
-    playerState: PlayerState = BlackPlayerState(),
-) : Player(playerState)
--- src/main/kotlin/omok/model/player/WhitePlayer.kt
+++
@@ -1,8 +0,0 @@
-package omok.model.player
-
-import omok.model.player.state.PlayerState
-import omok.model.player.state.WhitePlayerState
-
-class WhitePlayer(
-    playerState: PlayerState = WhitePlayerState(),
-) : Player(playerState)
