a7778ade101aa2d85c72dddca9fabe0fd9fe0004
cucumber99 <dlsguq1@gmail.com>
2025-03-25 17:33:44 +0900

    refactor: 열 좌표 객체 리팩터링

--- src/main/kotlin/omok/domain/model/position/Column.kt
+++ src/main/kotlin/omok/domain/model/position/Column.kt
@@ -1,14 +1,4 @@
 package omok.domain.model.position
 
 @JvmInline
-value class Column private constructor(val value: Int) {
-    companion object {
-        val COLUMNS = ('A'..'O').toList()
-        private const val INDEXING = 1
-
-        fun from(value: Char): Column {
-            require(value in COLUMNS) { "잘못된 위치입니다." }
-            return Column(COLUMNS.indexOf(value) + INDEXING)
-        }
-    }
-}
+value class Column(val value: Int)
