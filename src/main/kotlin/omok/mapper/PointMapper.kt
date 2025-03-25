78dead259f1ee45106d2f9410c7e839fd0411ba7
gahyunkim <mapledt001@naver.com>
2025-03-25 15:34:23 +0900

    refactor: blackRenjuRule에서의 Point 매핑 오류 해결

--- src/main/kotlin/omok/mapper/PointMapper.kt
+++ src/main/kotlin/omok/mapper/PointMapper.kt
@@ -4,5 +4,5 @@ import omok.model.stone.position.Position
 import rule.wrapper.point.Point
 
 class PointMapper {
-    fun from(position: Position): Point = Point(position.row.value, position.col.value)
+    fun from(position: Position): Point = Point(position.col.value + 1, position.row.value + 1)
 }
