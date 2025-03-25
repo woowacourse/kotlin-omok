dc996d77c82b6a7a97ace04d64492039a14d6c0b
cucumber99 <dlsguq1@gmail.com>
2025-03-25 15:05:57 +0900

    feat: 백돌 차례 상태 정의

--- src/main/kotlin/omok/domain/model/state/WhiteStoneTurn.kt
+++ src/main/kotlin/omok/domain/model/state/WhiteStoneTurn.kt
@@ -1,20 +1,9 @@
 package omok.domain.model.state
 
-import omok.domain.model.Board
-import omok.domain.model.position.Position
-import omok.domain.model.rule.OmokRule
 import omok.domain.model.stone.StoneType
 
-class WhiteStoneTurn(
-    board: Board,
-) : Running(board, WHITE_STONE_RULE) {
+data object WhiteStoneTurn : OmokState {
     override val stoneType: StoneType = StoneType.WHITE
 
-    override fun placeStone(onPlace: () -> Position): OmokState {
-        return super.placeStone(StoneType.WHITE, onPlace, ::BlackStoneTurn)
-    }
-
-    companion object {
-        private val WHITE_STONE_RULE = OmokRule { omokStone, board -> board.canPlace(omokStone.position) }
-    }
+    override fun updateState(): OmokState = BlackStoneTurn
 }
