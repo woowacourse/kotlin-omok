2875b23d898e23530fd20120a09a9e8f1c943876
ijh1298 <imjh0417@gmail.com>
2025-03-25 14:11:13 +0900

    refactor: 마지막 돌을 nullable로 변경

--- src/main/kotlin/omok/model/game/Game.kt
+++ src/main/kotlin/omok/model/game/Game.kt
@@ -1,19 +1,18 @@
 package omok.model.game
 
 import omok.model.Board
-import omok.model.stone.Point
 import omok.model.stone.Stone
 import omok.model.stone.StoneColor
 
 class Game(
     private val board: Board,
 ) {
-    private var _lastStone = Stone(Point(1, 1), StoneColor.WHITE)
-    val lastStone get() = _lastStone.copy()
+    private var _lastStone: Stone? = null
+    val lastStone get() = _lastStone?.copy()
 
     fun play(newStone: Stone) {
         board.place(newStone)
-        _lastStone = Stone(newStone.point, _lastStone.color)
+        _lastStone = newStone
     }
 
     fun gameState(newStone: Stone): GameState {
