ed7dea2508ffcb167d10e9a7c763a4bc40b72aa9
cucumber99 <dlsguq1@gmail.com>
2025-03-25 15:05:42 +0900

    feat: 흑돌 차례 상태 정의

--- src/main/kotlin/omok/domain/model/state/BlackStoneTurn.kt
+++ src/main/kotlin/omok/domain/model/state/BlackStoneTurn.kt
@@ -1,15 +1,9 @@
 package omok.domain.model.state
 
-import omok.domain.model.Board
-import omok.domain.model.position.Position
-import omok.domain.model.rule.RenjuRule
 import omok.domain.model.stone.StoneType
-import rule.BlackRenjuRule
 
-class BlackStoneTurn(board: Board) : Running(board, RenjuRule(BlackRenjuRule())) {
+data object BlackStoneTurn : OmokState {
     override val stoneType: StoneType = StoneType.BLACK
 
-    override fun placeStone(onPlace: () -> Position): OmokState {
-        return super.placeStone(StoneType.BLACK, onPlace, ::WhiteStoneTurn)
-    }
+    override fun updateState(): OmokState = WhiteStoneTurn
 }
