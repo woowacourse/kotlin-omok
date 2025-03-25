c226d9bc2b2716d2da433fffc9ed4d82ea0fa471
cucumber99 <dlsguq1@gmail.com>
2025-03-25 15:04:54 +0900

    refactor: 오목 게임 상태 재정의

--- src/main/kotlin/omok/domain/model/state/OmokState.kt
+++ src/main/kotlin/omok/domain/model/state/OmokState.kt
@@ -1,11 +1,9 @@
 package omok.domain.model.state
 
-import omok.domain.model.Board
-import omok.domain.model.position.Position
 import omok.domain.model.stone.StoneType
 
-sealed class OmokState(val board: Board) {
-    abstract val stoneType: StoneType
+interface OmokState {
+    val stoneType: StoneType
 
-    abstract fun placeStone(onPlace: () -> Position): OmokState
+    fun updateState(): OmokState
 }
