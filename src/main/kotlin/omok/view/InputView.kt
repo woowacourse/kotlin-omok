c8a998fafec80b72b4a0860495c525c7c09b261d
gahyunkim <mapledt001@naver.com>
2025-03-25 15:36:20 +0900

    refactor: boardSize 변경으로 인한 코드 수정 및 검증 추가

--- src/main/kotlin/omok/view/InputView.kt
+++ src/main/kotlin/omok/view/InputView.kt
@@ -1,23 +1,27 @@
 package omok.view
 
-import omok.view.OutputView.Companion.BOARD_SIZE
+import omok.model.board.Board
 
 class InputView {
-    fun inputStone(): Pair<Int, Int> {
+    fun inputStone(board: Board): Pair<Int, Int> {
         print(INPUT_STONE_POSITION)
         val input = readlnOrNull()?.trim().toString()
-        return parse(input)
+        return parse(input, board)
     }
 
-    private fun parse(input: String): Pair<Int, Int> {
+    private fun parse(
+        input: String,
+        board: Board,
+    ): Pair<Int, Int> {
         if (input.length < MIN_USER_INPUT) throw IllegalArgumentException(ERROR_INVALID_COORDINATE_FORMAT)
 
         val colChar = input[COL_CHAR_INDEX].uppercaseChar()
-        require(colChar in MIN_COL_CHAR..<MIN_COL_CHAR + BOARD_SIZE) { ERROR_COL_STRING }
+        require(colChar in MIN_COL_CHAR..<MIN_COL_CHAR + board.getWidth()) { ERROR_COL_STRING }
         val col = colChar - MIN_COL_CHAR
 
         val rowPart = input.substring(ROW_NUM_START_INDEX)
         val row = rowPart.toIntOrNull() ?: throw IllegalArgumentException(ERROR_ROW_NUM)
+        require(row in MIN_ROW_NUM..board.getWidth()) { ERROR_ROW_NUM }
 
         val rowIndex = row - ROW_NUM_START_INDEX
 
@@ -27,6 +31,7 @@ class InputView {
     companion object {
         private const val INPUT_STONE_POSITION = "위치를 입력하세요: "
         private const val MIN_COL_CHAR = 'A'
+        private const val MIN_ROW_NUM = 0
         private const val MIN_USER_INPUT = 2
         private const val COL_CHAR_INDEX = 0
         private const val ROW_NUM_START_INDEX = 1
