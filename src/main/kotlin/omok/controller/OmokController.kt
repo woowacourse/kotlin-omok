b93b5de35d0ceb049b6acac6c7a0869f78e0d842
ijh1298 <imjh0417@gmail.com>
2025-03-26 00:05:37 +0900

    refactor: 컨트롤러 내 게임을 반복 실행하는 메서드 수정

--- src/main/kotlin/omok/controller/OmokController.kt
+++ src/main/kotlin/omok/controller/OmokController.kt
@@ -2,7 +2,9 @@ package omok.controller
 
 import omok.model.Board
 import omok.model.adapter.RenjuRuleAdapter
+import omok.model.game.FoulConditionResult
 import omok.model.game.GameState
+import omok.model.game.InvalidMoveResult
 import omok.model.stone.Point
 import omok.model.stone.Stone
 import omok.model.stone.StoneColor
@@ -17,26 +19,47 @@ class OmokController(
         val board = Board(renjuRuleAdapter = RenjuRuleAdapter())
         outputView.printOmokStart()
         outputView.printBoard(board)
-        retryOnError { processTurn(board) }
+        playOmok(board)
     }
 
-    private fun processTurn(board: Board) {
+    private fun playOmok(board: Board) {
+        while (true) {
+            val stone = setStone(board)
+            val foulConditionResult: FoulConditionResult? = board.checkFoulCondition(stone)
+            val invalidMoveResult: InvalidMoveResult? = board.checkInvalidMove(stone)
+            if (shouldRetry(board.checkFoulCondition(stone), board.checkInvalidMove(stone))) {
+                continue
+            }
+            if (invalidMoveResult is InvalidMoveResult.FullBoard) {
+                break
+            }
+            board.place(stone)
+            val gameState = board.gameState(stone)
+            outputView.printBoard(board)
+            if (gameState != GameState.PLAYING) {
+                outputView.printWinner(gameState)
+                break
+            }
+        }
+    }
+
+    private fun setStone(board: Board): Stone {
         val lastStone = board.stones.lastStone
         val inputPoint: Point = inputView.readTurn(lastStone)
-        val stone = Stone(inputPoint, lastStone?.color ?: StoneColor.BLACK)
-        board.place(stone)
-        outputView.printBoard(board)
-
-        when (val gameState = board.gameState(stone)) {
-            GameState.PLAYING -> processTurn(board)
-            GameState.BLACK_OMOK -> outputView.printWinner(gameState)
-            GameState.WHITE_OMOK -> outputView.printWinner(gameState)
-        }
+        val nextColor: StoneColor = (lastStone?.color ?: StoneColor.WHITE).reverse()
+        return Stone(inputPoint, nextColor)
     }
 
-    private fun <T> retryOnError(function: () -> T): T =
-        runCatching { function() }.getOrElse { error ->
-            println(error.message)
-            retryOnError(function)
+    private fun shouldRetry(
+        foulConditionResult: FoulConditionResult?,
+        invalidMoveResult: InvalidMoveResult?,
+    ): Boolean {
+        if (foulConditionResult != null) {
+            return true
         }
+        if (invalidMoveResult != null && invalidMoveResult !is InvalidMoveResult.FullBoard) {
+            return true
+        }
+        return false
+    }
 }
