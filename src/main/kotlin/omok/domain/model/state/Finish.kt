91cae8ecef4ab5509c421c73b8bca50cf6949e97
cucumber99 <dlsguq1@gmail.com>
2025-03-25 15:05:19 +0900

    feat: 종료 상태 정의

--- src/main/kotlin/omok/domain/model/state/Finish.kt
+++ src/main/kotlin/omok/domain/model/state/Finish.kt
@@ -1,14 +1,9 @@
 package omok.domain.model.state
 
-import omok.domain.model.Board
-import omok.domain.model.position.Position
 import omok.domain.model.stone.StoneType
 
-class Finish(
-    board: Board,
-    override val stoneType: StoneType,
-) : OmokState(board) {
-    override fun placeStone(onPlace: () -> Position): OmokState {
-        error("이미 게임이 종료되었습니다.")
-    }
+data class Finish(val winner: StoneType) : OmokState {
+    override val stoneType: StoneType = winner
+
+    override fun updateState(): OmokState = this
 }
