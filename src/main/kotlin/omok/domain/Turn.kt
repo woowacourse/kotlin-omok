eefcb289f235edc07af3e713beff2e232585afce
devfeijoa <devfeijoa@gmail.com>
2025-03-26 04:32:22 +0900

    refactor: Turn이 플레이어 순서만 관리하도록 수정

--- src/main/kotlin/omok/domain/Turn.kt
+++ src/main/kotlin/omok/domain/Turn.kt
@@ -1,28 +1,16 @@
 package omok.domain
 
-data class Turn(private var turn: StoneType = StoneType.BLACK) {
-    fun next() {
-        turn =
-            if (turn == StoneType.BLACK) {
-                StoneType.WHITE
-            } else {
-                StoneType.BLACK
+class Turn(private var current: StoneType = StoneType.BLACK) {
+    fun currentPlayer(): StoneType = current
+
+    fun switch() {
+        current =
+            when (current) {
+                StoneType.BLACK -> StoneType.WHITE
+                StoneType.WHITE -> StoneType.BLACK
+                else -> throw IllegalStateException("Invalid turn state")
             }
     }
 
-    fun isWhite(): Boolean = if (turn == StoneType.WHITE) true else false
-
-    fun stone(position: String): Stone {
-        val regex = """([A-Z]+)(\d+)""".toRegex()
-        val matchResult = regex.matchEntire(position) ?: throw IllegalArgumentException(ERROR_NOT_FIND)
-        val (rowString, column) = matchResult.destructured
-        val row =
-            RowType.entries.find { it.name == rowString }?.value ?: throw IllegalArgumentException(ERROR_NOT_FIND)
-        val stone = Stone(Position(row, (column.toInt() - 1)), turn)
-        return stone
-    }
-
-    companion object {
-        private const val ERROR_NOT_FIND = "유효하지 않은 입력입니다. 다시 입력해주세요."
-    }
+    fun isBlackTurn(): Boolean = current == StoneType.BLACK
 }
