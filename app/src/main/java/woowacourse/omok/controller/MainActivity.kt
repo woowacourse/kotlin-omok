c25db879c8bbe4d22ae4707838906c41cbbe8dc0
junseo511 <prime54@g.skku.edu>
2025-03-25 14:49:43 +0900

    feat: 도메인과 안드로이드 뷰 연결

--- app/src/main/java/woowacourse/omok/controller/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/controller/MainActivity.kt
@@ -1,6 +1,7 @@
 package woowacourse.omok.controller
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
@@ -9,7 +10,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.children
+import com.google.android.material.snackbar.Snackbar
+import omok.domain.omokboard.ColumnPosition
+import omok.domain.omokboard.OmokBoard
+import omok.domain.omokboard.PlayingBoard
+import omok.domain.omokboard.Position
+import omok.domain.omokboard.RowPosition
+import omok.domain.player.PlayerStone
+import omok.domain.player.StoneColor
+import omok.domain.rule.place.AlreadyExistStoneRule
+import omok.domain.rule.place.ExternalRule
+import omok.domain.rule.place.InvalidPositionRule
+import omok.domain.rule.place.PlaceResult
+import omok.domain.rule.place.PlaceRule
 import woowacourse.omok.R
+import woowacourse.omok.domain.rule.judge.DrawRule
+import woowacourse.omok.domain.rule.judge.JudgeResult
+import woowacourse.omok.domain.rule.judge.JudgeRule
+import woowacourse.omok.domain.rule.judge.WinningRule
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +40,61 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val playingBoard = PlayingBoard(board = OmokBoard.create())
+        val placeRules: List<PlaceRule> = listOf(InvalidPositionRule(), AlreadyExistStoneRule(), ExternalRule())
+        val judgeRules: List<JudgeRule> = listOf(WinningRule(), DrawRule())
+
         val board = findViewById<TableLayout>(R.id.board)
+        board
+            .children
+            .filterIsInstance<TableRow>()
+            .forEachIndexed { rowIndex, row ->
+                row.children
+                    .filterIsInstance<ImageView>()
+                    .forEachIndexed { colIndex, button ->
+                        button.id = View.generateViewId()
+                        button.tag = Position(RowPosition(rowIndex + 1), ColumnPosition(colIndex + 1))
+                    }
+            }
+
+        var stoneColor = StoneColor.BLACK
         board
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEach { button ->
+                button.setOnClickListener {
+                    val position = button.tag as Position
+                    val playerStone = PlayerStone(stoneColor, position)
+                    when (val placeResult = playingBoard.placeStone(placeRules, playerStone)) {
+                        is PlaceResult.Success -> {
+                            button.setImageResource(
+                                when (stoneColor) {
+                                    StoneColor.BLACK -> R.drawable.black_stone
+                                    StoneColor.WHITE -> R.drawable.white_stone
+                                },
+                            )
+                            val judgeResult = playingBoard.judge(judgeRules, playerStone)
+                            if (judgeResult is JudgeResult.Finished) {
+                                board
+                                    .children
+                                    .filterIsInstance<TableRow>()
+                                    .flatMap { it.children }
+                                    .filterIsInstance<ImageView>()
+                                    .forEach { button ->
+                                        button.isEnabled = false
+                                    }
+                            }
+                            Snackbar.make(findViewById(R.id.main), judgeResult.toString(), Snackbar.LENGTH_SHORT).show()
+                            stoneColor = stoneColor.reversed()
+                        }
+
+                        is PlaceResult.Failure -> {
+                            Snackbar.make(findViewById(R.id.main), placeResult.toString(), Snackbar.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
     }
 }
