65e61aa5437d226873a92873c451f288fe95b8fc
cucumber99 <dlsguq1@gmail.com>
2025-03-25 15:06:22 +0900

    refactor: 보드 클래스 리팩터링

--- src/main/kotlin/omok/domain/model/Board.kt
+++ src/main/kotlin/omok/domain/model/Board.kt
@@ -1,93 +1,39 @@
 package omok.domain.model
 
-import omok.domain.model.position.Column
-import omok.domain.model.position.Direction
-import omok.domain.model.position.Position
-import omok.domain.model.position.Row
 import omok.domain.model.stone.OmokStone
 import omok.domain.model.stone.StoneType
 
 class Board(
-    val stones: Map<Position, StoneType> = mapOf(),
     val size: Int = DEFAULT_BOARD_SIZE,
+    val stones: List<OmokStone> = emptyList(),
 ) {
-    fun placeStone(
-        position: Position,
-        stoneType: StoneType,
-    ): Board {
-        require(canPlace(position)) { "바둑돌이 있는 곳에는 둘 수 없습니다." }
-        return Board(stones + mapOf(position to stoneType))
+    val blackStones: List<OmokStone>
+        get() = getStones(StoneType.BLACK)
+
+    val whiteStones: List<OmokStone>
+        get() = getStones(StoneType.WHITE)
+
+    fun placeStone(omokStone: OmokStone): Board {
+        validate(omokStone)
+        return Board(size, stones + listOf(omokStone))
     }
 
-    fun canPlace(position: Position): Boolean = stones[position] == null
+    fun getLastStone(): OmokStone? = stones.lastOrNull()
 
-    fun lastStoneOrNull(): OmokStone? {
-        val lastKey = stones.keys.lastOrNull() ?: return null
-        val lastValue = stones[lastKey] ?: return null
-        return OmokStone(lastKey, lastValue)
-    }
+    fun isFull(): Boolean = stones.size == size * size
 
-    fun getStones(stoneType: StoneType): List<OmokStone> {
-        return stones.filterValues { it == stoneType }
-            .map { OmokStone(it.key, stoneType) }
-    }
-
-    fun checkWin(): Boolean {
-        val directions =
-            listOf(
-                Direction(0, 1),
-                Direction(1, 0),
-                Direction(1, 1),
-                Direction(1, -1),
-            )
-
-        return stones.entries.any { (position, stone) ->
-            directions.any { direction ->
-                direction.countConsecutive(position, stone) >= 5
-            }
+    private fun validate(omokStone: OmokStone) {
+        require(stones.none { it.position == omokStone.position }) {
+            DUPLICATE_MESSAGE
         }
     }
 
-    private fun Direction.countConsecutive(
-        position: Position,
-        stoneType: StoneType,
-    ): Int {
-        return countInDirection(position, stoneType, this) + countInDirection(position, stoneType, this.inverse()) - 1
-    }
-
-    private fun countInDirection(
-        position: Position,
-        stoneType: StoneType,
-        direction: Direction,
-    ): Int {
-        var count = 1
-        var currentPos = position
-
-        while (true) {
-            val nextPos = currentPos.next(direction) ?: break
-            if (stones[nextPos] == stoneType) {
-                count++
-                currentPos = nextPos
-            } else {
-                break
-            }
-        }
-
-        return count
-    }
-
-    private fun Position.next(direction: Direction): Position? {
-        val nextRowValue = this.row.value + direction.rowDelta
-        val nextColIndex = this.column.value - 1 + direction.colDelta
-
-        if (nextRowValue !in 1..15 || nextColIndex !in 0..14) return null
-
-        val nextColumn = Column.from(('A' + nextColIndex))
-        val nextRow = Row(nextRowValue)
-        return Position(nextColumn, nextRow)
+    private fun getStones(stoneType: StoneType): List<OmokStone> {
+        return stones.filter { it.stoneType == stoneType }
     }
 
     companion object {
         private const val DEFAULT_BOARD_SIZE = 15
+        private const val DUPLICATE_MESSAGE = "이미 바둑돌이 존재하는 곳에는 둘 수 없습니다."
     }
 }
--- src/test/kotlin/omok/domain/model/BoardTest.kt
+++ src/test/kotlin/omok/domain/model/BoardTest.kt
@@ -1,14 +1,10 @@
 package omok.domain.model
 
-import io.kotest.matchers.shouldBe
-import omok.diagonalDownWinStones
 import omok.domain.model.position.Column
 import omok.domain.model.position.Position
 import omok.domain.model.position.Row
 import omok.domain.model.stone.OmokStone
 import omok.domain.model.stone.StoneType
-import omok.horizontalWinStones
-import omok.verticalWinStones
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
@@ -24,74 +20,27 @@ class BoardTest {
 
     @Test
     fun `바둑돌을 둔다`() {
+        // Given
         val position = Position(Column.from('A'), Row(1))
-        board = board.placeStone(position, StoneType.BLACK)
-        assertThat(board.stones[position]).isEqualTo(StoneType.BLACK)
+        val omokStone = OmokStone(position, StoneType.BLACK)
+
+        // When
+        board = board.placeStone(omokStone)
+
+        // Then
+        assertThat(board.stones.last()).isEqualTo(omokStone)
     }
 
     @Test
     fun `바둑돌이 이미 존재하는 위치는 둘 수 없다`() {
+        // Given
         val position = Position(Column.from('A'), Row(1))
-        board = board.placeStone(position, StoneType.BLACK)
-        assertThatThrownBy {
-            board.placeStone(
-                position,
-                StoneType.BLACK,
-            )
-        }.isInstanceOf(IllegalArgumentException::class.java)
+        val omokStone = OmokStone(position, StoneType.BLACK)
+        board = board.placeStone(omokStone)
+
+        // Then
+        assertThatThrownBy { board.placeStone(omokStone) }
+            .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("바둑돌이 있는 곳에는 둘 수 없습니다.")
-    }
-
-    @Test
-    fun `세로 다섯 개의 연속된 돌이 있을 경우 승리한다`() {
-        // Given
-        val board = Board(horizontalWinStones)
-
-        // Then
-        board.checkWin() shouldBe true
-    }
-
-    @Test
-    fun `대각선 다섯 개의 연속된 돌이 있을 경우 승리한다`() {
-        // Given
-        val board = Board(verticalWinStones)
-
-        // Then
-        board.checkWin() shouldBe true
-    }
-
-    @Test
-    fun `가로 다섯 개의 연속된 돌이 있을 경우 승리한다`() {
-        // Given
-        val board = Board(diagonalDownWinStones)
-
-        // Then
-        board.checkWin() shouldBe true
-    }
-
-    @Test
-    fun `바둑돌을 둘 위치가 빈 경우 바둑돌을 둘 수 있다`() {
-        // Given
-        val board = Board()
-        val position = Position(Column.from('A'), Row(1))
-
-        // Then
-        board.canPlace(position) shouldBe true
-    }
-
-    @Test
-    fun `마지막으로 둔 바둑돌을 반환한다`() {
-        // Given
-        val board = Board()
-        val position = Position(Column.from('A'), Row(1))
-        val stoneType = StoneType.BLACK
-
-        // When
-        val omokStone = OmokStone(position, stoneType)
-        val newBoard = board.placeStone(omokStone.position, omokStone.stoneType)
-        val lastStone = newBoard.lastStoneOrNull()
-
-        // Then
-        lastStone shouldBe omokStone
     }
 }
