f65a0e4cdb3da6ad68f94f064339120cb4bb8ba0
yrsel <xxknu88@gmail.com>
2025-03-25 16:51:49 +0900

    chore: 사용안되는 코드 제거

--- src/main/kotlin/domain/stone/StoneColor.kt
+++ src/main/kotlin/domain/stone/StoneColor.kt
@@ -3,11 +3,4 @@ package domain.stone
 enum class StoneColor {
     WHITE,
     BLACK,
-    ;
-
-    fun switch(): StoneColor =
-        when (this) {
-            WHITE -> BLACK
-            BLACK -> WHITE
-        }
 }
