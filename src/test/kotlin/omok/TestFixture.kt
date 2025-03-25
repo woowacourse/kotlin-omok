dfb523343919f54a0d4eaa056862cda70d2a30b3
cucumber99 <dlsguq1@gmail.com>
2025-03-25 15:08:23 +0900

    test: 테스트 요소 변경

--- src/test/kotlin/omok/TestFixture.kt
+++ src/test/kotlin/omok/TestFixture.kt
@@ -3,58 +3,56 @@ package omok
 import omok.domain.model.position.Column
 import omok.domain.model.position.Position
 import omok.domain.model.position.Row
+import omok.domain.model.stone.OmokStone
 import omok.domain.model.stone.StoneType
 
 val horizontalWinStones =
-    mapOf(
-        Position(Column.from('A'), Row(1)) to StoneType.BLACK,
-        Position(Column.from('B'), Row(1)) to StoneType.BLACK,
-        Position(Column.from('C'), Row(1)) to StoneType.BLACK,
-        Position(Column.from('D'), Row(1)) to StoneType.BLACK,
-        Position(Column.from('E'), Row(1)) to StoneType.BLACK,
+    listOf(
+        OmokStone(Position(Column.from('A'), Row(1)), StoneType.BLACK),
+        OmokStone(Position(Column.from('B'), Row(1)), StoneType.BLACK),
+        OmokStone(Position(Column.from('C'), Row(1)), StoneType.BLACK),
+        OmokStone(Position(Column.from('D'), Row(1)), StoneType.BLACK),
     )
 
 val verticalWinStones =
-    mapOf(
-        Position(Column.from('A'), Row(1)) to StoneType.BLACK,
-        Position(Column.from('A'), Row(2)) to StoneType.BLACK,
-        Position(Column.from('A'), Row(3)) to StoneType.BLACK,
-        Position(Column.from('A'), Row(4)) to StoneType.BLACK,
-        Position(Column.from('A'), Row(5)) to StoneType.BLACK,
+    listOf(
+        OmokStone(Position(Column.from('A'), Row(1)), StoneType.BLACK),
+        OmokStone(Position(Column.from('A'), Row(2)), StoneType.BLACK),
+        OmokStone(Position(Column.from('A'), Row(3)), StoneType.BLACK),
+        OmokStone(Position(Column.from('A'), Row(4)), StoneType.BLACK),
     )
 
 val diagonalDownWinStones =
-    mapOf(
-        Position(Column.from('A'), Row(1)) to StoneType.BLACK,
-        Position(Column.from('B'), Row(2)) to StoneType.BLACK,
-        Position(Column.from('C'), Row(3)) to StoneType.BLACK,
-        Position(Column.from('D'), Row(4)) to StoneType.BLACK,
-        Position(Column.from('E'), Row(5)) to StoneType.BLACK,
+    listOf(
+        OmokStone(Position(Column.from('A'), Row(1)), StoneType.BLACK),
+        OmokStone(Position(Column.from('B'), Row(2)), StoneType.BLACK),
+        OmokStone(Position(Column.from('C'), Row(3)), StoneType.BLACK),
+        OmokStone(Position(Column.from('D'), Row(4)), StoneType.BLACK),
     )
 
 val doubleThreeFixture =
-    mapOf(
-        Position(Column.from('B'), Row(6)) to StoneType.BLACK,
-        Position(Column.from('E'), Row(6)) to StoneType.BLACK,
-        Position(Column.from('C'), Row(5)) to StoneType.BLACK,
-        Position(Column.from('E'), Row(5)) to StoneType.BLACK,
+    listOf(
+        OmokStone(Position(Column.from('B'), Row(6)), StoneType.BLACK),
+        OmokStone(Position(Column.from('E'), Row(6)), StoneType.BLACK),
+        OmokStone(Position(Column.from('C'), Row(5)), StoneType.BLACK),
+        OmokStone(Position(Column.from('E'), Row(5)), StoneType.BLACK),
     )
 
 val doubleFourFixture =
-    mapOf(
-        Position(Column.from('C'), Row(10)) to StoneType.BLACK,
-        Position(Column.from('C'), Row(11)) to StoneType.BLACK,
-        Position(Column.from('C'), Row(12)) to StoneType.BLACK,
-        Position(Column.from('C'), Row(14)) to StoneType.BLACK,
-        Position(Column.from('C'), Row(15)) to StoneType.BLACK,
-        Position(Column.from('D'), Row(12)) to StoneType.BLACK,
+    listOf(
+        OmokStone(Position(Column.from('C'), Row(10)), StoneType.BLACK),
+        OmokStone(Position(Column.from('C'), Row(11)), StoneType.BLACK),
+        OmokStone(Position(Column.from('C'), Row(12)), StoneType.BLACK),
+        OmokStone(Position(Column.from('C'), Row(14)), StoneType.BLACK),
+        OmokStone(Position(Column.from('C'), Row(15)), StoneType.BLACK),
+        OmokStone(Position(Column.from('D'), Row(12)), StoneType.BLACK),
     )
 
 val stoneLongMoveFixture =
-    mapOf(
-        Position(Column.from('B'), Row(2)) to StoneType.BLACK,
-        Position(Column.from('C'), Row(3)) to StoneType.BLACK,
-        Position(Column.from('D'), Row(4)) to StoneType.BLACK,
-        Position(Column.from('F'), Row(6)) to StoneType.BLACK,
-        Position(Column.from('G'), Row(7)) to StoneType.BLACK,
+    listOf(
+        OmokStone(Position(Column.from('B'), Row(2)), StoneType.BLACK),
+        OmokStone(Position(Column.from('C'), Row(3)), StoneType.BLACK),
+        OmokStone(Position(Column.from('D'), Row(4)), StoneType.BLACK),
+        OmokStone(Position(Column.from('F'), Row(6)), StoneType.BLACK),
+        OmokStone(Position(Column.from('G'), Row(7)), StoneType.BLACK),
     )
