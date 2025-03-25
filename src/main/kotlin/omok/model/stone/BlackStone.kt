91e632756bfe2daaa7f95c05697aa57f395d07fc
박지민 <pjm5489@naver.com>
2025-03-25 13:40:15 +0900

    chore: WhiteStone, BlackStone 클래스 삭제

--- src/main/kotlin/omok/model/stone/BlackStone.kt
+++
@@ -1,9 +0,0 @@
-package omok.model.stone
-
-import omok.model.board.Position
-
-data class BlackStone(
-    override val position: Position,
-) : Stone {
-    override val color: StoneColor = StoneColor.BLACK
-}
--- src/main/kotlin/omok/model/stone/WhiteStone.kt
+++
@@ -1,9 +0,0 @@
-package omok.model.stone
-
-import omok.model.board.Position
-
-class WhiteStone(
-    override val position: Position,
-) : Stone {
-    override val color: StoneColor = StoneColor.WHITE
-}
