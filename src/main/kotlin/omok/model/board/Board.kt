45d31d95c5883f16a496aff85cceb94d5f59931f
gahyunkim <mapledt001@naver.com>
2025-03-25 15:35:20 +0900

    refactor: bordSize 및 black,whiteStones 필터링 코드 추가 및 수정

--- src/main/kotlin/omok/model/board/Board.kt
+++ src/main/kotlin/omok/model/board/Board.kt
@@ -5,21 +5,30 @@ import omok.model.stone.position.Position
 
 class Board private constructor(
     val stonesMap: Map<Position, StoneColor> = emptyMap(),
+    private val dimensions: BoardDimensions,
 ) {
+    fun getWidth() = dimensions.width
+
+    fun getHeight() = dimensions.height
+
+    fun getBlackStones() = stonesMap.filter { it.value == StoneColor.BLACK }.map { it.key }
+
+    fun getWhiteStones() = stonesMap.filter { it.value == StoneColor.WHITE }.map { it.key }
+
     fun positionAt(
         position: Position,
         stoneColor: StoneColor,
     ): Board {
         require(!stonesMap.containsKey(position)) { ERROR_STONE_ALREADY_EXITS }
-        return Board(stonesMap + (position to stoneColor))
+        return Board(stonesMap + (position to stoneColor), dimensions)
     }
 
     companion object {
         private const val ERROR_STONE_ALREADY_EXITS = "해당하는 위치에 돌이 존재합니다"
 
-        fun initBoard(): Board {
+        fun initBoard(dimensions: BoardDimensions): Board {
             val initStonesMap: Map<Position, StoneColor> = emptyMap()
-            return Board(initStonesMap)
+            return Board(initStonesMap, dimensions)
         }
     }
 }
