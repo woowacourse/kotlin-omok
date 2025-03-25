03a8c069636bde9397eb405db7093fce6b5dc9c2
gahyunkim <mapledt001@naver.com>
2025-03-25 15:33:59 +0900

    refactor: Position에서의 검증 삭제

--- src/main/kotlin/omok/model/stone/position/Position.kt
+++ src/main/kotlin/omok/model/stone/position/Position.kt
@@ -1,20 +1,6 @@
 package omok.model.stone.position
 
-import omok.view.OutputView.Companion.BOARD_SIZE
-
 data class Position(
     val row: Row,
     val col: Col,
-) {
-    init {
-        require(row.value in MIN_RANGE until MIN_RANGE + BOARD_SIZE) { ERROR_ROW_RANGE }
-        require(col.value in MIN_RANGE until MIN_RANGE + BOARD_SIZE) { ERROR_COL_RANGE }
-    }
-
-    companion object {
-        private const val ERROR_ROW_RANGE = "가로 좌표는 오목판의 범위를 벗어날 수 없습니다"
-        private const val ERROR_COL_RANGE = "세로 좌표는 오목판의 범위를 벗어날 수 없습니다"
-
-        private const val MIN_RANGE = 0
-    }
-}
+)
