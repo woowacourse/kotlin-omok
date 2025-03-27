ab8cb62fb6b83c6dbdeecccf7b892a57109c15c9
DoableTuple <doabletuple@gmail.com>
2025-03-27 15:46:48 +0900

    feat(MainActivity): 앱을 재시작하면 이전 게임이 복구된다

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -1,6 +1,9 @@
 package woowacourse.omok
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
@@ -9,10 +12,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.children
+import woowacourse.omok.database.OmokContract
+import woowacourse.omok.database.OmokDbHelper
 import woowacourse.omok.model.Board
 import woowacourse.omok.model.Color
 import woowacourse.omok.model.Game
 import woowacourse.omok.model.MoveResult
+import woowacourse.omok.model.Stone
 import woowacourse.omok.model.position.Col
 import woowacourse.omok.model.position.Position
 import woowacourse.omok.model.position.Row
@@ -22,13 +28,38 @@ import woowacourse.omok.view.OutputViewAndroid
 class MainActivity : AppCompatActivity() {
     private val game = Game(Board(), RenjuRule())
     private val outputView = OutputViewAndroid()
+    private val dbHelper = OmokDbHelper(this)
+
+    private fun restoreGame(boardLayout: TableLayout) {
+        val stones: List<Stone> = queryAll()
+        stones.forEach { stone ->
+            game.processTurn(stone.position, stone.color)
+            val stoneImage =
+                when (stone.color) {
+                    Color.BLACK -> R.drawable.black_stone
+                    Color.WHITE -> R.drawable.white_stone
+                }
+            boardLayout.filterImageViews().forEachIndexed { index, view ->
+                if (index % 15 == stone.position.x.value - 1 && index / 15 == stone.position.y.value - 1) {
+                    view.setImageResource(stoneImage)
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initialize()
         val board: TableLayout = findViewById(R.id.board)
+        restoreGame(board)
+
         outputView.printOmokStart(board)
         setListeners(board)
+    }
+
+    override fun onDestroy() {
+        dbHelper.close()
+        super.onDestroy()
     }
 
     private fun initialize() {
@@ -41,6 +72,51 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun insertData(
+        x: Col,
+        y: Row,
+        color: Color,
+    ) {
+        val db = dbHelper.writableDatabase
+        val values =
+            ContentValues().apply {
+                put(OmokContract.COLUMN_NAME_X, x.value)
+                put(OmokContract.COLUMN_NAME_Y, y.value)
+                put(OmokContract.COLUMN_NAME_COLOR, color.name)
+            }
+        db.insert(OmokContract.TABLE_NAME, null, values)
+        queryAll().forEach {
+            Log.i(
+                "SQL",
+                "${it.position.x.value} ${it.position.y.value} ${it.color.name}",
+            )
+        }
+        Log.i("SQL", "----------------------")
+    }
+
+    private fun queryAll(): List<Stone> {
+        val dbReader = dbHelper.readableDatabase
+        val result = mutableListOf<Stone>()
+
+        val cursor: Cursor = dbReader.rawQuery("SELECT * FROM ${OmokContract.TABLE_NAME}", null)
+
+        with(cursor) {
+            while (moveToNext()) {
+                val x: Int = getInt(getColumnIndexOrThrow(OmokContract.COLUMN_NAME_X))
+                val y: Int = getInt(getColumnIndexOrThrow(OmokContract.COLUMN_NAME_Y))
+                val color: Color =
+                    when (getString(getColumnIndexOrThrow(OmokContract.COLUMN_NAME_COLOR))) {
+                        "BLACK" -> Color.BLACK
+                        "WHITE" -> Color.WHITE
+                        else -> throw IllegalStateException()
+                    }
+                result.add(Stone(Position(Col(x), Row(y)), color))
+            }
+        }
+        cursor.close()
+        return result
+    }
+
     private fun setListeners(board: TableLayout) {
         board.filterImageViews().forEachIndexed { index, view ->
             view.setOnClickListener { onClick(index, view, board) }
@@ -50,7 +126,7 @@ class MainActivity : AppCompatActivity() {
     private fun onClick(
         index: Int,
         view: ImageView,
-        board: TableLayout,
+        boardLayout: TableLayout,
     ) {
         val color: Color = game.chooseTurn()
         val stoneImage =
@@ -62,20 +138,28 @@ class MainActivity : AppCompatActivity() {
         val x = Col(index % game.board.col.value + 1)
         val y = Row(index / game.board.row.value + 1)
         when (val moveResult: MoveResult = game.processTurn(Position(x, y), color)) {
-            is MoveResult.Success.Playing -> view.setImageResource(stoneImage)
+            is MoveResult.Success.Playing -> {
+                view.setImageResource(stoneImage)
+                insertData(x, y, color)
+            }
+
             is MoveResult.Success.Finished -> {
                 view.setImageResource(stoneImage)
-                outputView.printMoveResult(moveResult, this, board)
-                board.filterImageViews().forEach { it.setOnClickListener(null) }
+                insertData(x, y, color)
+                outputView.printMoveResult(moveResult, this, boardLayout)
+                boardLayout.filterImageViews().forEach { it.setOnClickListener(null) }
+                dbHelper.writableDatabase.delete(OmokContract.TABLE_NAME, null, null)
                 return
             }
+
             is MoveResult.Failure -> {
-                outputView.printMoveResult(moveResult, this, board)
+                outputView.printMoveResult(moveResult, this, boardLayout)
             }
         }
     }
 
     private fun TableLayout.filterImageViews(): Sequence<ImageView> {
-        return children.filterIsInstance<TableRow>().flatMap { it.children }.filterIsInstance<ImageView>()
+        return children.filterIsInstance<TableRow>().flatMap { it.children }
+            .filterIsInstance<ImageView>()
     }
 }
