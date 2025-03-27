c236a60b88bb5281d92b120684a148f14a881bfa
지원 <cheerfulboys@naver.com>
2025-03-27 11:03:38 +0900

    feat: 플레이어 색상에 따라 돌을 배치하는 기능 및 순서 알림 토스트 메세지 추가

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.children
+import omok.domain.Board
+import omok.domain.Position
+import omok.domain.RenjuRuleAdapter
+import omok.domain.Turn
 
 class MainActivity : AppCompatActivity() {
+    private val omokBoard: Board = Board(RenjuRuleAdapter())
+    private val turn = Turn()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,6 +34,29 @@ class MainActivity : AppCompatActivity() {
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { index, view ->
+                val row = index / Board.BOARD_SIZE
+                val column = index % Board.BOARD_SIZE
+                view.setOnClickListener {
+                    putStone(view, row, column)
+                    showTurnColorToast()
+                } }
+        showTurnColorToast()
+    }
+
+    private fun putStone(view: ImageView, row: Int, column: Int) {
+        omokBoard.put(Position(row, column), turn.color)
+        if (turn.isWhite()) {
+            view.setImageResource(R.drawable.white_stone)
+        } else {
+            view.setImageResource(R.drawable.black_stone)
+        }
+        turn.next()
+    }
+
+    private fun showTurnColorToast() {
+        val nextTurnColor: String = if (turn.isWhite()) "백" else "흑"
+        val toastTurn = Toast.makeText(this, "${nextTurnColor}의 차례입니다.", Toast.LENGTH_SHORT)
+        toastTurn.show()
     }
 }
