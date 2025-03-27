4dd4ef2ef5c5a299a82fea2d45454d7e950b3436
chanho0908 <uni070@naver.com>
2025-03-27 11:31:05 +0900

    feat : 게임을 진행하는 서비스 객체 추가

---
+++ app/src/main/java/woowacourse/omok/domain/service/OmokGame.kt
@@ -0,0 +1,53 @@
+package woowacourse.omok.domain.service
+
+import woowacourse.omok.domain.board.BoardStatus
+import woowacourse.omok.domain.board.OmokBoard
+import woowacourse.omok.domain.exception.ResultState
+import woowacourse.omok.domain.exception.execute
+import woowacourse.omok.domain.point.OmokPoints
+import woowacourse.omok.domain.point.Point
+import woowacourse.omok.domain.stone.StoneColor
+import woowacourse.omok.ui.event.GameEventListener
+
+class OmokGame(
+    private val omokBoard: OmokBoard,
+    private val event: GameEventListener,
+) {
+    private var stone = StoneColor.BLACK
+
+    fun play(point: Point) {
+        val newStone = point.copy(status = BoardStatus.Moved(stone))
+        when (val result = canMove(newStone)) {
+            is ResultState.Error -> {
+                event.onFailToAddStone(result.exceptions)
+            }
+
+            is ResultState.Success -> {
+                omokBoard.addStone(newStone)
+                event.onPlacedStone(stone)
+                checkOmok(newStone)
+            }
+        }
+    }
+
+    private fun checkOmok(point: Point) {
+        if (omokBoard.isOmok(point)) {
+            event.onFinishedGame(stone)
+        }
+        stone = stone.toggle()
+    }
+
+    private fun canMove(point: Point) =
+        execute {
+            omokBoard.pointValidation(point)
+        }
+
+    companion object {
+        fun create(event: GameEventListener): OmokGame {
+            val points = OmokPoints()
+            val omokBoard = OmokBoard(points)
+            val game = OmokGame(omokBoard, event)
+            return game
+        }
+    }
+}
