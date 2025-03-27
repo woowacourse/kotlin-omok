83a4f9215ec1a027bf85058fdc4b689c9b6f8b24
gahyunkim <mapledt001@naver.com>
2025-03-27 13:46:40 +0900

    feat: SQLite 적용을 위한 코드 추가

---
+++ app/src/main/java/woowacourse/omok/data/OmokContract.kt
@@ -0,0 +1,21 @@
+package woowacourse.omok.data
+
+import android.provider.BaseColumns
+
+object OmokContract {
+    const val TABLE_NAME = "omok"
+
+    const val COLUMN_PLACE_ID = BaseColumns._ID
+    const val COLUMN_ROW_POSITION = "row"
+    const val COLUMN_COL_POSITION = "col"
+    const val COLUMN_STONE_COLOR = "stoneColor"
+
+    const val SQL_CREATE_ENTRIES =
+        "CREATE TABLE $TABLE_NAME (" +
+                "$COLUMN_PLACE_ID INTEGER PRIMARY KEY," +
+                "$COLUMN_ROW_POSITION INTEGER," +
+                "$COLUMN_COL_POSITION INTEGER," +
+                "$COLUMN_STONE_COLOR TEXT)"
+
+    const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS $TABLE_NAME"
+}
