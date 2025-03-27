edf5dcd806f35fea69331277fc778f98cd49c908
yrsel <xxknu88@gmail.com>
2025-03-27 15:07:49 +0900

    refactor: 룰 의존성 주입할 수 있도록 수정

--- src/main/kotlin/rule/adapter/RuleAdapter.kt
+++ src/main/kotlin/rule/adapter/RuleAdapter.kt
@@ -1,62 +1,41 @@
 package rule.adapter
 
 import domain.stone.Stone
-import domain.stone.StoneColor
-import domain.stone.StoneColor.BLACK
-import domain.stone.StoneColor.WHITE
 import domain.stone.Stones
-import rule.lib.BlackRenjuRule
-import rule.lib.WhiteRenjuRule
+import rule.lib.OmokRule
+import rule.lib.RenjuRule
 import rule.lib.type.Violation
 
-class RuleAdapter : Rule {
-    private val blackRenjuRule = BlackRenjuRule()
-    private val whiteRenjuRule = WhiteRenjuRule()
-
-    override fun isWinByStoneColor(
-        stoneColor: StoneColor,
+class RuleAdapter(
+    private val omokRule: OmokRule,
+) : Rule {
+    override fun isWin(
         playerStones: Stones,
         otherStones: Stones,
         placedStone: Stone,
     ): Boolean =
-        when (stoneColor) {
-            BLACK -> {
-                blackRenjuRule.checkWin(
-                    blackStones = playerStones.value,
-                    whiteStones = otherStones.value,
-                    startPosition = placedStone.position,
-                )
-            }
-
-            WHITE -> {
-                whiteRenjuRule.checkWin(
-                    blackStones = emptyList(),
-                    whiteStones = playerStones.value,
-                    startPosition = placedStone.position,
-                )
-            }
-        }
+        omokRule.checkWin(
+            targetStone = playerStones.value,
+            otherStones = otherStones.value,
+            startPosition = placedStone.position,
+        )
 
     override fun violation(
-        stoneColor: StoneColor,
         playerStones: Stones,
         otherStones: Stones,
         placedStone: Stone,
-    ): Violation =
-        when (stoneColor) {
-            BLACK -> {
-                blackRenjuRule.checkAnyFoulCondition(
-                    blackStones = playerStones.value,
-                    whiteStones = otherStones.value,
-                    startPosition = placedStone.position,
-                )
-            }
-
-            WHITE ->
-                whiteRenjuRule.checkDuplicatePosition(
-                    blackStones = otherStones.value,
-                    whiteStones = playerStones.value,
-                    curPosition = placedStone.position,
-                )
+    ): Violation {
+        if (omokRule is RenjuRule) {
+            return omokRule.checkAnyFoulCondition(
+                blackStones = playerStones.value,
+                whiteStones = otherStones.value,
+                startPosition = placedStone.position,
+            )
         }
+        return omokRule.checkDuplicatePosition(
+            blackStones = playerStones.value,
+            whiteStones = otherStones.value,
+            curPosition = placedStone.position,
+        )
+    }
 }
