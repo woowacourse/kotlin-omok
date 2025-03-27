245cabb2718e4da98d9d0cbc4560f17096b20648
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-03-27 10:57:15 +0900

    feat: domain과 안드로이드 연결

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.children
+import domain.domain.Point
+import domain.domain.state.BlackTurn
+import domain.domain.state.Finished
+import domain.domain.state.Playing
+import domain.domain.state.Ready
+import domain.domain.state.State
+import domain.domain.state.WhiteTurn
+import domain.domain.stone.StoneColor
 
 class MainActivity : AppCompatActivity() {
+    private var state: State = Ready()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,11 +33,66 @@ class MainActivity : AppCompatActivity() {
         }
 
         val board = findViewById<TableLayout>(R.id.board)
-        board
-            .children
-            .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+        val rows = board.children.filterIsInstance<TableRow>().toList().reversed()
+
+        rows.forEachIndexed { rowIndex, row ->
+            row.children
+                .filterIsInstance<ImageView>()
+                .forEachIndexed { colIndex, imageView ->
+                    imageView.setOnClickListener {
+                        val currentState = state
+                        val point = Point(colIndex, rowIndex)
+
+                        placeStone(point, imageView, currentState)
+                    }
+                }
+        }
+    }
+
+    private fun placeStone(
+        point: Point,
+        imageView: ImageView,
+        currentState: State,
+    ) {
+        if (currentState !is Playing) return
+
+        runCatching {
+            state =
+                currentState.place(
+                    point,
+                    15,
+                    onBoardUpdated = { newBlackStones, newWhiteStones ->
+                        if (point in newBlackStones) currentState.blackStones + point
+                        if (point in newWhiteStones) currentState.whiteStones + point
+
+                        displayStone(state, imageView)
+                    },
+                )
+        }.onFailure {
+            Toast.makeText(applicationContext, it.message, Toast.LENGTH_LONG).show()
+        }
+        val updatedState = state
+
+        if (updatedState is Finished.Win) {
+            displayWinner(updatedState.winnerColor)
+        }
+    }
+
+    private fun displayStone(
+        state: State,
+        imageView: ImageView,
+    ) {
+        when (state) {
+            is BlackTurn -> imageView.setImageResource(R.drawable.black_stone)
+            is WhiteTurn -> imageView.setImageResource(R.drawable.white_stone)
+            else -> imageView.setImageResource(R.drawable.black_stone)
+        }
+    }
+
+    private fun displayWinner(stoneColor: StoneColor) {
+        when (stoneColor) {
+            StoneColor.BLACK -> Toast.makeText(applicationContext, "흑돌이 우승했습니다!", Toast.LENGTH_LONG).show()
+            else -> Toast.makeText(applicationContext, "백돌이 우승했습니다!", Toast.LENGTH_LONG).show()
+        }
     }
 }
