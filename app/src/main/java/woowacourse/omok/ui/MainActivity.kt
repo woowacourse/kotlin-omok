1cd1a7a93f43d37b3783c22dce3182657735b765
chanho0908 <uni070@naver.com>
2025-03-27 11:32:03 +0900

    feat : 게임 진행 인터페이스 리스너 추가 및 구현

---
+++ app/src/main/java/woowacourse/omok/ui/MainActivity.kt
@@ -0,0 +1,113 @@
+package woowacourse.omok.ui
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.children
+import woowacourse.omok.R
+import woowacourse.omok.domain.board.BoardStatus
+import woowacourse.omok.domain.board.Column
+import woowacourse.omok.domain.board.Row
+import woowacourse.omok.domain.exception.Exceptions
+import woowacourse.omok.domain.exception.OmokExceptions
+import woowacourse.omok.domain.exception.RendjuExceptions
+import woowacourse.omok.domain.point.Point
+import woowacourse.omok.domain.service.OmokGame
+import woowacourse.omok.domain.stone.StoneColor
+import woowacourse.omok.ui.event.GameEventListener
+
+class MainActivity : AppCompatActivity(), GameEventListener {
+    private var selectedImageView: ImageView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val game = OmokGame.create(this)
+        val board: TableLayout = findViewById(R.id.board)
+        board
+            .children
+            .filterIsInstance<TableRow>()
+            .forEachIndexed { rowIndex, row ->
+                setRowListener(row, rowIndex, game)
+            }
+    }
+
+    override fun onClickPoint(
+        point: Point,
+        game: OmokGame,
+    ) = game.play(point)
+
+    override fun onPlacedStone(stoneColor: StoneColor) {
+        val stoneDrawable =
+            when (stoneColor) {
+                StoneColor.WHITE -> R.drawable.white_stone
+                StoneColor.BLACK -> R.drawable.black_stone
+            }
+
+        selectedImageView?.setImageResource(stoneDrawable)
+    }
+
+    override fun onFinishedGame(color: StoneColor) {
+        val stoneUiText = getStoneUiText(color)
+        val uiText = getString(R.string.text_winner, stoneUiText)
+        Toast.makeText(this, uiText, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onFailToAddStone(e: Exceptions) {
+        val errorUiText = getErrorMessageUiText(e)
+        Toast.makeText(this, errorUiText, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getErrorMessageUiText(e: Exceptions): String {
+        val errorTextResource =
+            when (e) {
+                is OmokExceptions.OccupiedExceptions -> R.string.text_occupied
+                RendjuExceptions.DoubleFourExceptions -> R.string.text_double_four
+                RendjuExceptions.DoubleThreeExceptions -> R.string.text_double_three
+                RendjuExceptions.OverLineExceptions -> R.string.text_over_line
+                Exceptions.UnknownException -> R.string.text_unknown
+            }
+
+        val errorUiText = getString(errorTextResource)
+        return errorUiText
+    }
+
+    private fun getStoneUiText(color: StoneColor): String {
+        val colorTextResource =
+            when (color) {
+                StoneColor.WHITE -> R.string.text_white_stone
+                StoneColor.BLACK -> R.string.text_black_stone
+            }
+        val colorUiText = getString(colorTextResource)
+        return colorUiText
+    }
+
+    private fun setRowListener(
+        row: TableRow,
+        rowIndex: Int,
+        game: OmokGame,
+    ) {
+        row.children
+            .filterIsInstance<ImageView>()
+            .forEachIndexed { colIndex, view ->
+                val newTag =
+                    Point(Column(15 - rowIndex), Row(colIndex + 1), BoardStatus.Empty)
+                view.tag = newTag
+                view.setOnClickListener {
+                    selectedImageView = view
+                    onClickPoint(newTag, game)
+                }
+            }
+    }
+}
