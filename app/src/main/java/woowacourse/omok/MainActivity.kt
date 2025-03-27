fcec0b903d731190ca9e6d9524fe11a8409b258c
gahyunkim <mapledt001@naver.com>
2025-03-27 10:36:20 +0900

    feat: error별 토스트 메시지 설정 구현

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.children
 import omok.mapper.BlackRuleChecker
-import omok.mapper.NoViolation
 import omok.mapper.PointMapper
 import omok.model.game.Game
 import omok.model.stone.StoneColor
@@ -19,6 +18,11 @@ import omok.model.stone.position.Col
 import omok.model.stone.position.Row
 import omok.model.stone.position.Position
 import rule.BlackRenjuRule
+import woowacourse.omok.model.rule.PlacementError.AlreadyOccupiedViolation
+import woowacourse.omok.model.rule.PlacementError.DoubleFourViolation
+import woowacourse.omok.model.rule.PlacementError.DoubleThreeViolation
+import woowacourse.omok.model.rule.PlacementError.NoViolation
+import woowacourse.omok.model.rule.PlacementError.OverlineViolation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,20 +59,32 @@ class MainActivity : AppCompatActivity() {
                         val stoneRes =
                             if (game.turn == StoneColor.BLACK) R.drawable.black_stone else R.drawable.white_stone
 
-                        val violation = game.placeStone(position)
+                        val violation = game.playTurn(position)
+                        val result = when(violation){
+                            AlreadyOccupiedViolation -> "현재 위치에 돌이 있습니다"
+                            DoubleThreeViolation -> "3-3 반칙이 발생했습니다"
+                            DoubleFourViolation -> "4-4 반칙이 발생했습니다"
+                            OverlineViolation -> "장목 반칙이 발생했습니다"
+                            NoViolation -> ""
+                        }
 
                         if (violation != NoViolation) {
-                            Toast.makeText(this, violation.message, Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
                             return@setOnClickListener
                         }
 
                         cell.setImageResource(stoneRes)
-                        cell.isClickable = false
+
+                        val winColor = when(game.lastStone?.stoneColor){
+                            StoneColor.WHITE -> "백"
+                            StoneColor.BLACK -> "흑"
+                            null -> ""
+                        }
 
                         if (game.isOmok()) {
                             Toast.makeText(
                                 this,
-                                "${game.lastStone?.stoneColor}이 우승했습니다!",
+                                "${winColor}이 우승했습니다!",
                                 Toast.LENGTH_LONG
                             ).show()
                         }
