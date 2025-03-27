87c096aca8cd747fc25fef47cc53327630498d79
devfeijoa <devfeijoa@gmail.com>
2025-03-27 20:01:36 +0900

    refactor: position parser 수정

--- src/main/kotlin/omok/view/PositionParser.kt
+++ src/main/kotlin/omok/view/PositionParser.kt
@@ -3,18 +3,21 @@ package omok.view
 import omok.domain.Position
 import omok.domain.RowType
 
-class PositionParser {
-    fun parse(input: String): Position {
-        val regex = """([A-Z]+)(\d+)""".toRegex()
-        val matchResult = regex.matchEntire(input) ?: throw IllegalArgumentException(ERROR_NOT_FIND)
+object PositionParser {
+    private const val ERROR_NOT_FIND: String = "유효하지 않은 입력입니다. 다시 입력해주세요."
+    private val POSITION_PATTERN: Regex = """([A-Z]+)(\d+)""".toRegex()
 
+    fun encode(input: String): Position {
+        val matchResult =
+            POSITION_PATTERN.matchEntire(input)
+                ?: throw IllegalArgumentException(ERROR_NOT_FIND)
         val (rowString, column) = matchResult.destructured
         val row = RowType.valueOf(rowString).value
         val col = column.toInt() - 1
         return Position(row, col)
     }
 
-    companion object {
-        private const val ERROR_NOT_FIND = "유효하지 않은 입력입니다. 다시 입력해주세요."
+    fun decode(position: Position): String {
+        return "${RowType.from(position.x)}${position.y + 1}"
     }
 }
