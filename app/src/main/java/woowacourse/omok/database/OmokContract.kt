8df824bb5010d7261b9f2dc86cd3ad02719bda46
DoableTuple <doabletuple@gmail.com>
2025-03-27 15:25:57 +0900

    feat(OmokContract): 데이터베이스 정의

---
+++ app/src/main/java/woowacourse/omok/database/OmokContract.kt
@@ -0,0 +1,20 @@
+package woowacourse.omok.database
+
+import android.provider.BaseColumns
+
+object OmokContract {
+    const val TABLE_NAME = "omok"
+    const val COLUMN_NAME_X = "x"
+    const val COLUMN_NAME_Y = "y"
+    const val COLUMN_NAME_COLOR = "color"
+    const val COLUMN_NAME_ID = BaseColumns._ID
+
+    const val SQL_CREATE_ENTRIES =
+        "CREATE TABLE $TABLE_NAME (" +
+            "$COLUMN_NAME_ID INTEGER PRIMARY KEY," +
+            "$COLUMN_NAME_X INTEGER," +
+            "$COLUMN_NAME_Y INTEGER," +
+            "$COLUMN_NAME_COLOR TEXT)"
+
+    const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS $TABLE_NAME"
+}
