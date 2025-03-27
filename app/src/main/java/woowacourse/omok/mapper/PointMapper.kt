3155609fd13f5e3698f0ae27fbaa2d19e702b705
gahyunkim <mapledt001@naver.com>
2025-03-27 14:34:56 +0900

    refactor: PointMapper를 함수형 인터페이스로 수정

--- app/src/main/java/woowacourse/omok/mapper/PointMapper.kt
+++ app/src/main/java/woowacourse/omok/mapper/PointMapper.kt
@@ -3,6 +3,6 @@ package omok.mapper
 import omok.model.stone.position.Position
 import rule.wrapper.point.Point
 
-class PointMapper {
-    fun from(position: Position): Point = Point(position.col.value + 1, position.row.value + 1)
+fun interface PointMapper {
+    fun from(position: Position): Point
 }
