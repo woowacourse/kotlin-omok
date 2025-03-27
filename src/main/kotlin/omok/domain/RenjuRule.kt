99ff4aec801c2df7079954b7fd54a099196e30c4
devfeijoa <devfeijoa@gmail.com>
2025-03-27 20:06:09 +0900

    refactor: 렌주룰 수정

--- src/main/kotlin/omok/domain/RenjuRule.kt
+++ src/main/kotlin/omok/domain/RenjuRule.kt
@@ -30,7 +30,7 @@ class RenjuRule : Rule {
     ): Boolean {
         val grid = convertToLibraryFormat(board)
         return checkFiveInRow(grid, lastMove, color) &&
-            checkNoOverline(grid, lastMove, color)
+                checkNoOverline(grid, lastMove, color)
     }
 
     // 흑돌 금수 체크
@@ -41,10 +41,10 @@ class RenjuRule : Rule {
         val grid = convertToLibraryFormat(board)
         val posPair = position.toPair()
         return !(
-            threeThreeRule.validate(grid, posPair) ||
-                fourFourRule.validate(grid, posPair) ||
-                moreThanFiveRule.validate(grid, posPair)
-        )
+                threeThreeRule.validate(grid, posPair) ||
+                        fourFourRule.validate(grid, posPair) ||
+                        moreThanFiveRule.validate(grid, posPair)
+                )
     }
 
     // 5목 승리 조건
@@ -56,7 +56,7 @@ class RenjuRule : Rule {
         directions.forEach { (dx, dy) ->
             val count =
                 countDirection(grid, pos, dx, dy, color) +
-                    countDirection(grid, pos, -dx, -dy, color) + 1
+                        countDirection(grid, pos, -dx, -dy, color) + 1
             if (count == 5) return true
         }
         return false
@@ -70,7 +70,7 @@ class RenjuRule : Rule {
         directions.forEach { (dx, dy) ->
             val count =
                 countDirection(grid, pos, dx, dy, color) +
-                    countDirection(grid, pos, -dx, -dy, color) + 1
+                        countDirection(grid, pos, -dx, -dy, color) + 1
             if (count >= 6) return false
         }
         return true
@@ -84,8 +84,8 @@ class RenjuRule : Rule {
         color: StoneType,
     ): Int {
         var count = 0
-        var x = start.row + dx
-        var y = start.column + dy
+        var x = start.x + dx
+        var y = start.y + dy
 
         while (x in 0 until BOARD_SIZE && y in 0 until BOARD_SIZE) {
             if (grid[x][y] == color) {
@@ -108,16 +108,16 @@ class RenjuRule : Rule {
         }
     }
 
-    private fun Position.toPair() = Pair(this.row, this.column)
+    private fun Position.toPair() = Pair(this.x, this.y)
 
     companion object {
         private const val BOARD_SIZE = 15
         private val directions =
             listOf(
-                Pair(1, 0), // 가로
-                Pair(0, 1), // 세로
-                Pair(1, 1), // 대각선
-                Pair(1, -1), // 대각선
+                Pair(1, 0),
+                Pair(0, 1),
+                Pair(1, 1),
+                Pair(1, -1),
             )
     }
 }
