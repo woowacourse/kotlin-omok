afc5b293b65820a37fe4c06d1864021241ec22db
ijh1298 <imjh0417@gmail.com>
2025-03-27 15:54:56 +0900

    feat: OmokContract 오브젝트 추가

---
+++ app/src/main/java/woowacourse/omok/database/OmokContract.kt
@@ -0,0 +1,23 @@
+package woowacourse.omok.database
+
+import android.provider.BaseColumns
+
+object OmokContract {
+    object OmokStone {
+        const val TABLE_NAME = "stones"
+
+        const val COLUMN_NAME_COLOR = "color"
+        const val COLUMN_NAME_ROW = "row"
+        const val COLUMN_NAME_COLUMN = "column"
+        const val COLUMN_NAME_ID = BaseColumns._ID
+
+        const val SQL_CREATE_ENTRIES =
+            "CREATE TABLE $TABLE_NAME (" +
+                "$COLUMN_NAME_ID INTEGER PRIMARY KEY," +
+                "$COLUMN_NAME_COLOR TEXT," +
+                "$COLUMN_NAME_ROW INTEGER," +
+                "$COLUMN_NAME_COLUMN INTEGER)"
+
+        const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS $TABLE_NAME"
+    }
+}
