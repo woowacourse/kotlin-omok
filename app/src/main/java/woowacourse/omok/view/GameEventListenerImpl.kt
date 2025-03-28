1c75253f0cf956b38df6dce62fc8f161b9abe222
moondev03 <moondev03@gmail.com>
2025-03-28 17:58:20 +0900

    refactor: GameEventListener 개선
    - Activity 참조로 인한 메모리 누수 방지를 위해 WeakReference 사용

--- app/src/main/java/woowacourse/omok/view/GameEventListenerImpl.kt
+++ app/src/main/java/woowacourse/omok/view/GameEventListenerImpl.kt
@@ -7,31 +7,41 @@ import woowacourse.omok.domain.board.Point
 import woowacourse.omok.domain.board.result.Finished
 import woowacourse.omok.domain.board.result.OnGoing
 import woowacourse.omok.domain.board.result.PlaceStoneResult
+import java.lang.ref.WeakReference
 
 class GameEventListenerImpl(
-    private val activity: MainActivity,
+    activity: MainActivity,
 ) : GameEventListener {
+    private val weakActivity = WeakReference(activity)
+
     override fun onBoardUpdated(
         point: Point,
         state: CellState,
     ) {
-        activity.updateBoardUI(point, state)
-        activity.movesDao.saveMove(1, point to state)
+        weakActivity.get()?.let { activity ->
+            activity.updateBoardUI(point, state)
+            activity.movesDao.saveMove(1, point to state)
+        }
     }
 
     override fun onGameWon(winnerState: CellState?) {
-        activity.showGameOverDialog(winnerState)
+        weakActivity.get()?.showGameOverDialog(winnerState)
     }
 
     override fun onShowMessage(result: PlaceStoneResult) {
-        val messageRes =
-            when (result) {
-                is OnGoing.AlreadyPlaced -> R.string.already_placed_error_message
-                is OnGoing.RuleViolation -> R.string.violation_error_message
-                is OnGoing.InvalidMove -> R.string.invalid_point_error_message
-                is Finished.BoardFull -> R.string.board_full_error_message
-                else -> null
+        weakActivity.get()?.let { activity ->
+            val messageRes =
+                when (result) {
+                    is OnGoing.AlreadyPlaced -> R.string.already_placed_error_message
+                    is OnGoing.RuleViolation -> R.string.violation_error_message
+                    is OnGoing.InvalidMove -> R.string.invalid_point_error_message
+                    is Finished.BoardFull -> R.string.board_full_error_message
+                    else -> null
+                }
+
+            messageRes?.let {
+                Toast.makeText(activity, activity.getString(it), Toast.LENGTH_SHORT).show()
             }
-        messageRes?.let { Toast.makeText(activity, activity.getString(it), Toast.LENGTH_SHORT).show() }
+        }
     }
 }
