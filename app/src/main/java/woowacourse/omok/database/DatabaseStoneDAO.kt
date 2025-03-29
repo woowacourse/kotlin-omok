1800ebbcb1562505e0f0290e26d39309e06c5a88
지원 <cheerfulboys@naver.com>
2025-03-29 22:22:58 +0900

    style: ktlint 적용

--- app/src/main/java/woowacourse/omok/database/DatabaseStoneDAO.kt
+++ app/src/main/java/woowacourse/omok/database/DatabaseStoneDAO.kt
@@ -15,10 +15,11 @@ class DatabaseStoneDAO(private val dbHelper: DbHelper) : StoneDAO {
     ) {
         val db = dbHelper.writableDatabase
 
-        val cursor = db.rawQuery(
-            "SELECT * FROM board WHERE position_row = ? AND position_column = ? AND color = ?",
-            arrayOf(row.toString(), column.toString(), color)
-        )
+        val cursor =
+            db.rawQuery(
+                "SELECT * FROM board WHERE position_row = ? AND position_column = ? AND color = ?",
+                arrayOf(row.toString(), column.toString(), color),
+            )
 
         if (cursor.count > 0) {
             cursor.close()
