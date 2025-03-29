b8042961af46dd8ff10995ac08dc74aad1f383bd
박지민 <pjm5489@naver.com>
2025-03-29 16:33:13 +0900

    feat: BoardDao 생성 및 crud 기능 구현

---
+++ app/src/main/java/woowacourse/omok/db/BoardDaoImpl.kt
@@ -0,0 +1,42 @@
+package woowacourse.omok.db
+
+import android.content.ContentValues
+import android.content.Context
+
+class BoardDaoImpl(context: Context) : BoardDao {
+    override val dbHelper = DatabaseHelper(context)
+
+    override fun insertStone(boardDto: BoardDto) {
+        val db = dbHelper.writableDatabase
+        val values =
+            ContentValues().apply {
+                put("x", boardDto.x)
+                put("y", boardDto.y)
+                put("state", boardDto.state)
+            }
+        db.insert(DatabaseHelper.TABLE_NAME, null, values)
+        db.close()
+    }
+
+    override fun getAllStones(): List<BoardDto> {
+        val db = dbHelper.readableDatabase
+        val cursor = db.rawQuery("SELECT x, y, state FROM ${DatabaseHelper.TABLE_NAME}", null)
+        val moves = mutableListOf<BoardDto>()
+
+        while (cursor.moveToNext()) {
+            val x = cursor.getInt(0)
+            val y = cursor.getInt(1)
+            val state = cursor.getString(2)
+            moves.add(BoardDto(x, y, state))
+        }
+        cursor.close()
+        db.close()
+        return moves
+    }
+
+    override fun clearBoard() {
+        val db = dbHelper.writableDatabase
+        db.execSQL("DELETE FROM ${DatabaseHelper.TABLE_NAME}")
+        db.close()
+    }
+}
