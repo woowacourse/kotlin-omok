d30720941ba25a6cceb59067207526123e458141
yrsel <xxknu88@gmail.com>
2025-03-29 16:27:03 +0900

    feat: 오목게임 SQL문 관리 객체

---
+++ app/src/main/java/woowacourse/omok/data/OmokContract.kt
@@ -0,0 +1,27 @@
+package woowacourse.omok.data
+
+import android.provider.BaseColumns
+
+object OmokContract {
+    const val TABLE_NAME = "boards"
+
+    const val COLUMN_NAME_ID = BaseColumns._ID
+    const val COLUMN_NAME_STONE_COLOR = "stone_color"
+    const val COLUMN_NAME_ROW = "row"
+    const val COLUMN_NAME_COL = "column"
+
+    const val SQL_CREATE_ENTRIES =
+        "CREATE TABLE $TABLE_NAME (" +
+            "$COLUMN_NAME_ID INTEGER PRIMARY KEY," +
+            "$COLUMN_NAME_STONE_COLOR TEXT," +
+            "$COLUMN_NAME_ROW INTEGER," +
+            "$COLUMN_NAME_COL INTEGER);"
+
+    const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS $TABLE_NAME"
+
+    const val SQL_INSERT_STONE =
+        """ INSERT INTO $TABLE_NAME
+            ($COLUMN_NAME_STONE_COLOR, $COLUMN_NAME_ROW, $COLUMN_NAME_COL)
+            VALUES(?, ?, ?) 
+        """
+}
