0984cee6c175c614401fc2562ac8ccc6d5b00424
박지민 <pjm5489@naver.com>
2025-03-29 20:16:05 +0900

    feat: TurnDao 생성

---
+++ app/src/main/java/woowacourse/omok/db/TurnDao.kt
@@ -0,0 +1,13 @@
+package woowacourse.omok.db
+
+import woowacourse.omok.model.stone.StoneColor
+
+interface TurnDao {
+    fun saveTurn(color: StoneColor)
+
+    fun getLastTurn(): StoneColor
+
+    fun deleteTurn()
+
+    fun initializeTurn()
+}
