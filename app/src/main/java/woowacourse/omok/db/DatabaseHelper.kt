f895de447934e2476604b5a7c613e157e523c9c5
박지민 <pjm5489@naver.com>
2025-03-29 20:17:45 +0900

    feat: 테이블 생성 로직 구현

--- app/src/main/java/woowacourse/omok/db/DatabaseHelper.kt
+++ app/src/main/java/woowacourse/omok/db/DatabaseHelper.kt
@@ -8,11 +8,21 @@ class DatabaseHelper(context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(
-            "CREATE TABLE $TABLE_NAME (" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "x INTEGER, " +
-                "y INTEGER, " +
-                "state TEXT)",
+            """
+            CREATE TABLE $OMOK_BOARD (
+                x INTEGER,
+                y INTEGER,
+                state TEXT
+            )
+            """.trimIndent(),
+        )
+        db.execSQL(
+            """
+            CREATE TABLE $CURRENT_TURN (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               color TEXT
+            )
+            """.trimIndent(),
         )
     }
 
@@ -21,13 +31,15 @@ class DatabaseHelper(context: Context) :
         oldVersion: Int,
         newVersion: Int,
     ) {
-        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        db.execSQL("DROP TABLE IF EXISTS $OMOK_BOARD")
+        db.execSQL("DROP TABLE IF EXISTS $CURRENT_TURN")
         onCreate(db)
     }
 
     companion object {
         private const val DATABASE_NAME = "omok.db"
-        private const val DATABASE_VERSION = 1
-        const val TABLE_NAME = "omok_board"
+        private const val DATABASE_VERSION = 2
+        const val OMOK_BOARD = "omok_board"
+        const val CURRENT_TURN = "current_turn"
     }
 }
