f544094c7d81a3a0efef9e129a1a672b40f5b2f8
chanho0908 <uni070@naver.com>
2025-03-30 01:57:59 +0900

    feat : 방 추가/삭제 기능 추가

--- app/src/main/java/woowacourse/omok/domain/room/Rooms.kt
+++ app/src/main/java/woowacourse/omok/domain/room/Rooms.kt
@@ -1,11 +1,23 @@
 package woowacourse.omok.domain.room
 
-class Rooms(private val items: List<Room>) {
-    operator fun plus(room: Room): Rooms {
-        return Rooms(items + room)
+import woowacourse.omok.domain.repository.RoomRepository
+
+class Rooms(private val roomRepository: RoomRepository) {
+    private val _roomList: MutableList<Room> = roomRepository.readAll().toMutableList()
+    val roomList: List<Room> get() = _roomList.toList()
+
+    operator fun plus(roomName: String): Long {
+        val room = Room(roomName = roomName)
+        val roomId = roomRepository.save(Room(roomName = roomName))
+        _roomList.add(room.copy(id = roomId))
+        return roomId
     }
 
-    fun remove(room: Room) {
-        items.toMutableList().remove(room)
+    operator fun minus(index: Int) {
+        if (index in _roomList.indices) {
+            val room = _roomList[index]
+            roomRepository.delete(room.id)
+            _roomList.removeAt(index)
+        }
     }
 }
--- app/src/main/java/woowacourse/omok/ui/room/RoomActivity.kt
+++ app/src/main/java/woowacourse/omok/ui/room/RoomActivity.kt
@@ -12,14 +12,14 @@ import woowacourse.omok.data.db.omok.OmokDbHelper
 import woowacourse.omok.data.repository.RoomRepositoryImpl
 import woowacourse.omok.databinding.ActivityRoomBinding
 import woowacourse.omok.domain.repository.RoomRepository
-import woowacourse.omok.domain.room.Room
+import woowacourse.omok.domain.room.Rooms
 import woowacourse.omok.ui.dialog.RoomNameDialog
 import woowacourse.omok.ui.main.MainActivity
 import woowacourse.omok.ui.room.rv.RoomRvAdapter
-import kotlin.concurrent.thread
 
 class RoomActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRoomBinding
+    private lateinit var room: Rooms
     private lateinit var roomRepository: RoomRepository
     private lateinit var adapter: RoomRvAdapter
 
@@ -28,41 +28,26 @@ class RoomActivity : AppCompatActivity() {
         binding = ActivityRoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initializeDataSource()
-        initializeView(restoreSavedRoom())
+        initializeView()
         initializeListener()
+        initializeDataSource()
+        setRecyclerView()
     }
 
-    private fun initializeView(rooms: List<Room>) {
+    private fun initializeView() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        adapter =
-            RoomRvAdapter(
-                onClickDelete = {
-                    deleteRoom(it)
-                },
-                onClickJoin = {
-                    val selectedRoom = rooms[it]
-                    navigateToMain(selectedRoom.id)
-                },
-            )
-
-        val layoutManager = LinearLayoutManager(this)
-        binding.rv.adapter = adapter
-        binding.rv.layoutManager = layoutManager
-
-        adapter.submitList(rooms)
     }
 
     private fun initializeListener() {
         binding.makeRoomBtn.setOnClickListener {
             RoomNameDialog(
                 onClickComplete = {
-                    val roomId = addRoom(it)
+                    val roomId = room + it
+                    updateRecyclerView()
                     navigateToMain(roomId)
                 },
             ).show(supportFragmentManager, "Room")
@@ -73,38 +58,37 @@ class RoomActivity : AppCompatActivity() {
         val dbHelper = OmokDbHelper(this)
         val dao = RoomDao(dbHelper)
         roomRepository = RoomRepositoryImpl(dao)
+        room = Rooms(roomRepository)
     }
 
-    private fun restoreSavedRoom(): List<Room> {
-        return roomRepository.readAll()
+    private fun setRecyclerView() {
+        adapter =
+            RoomRvAdapter(
+                onClickDelete = {
+                    room - it
+                    updateRecyclerView()
+                },
+                onClickJoin = {
+                    val selectedRoom = room.roomList[it]
+                    navigateToMain(selectedRoom.id)
+                },
+            )
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rv.adapter = adapter
+        binding.rv.layoutManager = layoutManager
+
+        updateRecyclerView()
     }
 
-    private fun addRoom(roomName: String): Long {
-        var roomId = 0L
-        executeRoomUpdate {
-            roomId = roomRepository.save(Room(roomName = roomName))
-        }
-        return roomId
+    private fun updateRecyclerView() {
+        adapter.submitList(room.roomList)
     }
-
-    private fun deleteRoom(position: Int) =
-        executeRoomUpdate {
-            val room = adapter.currentList[position]
-            roomRepository.delete(room.id)
-        }
-
-    private fun executeRoomUpdate(action: () -> Unit) =
-        thread {
-            action()
-            runOnUiThread {
-                adapter.submitList(roomRepository.readAll())
-            }
-        }.join()
 
     private fun navigateToMain(roomId: Long) {
         val intent =
             Intent(this, MainActivity::class.java).apply {
-                intent.putExtra("roomId", roomId)
+                putExtra("roomId", roomId)
             }
         startActivity(intent)
     }
