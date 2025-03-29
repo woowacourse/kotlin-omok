5cfa59fab7a73673613c118be2c2bce32e353c55
gahyunkim <mapledt001@naver.com>
2025-03-30 00:39:55 +0900

    refactor: use 확장 함수를 사용한 코드 수정

--- app/src/main/java/woowacourse/omok/data/OmokDao.kt
+++ app/src/main/java/woowacourse/omok/data/OmokDao.kt
@@ -2,7 +2,6 @@ package woowacourse.omok.data
 
 import android.content.ContentValues
 import android.content.Context
-import android.database.Cursor
 import android.util.Log
 import omok.model.stone.Stone
 import omok.model.stone.StoneColor
@@ -16,9 +15,9 @@ class OmokDao(
     val dbHelper: DbHelper = DbHelper(context)
 
     fun deleteDatabase() {
-        val db = dbHelper.writableDatabase
-        db.delete(OmokContract.TABLE_NAME, null, null)
-        db.close()
+        dbHelper.writableDatabase.use { db ->
+            db.delete(OmokContract.TABLE_NAME, null, null)
+        }
     }
 
     fun insertOmok(
@@ -26,73 +25,66 @@ class OmokDao(
         col: Int,
         stoneColor: String,
     ) {
-        val db = dbHelper.writableDatabase
+        dbHelper.writableDatabase.use { db ->
+            val values =
+                ContentValues().apply {
+                    put(OmokContract.COLUMN_ROW_POSITION, row)
+                    put(OmokContract.COLUMN_COL_POSITION, col)
+                    put(OmokContract.COLUMN_STONE_COLOR, stoneColor)
+                }
 
-        val values =
-            ContentValues().apply {
-                put(OmokContract.COLUMN_ROW_POSITION, row)
-                put(OmokContract.COLUMN_COL_POSITION, col)
-                put(OmokContract.COLUMN_STONE_COLOR, stoneColor)
+            val newRowId = db.insert(OmokContract.TABLE_NAME, null, values)
+            if (newRowId == -1L) {
+                Log.e("MainActivity", "insert failed")
+            } else {
+                Log.d("MainActivity", "insert success: $newRowId")
             }
-
-        val newRowId = db.insert(OmokContract.TABLE_NAME, null, values)
-        if (newRowId == -1L) {
-            Log.e("MainActivity", "insert failed")
-        } else {
-            Log.d("MainActivity", "insert success: $newRowId")
         }
-        db.close()
     }
 
-    fun hasOmokData(): Boolean {
-        val db = dbHelper.readableDatabase
-        val query = "SELECT EXISTS (SELECT 1 FROM ${OmokContract.TABLE_NAME} LIMIT 1)"
-        val cursor = db.rawQuery(query, null)
-
-        var exists = false
-        if (cursor.moveToFirst()) {
-            exists = cursor.getInt(0) == 1
+    fun hasOmokData(): Boolean =
+        dbHelper.readableDatabase.use { db ->
+            val query = "SELECT EXISTS (SELECT 1 FROM ${OmokContract.TABLE_NAME} LIMIT 1)"
+            db.rawQuery(query, null).use { cursor ->
+                cursor.moveToFirst() && cursor.getInt(0) == 1
+            }
         }
 
-        cursor.close()
-        db.close()
-        return exists
-    }
+    fun getAllStones(): List<Stone> =
+        dbHelper.readableDatabase.use { db ->
+            val result = mutableListOf<Stone>()
 
-    fun getAllStones(): List<Stone> {
-        val dbReader = dbHelper.readableDatabase
-        val result = mutableListOf<Stone>()
-
-        val cursor: Cursor =
-            dbReader.query(
-                OmokContract.TABLE_NAME,
-                arrayOf(
-                    OmokContract.COLUMN_ROW_POSITION,
-                    OmokContract.COLUMN_COL_POSITION,
-                    OmokContract.COLUMN_STONE_COLOR,
-                ),
-                null,
-                null,
-                null,
-                null,
-                null,
-            )
-
-        with(cursor) {
-            while (moveToNext()) {
-                val row = getInt(getColumnIndexOrThrow(OmokContract.COLUMN_ROW_POSITION))
-                val col = getInt(getColumnIndexOrThrow(OmokContract.COLUMN_COL_POSITION))
-                val stoneColor = getString(getColumnIndexOrThrow(OmokContract.COLUMN_STONE_COLOR))
-                val color =
-                    when (stoneColor) {
-                        "BLACK" -> StoneColor.BLACK
-                        "WHITE" -> StoneColor.WHITE
-                        else -> continue
+            db
+                .query(
+                    OmokContract.TABLE_NAME,
+                    arrayOf(
+                        OmokContract.COLUMN_ROW_POSITION,
+                        OmokContract.COLUMN_COL_POSITION,
+                        OmokContract.COLUMN_STONE_COLOR,
+                    ),
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                ).use { cursor ->
+                    while (cursor.moveToNext()) {
+                        val row =
+                            cursor.getInt(cursor.getColumnIndexOrThrow(OmokContract.COLUMN_ROW_POSITION))
+                        val col =
+                            cursor.getInt(cursor.getColumnIndexOrThrow(OmokContract.COLUMN_COL_POSITION))
+                        val stoneColor =
+                            cursor.getString(cursor.getColumnIndexOrThrow(OmokContract.COLUMN_STONE_COLOR))
+                        val color =
+                            when (stoneColor) {
+                                "BLACK" -> StoneColor.BLACK
+                                "WHITE" -> StoneColor.WHITE
+                                else -> continue
+                            }
+                        result.add(Stone(Position(Row(row), Col(col)), color))
                     }
-                result.add(Stone(Position(Row(row), Col(col)), color))
-            }
+                }
+
+            result
         }
-        cursor.close()
-        return result
-    }
 }
