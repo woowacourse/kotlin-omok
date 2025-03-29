d27dac7fb92e28d1a97c6f534cc52167673c23ec
박지민 <pjm5489@naver.com>
2025-03-29 16:34:55 +0900

    feat: SQLite DB 생성

---
+++ app/src/main/java/woowacourse/omok/db/DatabaseHelper.kt
@@ -0,0 +1,33 @@
+package woowacourse.omok.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    companion object {
+        private const val DATABASE_NAME = "omok.db"
+        private const val DATABASE_VERSION = 1
+        const val TABLE_NAME = "omok_board"
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            "CREATE TABLE $TABLE_NAME (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "x INTEGER, " +
+                "y INTEGER, " +
+                "state TEXT)",
+        )
+    }
+
+    override fun onUpgrade(
+        db: SQLiteDatabase,
+        oldVersion: Int,
+        newVersion: Int,
+    ) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+}
