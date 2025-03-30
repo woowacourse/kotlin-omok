d9f33b0d883c258e1dfa1ad0c01608217e855678
devfeijoa <devfeijoa@gmail.com>
2025-03-31 01:08:35 +0900

    refactor: Turn 테스트에 맞지 않는 테스트 제거

--- app/src/test/java/woowacourse/omok/domain/TurnTest.kt
+++ app/src/test/java/woowacourse/omok/domain/TurnTest.kt
@@ -1,7 +1,6 @@
 package woowacourse.omok.domain
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class TurnTest {
@@ -15,12 +14,5 @@ class TurnTest {
 
         turn.switch()
         assertEquals(StoneType.BLACK, turn.current)
-    }
-
-    @Test
-    fun `돌 배치 검증`() {
-        val board = Board.initial()
-        board.put(Position(3, 3), StoneType.BLACK)
-        assertTrue(board.stones.any { it.position == Position(3, 3) })
     }
 }
