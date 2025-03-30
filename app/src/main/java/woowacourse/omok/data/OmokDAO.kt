a6cbf1f66ef8413fa1b146e71cec3828b489a348
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-31 08:12:51 +0900

    refactor: closeable에서 use로 변경

--- app/src/main/java/woowacourse/omok/data/OmokDAO.kt
+++ app/src/main/java/woowacourse/omok/data/OmokDAO.kt
@@ -19,15 +19,17 @@ class OmokDAO(context: Context) {
 
     fun loadAllStones(): List<Triple<Int, Int, String>> {
         val result = mutableListOf<Triple<Int, Int, String>>()
-        val cursor = db.query(OmokDbHelper.TABLE_NAME, arrayOf("x", "y", "stone"), null, null, null, null, null)
 
-        while (cursor.moveToNext()) {
-            val x = cursor.getInt(cursor.getColumnIndexOrThrow("x"))
-            val y = cursor.getInt(cursor.getColumnIndexOrThrow("y"))
-            val stone = cursor.getString(cursor.getColumnIndexOrThrow("stone"))
-            result.add(Triple(x, y, stone))
-        }
-        cursor.close()
+        db.query(OmokDbHelper.TABLE_NAME, arrayOf("x", "y", "stone"), null, null, null, null, null)
+            .use { cursor ->
+                while (cursor.moveToNext()) {
+                    val x = cursor.getInt(cursor.getColumnIndexOrThrow("x"))
+                    val y = cursor.getInt(cursor.getColumnIndexOrThrow("y"))
+                    val stone = cursor.getString(cursor.getColumnIndexOrThrow("stone"))
+                    result.add(Triple(x, y, stone))
+                }
+            }
+
         return result
     }
 
