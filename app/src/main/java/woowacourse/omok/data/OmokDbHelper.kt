26c0debd189cf41d8464d311a429e62cf220bebe
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-31 07:21:27 +0900

    feat: db downgrade 메서드 생성

--- app/src/main/java/woowacourse/omok/data/OmokDbHelper.kt
+++ app/src/main/java/woowacourse/omok/data/OmokDbHelper.kt
@@ -33,6 +33,12 @@ class OmokDbHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, null, 
         onCreate(db)
     }
 
+    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        db.execSQL("DROP TABLE IF EXISTS turn")
+        onCreate(db)
+    }
+
     companion object {
         const val DB_NAME = "OmokGame.db"
         const val DB_VERSION = 3
