5aea5beaabfa3057cd6a87bef7ab91394bfc6c7d
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-03-31 06:52:47 +0900

    refactor: 함수 분리

--- app/src/main/java/woowacourse/omok/MainActivity.kt
+++ app/src/main/java/woowacourse/omok/MainActivity.kt
@@ -2,6 +2,7 @@ package woowacourse.omok
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
@@ -30,40 +31,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        dbController = OmokDbController(this)
-        game = OmokGameImpl(BoardImpl.createEmpty(), OmokRuleAdapter())
-        playerState = BlackPlayerState(game)
-
-        val savedTurn = dbController.loadTurn()
-        playerState = when (savedTurn) {
-            StoneState.WHITE.name -> WhitePlayerState(game)
-            else -> BlackPlayerState(game)
-        }
-
-        val board = findViewById<TableLayout>(R.id.board)
-
-        val savedStones = dbController.loadAllStones() // Triple<Int, Int, String>
-        savedStones.forEach { (x, y, stoneString) ->
-            val imageView = ((board.getChildAt(x - 1) as TableRow).getChildAt(y - 1) as ImageView)
-
-            val stoneState = StoneState.valueOf(stoneString)
-            val resId = when (stoneState) {
-                StoneState.BLACK -> R.drawable.black_stone
-                StoneState.WHITE -> R.drawable.white_stone
-                else -> null
-            }
-
-            resId?.let { imageView.setImageResource(it) }
-        }
-
+        setUpUI()
+        initializeGame()
+        val board = savedGame()
         board
             .children
             .filterIsInstance<TableRow>()
@@ -74,46 +44,94 @@ class MainActivity : AppCompatActivity() {
                         imageView.tag = "$rowIndex,$colIndex"
                         imageView.setOnClickListener {
                             val (row, col) = (it.tag as String).split(",").map { it.toInt() }
-                            val position = Position(row + 1, col + 1)
-
-                            val imageStoneState = playerState
-
-                            try {
-                                dbController.saveStone(row + 1, col + 1, imageStoneState.stoneState().toString())
-
-                                playerState = playerState.state(position)
-                                dbController.saveTurn(playerState.stoneState().name)
-                                val resId = when (imageStoneState) {
-                                    is BlackPlayerState -> R.drawable.black_stone
-                                    is WhitePlayerState -> R.drawable.white_stone
-                                    else -> return@setOnClickListener
-                                }
-                                (it as ImageView).setImageResource(resId)
-
-                                if (playerState is Finish) {
-                                    val winner = (playerState as Finish).winner()
-                                    Log.d("Omok", "게임 종료! 승자: $winner")
-
-                                    val message = when (winner) {
-                                        StoneState.BLACK -> "흑돌이 이겼습니다!"
-                                        StoneState.WHITE -> "백돌이 이겼습니다!"
-                                        StoneState.NONE -> "무승부입니다!"
-                                    }
-                                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-                                    dbController.clearBoard()
-                                }
-                            } catch (e: IllegalArgumentException) {
-                                Toast.makeText(
-                                    this,
-                                    e.message ?: "알 수 없는 오류 발생",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            } catch (e: IllegalStateException) {
-                                Toast.makeText(this, e.message ?: "금수입니다.", Toast.LENGTH_SHORT)
-                                    .show()
-                            }
+                            handleStoneClick(row, col, it)
                         }
                     }
             }
+    }
+
+    private fun handleStoneClick(row: Int, col: Int, view: View) {
+        val position = Position(row + 1, col + 1)
+        val imageStoneState = playerState
+
+        try {
+            dbController.saveStone(row + 1, col + 1, imageStoneState.stoneState().toString())
+
+            playerState = playerState.state(position)
+            dbController.saveTurn(playerState.stoneState().name)
+            val resId = when (imageStoneState) {
+                is BlackPlayerState -> R.drawable.black_stone
+                is WhitePlayerState -> R.drawable.white_stone
+                else -> return
+            }
+            (view as ImageView).setImageResource(resId)
+
+            if (playerState is Finish) {
+                val winner = (playerState as Finish).winner()
+                Log.d("Omok", "게임 종료! 승자: $winner")
+
+                val message = when (winner) {
+                    StoneState.BLACK -> "흑돌이 이겼습니다!"
+                    StoneState.WHITE -> "백돌이 이겼습니다!"
+                    StoneState.NONE -> "무승부입니다!"
+                }
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                dbController.clearBoard()
+            }
+        } catch (e: IllegalArgumentException) {
+            Toast.makeText(
+                this,
+                e.message ?: "알 수 없는 오류 발생",
+                Toast.LENGTH_SHORT
+            ).show()
+        } catch (e: IllegalStateException) {
+            Toast.makeText(this, e.message ?: "금수입니다.", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun savedGame(): TableLayout {
+        savedTurn()
+        val board = findViewById<TableLayout>(R.id.board)
+        savedStone(board)
+        return board
+    }
+
+    private fun initializeGame() {
+        dbController = OmokDbController(this)
+        game = OmokGameImpl(BoardImpl.createEmpty(), OmokRuleAdapter())
+        playerState = BlackPlayerState(game)
+    }
+
+    private fun setUpUI() {
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun savedStone(board: TableLayout?) {
+        val savedStones = dbController.loadAllStones() // Triple<Int, Int, String>
+        savedStones.forEach { (x, y, stoneString) ->
+            val imageView = ((board?.getChildAt(x - 1) as TableRow).getChildAt(y - 1) as ImageView)
+            val stoneState = StoneState.valueOf(stoneString)
+            val resId = when (stoneState) {
+                StoneState.BLACK -> R.drawable.black_stone
+                StoneState.WHITE -> R.drawable.white_stone
+                else -> null
+            }
+            resId?.let { imageView.setImageResource(it) }
+        }
+    }
+
+    private fun savedTurn() {
+        val savedTurn = dbController.loadTurn()
+        playerState = when (savedTurn) {
+            StoneState.WHITE.name -> WhitePlayerState(game)
+            else -> BlackPlayerState(game)
+        }
     }
 }
