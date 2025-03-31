cf6db13807517ebd9b6a0a32b0b5faed19b3099c
devfeijoa <devfeijoa@gmail.com>
2025-03-31 09:40:38 +0900

    refactor: 컨트롤러 수정

--- app/src/main/java/woowacourse/omok/Main.kt
+++ app/src/main/java/woowacourse/omok/Main.kt
@@ -1,12 +1,16 @@
 package woowacourse.omok
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import woowacourse.omok.controller.OmokGame
 import woowacourse.omok.view.InputView
 import woowacourse.omok.view.OutputView
 
+@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
 fun main() {
     val inputView = InputView()
     val outputView = OutputView()
     val omokGame = OmokGame(inputView, outputView)
+
     omokGame.start()
 }
--- app/src/main/java/woowacourse/omok/controller/OmokGame.kt
+++ app/src/main/java/woowacourse/omok/controller/OmokGame.kt
@@ -3,6 +3,9 @@ package woowacourse.omok.controller
 import android.os.Build
 import androidx.annotation.RequiresApi
 import woowacourse.omok.domain.Board
+import woowacourse.omok.domain.Position
+import woowacourse.omok.domain.StoneType
+import woowacourse.omok.domain.Turn
 import woowacourse.omok.library.BudoolRenjuRule
 import woowacourse.omok.view.InputView
 import woowacourse.omok.view.OutputView
@@ -11,17 +14,46 @@ class OmokGame(
     private val inputView: InputView,
     private val outputView: OutputView,
 ) {
+    val board = Board.initial()
+    val turn = Turn()
+    val rule = BudoolRenjuRule()
+
     @RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
     fun start() {
-        val board = Board.initial()
-        val rule = BudoolRenjuRule()
+        outputView.printStartMessage()
+        var lastPosition: Position? = null
 
-        val current = board.currentTurn
-        outputView.print(board.currentTurn)
-        val position = inputView.readPosition()
+        while (true) {
+            outputView.drawBoard(board)
+            outputView.printTurn(turn.currentPlayer, lastPosition)
 
-        if (rule.checkWin(board, position)) {
+            val position = inputView.readPosition()
+            try {
+                board.put(position, turn.currentPlayer)
+
+                if (rule.checkWin(board, position)) {
+                    outputView.drawBoard(board)
+                    outputView.showWin(turn.currentPlayer)
+                    return
+                }
+
+                if (turn.currentPlayer == StoneType.BLACK) {
+                    if (rule.checkDoubleThreeFoul(board, position)) {
+                        throw IllegalStateException("삼삼 금수입니다.")
+                    }
+                    if (rule.checkDoubleFourFoul(board, position)) {
+                        throw IllegalStateException("사사 금수입니다.")
+                    }
+                    if (rule.checkOverline(board, position)) {
+                        throw IllegalStateException("장목 금수입니다.")
+                    }
+                }
+
+                lastPosition = position
+                turn.switch()
+            } catch (e: Exception) {
+                outputView.showError(e.message ?: "알 수 없는 오류가 발생했습니다.")
+            }
         }
-        board.put(position, current)
     }
 }
