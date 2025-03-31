cfc7e19f1217c900e17e029337833fc6fbb46a0c
junseo511 <prime54@g.skku.edu>
2025-03-31 17:09:49 +0900

    refactor: 데이터베이스 이름 변경

--- app/src/main/java/woowacourse/omok/data/db/OmokDbHelper.kt
+++ app/src/main/java/woowacourse/omok/data/db/OmokDbHelper.kt
@@ -9,7 +9,7 @@ class OmokDbHelper(
 ) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
         const val DATABASE_VERSION = 1
-        const val DATABASE_NAME = "Wtc.db"
+        const val DATABASE_NAME = "Omok.db"
     }
 
     override fun onCreate(db: SQLiteDatabase) {
