6eab69a2a6be9f62a554e68965df2494ebb5d2b7
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-03-31 14:12:51 +0900

    feat: implement dto History

---
+++ app/src/main/java/woowacourse/omok/data/History.kt
@@ -0,0 +1,16 @@
+package woowacourse.omok.data
+
+import omok.model.entity.Stone
+import omok.model.entity.position.GridElement
+
+class History(
+    val turn: String,
+    val row: Int,
+    val column: Int,
+) {
+    constructor(turn: Stone, row: GridElement, column: GridElement) : this(
+        turn.name,
+        row.value,
+        column.value,
+    )
+}
