db93d33412f1130af5f08ae877574efb0eb59ab9
gahyunkim <mapledt001@naver.com>
2025-03-31 15:27:36 +0900

    refactor: GameState 내부의 Violation 클래스 삭제

--- app/src/main/java/woowacourse/omok/controller/GameState.kt
+++ app/src/main/java/woowacourse/omok/controller/GameState.kt
@@ -1,14 +1,9 @@
 package woowacourse.omok.controller
 
-import woowacourse.omok.model.rule.PlacementError
 import woowacourse.omok.model.stone.StoneColor
 
 sealed class GameState {
     data object Playing : GameState()
-
-    data class Violation(
-        val error: PlacementError,
-    ) : GameState()
 
     data class Win(
         val winner: StoneColor?,
