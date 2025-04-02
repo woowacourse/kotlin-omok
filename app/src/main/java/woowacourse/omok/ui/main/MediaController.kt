c30549a3a6d2d861392a4dec0f643377022c3fc8
chanho0908 <uni070@naver.com>
2025-04-02 10:53:40 +0900

    refactor : 미디어 컨트롤러에서 외부에서 전달받은 노래를 사용하도록 수정

--- app/src/main/java/woowacourse/omok/ui/main/MediaController.kt
+++ app/src/main/java/woowacourse/omok/ui/main/MediaController.kt
@@ -2,21 +2,19 @@ package woowacourse.omok.ui.main
 
 import android.content.Context
 import android.media.MediaPlayer
-import androidx.annotation.RawRes
-import woowacourse.omok.R
 
 class MediaController(
     private val context: Context,
-    @RawRes resourceId: Int,
+    private val resourceId: Int,
 ) {
     private var mediaPlayer: MediaPlayer? = null
 
     fun play() {
-        mediaPlayer = MediaPlayer.create(context, R.raw.apple)
+        mediaPlayer = MediaPlayer.create(context, resourceId)
         mediaPlayer?.start()
     }
 
-    fun stop()  {
+    fun stop() {
         mediaPlayer?.release()
         mediaPlayer = null
     }
