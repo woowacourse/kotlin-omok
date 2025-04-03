cb45b99ebbd67fff86ccf91069c55703dab7489c
junseo511 <prime54@g.skku.edu>
2025-04-04 00:06:57 +0900

    refactor: 오목 로비 로직 매니저로 분리

---
+++ app/src/main/java/woowacourse/omok/domain/model/omokboard/LobbyManager.kt
@@ -0,0 +1,21 @@
+package woowacourse.omok.domain.model.omokboard
+
+import android.content.Context
+import woowacourse.omok.OmokApplication
+import woowacourse.omok.data.dao.OmokGameDao
+import woowacourse.omok.domain.model.player.PlayerName
+import woowacourse.omok.ui.mapper.toData
+import woowacourse.omok.ui.mapper.toUI
+
+class LobbyManager(
+    context: Context,
+) {
+    private val omokGameDao: OmokGameDao = (context.applicationContext as OmokApplication).omokGameDao
+
+    fun getAllGames(): List<OmokGameEntity> = omokGameDao.fetchAllGames().toUI()
+
+    fun createNewGame(): Int {
+        val newGame = OmokGameEntity(host = PlayerName.create())
+        return omokGameDao.createGame(newGame.toData())
+    }
+}
--- app/src/main/java/woowacourse/omok/ui/lobby/LobbyActivity.kt
+++ app/src/main/java/woowacourse/omok/ui/lobby/LobbyActivity.kt
@@ -2,45 +2,35 @@ package woowacourse.omok.ui.lobby
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import woowacourse.omok.OmokApplication
-import woowacourse.omok.data.dao.OmokGameDao
 import woowacourse.omok.databinding.ActivityLobbyBinding
-import woowacourse.omok.domain.model.omokboard.OmokGameEntity
-import woowacourse.omok.domain.model.player.PlayerName
+import woowacourse.omok.domain.model.omokboard.LobbyManager
 import woowacourse.omok.ui.game.OmokGameActivity
-import woowacourse.omok.ui.mapper.toData
-import woowacourse.omok.ui.mapper.toUI
 
 class LobbyActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLobbyBinding
-    private lateinit var omokGameDao: OmokGameDao
+    private lateinit var lobbyManager: LobbyManager
     private val omokGameAdapter = OmokGameAdapter(::navigateToOmokGame)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLobbyBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        lobbyManager = LobbyManager(applicationContext)
+
         binding.rvLobbyRooms.adapter = omokGameAdapter
-
-        omokGameDao = (applicationContext as OmokApplication).omokGameDao
-
         setupClickListener()
     }
 
     override fun onResume() {
         super.onResume()
-        val games = omokGameDao.fetchAllGames()
-        omokGameAdapter.submitList(games.toUI())
+        val games = lobbyManager.getAllGames()
+        omokGameAdapter.submitList(games)
     }
 
     private fun setupClickListener() {
-        createNewOmokRoom()
-    }
-
-    private fun createNewOmokRoom() {
         binding.fabLobbyCreateRoom.setOnClickListener {
-            val newGame = OmokGameEntity(host = PlayerName.create())
-            val newGameId = omokGameDao.createGame(newGame.toData())
+            val newGameId = lobbyManager.createNewGame()
             navigateToOmokGame(newGameId)
         }
     }
