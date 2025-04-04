091f072e50e2b3ea90c85e10b5e1712240868434
gahyunkim <mapledt001@naver.com>
2025-04-04 16:00:25 +0900

    refactor: 세부 내용 변경

--- app/src/main/java/woowacourse/omok/data/DbHelper.kt
+++ app/src/main/java/woowacourse/omok/data/DbHelper.kt
@@ -8,7 +8,7 @@ class DbHelper(
     context: Context,
 ) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
-        const val DATABASE_VERSION = 1
+        const val DATABASE_VERSION = 2
         const val DATABASE_NAME = "Omok.db"
     }
 
